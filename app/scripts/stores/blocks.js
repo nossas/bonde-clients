728b6e5f3b50b3eddde0d8e6044ee3abd02ea273
Nícolas Iensen <nicolas.iensen@gmail.com>
2015-07-13 16:36:09 -0300

    [#98297248] add blocks store

---
+++ app/scripts/stores/blocks.js
@@ -0,0 +1,12 @@
+const initialState = [
+  {
+    uuid: "ffxx1122A2"
+  }
+]
+
+export default function blocks(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
