68cba60ec7ad40e149b59d53f7dcb29a94383856
Nícolas Iensen <nicolas.iensen@gmail.com>
2015-07-13 22:10:31 -0300

    [#98297248] prepare dashboard menu to be initialized without user nor mobilization

--- app/scripts/components/DashboardMenu.jsx
+++ app/scripts/components/DashboardMenu.jsx
@@ -4,16 +4,28 @@ export default class DashboardMenu extends React.Component {
   render(){
     var user = this.props.user
     var mobilization = this.props.mobilization
+    var element;
 
-    return (
+    if(mobilization && user){
+      element = <div>
+          <div>
+            {user.first_name} {user.last_name}
+          </div>
+          <div>
+            <h3 className="silver">Mobilização</h3>
+            {mobilization.name}
+          </div>
+        </div>
+    } else {
+      /* TODO: add FontAwesome spinner http://fontawesome.io/examples/#animated */
+      element = <div>
+        Carregando...
+      </div>
+    }
+
+    return(
       <div className="bg-gray p2 white">
-        <div>
-          {user.first_name} {user.last_name}
-        </div>
-        <div>
-          <h3 className="silver">Mobilização</h3>
-          {mobilization.name}
-        </div>
+        {element}
       </div>
     )
   }
