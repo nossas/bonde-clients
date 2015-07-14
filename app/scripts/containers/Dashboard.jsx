fd973bf59ff502ef8c7d524ab2ea7b6bcf590f9c
Nícolas Iensen <nicolas.iensen@gmail.com>
2015-07-13 22:12:19 -0300

    [#98297248] fetch mobilizations on Dashboard component mount

--- app/scripts/containers/Dashboard.jsx
+++ app/scripts/containers/Dashboard.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import DashboardMenu from './../components/DashboardMenu.jsx'
+import * as MobilizationActions from './../actions/MobilizationActions'
 import { connect } from 'redux/react'
+import { bindActionCreators } from 'redux'
 
 @connect(state => ({
   mobilizations: state.mobilizations
 }))
 
 export default class Dashboard extends React.Component {
+  componentDidMount(){
+    const { dispatch } = this.props
+    const actions = bindActionCreators(MobilizationActions, dispatch)
+    actions.fetchMobilizations()
+  }
+
   render(){
     const mobilization = this.props.mobilizations[0]
 
--- app/scripts/stores/mobilizations.js
+++ app/scripts/stores/mobilizations.js
@@ -1,12 +1,9 @@
-const initialState = [
-  {
-    name: "Bicicletada contra a redução da maioridade penal",
-    id: 1
-  }
-]
+import { FETCH_MOBILIZATIONS } from './../constants/ActionTypes'
 
-export default function mobilizations(state = initialState, action) {
+export default function mobilizations(state = [], action) {
   switch (action.type) {
+    case FETCH_MOBILIZATIONS:
+      return action.mobilizations
     default:
       return state
   }
