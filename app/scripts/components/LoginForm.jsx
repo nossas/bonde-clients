384b8bcefaf7e6e0f051c142e658d6b7bca8a656
Josias Schneider <josias.ds@gmail.com>
2015-08-04 17:37:43 -0300

    [#98824248] adds simple validation on Login form

--- app/scripts/components/LoginForm.jsx
+++ app/scripts/components/LoginForm.jsx
@@ -14,20 +14,48 @@ export default class LoginForm extends React.Component {
     super(props, context)
     this.state = {
       email: null,
-      password: null
+      password: null,
+      errorMessage: null
     }
+  }
+
+  validateForm() {
+    if (!this.state.email) {
+      this.setState({ errorMessage: 'Informe o email.' })
+    } else if (!this.state.password) {
+      this.setState({ errorMessage: 'Informe a senha.' })
+    } else {
+      this.setState({ errorMessage: null })
+      return true
+    }
+    return false
   }
 
   handleSubmit(event) {
     event.preventDefault()
-    Auth.emailSignIn(this.state).
-      then(function(user){
-        // TODO change this to mobilizations index when we have that page
-        this.transitionTo(Paths.editMobilization(1))
-      }.bind(this)).
-      fail(function(error){
-        console.log(error)
-      })
+
+    if (this.validateForm()) {
+      Auth.emailSignIn(this.state).
+        then(function(user){
+          // TODO change this to mobilizations index when we have that page
+          this.transitionTo(Paths.editMobilization(1))
+        }.bind(this)).
+        fail(function(error){
+          this.handleLoginError(error.reason)
+        }.bind(this))
+    }
+  }
+
+  handleLoginError(error) {
+    this.setState({ errorMessage: error })
+  }
+
+  renderErrorMessage() {
+    if (this.state.errorMessage) {
+      return (
+        <div className="red mb2">{this.state.errorMessage}</div>
+      )
+    }
   }
 
   render() {
@@ -44,6 +72,8 @@ export default class LoginForm extends React.Component {
           type="password"
           className="field-light block full-width mb2"
           valueLink={this.linkState('password')} />
+
+        {this.renderErrorMessage()}
 
         <input type="submit" className="button right" value="Entrar" />
       </form>
