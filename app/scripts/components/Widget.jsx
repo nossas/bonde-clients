e5272c3899e23ffed53d2d1e5518435c6ef9cdf2
Nícolas Iensen <nicolas.iensen@gmail.com>
2015-08-10 18:52:08 -0300

    [#98823500] add support to responsive block size

--- app/scripts/components/Widget.jsx
+++ app/scripts/components/Widget.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import ContentWidget from "./ContentWidget.jsx"
+import classNames from 'classnames'
 
 export default class Widget extends React.Component {
   render(){
-    var className = "px2 col col-" + this.props.widget.size
+    const className = classNames(
+      'px2', 'col',
+      'col-' + this.props.widget.sm_size,
+      'sm-col-' + this.props.widget.sm_size,
+      'md-col-' + this.props.widget.md_size,
+      'lg-col-' + this.props.widget.lg_size
+    )
 
     return(
       <div className={className}>
