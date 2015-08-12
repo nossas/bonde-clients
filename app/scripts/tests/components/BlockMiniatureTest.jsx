84ed8e783b35e6cc226d23a5f43a3fb915b552bc
nicolasiensen <nicolas.iensen@gmail.com>
2015-08-12 13:44:22 -0300

    [#98823500] fix BlockMiniature tests

--- app/scripts/tests/components/BlockMiniatureTest.jsx
+++ app/scripts/tests/components/BlockMiniatureTest.jsx
@@ -1,29 +1,33 @@
 import React from 'react/addons'
 import { BlockMiniature } from './../../components'
+import { BLOCK_LAYOUTS } from './../../constants/BlockLayouts'
 
 const { TestUtils } = React.addons
-
-const onClick = () => { alert('miniature was clicked!') }
+let layout, component
 
 describe('BlockMiniature', () => {
   describe('#render', () => {
-    [1, 2, 3, 4, 5].forEach((columns) => {
-      it(`should render with ${columns} column, selected or unselected, bind onClick events`, () => {
-        const component = TestUtils.renderIntoDocument(
-          <BlockMiniature sizes={Array.apply(null, new Array(columns)).map(() => { return 4 })} selectedSizes={[4, 4, 4]} onClick={onClick.bind(component)} />
-        )
-        const container = TestUtils.scryRenderedDOMComponentsWithTag(component, 'div')[0]
-        expect(container.props.onClick.toString()).to.equal(onClick.bind(component).toString())
-        const columnsContainer = React.findDOMNode(component).childNodes[0]
-        expect(columnsContainer.childNodes).to.have.length(columns)
-        if(columns == 3) {
-          expect(columnsContainer.getAttribute('class')).not.to.include('bg-white')
-          expect(columnsContainer.getAttribute('class')).to.include('bg-silver')
-        } else {
-          expect(columnsContainer.getAttribute('class')).to.include('bg-white')
-          expect(columnsContainer.getAttribute('class')).not.to.include('bg-silver')
-        }
-      })
+    before(() => {
+      layout = [
+        {sm_size: 12, md_size: 12, lg_size: 6},
+        {sm_size: 12, md_size: 12, lg_size: 6}
+      ]
+
+      component = TestUtils.renderIntoDocument(
+        <BlockMiniature layout={layout} selectedLayout={layout} />
+      )
+    })
+
+    it('should render a layout miniature', () => {
+      expect(
+        TestUtils.scryRenderedDOMComponentsWithClass(component, "col-6")
+      ).to.have.length(2)
+    })
+
+    it('should be active if it is the selected layout', () => {
+      expect(
+        TestUtils.scryRenderedDOMComponentsWithClass(component, "is-active")
+      ).to.have.length(1)
     })
   })
 })
