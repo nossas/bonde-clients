c74938ba0e54c73810b3442efb14b9a465a55561
Daniel Weinmann <danielweinmann@gmail.com>
2015-08-16 02:24:26 -0300

    Adds top steps to NewMobilization

--- app/scripts/pages/NewMobilization.jsx
+++ app/scripts/pages/NewMobilization.jsx
@@ -74,8 +74,12 @@ export default class NewMobilization extends React.Component {
 
     return(
       <div className="flex-auto bg-silver gray">
-        <h2 className="bg-white mt0 py3 px4">
-          Nova mobilização
+        <h2 className="bg-white px3 m0 clearfix" style={{paddingTop: '2rem'}}>
+          <div className="col col-4 mt0">Nova mobilização</div>
+          <ul className="list-reset m0 col col-8" style={{marginTop: '-25px'}}>
+            <li className="inline-block py3 mr3 border-bottom" style={{borderWidth: '3px'}}>1. Nome e objetivo</li>
+            <li className="inline-block muted">2. Cidade</li>
+          </ul>
         </h2>
         <div className="p3">
           <h3 className="h2 mt0 mb3 center">Conte mais sobre sua mobilização</h3>
