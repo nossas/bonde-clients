97efb6a98feb5215f63608ae963bac719e7dd6b9
nicolasiensen <nicolas.iensen@gmail.com>
2015-08-29 11:47:06 -0300

    Add route to mobilizations list

--- src/views/createRoutes.js
+++ src/views/createRoutes.js
@@ -2,11 +2,17 @@ import React from 'react';
 import { Route } from 'react-router';
 import Application from '../../app/scripts/containers/Application.jsx';
 import Login from '../../app/scripts/pages/Login.jsx';
+import Mobilizations from '../../app/scripts/containers/Mobilizations.jsx';
+import ListMobilizations from '../../app/scripts/pages/ListMobilizations.jsx';
+import TopMenu from '../../app/scripts/components/TopMenu.jsx';
 
 export default function(store) {
   return (
-    <Route path="/" component={Application}>
+    <Route component={Application}>
       <Route path="/login" component={Login} />
+      <Route component={Mobilizations}>
+        <Route path="/" components={{main: ListMobilizations, topMenu: TopMenu}} />
+      </Route>
     </Route>
   );
 }
