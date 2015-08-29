d2d3dd5331fd3b1fb8b04bc0a0d287fc89acb46c
nicolasiensen <nicolas.iensen@gmail.com>
2015-08-29 11:46:34 -0300

    Add mobiliations reducer to the new store

--- src/ducks/reducer.js
+++ src/ducks/reducer.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
-import auth from './auth';
+import mobilizations from './../../app/scripts/reducers/mobilizations'
 
 export default combineReducers({
-  auth
+  mobilizations
 });
