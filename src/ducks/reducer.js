112fcf3a84a1bfd8fe1a88120db35551d9b0f19a
nicolasiensen <nicolas.iensen@gmail.com>
2015-08-29 12:55:03 -0300

    Add route /mobilizations/:id to the SSR solution

--- src/ducks/reducer.js
+++ src/ducks/reducer.js
@@ -1,10 +1,14 @@
 import { createFormReducer } from 'redux-form'
 import { combineReducers } from 'redux';
 import mobilizations from './../../app/scripts/reducers/mobilizations'
+import blocks from './../../app/scripts/reducers/blocks'
+import widgets from './../../app/scripts/reducers/widgets'
 
 const mobilizationBasics = createFormReducer('mobilizationBasics', ['name', 'goal'])
 
 export default combineReducers({
   mobilizations,
-  mobilizationBasics
+  mobilizationBasics,
+  blocks,
+  widgets
 });
--- src/views/createRoutes.js
+++ src/views/createRoutes.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Route } from 'react-router'
-import { Application, Mobilizations } from '../../app/scripts/containers'
-import { Login, ListMobilizations, NewMobilization } from '../../app/scripts/pages'
+import { Application, Mobilizations, Mobilization } from '../../app/scripts/containers'
+import { Login, ListMobilizations, NewMobilization, ShowMobilization } from '../../app/scripts/pages'
 import { TopMenu } from '../../app/scripts/components'
 
 export default function(store) {
@@ -11,6 +11,9 @@ export default function(store) {
       <Route component={Mobilizations}>
         <Route path="/" components={{main: ListMobilizations, topMenu: TopMenu}} />
         <Route path="/mobilizations/new" components={{main: NewMobilization, topMenu: TopMenu}} />
+          <Route component={Mobilization} >
+            <Route path="/mobilizations/:mobilization_id" components={{main: ShowMobilization}} />
+          </Route>
       </Route>
     </Route>
   );
