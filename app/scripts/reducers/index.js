918aee78877846ab2206f75b88484e2b33da8d73
Josias Schneider <josias.ds@gmail.com>
2015-09-01 17:59:11 -0300

    [#98310562] WIP: create form for edit mobilization fonts

--- app/scripts/reducers/index.js
+++ app/scripts/reducers/index.js
@@ -4,7 +4,9 @@ export { default as auth } from './auth';
 export { default as mobilizations } from './mobilizations';
 export { default as blocks } from './blocks';
 export { default as widgets } from './widgets';
+
 export const loginForm = createFormReducer('loginForm', ['email', 'password'])
+export const mobilizationFonts = createFormReducer('mobilizationFonts', ['headerFont', 'bodyFont'])
 export const mobilizationBasics = createFormReducer('mobilizationBasics', ['name', 'goal'])
 export const mobilizationCity = createFormReducer('mobilizationCity', ['colorScheme'])
 export const mobilizationAnalytics = createFormReducer('mobilizationAnalytics', ['id'])
