61a64f2cf388c47dc323e40a9c40c303c07d6787
Daniel Weinmann <danielweinmann@gmail.com>
2015-09-01 00:42:45 -0300

    Updates texts on form widget configuration

--- app/scripts/components/FormWidgetInputForm.jsx
+++ app/scripts/components/FormWidgetInputForm.jsx
@@ -178,6 +178,7 @@ export default class FormWidgetInput extends React.Component {
               <div className="col col-8">
                 <input 
                   className="field-light block full-width"
+                  placeholder="Ex: Email"
                   style={{height: '52px'}}
                   type="text"
                   value={this.state.label}
@@ -186,11 +187,12 @@ export default class FormWidgetInput extends React.Component {
             </div>
             <div className="flex flex-center mb2">
               <div className="col col-4">
-                <label className="h5 bold">Texto de apoio</label>
+                <label className="h5 bold">Texto de ajuda</label>
               </div>
               <div className="col col-8">
                 <input 
                   className="field-light block full-width"
+                  placeholder="Ex: Insira aqui o seu email"
                   style={{height: '52px'}}
                   type="text"
                   value={this.state.placeholder}
