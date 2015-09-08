f5f4bfd2d4e3f9ad3dfce89195f086768829e051
nicolasiensen <nicolas.iensen@gmail.com>
2015-09-08 13:40:48 -0300

    Load current user in SSR time

--- app/scripts/containers/Application.jsx
+++ app/scripts/containers/Application.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { connect } from 'react-redux'
 import PubSub from 'pubsub-js'
 import $ from 'jquery'
+import {isLoaded as isAuthLoaded, load as loadAuth, logout} from './../reducers/auth'
 
 import '../../styles/main.scss'
 import '../../../node_modules/font-awesome/scss/font-awesome.scss'
@@ -13,5 +14,13 @@ export default class Application extends React.Component {
         {this.props.children && React.cloneElement(this.props.children)}
       </div>
     )
+  }
+
+  static fetchData(store) {
+    const promises = [];
+    if (!isAuthLoaded(store.getState())) {
+      promises.push(store.dispatch(loadAuth()));
+    }
+    return Promise.all(promises);
   }
 }
