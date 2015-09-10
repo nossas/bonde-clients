858212ce5d8f09cd368d636268e4dcb1ddd59d75
nicolasiensen <nicolas.iensen@gmail.com>
2015-09-10 15:53:45 -0300

    Add support to blocks fetching in the Mobilization component

--- app/scripts/containers/Mobilization.jsx
+++ app/scripts/containers/Mobilization.jsx
@@ -1,19 +1,36 @@
-import React from 'react'
+import React, { PropTypes } from 'react'
 import { connect } from 'react-redux'
+import { fetchBlocks, isBlocksLoaded } from './../reducers/blocks'
 
-@connect(state => ({ auth: state.auth }))
+@connect(state => ({
+  auth: state.auth,
+  blocks: state.blocks
+}))
+
 export default class Mobilization extends React.Component {
-  renderTopMenu() {
-    return this.props.topMenu && React.cloneElement(this.props.topMenu, {...this.props})
+  static propTypes = {
+    main: PropTypes.object.isRequired,
+    topMenu: PropTypes.object,
+    sidebar: PropTypes.object
   }
 
-  render(){
-    return(
+  static fetchData(store, params) {
+    const promises = []
+    if (!isBlocksLoaded(store.getState())) {
+      const action = fetchBlocks({mobilization_id: params.mobilization_id})
+      const promise = store.dispatch(action)
+      promises.push(promise)
+    }
+    return Promise.all(promises)
+  }
+
+  render() {
+    return (
       <div>
-        { this.renderTopMenu() }
+        { this.props.topMenu && React.cloneElement(this.props.topMenu, {...this.props}) }
         <div className="flex flex-stretch">
           { this.props.sidebar && React.cloneElement(this.props.sidebar, {...this.props}) }
-          { this.props.main && React.cloneElement(this.props.main, {...this.props}) }
+          { React.cloneElement(this.props.main, {...this.props}) }
         </div>
       </div>
     )
--- app/scripts/reducers/blocks.js
+++ app/scripts/reducers/blocks.js
@@ -1,38 +1,74 @@
-import { FETCH_BLOCKS, EDIT_BLOCK, REMOVE_BLOCK, MOVE_BLOCK_UP, MOVE_BLOCK_DOWN } from './../constants/ActionTypes'
+import superagent from 'superagent'
 
-export default function blocks(state = [], action) {
+const REQUEST_FETCH_BLOCKS = 'REQUEST_FETCH_BLOCKS'
+const SUCCESS_FETCH_BLOCKS = 'SUCCESS_FETCH_BLOCKS'
+const FAILURE_FETCH_BLOCKS = 'FAILURE_FETCH_BLOCKS'
+const EDIT_BLOCK = 'EDIT_BLOCK'
+const REMOVE_BLOCK = 'REMOVE_BLOCK'
+const MOVE_BLOCK_UP = 'MOVE_BLOCK_UP'
+const MOVE_BLOCK_DOWN = 'MOVE_BLOCK_DOWN'
+
+const initialState = {
+  loaded: false,
+  data: []
+}
+
+export default function blocks(state = initialState, action) {
   switch (action.type) {
-    case FETCH_BLOCKS:
-      return action.blocks
+    case REQUEST_FETCH_BLOCKS:
+      return {...state, loaded: false}
+    case SUCCESS_FETCH_BLOCKS:
+      return {...state, loaded: true, data: action.result }
+    case FAILURE_FETCH_BLOCKS:
+      return {...state, loaded: true}
     case EDIT_BLOCK:
       return state.map(block =>
-        block.id == action.block.id ? action.block : block
+        block.id === action.block.id ? action.block : block
       )
     case MOVE_BLOCK_UP:
       return state.map((block, index) => {
-        if (index + 1 < state.length && state[index + 1].id == action.block.id) {
+        if (index + 1 < state.length && state[index + 1].id === action.block.id) {
           return action.block
-        } else if (block.id == action.block.id) {
+        } else if (block.id === action.block.id) {
           return state[index - 1]
-        } else {
-          return block
         }
+        return block
       })
     case MOVE_BLOCK_DOWN:
       return state.map((block, index) => {
-        if (index > 0 && state[index - 1].id == action.block.id) {
+        if (index > 0 && state[index - 1].id === action.block.id) {
           return action.block
-        } else if (block.id == action.block.id) {
+        } else if (block.id === action.block.id) {
           return state[index + 1]
-        } else {
-          return block
         }
+        return block
       })
     case REMOVE_BLOCK:
       return state.filter(block =>
-        action.block.id != block.id
+        action.block.id !== block.id
       )
     default:
       return state
+  }
+}
+
+export function isBlocksLoaded(globalState) {
+  return globalState.blocks.loaded
+}
+
+export function fetchBlocks(options) {
+  return {
+    types: [REQUEST_FETCH_BLOCKS, SUCCESS_FETCH_BLOCKS, FAILURE_FETCH_BLOCKS],
+    promise: function() {
+      return new Promise(function(resolve, reject) {
+        superagent.get(`http://localhost:3000/mobilizations/${options.mobilization_id}/blocks`).end((err, res) => {
+          if (err) {
+            reject(res.body || err)
+          } else {
+            resolve(res.body)
+          }
+        })
+      })
+    }
   }
 }
