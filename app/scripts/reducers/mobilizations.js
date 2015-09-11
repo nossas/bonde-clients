65828354223c5018fab9e3cc40a639105528bce1
nicolasiensen <nicolas.iensen@gmail.com>
2015-09-11 15:43:00 -0300

    Add API_URL env var

--- app/scripts/reducers/mobilizations.js
+++ app/scripts/reducers/mobilizations.js
@@ -47,7 +47,7 @@ export function loadMobilizations() {
     types: [REQUEST_FETCH_MOBILIZATIONS, SUCCESS_FETCH_MOBILIZATIONS, FAILURE_FETCH_MOBILIZATIONS],
     promise: function() {
       return new Promise(function(resolve, reject) {
-        superagent.get('http://localhost:3000/mobilizations').end((err, res) => {
+        superagent.get(`${process.env.API_URL}/mobilizations`).end((err, res) => {
           if (err) {
             reject(res.body || err)
           } else {
