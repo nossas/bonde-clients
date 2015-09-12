51e07cbd7de8e9502c8bf259515a0fcac5f87dea
nicolasiensen <nicolas.iensen@gmail.com>
2015-09-12 19:34:44 -0300

    Add MobilizationFonts to the SSR routes

--- src/views/createRoutes.js
+++ src/views/createRoutes.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import { Route } from 'react-router'
 import { Application, Mobilizations, Mobilization } from '../../app/scripts/containers'
-import { Login, Logout, ListMobilizations, NewMobilization, ShowMobilization,
-  EditMobilization, MobilizationBasics, MobilizationCity,
-  MobilizationAnalytics, NewBlock, FormWidgetFields,
-  FormWidgetForm } from '../../app/scripts/pages'
 import { TopMenu, MobilizationMenu } from '../../app/scripts/components'
 
-export default function(store) {
+import {
+  Login, Logout, ListMobilizations, NewMobilization, ShowMobilization,
+  EditMobilization, MobilizationBasics, MobilizationCity,
+  MobilizationAnalytics, NewBlock, FormWidgetFields,
+  FormWidgetForm, MobilizationFonts
+} from '../../app/scripts/pages'
+
+export default function() {
   return (
     <Route component={Application}>
       <Route path="/login" component={Login} />
@@ -22,11 +25,12 @@ export default function(store) {
           <Route path="/mobilizations/:mobilization_id/city" components={{main: MobilizationCity, sidebar: MobilizationMenu, topMenu: TopMenu}} />
           <Route path="/mobilizations/:mobilization_id/cityNew" components={{main: MobilizationCity, topMenu: TopMenu}} />
           <Route path="/mobilizations/:mobilization_id/analytics" components={{main: MobilizationAnalytics, sidebar: MobilizationMenu, topMenu: TopMenu}} />
+          <Route path="/mobilizations/:mobilization_id/fonts" components={{main: MobilizationFonts, sidebar: MobilizationMenu, topMenu: TopMenu}} />
           <Route path="/mobilizations/:mobilization_id/blocks/new" components={{main: NewBlock, sidebar: MobilizationMenu, topMenu: TopMenu}} />
           <Route path="/mobilizations/:mobilization_id/widgets/:widget_id/fields" components={{main: FormWidgetFields, sidebar: MobilizationMenu, topMenu: TopMenu}} />
           <Route path="/mobilizations/:mobilization_id/widgets/:widget_id/form" components={{main: FormWidgetForm, sidebar: MobilizationMenu, topMenu: TopMenu}} />
         </Route>
       </Route>
     </Route>
-  );
+  )
 }
