94235e555c00fbfb971e4124ff7556e105db9b07
nicolasiensen <nicolas.iensen@gmail.com>
2015-09-12 19:21:12 -0300

    Refactor app/scripts/components/DropDownMenuItem.jsx

--- app/scripts/components/DropDownMenuItem.jsx
+++ app/scripts/components/DropDownMenuItem.jsx
@@ -1,18 +1,32 @@
-import React from 'react'
+import React, { PropTypes } from 'react'
 import classnames from 'classnames'
 
 export default class DropDownMenuItem extends React.Component {
-  handleClick(event){
+  static propTypes = {
+    href: PropTypes.string,
+    children: PropTypes.object,
+    onClick: PropTypes.func,
+    onItemClick: PropTypes.func,
+    disabled: PropTypes.bool
+  }
+
+  handleClick(event) {
     (this.props.onClick || this.props.disabled) && event.preventDefault()
-    if(!this.props.disabled) {
+    if (!this.props.disabled) {
       this.props.onItemClick && this.props.onItemClick()
       this.props.onClick && this.props.onClick()
     }
   }
 
-  render(){
-    return(
-      <a className={classnames("button button-transparent full-width left-align block", (this.props.disabled ? 'muted' : ''))} disabled={this.props.disabled} onClick={::this.handleClick} href={this.props.href}>{this.props.children}</a>
+  render() {
+    return (
+      <a
+        className={classnames('button button-transparent full-width left-align block', (this.props.disabled ? 'muted' : ''))}
+        disabled={this.props.disabled}
+        onClick={::this.handleClick}
+        href={this.props.href}>
+        {this.props.children}
+      </a>
     )
   }
 }
