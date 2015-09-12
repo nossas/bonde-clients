86a8dd060ccdfb28ad7cab4a614944079da1e20a
nicolasiensen <nicolas.iensen@gmail.com>
2015-09-12 16:34:16 -0300

    Change NewBlock component to support new blocks reducer

--- app/scripts/pages/NewBlock.jsx
+++ app/scripts/pages/NewBlock.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { PropTypes } from 'react'
 import ReactS3Uploader from 'react-s3-uploader'
-import { bindActionCreators } from 'redux'
-import * as BlockActions from './../actions/BlockActions'
 import classnames from 'classnames'
 import { BlockMiniature, ColorPicker, Progress, CloseButton } from './../components'
 import { BLOCK_LAYOUTS } from '../constants/BlockLayouts'
 import * as Paths from '../Paths'
+import {addBlock} from '../reducers/blocks'
 
 export default class NewBlock extends React.Component {
+  static propTypes = {
+    dispatch: PropTypes.object.isRequired,
+    blocks: PropTypes.object.isRequired,
+    auth: PropTypes.object.isRequired,
+    mobilization: PropTypes.object.isRequired
+  }
+
+  static contextTypes = {
+    router: React.PropTypes.object.isRequired
+  }
 
   constructor(props, context) {
     super(props, context)
@@ -16,6 +25,15 @@ export default class NewBlock extends React.Component {
       bgClass: 'bg-1',
       bgImage: null,
       uploadProgress: null
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const {blocks, mobilization} = this.props
+
+    if (blocks.data.length !== nextProps.blocks.data.length) {
+      const {router} = this.context
+      router.transitionTo(Paths.editMobilization(mobilization.id) + '?newBlock=true')
     }
   }
 
@@ -29,9 +47,8 @@ export default class NewBlock extends React.Component {
 
   handleAddBlockClick() {
     const { dispatch, auth } = this.props
-    const bindedBlockActions = bindActionCreators(BlockActions, dispatch)
-    bindedBlockActions.addBlock({
-      router: this.context.router,
+
+    const action = addBlock({
       mobilization_id: this.props.mobilization.id,
       credentials: auth.credentials,
       block: {
@@ -42,6 +59,8 @@ export default class NewBlock extends React.Component {
         })
       }
     })
+
+    dispatch(action)
   }
 
   handleCancelClick() {
@@ -103,9 +122,9 @@ export default class NewBlock extends React.Component {
     }
   }
 
-  render(){
+  render() {
     return (
-      <div className={classnames("flex-auto bg-silver gray relative", this.props.mobilization.color_scheme)}>
+      <div className={classnames('flex-auto bg-silver gray relative', this.props.mobilization.color_scheme)}>
         <h2 className="bg-white mt0 py3 px4">Adicione um bloco de conteúdo</h2>
         <div className="py3 px4">
           <p
@@ -115,7 +134,7 @@ export default class NewBlock extends React.Component {
           <label className="caps h6 bold mb1 block">Tipo de bloco</label>
           <div className="mxn1">
             {BLOCK_LAYOUTS.map((layout, index) => {
-              return(
+              return (
                 <BlockMiniature
                   key={index}
                   layout={layout}
@@ -153,8 +172,4 @@ export default class NewBlock extends React.Component {
       </div>
     )
   }
-}
-
-NewBlock.contextTypes = {
-  router: React.PropTypes.object.isRequired
 }
