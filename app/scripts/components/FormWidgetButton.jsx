c8989f488ba2aeb664c2d38c26dc1858469a66fc
nicolasiensen <nicolas.iensen@gmail.com>
2015-09-21 10:47:19 -0300

    [#100327136] add validation to the FormWidgetButton

--- app/scripts/components/FormWidgetButton.jsx
+++ app/scripts/components/FormWidgetButton.jsx
@@ -3,6 +3,8 @@ import $ from 'jquery'
 import { bindActionCreators } from 'redux'
 import * as FormEntryActions from './../actions/FormEntryActions'
 
+const emailRegEx = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i
+
 export default class FormWidgetButton extends React.Component {
   static propTypes = {
     editable: PropTypes.bool,
@@ -16,7 +18,8 @@ export default class FormWidgetButton extends React.Component {
     super(props, context)
     this.state = {
       loading: false,
-      success: false
+      success: false,
+      errors: []
     }
   }
 
@@ -26,39 +29,66 @@ export default class FormWidgetButton extends React.Component {
     }
   }
 
+  validate(fieldsWithValue) {
+    const errors = []
+    fieldsWithValue.forEach((field) => {
+      if (field.required && field.value === '') {
+        errors.push(`${field.label} não pode ficar em branco`)
+      } else if (field.value !== '' && field.kind === 'email' && !field.value.match(emailRegEx)) {
+        errors.push(`${field.label} inválido`)
+      }
+    })
+    return errors
+  }
+
   handleClick() {
     if (!this.props.editable) {
       const { dispatch, mobilization, widget } = this.props
       const { settings } = widget
       const { fields } = settings
       const bindedFormEntryActions = bindActionCreators(FormEntryActions, dispatch)
-      this.setState({
-        loading: true
-      })
       const fieldsWithValue = fields.map((field) => {
         return {...field, value: $('#input-' + field.uid).val()}
       })
-      bindedFormEntryActions.addFormEntry({
-        mobilization_id: mobilization.id,
-        form_entry: {
-          widget_id: widget.id,
-          fields: JSON.stringify(fieldsWithValue)
-        }
-      })
+      const errors = this.validate(fieldsWithValue)
+      this.setState({errors: errors})
+
+      if (errors.length === 0) {
+        this.setState({loading: true})
+        bindedFormEntryActions.addFormEntry({
+          mobilization_id: mobilization.id,
+          form_entry: {
+            widget_id: widget.id,
+            fields: JSON.stringify(fieldsWithValue)
+          }
+        })
+      }
     }
   }
 
   render() {
+    const {errors} = this.state
+
     return (
       <div>
         <button
           disabled={this.state.loading}
-          className="caps button bg-darken-4 p2 full-width mt1"
+          className="caps button bg-darken-4 p2 full-width mt1 mb2"
           onClick={::this.handleClick}>
           {this.state.loading ? 'Enviando...' : this.props.buttonText }
         </button>
+        {
+          this.state.errors.length > 0 &&
+          <div className="red bold mb1 rounded-right" style={{backgroundColor: 'rgba(69, 17, 17, 0.75)'}}>
+            {
+              errors.map((error) => {
+                return <div className="p2 border-left border-red" style={{borderWidth: '4px'}}>{error}</div>
+              })
+            }
+          </div>
+        }
         { this.state.success &&
-          <div className="center mt2">Sua ação foi registrada com sucesso!</div> }
+          <div className="center">Sua ação foi registrada com sucesso!</div> }
       </div>
     )
   }
