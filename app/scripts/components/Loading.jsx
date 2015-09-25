25df6fc5269b94e85d76d4c8efaf6fadd50ec5df
nicolasiensen <nicolas.iensen@gmail.com>
2015-09-25 12:13:27 -0300

    [#102387980] change loading icon

--- app/scripts/components/Loading.jsx
+++ app/scripts/components/Loading.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 export default class Loading extends React.Component {
   render() {
-    return(
-      <div className="absolute top-0 right-0 bottom-0 left-0 bg-darken-4 flex flex-center">
-        <div className="mx-auto" style={{zIndex: 9999}}>
-          <i className="fa fa-refresh fa-spin fa-3x fa-w white" />
+    return (
+      <div className="absolute top-0 right-0 bottom-0 left-0 bg-darken-4 flex flex-center z4">
+        <div className="mx-auto">
+          <i className="fa fa-circle-o-notch fa-spin fa-3x fa-w white" />
         </div>
       </div>
     )
