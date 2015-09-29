f2f6da9052d596d030207bedd38256d380287123
nicolasiensen <nicolas.iensen@gmail.com>
2015-09-29 17:29:40 -0300

    [#102391524] move route /mobilizations/:mobilization_id to the bottom and move route /mobilizations/:mobilization_id/cityNew out of MobilizationDashboard

--- src/views/createRoutes.js
+++ src/views/createRoutes.js
@@ -30,16 +30,15 @@ export default function(store) {
     <Route component={Application}>
       <Route path="/login" component={Login} />
       <Route path="/logout" component={Logout} />
-      <Route path="/mobilizations/:mobilization_id" component={ShowMobilization} />
       <Route component={RequireLogin} onEnter={RequireLogin.onEnter(store)}>
         <Route component={UserDashboard}>
           <Route path="/" component={ListMobilizations} />
           <Route path="/mobilizations/new" component={NewMobilization} />
+          <Route path="/mobilizations/:mobilization_id/cityNew" component={MobilizationCity} />
           <Route component={MobilizationDashboard} >
             <Route path="/mobilizations/:mobilization_id/edit" component={EditMobilization} />
             <Route path="/mobilizations/:mobilization_id/basics" component={MobilizationBasics} />
             <Route path="/mobilizations/:mobilization_id/city" component={MobilizationCity} />
-            <Route path="/mobilizations/:mobilization_id/cityNew" component={MobilizationCity} />
             <Route path="/mobilizations/:mobilization_id/analytics" component={MobilizationAnalytics} />
             <Route path="/mobilizations/:mobilization_id/fonts" component={MobilizationFonts} />
             <Route path="/mobilizations/:mobilization_id/sharing" component={MobilizationSharing} />
@@ -49,6 +48,7 @@ export default function(store) {
           </Route>
         </Route>
       </Route>
+      <Route path="/mobilizations/:mobilization_id" component={ShowMobilization} />
     </Route>
   )
 }
