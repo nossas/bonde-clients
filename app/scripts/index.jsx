64abe8a1357ed342e53a12a4c56ae9c404a712d4
nicolasiensen <nicolas.iensen@gmail.com>
2015-10-14 12:56:05 -0300

    [#98822994] clean up app folder

--- app/scripts/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-var Root = require('./Root.jsx')
-import HashHistory from 'react-router/lib/HashHistory'
-
-const history = new HashHistory()
-
-React.render(<Root history={history} />, document.body)
