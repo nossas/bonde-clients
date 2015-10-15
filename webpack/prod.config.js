633d5c8b3453accb235628665877dc3fc19144f9
nicolasiensen <nicolas.iensen@gmail.com>
2015-10-15 10:17:58 -0300

    Debug production env vars

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -63,7 +63,7 @@ module.exports = {
         // Useful to reduce the size of client-side libraries, e.g. react
         NODE_ENV: JSON.stringify('production'),
         API_URL: JSON.stringify('http://hub-api.herokuapp.com'),
-        APP_DOMAIN: JSON.stringify('reboo.org')
+        APP_DOMAIN: JSON.stringify(process.env.APP_DOMAIN)
       }
     }),
 
