ef9f50cdc3df50c0ca6c188446c720619a213455
nicolasiensen <nicolas.iensen@gmail.com>
2015-10-16 11:06:20 -0300

    [#98822994] add MobilizationCustomDomain component

---
+++ app/scripts/pages/MobilizationCustomDomain.jsx
@@ -0,0 +1,11 @@
+import React, { PropTypes } from 'react'
+
+const MobilizationCustomDomain = React.createClass({
+  render() {
+    return (
+      <div>MobilizationCustomDomain</div>
+    )
+  }
+})
+
+export default MobilizationCustomDomain
