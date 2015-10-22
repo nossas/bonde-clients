b2fecefb99426cb1587cb09ccde5f22bd8f809e5
nicolasiensen <nicolas.iensen@gmail.com>
2015-10-22 09:59:55 -0200

    [#98824172] do some refactoring in the Widget component

--- app/scripts/components/Widget.jsx
+++ app/scripts/components/Widget.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { PropTypes } from 'react'
 import { DraftWidget, ContentWidget, FormWidget } from './'
-import classNames from 'classnames'
+import classnames from 'classnames'
 
 export default class Widget extends React.Component {
+  static propTypes = {
+    widget: PropTypes.object.isRequired
+  }
+
   renderWidget() {
     const { widget } = this.props
     switch (widget.kind) {
@@ -18,22 +22,24 @@ export default class Widget extends React.Component {
         return (
           <FormWidget {...this.props} />
         )
+      default:
+        return (
+          <DraftWidget {...this.props} />
+        )
     }
   }
 
-  render(){
-    const className = classNames(
-      'px2', 'col',
+  render() {
+    const className = classnames(
+      'px2', 'col', 'mb4', 'md-mb0',
       'col-' + this.props.widget.sm_size,
       'sm-col-' + this.props.widget.sm_size,
       'md-col-' + this.props.widget.md_size,
       'lg-col-' + this.props.widget.lg_size
     )
 
-    return(
-      <div className={className}>
-        { this.renderWidget() }
-      </div>
+    return (
+      <div className={className}>{this.renderWidget()}</div>
     )
   }
 }
