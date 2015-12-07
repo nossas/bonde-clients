87d24b323bbec81dc81a4a215b0bce74c678b813
Leticia Figueira <jlcfigueira@gmail.com>
2015-12-07 10:50:57 -0200

    [Finishes #108247328] Lets user choose when links will be opened in a new tab

--- app/scripts/components/WYSIHTMLToolbarCreateLink.jsx
+++ app/scripts/components/WYSIHTMLToolbarCreateLink.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
 
 export default class WYSIHTMLToolbarCreateLink extends React.Component {
+  constructor(props, context) {
+    super(props, context)
+    this.state = { newTab: "_self" }
+  }
+
+  handleChange(e) {
+    this.setState({ newTab: e.target.checked ? "_blank" : "_self" })
+  }
+
   render(){
     return(
       <div data-wysihtml5-dialog="createLink" style={{display: "none"}} className="white p2 bg-darken-3">
         <input type="text" data-wysihtml5-dialog-field="href" defaultValue="http://" className="field-light mr1" />
         <a data-wysihtml5-dialog-action="save" className="button button-outline">Inserir</a>
+        <span className="ml3">
+          <label htmlFor="newTab">Nova aba?</label>
+          <input type="checkbox"
+            name="newTab"
+            onChange={ this.handleChange.bind(this) }
+            value={this.state.newTab}
+            data-wysihtml5-dialog-field="target"
+          />
+        </span>
       </div>
     )
   }
