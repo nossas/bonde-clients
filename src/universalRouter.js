16e9d34ee44a880e959b7bf29b517b2ef45e5da6
Nícolas Iensen <nicolas.iensen@gmail.com>
2016-02-17 20:08:11 +0100

    Undo last change

--- src/universalRouter.js
+++ src/universalRouter.js
@@ -41,8 +41,6 @@ export default function universalRouter(location, history, store, host) {
         });
       }
 
-      initialState = initialState || {}
-
       if (history) {  // only on client side
         initialState.history = history;
       }
