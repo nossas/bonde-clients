fee708ec473122f01eece0660e213c762ecacaac
Nícolas Iensen <nicolas.iensen@gmail.com>
2016-02-20 14:58:33 +0100

    Remove UglifyJsPlugin to fix font-awesome scss compilation

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -70,12 +70,6 @@ module.exports = {
     // optimizations
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-          warnings: false
-        }
-    }),
-
     webpackIsomorphicToolsPlugin
   ]
 };
