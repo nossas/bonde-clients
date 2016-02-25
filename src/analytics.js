24ea4766269e4cdb7b78e6611e1c370e71485cfe
Lucas Pirola <lucaspirola@gmail.com>
2016-02-25 14:44:23 -0300

    [#114472007] Add verification before send data to GA

--- src/analytics.js
+++ src/analytics.js
@@ -3,8 +3,14 @@ const analytics = `(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
+  var page = document.location.pathname;
+  if (page == '/') {
+    var page = window.__data.mobilizations.data[0].id;
+    ga('send', 'pageview', page);
+  }
+
   ga('create', 'UA-26278513-30', 'auto');
-  ga('send', 'pageview');
+  ga('send', 'pageview', page);
 `
 
 export const mobTracker = `ga('create', '{mobTrackingId}', 'auto', {'name': 'mobTracker'});`
