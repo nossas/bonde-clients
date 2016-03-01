a67f5ed90ba933a9abe18bcacf2e74618b73f1ea
Leticia Figueira <jlcfigueira@gmail.com>
2016-02-29 23:24:21 -0300

    Fixes menu height and renames Our Cities' logo file

--- app/scripts/containers/UserDashboard.jsx
+++ app/scripts/containers/UserDashboard.jsx
@@ -53,7 +53,6 @@ export default class UserDashboard extends React.Component {
       position: 'absolute',
       top: 0,
       right: 0,
-      bottom: 0,
       left: 0,
       display: 'flex',
       flexDirection: 'column'
