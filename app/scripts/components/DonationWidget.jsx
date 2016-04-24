9aed04df868c506f2695089135a6301d94600803
Lucas Pirola <lucaspirola@gmail.com>
2016-04-24 12:38:02 -0300

    [#117815037] set default color to publish

--- app/scripts/components/DonationWidget.jsx
+++ app/scripts/components/DonationWidget.jsx
@@ -67,10 +67,11 @@ export default class DonationWidget extends React.Component {
     // DEFINIR AS OPÇÕES
     // e abrir o modal
     var params = {
+      "createToken"       : false,
       "amount"            : widget.settings['donation_value'+selected_value] + "00",
       "customerData"      : widget.settings.customer_data,
       "paymentMethods"    : widget.settings.payment_methods === "true" ? 'credit_card,boleto' : 'credit_card',
-      "uiColor"           : widget.settings.main_color,
+      //"uiColor"           : widget.settings.main_color,
       "paymentButtonText" : widget.settings.button_text
     };
     checkout.open(params);
