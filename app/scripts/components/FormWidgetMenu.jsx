22d4b694f4a4d421adb6ed6530c1712ef8a462a6
Lucas Pirola <lucaspirola@gmail.com>
2016-05-10 15:01:48 -0300

    [#110173274] Better code style and formatting

--- app/scripts/components/FormWidgetMenu.jsx
+++ app/scripts/components/FormWidgetMenu.jsx
@@ -1,28 +1,26 @@
 import React from 'react'
-import { Link } from 'react-router'
 import * as Paths from '../Paths'
-import classNames from 'classnames'
 import { TabMenuItem } from './'
 
 export default class FormWidgetMenu extends React.Component {
-  render(){
+  render () {
     const { mobilization, widget, location } = this.props
     const fieldsPath = Paths.fieldsMobilizationWidget(mobilization.id, widget.id)
     const formPath = Paths.formMobilizationWidget(mobilization.id, widget.id)
 
-    return(
-      <div className="bg-white px3 clearfix">
-        <h2 className="mb3">Configure o formulário da sua ação</h2>
+    return (
+      <div className='bg-white px3 clearfix'>
+        <h2 className='mb3'>Configure o formulário da sua ação</h2>
         <div>
-          <ul className="list-reset mb0">
+          <ul className='list-reset mb0'>
             <TabMenuItem
               path={fieldsPath}
-              text="Campos do formulário"
-              isActive={fieldsPath == location.pathname} />
+              text='Campos do formulário'
+              isActive={fieldsPath === location.pathname} />
             <TabMenuItem
               path={formPath}
-              text="Ajustes"
-              isActive={formPath == location.pathname} />
+              text='Ajustes'
+              isActive={formPath === location.pathname} />
           </ul>
         </div>
       </div>
