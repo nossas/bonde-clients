de9e9cdf0b62584a87bd61e1eb273dd25b6e23b1
Igor Santos <igr.exe@gmail.com>
2016-06-16 10:07:28 -0300

    Add config karma + webpack for run enzyme tests

--- karma.conf.js
+++ karma.conf.js
@@ -23,8 +23,19 @@ module.exports = function(config) {
 
       module: {
         loaders: [
-          { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader?optional=runtime' }
+          {
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader?optional=runtime'
+          }
         ]
+      },
+
+      externals: {
+        'cheerio': 'window',
+        'react/addons': true,
+        'react/lib/ExecutionEnvironment': true,
+        'react/lib/ReactContext': true
       },
 
       plugins: [
