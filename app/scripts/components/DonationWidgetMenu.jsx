f8c0dead9109ea0b50e504b04842974df1e17899
Igor Santos <igr.exe@gmail.com>
2016-06-22 15:39:46 -0300

    Add tests and refactor DonationWidgetMenu component

--- app/scripts/components/DonationWidgetMenu.jsx
+++ app/scripts/components/DonationWidgetMenu.jsx
@@ -1,29 +1,35 @@
-import React from 'react'
+import React, { PropTypes } from 'react'
 import * as Paths from '../Paths'
 import { TabMenuItem } from './'
 
-export default class DonationWidgetMenu extends React.Component {
-  render () {
-    const { mobilization, widget, location } = this.props
-    const donationPath = Paths.donationMobilizationWidget(mobilization.id, widget.id)
-    const autofirePath = Paths.autofireMobilizationWidget(mobilization.id, widget.id)
 
-    return (
-      <div className='bg-white px3 clearfix'>
-        <h2 className="mb3">Configure o bloco de doação</h2>
-        <div>
-          <ul className='list-reset mb0'>
-            <TabMenuItem
-              path={donationPath}
-              text='Ajustes'
-              isActive={donationPath === location.pathname} />
-            <TabMenuItem
-              path={autofirePath}
-              text='Mensagem agradecimento'
-              isActive={autofirePath === location.pathname} />
-          </ul>
-        </div>
+const DonationWidgetMenu = ({ mobilization, widget, location }) => {
+  const donationPath = Paths.donationMobilizationWidget(mobilization.id, widget.id)
+  const autofirePath = Paths.autofireMobilizationWidget(mobilization.id, widget.id)
+
+  return (
+    <div className='bg-white px3 clearfix'>
+      <h2 className="mb3">Configure o bloco de doação</h2>
+      <div>
+        <ul className='list-reset mb0'>
+          <TabMenuItem
+            path={donationPath}
+            text='Ajustes'
+            isActive={donationPath === location.pathname} />
+          <TabMenuItem
+            path={autofirePath}
+            text='Mensagem agradecimento'
+            isActive={autofirePath === location.pathname} />
+        </ul>
       </div>
-    )
-  }
+    </div>
+  )
 }
+
+DonationWidgetMenu.propTypes = {
+  mobilization: PropTypes.object.isRequired,
+  widget: PropTypes.object.isRequired,
+  location: PropTypes.object.isRequired
+}
+
+export default DonationWidgetMenu
