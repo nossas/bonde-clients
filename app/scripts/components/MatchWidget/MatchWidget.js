78b31d9c56c952006b94154b5b2666afb3fdb36b
Igor Santos <igr.exe@gmail.com>
2016-07-01 09:48:24 -0300

    Adds overlay-click with OverlayWidget

--- app/scripts/components/MatchWidget/MatchWidget.js
+++ app/scripts/components/MatchWidget/MatchWidget.js
@@ -1,29 +1,41 @@
 import React, { Component } from 'react'
+
+import { OverlayWidget } from '../OverlayWidget'
 import Choices from './Choices'
 
 
 class MatchWidget extends Component {
 
   constructor(props, context) {
-    super(props,context)
+    super(props, context)
     this.state = {
       numberSelected: undefined,
       letterSelected: undefined
     }
   }
 
+  redirectTo(e) {
+    if (e) e.preventDefault()
+    // TODO: Redirect URL edit MatchWidget
+    console.log('TODO: Redirect URL edit MatchWidget')
+  }
+
   render() {
+    // store state
+    const { editable } = this.props
     const { numberChoices, letterChoices } = this.props
     return (
-      <div className="match-widget p3 bg-darken-3 relative">
-        <Choices selected={this.state.numberSelected} options={numberChoices} onSelected={(selected) => {
-          this.setState({numberSelected: selected})
-        }}/>
-        <Choices selected={this.state.letterSelected} options={letterChoices} onSelected={(selected) => {
-          this.setState({letterSelected: selected})
-        }} />
-        <button className="match" disabled={!this.state.numberSelected && !this.state.letterSelected}>Combinar</button>
-      </div>
+      <OverlayWidget editable={editable} onClick={::this.redirectTo}>
+        <div className="match-widget p3 bg-darken-3 relative">
+          <Choices selected={this.state.numberSelected} options={numberChoices} onSelected={(selected) => {
+            this.setState({numberSelected: selected})
+          }}/>
+          <Choices selected={this.state.letterSelected} options={letterChoices} onSelected={(selected) => {
+            this.setState({letterSelected: selected})
+          }} />
+          <button className="match" disabled={!this.state.numberSelected && !this.state.letterSelected}>Combinar</button>
+        </div>
+      </OverlayWidget>
     )
   }
 }
