6913262122922be927d5d0371337f8c838e3d7a0
Lucas Pirola <lucaspirola@gmail.com>
2016-07-07 01:14:16 -0300

    Add continuos integration config and deploy

---
+++ bin/deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -e
+
+rm -rf ~/.dokku
+git clone https://github.com/dokku/dokku.git ~/.dokku
+
+DOKKU_HOST="reboo-staging.org"
+if [[ "$CIRCLE_BRANCH" == "master" ]]; then
+  DOKKU_HOST="reboo.org"
+fi
+
+REPO_URI="dokku@$DOKKU_HOST:000-client"
+# REPO_SSL="dokku@$DOKKU_HOST:000-client-ssl"
+
+git remote add dokku $REPO_URI
+# git remote add dokku-ssl $REPO_SSL
+
+git push dokku $CIRCLE_BRANCH:master
+# git push dokku-ssl $CIRCLE_BRANCH:master
