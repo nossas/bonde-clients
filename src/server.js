87ea5f8b0b7ecfdcebc9eab64a48ffd4c633d837
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2016-07-18 21:12:01 -0300

    Add redirect domains blacklist validation to not redirect to www #105.

--- src/server.js
+++ src/server.js
@@ -15,6 +15,7 @@ import ApiClient from './ApiClient';
 import universalRouter from './universalRouter';
 import Html from './Html';
 import PrettyError from 'pretty-error';
+import fs from 'fs'
 
 const pretty = new PrettyError();
 const app = new Express();
@@ -70,8 +71,11 @@ app.use((req, res, next) => {
   const host = req.headers.host
   const isAppSubdomain = host.indexOf(`app.${process.env.APP_DOMAIN}`) !== -1
   const www = host.match(/^www\./)
+  const domains = fs.readFileSync('./src/redirect.blacklist')
+  const lines = domains.toString().split('\n')
+  const blacklist = lines.some(line => { if (line) return host.match(line) })
 
-  if (!isAppSubdomain && !__DISABLE_SSR__ && !www) {
+  if (!isAppSubdomain && !__DISABLE_SSR__ && !www && !blacklist) {
     res.redirect(301, `${req.protocol}://www.${host}`)
     return
   }
