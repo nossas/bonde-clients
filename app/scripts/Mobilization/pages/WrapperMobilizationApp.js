8e09c4a970247c6dfa671b4d8c61365c730c5744
Igor Santos <igr.exe@gmail.com>
2016-08-03 17:38:12 -0300

    Fix layout for wrapper mobilization module

--- app/scripts/Mobilization/pages/WrapperMobilizationApp.js
+++ app/scripts/Mobilization/pages/WrapperMobilizationApp.js
@@ -6,7 +6,7 @@ class WrapperMobilizationApp extends Component {
   render() {
     const { children, ...otherProps } = this.props
     return (
-      <div className="wrap-mobilization">
+      <div className="wrap-mobilization flex flex-auto overflow-hidden">
         {
           React.cloneElement(children, {...otherProps})
         }
