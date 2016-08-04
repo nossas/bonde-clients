7e8a660b8140e22e9cacf745ac119eaa09649aa6
Igor Santos <igr.exe@gmail.com>
2016-08-04 19:14:51 -0300

    Add layout to TargetList [ci skip] #127

--- app/scripts/Widget/plugins/PressureWidget/Widget.js
+++ app/scripts/Widget/plugins/PressureWidget/Widget.js
@@ -22,16 +22,43 @@ const PressureWidget = () => {
     boxShadow: "inset 0px 4px 5px -2px rgba(235,235,235,1)",
   }
 
+  const targetItemClassname = 'target-item py1 px2 mr1 bg-white rounded'
+  const imageClassname = 'left circle mr2'
+
 
   return (
-    <div className="pressurewidget">
+    <div className="pressure-widget">
       <h2 className="center py2 px3 m0 white rounded-top" style={{backgroundColor: bgColor}}>Envie um e-mail para quem pode tomar essa decisão</h2>
       {/* TODO: Change HTML by TargetList */}
-      <div className="targetlist flex" style={{backgroundColor: '#eeeeee'}}>
-        <p className="bold px2 py1">Quem você pode pressionar</p>
+      <div className="target-list px2 py1 clearfix" style={{backgroundColor: '#eeeeee'}}>
+        <p className="bold">Quem você pode pressionar</p>
+        <div className="flex pl2">
+          {/* TODO: Change HTML by TargetListItem */}
+          <div className={targetItemClassname}>
+            <img className={imageClassname} src="https://placeholdit.imgix.net/~text?txtsize=8&txt=50%C3%9750&w=50&h=50" height="55" width="55" />
+            <p className="black h6">
+              <span className="bold flex">Lúcia Regina Mendes Espagolla</span>
+              <span>Diretora Regional de Ensino Norte-1</span>
+            </p>
+          </div>
+          <div className={targetItemClassname}>
+            <img className={imageClassname} src="https://placeholdit.imgix.net/~text?txtsize=8&txt=50%C3%9750&w=50&h=50" height="55" width="55" />
+            <p className="black h6 m0">
+              <span className="bold flex">Lúcia Regina Mendes Espagolla</span>
+              <span>Diretora Regional de Ensino Norte-1</span>
+            </p>
+          </div>
+          <div className={targetItemClassname}>
+            <img className={imageClassname} src="https://placeholdit.imgix.net/~text?txtsize=8&txt=50%C3%9750&w=50&h=50" height="55" width="55" />
+            <p className="black h6 m0">
+              <span className="bold flex">Lúcia Regina Mendes Espagolla</span>
+              <span>Diretora Regional de Ensino Norte-1</span>
+            </p>
+          </div>
+        </div>
       </div>
       {/* TODO: Change HTML by AtivistForm */}
-      <form className="ativistform bg-white">
+      <form className="ativist-form bg-white">
         <div className={controlClassname}>
           <label className="flex" htmlFor="emailId">E-mail</label>
           <input className="col-12" style={inputReset} id="emailId" type="email" name="email" placeholder="exemplo@email.com" />
