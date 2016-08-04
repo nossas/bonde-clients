189d84b24e5396762174734b449d54414d74c79e
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2016-08-04 13:44:21 -0300

    Fix match share container path and isolate match widget paths into a separated file. #109

--- app/scripts/Paths.js
+++ app/scripts/Paths.js
@@ -78,14 +78,4 @@ export function donationMobilizationWidget(mobilization_id, widget_id) {
   return `/mobilizations/${mobilization_id}/widgets/${widget_id}/donation`
 }
 
-export const matchChoicesMobilizationWidget = (mobilization_id, widget_id) => {
-  return `/mobilizations/${mobilization_id}/widgets/${widget_id}/matches/choices`
-}
-
-export const matchGoalsMobilizationWidget = (mobilization_id, widget_id) => {
-  return `/mobilizations/${mobilization_id}/widgets/${widget_id}/matches/goals`
-}
-
-export const shareMatchWrapper = (widget_id, match_id) => {
-  return `/widget/${widget_id}/matches/${match_id}/share`
-}
+export * from './Match/paths'
