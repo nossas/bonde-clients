30dd69029e9148a20848912ad0a3a36a799c9d78
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2016-08-04 23:34:59 -0300

    Fix target item card horizontal fluid layout. #127

--- app/scripts/Widget/plugins/PressureWidget/Widget.js
+++ app/scripts/Widget/plugins/PressureWidget/Widget.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import classnames from 'classnames'
 
 /* TODO: Change static content by props
  * - title
@@ -22,39 +23,42 @@ const PressureWidget = () => {
     boxShadow: "inset 0px 4px 5px -2px rgba(235,235,235,1)",
   }
 
+  const targetList = {
+    overflowX: 'auto'
+  }
+  const targetItem = {
+    width: '200px',
+    height: '75px',
+    float: 'left'
+  }
+
+
   const targetItemClassname = 'target-item py1 px2 mr1 bg-white rounded'
   const imageClassname = 'left circle mr2'
 
+  const targets = Array(3).fill({
+    image: 'https://placeholdit.imgix.net/~text?txtsize=8&txt=50%C3%9750&w=50&h=50',
+    name: 'Lúcia Regina Mendes Espagolla',
+    office: 'Diretora Regional de Ensino Norte-1'
+  })
 
   return (
     <div className="pressure-widget">
       <h2 className="center py2 px3 m0 white rounded-top" style={{backgroundColor: bgColor}}>Envie um e-mail para quem pode tomar essa decisão</h2>
       {/* TODO: Change HTML by TargetList */}
-      <div className="target-list px2 py1 clearfix" style={{backgroundColor: '#eeeeee'}}>
+      <div className="target-list px2 py1 clearfix" style={{backgroundColor: '#eeeeee', ...targetList}}>
         <p className="bold">Quem você pode pressionar</p>
-        <div className="flex pl2">
+        <div className="clearfix pl2" style={{ width: `${240 * targets.length}px` }}>
           {/* TODO: Change HTML by TargetListItem */}
-          <div className={targetItemClassname}>
-            <img className={imageClassname} src="https://placeholdit.imgix.net/~text?txtsize=8&txt=50%C3%9750&w=50&h=50" height="55" width="55" />
-            <p className="black h6">
-              <span className="bold flex">Lúcia Regina Mendes Espagolla</span>
-              <span>Diretora Regional de Ensino Norte-1</span>
-            </p>
-          </div>
-          <div className={targetItemClassname}>
-            <img className={imageClassname} src="https://placeholdit.imgix.net/~text?txtsize=8&txt=50%C3%9750&w=50&h=50" height="55" width="55" />
-            <p className="black h6 m0">
-              <span className="bold flex">Lúcia Regina Mendes Espagolla</span>
-              <span>Diretora Regional de Ensino Norte-1</span>
-            </p>
-          </div>
-          <div className={targetItemClassname}>
-            <img className={imageClassname} src="https://placeholdit.imgix.net/~text?txtsize=8&txt=50%C3%9750&w=50&h=50" height="55" width="55" />
-            <p className="black h6 m0">
-              <span className="bold flex">Lúcia Regina Mendes Espagolla</span>
-              <span>Diretora Regional de Ensino Norte-1</span>
-            </p>
-          </div>
+          {targets.map(target => (
+            <div className={targetItemClassname} style={targetItem}>
+              <img className={imageClassname} src={target.image} height="55" width="55" />
+              <p className="black h6">
+                <span className="bold flex">{target.name}</span>
+                <span>{target.office}</span>
+              </p>
+            </div>
+          ))}
         </div>
       </div>
       {/* TODO: Change HTML by AtivistForm */}
