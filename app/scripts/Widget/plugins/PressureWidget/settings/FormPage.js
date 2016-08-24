4d5156842f1b6c7af1d08e6da57ff7a15ec3249b
Igor Santos <igr.exe@gmail.com>
2016-08-24 18:13:10 -0300

    Refactor pages to usenew Components forms #154 [ci skip]

--- app/scripts/Widget/plugins/PressureWidget/settings/FormPage.js
+++ app/scripts/Widget/plugins/PressureWidget/settings/FormPage.js
@@ -1,110 +1,78 @@
 import React, { Component, PropTypes } from 'react'
 import { reduxForm } from 'redux-form'
 
-import { editWidget } from '../../../actions'
+import * as WidgetActions from '../../../actions'
 import { FormFooter } from '../../../components'
-import { Control, Input, RadioButton, ColorInput } from '../../../components/FormUtils'
+import {
+  FormRedux,
+  FormGroup,
+  ControlLabel,
+  FormControl,
+  ColorPicker,
+  RadioGroup,
+  Radio
+} from '../../../../Dashboard/Forms'
 
 import { Base as PressureBase } from '../components/settings'
 
 
 class FormPage extends Component {
 
-  constructor(props, context) {
-    super(props, context)
-
-    this.state = {
-      submitted: false,
-    }
-
-    const { title_text, button_text, show_counter, count_text, main_color } = this.props.widget.settings || {
-      title_text: '',
-      button_text: '',
-      show_counter: 'false',
-      count_text: 'pressões feitas',
-      main_color: '#f23392'
-    }
-
-    this.props.initializeForm({ title_text, button_text, show_counter, count_text, main_color })
-  }
-
-  componentWillReceiveProps(nextProps) {
-    if (this.props.saving && !nextProps.saving && !nextProps.requestError) {
-      this.setState({ submitted: true })
-    }
-  }
-
-  handleSubmit(e) {
-    e.preventDefault()
-    const { data, valid, touchAll } = this.props      // redux form
-    const { auth, mobilization, widget } = this.props // containers
-    const { editWidgetAction } = this.props           // connect actions
-    if (valid) {
-      editWidgetAction({
-        mobilization_id: mobilization.id,
-        widget_id: widget.id,
-        credentials: auth.credentials,
-        widget: { settings: {
-          ...widget.settings,
-          ...data
-        }}
-      })
-    } else {
-      touchAll()
-    }
+  handleSubmit(values, dispatch) {
+    const { widget, credentials, editWidget, ...props } = this.props
+    const settings = widget.settings || {}
+    const data = { ...widget, settings: { ...settings, ...values } }
+    const params = { credentials, mobilization_id: props.mobilization.id }
+    return dispatch(editWidget(data, params))
   }
 
   render() {
-
-    const {
-      children, location, mobilization, widget, saving,
-      data: { title_text, button_text, show_counter, count_text, main_color },
-      ...inputProps
-    } = this.props
+    const { fields: { title_text, button_text, show_counter, count_text, main_color }, ...props } = this.props
 
     return (
-      <PressureBase location={location} mobilization={mobilization} widget={widget}>
-        <form onSubmit={::this.handleSubmit}>
-          <Control id="title-text-id" label="Título do formulário" name="title_text" {...inputProps}>
-            <Input type="text" value={title_text} placeholder="Envie um e-mail para quem pode tomar essa decisão" />
-          </Control>
-          <Control id="button-text-id" label="Texto do botão" name='button_text' {...inputProps}>
-            <Input type="text" value={button_text} placeholder='Enviar e-mail' />
-          </Control>
-          <Control id="main-color-id" label="Cor do formulário" name="main_color" {...inputProps}>
-            <ColorInput value={main_color} />
-          </Control>
-          <Control label="Mostrar contador de pessão" name='show_counter' {...inputProps}>
-            <Input type="radio" value={show_counter}>
-              <RadioButton className="mr1 caps" value="true">Sim</RadioButton>
-              <RadioButton value="false" className="caps">Não</RadioButton>
-            </Input>
-          </Control>
-          {(show_counter === 'true' ? (
-            <Control label="Texto do contador" name="count_text" {...inputProps}>
-              <Input type="text" value={count_text} placeholder="pressões feitas" />
-            </Control>
+      <PressureBase location={props.location} mobilization={props.mobilization} widget={props.widget}>
+        <FormRedux onSubmit={::this.handleSubmit} {...props}>
+          <FormGroup controlId="title-text-id" {...title_text}>
+            <ControlLabel>Título do formulário</ControlLabel>
+            <FormControl type="text" placeholder="Envie um e-mail para quem pode tomar essa decisão" />
+          </FormGroup>
+          <FormGroup controlId="button-text-id" {...button_text}>
+            <ControlLabel>Texto do botão</ControlLabel>
+            <FormControl type="text" placeholder="Enviar e-mail" />
+          </FormGroup>
+          <FormGroup controlId="main-color-id" {...main_color}>
+            <ControlLabel>Cor do formulário</ControlLabel>
+            <ColorPicker />
+          </FormGroup>
+          <FormGroup controlId="show-counter-id" {...show_counter}>
+            <ControlLabel>Mostrar contador de pressão</ControlLabel>
+            <RadioGroup>
+              <Radio value="true">Sim</Radio>
+              <Radio value="false">Não</Radio>
+            </RadioGroup>
+          </FormGroup>
+          {(show_counter.value === 'true' ? (
+            <FormGroup controlId="count-text-id" {...count_text}>
+              <ControlLabel>Texto do contador</ControlLabel>
+              <FormControl type="text" placeholder="pressões feitas" />
+            </FormGroup>
           ) : null)}
-          <FormFooter submitted={this.state.submitted} saving={saving} />
-        </form>
+        </FormRedux>
       </PressureBase>
     )
   }
 }
 
 FormPage.propTypes = {
-  location: PropTypes.shape({
-    pathname: PropTypes.string
-  }).isRequired,
-  mobilization: PropTypes.object.isRequired, // MobilizationDashboardContainer
-  widget: PropTypes.object.isRequired, // MobilizationDashboardContainer
-  auth: PropTypes.object.isRequired, // UserDashboardContainer
-  saving: PropTypes.bool.isRequired, // connect redux
-  editWidgetAction: PropTypes.func.isRequired,
+  mobilization: PropTypes.object.isRequired,
+  widget: PropTypes.object.isRequired,
+  credentials: PropTypes.object.isRequired,
+  editWidget: PropTypes.func.isRequired,
   // Redux form
   fields: PropTypes.object.isRequired,
   handleSubmit: PropTypes.func.isRequired,
-  submitting: PropTypes.bool.isRequired
+  submitting: PropTypes.bool.isRequired,
+  error: PropTypes.string,
 }
 
 const fields = ['title_text', 'button_text', 'show_counter', 'count_text', 'main_color']
@@ -132,7 +100,5 @@ export default reduxForm({
     main_color: '#f23392',
     ...ownProps.widget.settings || {}
   },
-  form: state.widgetForm,
-  saving: state.widgets.saving,
-  requestError: state.widgets.error
-}), { editWidget })(FormPage)
+  credentials: state.auth.credentials,
+}), { ...WidgetActions })(FormPage)
