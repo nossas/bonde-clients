c023432922544ff5260c50372d012bb19e64b43b
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2016-08-31 12:25:41 -0300

    Update redux-thunk and add it when create store. #154

--- src/redux/create.js
+++ src/redux/create.js
@@ -1,29 +1,34 @@
-import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
-import createMiddleware from './clientMiddleware';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
+import createMiddleware from './clientMiddleware'
+import thunk from 'redux-thunk'
 
 export default function createApiClientStore(client, initialState) {
-  const middleware = createMiddleware(client);
-  let finalCreateStore;
+  const middleware = createMiddleware(client)
+  let finalCreateStore
   if (__DEVELOPMENT__ && __CLIENT__ && __DEVTOOLS__) {
-    const { devTools, persistState } = require('redux-devtools');
+    const { devTools, persistState } = require('redux-devtools')
     finalCreateStore = compose(
       applyMiddleware(middleware),
+      applyMiddleware(thunk),
       devTools(),
       persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/)),
-    )(createStore);
+    )(createStore)
   } else {
-    finalCreateStore = applyMiddleware(middleware)(createStore);
+    finalCreateStore = compose(
+      applyMiddleware(middleware),
+      applyMiddleware(thunk)
+    )(createStore)
   }
 
-  const rootReducer = require('../ducks/reducer');
-  const store = finalCreateStore(rootReducer, initialState);
-  store.client = client;
+  const rootReducer = require('../ducks/reducer')
+  const store = finalCreateStore(rootReducer, initialState)
+  store.client = client
 
   if (__DEVELOPMENT__ && module.hot) {
     module.hot.accept('../ducks/reducer', () => {
-      store.replaceReducer(require('../ducks/reducer'));
-    });
+      store.replaceReducer(require('../ducks/reducer'))
+    })
   }
 
-  return store;
+  return store
 }
