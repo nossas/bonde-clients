504bf21200c89eed3e3770e846bedd668e7654cc
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2016-09-13 17:31:09 -0300

    Add `Source Sans Pro` as a default font. #188

--- app/scripts/containers/Application.jsx
+++ app/scripts/containers/Application.jsx
@@ -39,6 +39,10 @@ export default class Application extends React.Component {
     return (
       <div>
         {React.cloneElement(this.props.children, {auth: this.props.auth})}
+        <link
+          href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700"
+          rel="stylesheet"
+        />
       </div>
     )
   }
