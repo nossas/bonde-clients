2cb29d59b1dad16448c622a41c229b907fe3514a
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2016-09-23 18:23:53 -0300

    Fix PressureWidget TargetList component layout. #184 - Pass inline css style to a separated .scss file; - Fix TargetList component tests.

--- app/scripts/Widget/plugins/PressureWidget/__tests__/components/TargetList.spec.js
+++ app/scripts/Widget/plugins/PressureWidget/__tests__/components/TargetList.spec.js
@@ -1,42 +1,33 @@
 import React from 'react'
 import { expect } from 'chai'
-import { mount } from 'enzyme'
+import { shallow } from 'enzyme'
 
 import { TargetList } from '../../components'
 
-
 describe('<TargetList />', () => {
-  let component
+  let wrapper
 
   beforeEach(() => {
-    component = mount(<TargetList />)
+    wrapper = shallow(<TargetList />)
   })
 
   it('should render ok by default', () => {
-    expect(component).to.be.ok
-    expect(component.find('.target-item').length).to.equal(0)
+    expect(wrapper).to.be.ok
+    expect(wrapper.find('.target-item').length).to.equal(0)
   })
 
   it('should render .target-item according targets passed', () => {
-    component.setProps({
+    wrapper.setProps({
       targets: ['Igor Santos <igor@nossascidades.org>']
     })
-    expect(component.find('.target-item').length).to.equal(1)
-  })
-
-  it('should hidden target with overflowX auto', () => {
-    const targets = ['Igor Santos <igor@nossascidades.org>']
-    component.setProps({targets: targets})
-
-    expect(component.find('.target-list > div').props().style.overflowX).to.equal('auto')
-    expect(component.find('.target-list > div > div').props().style.width).to.equal(`${180 * targets.length}px`)
+    expect(wrapper.find('.target-item').length).to.equal(1)
   })
 
   it('should render target parsed to Name <user@host.com>', () => {
     const targets = ['Igor Santos <igor@nossascidades.org>']
-    component.setProps({targets: targets})
+    wrapper.setProps({targets: targets})
 
-    expect(component.find('.target-item span').at(0).text()).to.equal('Igor Santos')
-    expect(component.find('.target-item span').at(1).text()).to.equal('igor@nossascidades.org')
+    expect(wrapper.find('.target-item span').at(0).text()).to.equal('Igor Santos')
+    expect(wrapper.find('.target-item span').at(1).text()).to.equal('igor@nossascidades.org')
   })
 })
