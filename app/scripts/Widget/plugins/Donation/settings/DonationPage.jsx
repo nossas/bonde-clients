eff4153363be74f62689cae7fac3be6da2ec82ed
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2016-09-23 14:15:55 -0300

    Fix DonationWidget settings page alignment layout. #184

--- app/scripts/Widget/plugins/Donation/settings/DonationPage.jsx
+++ app/scripts/Widget/plugins/Donation/settings/DonationPage.jsx
@@ -46,92 +46,94 @@ class DonationPage extends React.Component {
     return (
       <div className="flex-auto flex flex-column bg-silver gray relative">
         <Menu mobilization={props.mobilization} widget={props.widget} {...props} />
-        <div className="p3 flex-auto overflow-scroll">
-          <FormRedux onSubmit={::this.handleSubmit} {...props}>
-            <FormGroup controlId="title-text-id" {...title_text}>
-              <ControlLabel>Título do bloco de doação</ControlLabel>
-              <FormControl type="text" placeholder="Ex.: Escolha um valor e contribua agora!" />
-            </FormGroup>
+        <div className="clearfix overflow-auto">
+          <div className="col-6 clearfix py3 pr4 pl5">
+            <FormRedux onSubmit={::this.handleSubmit} {...props}>
+              <FormGroup controlId="title-text-id" {...title_text}>
+                <ControlLabel>Título do bloco de doação</ControlLabel>
+                <FormControl type="text" placeholder="Ex.: Escolha um valor e contribua agora!" />
+              </FormGroup>
 
-            <FormGroup controlId="payment-type-id" {...payment_type}>
-              <ControlLabel>Tipo de doação</ControlLabel>
-              <RadioGroup>
-                <Radio value="unique">Única</Radio>
-                <Radio value="recurring">Recorrente</Radio>
-                <Radio value="users_choice">Usuário escolhe</Radio>
-              </RadioGroup>
-            </FormGroup>
+              <FormGroup controlId="payment-type-id" {...payment_type}>
+                <ControlLabel>Tipo de doação</ControlLabel>
+                <RadioGroup>
+                  <Radio value="unique">Única</Radio>
+                  <Radio value="recurring">Recorrente</Radio>
+                  <Radio value="users_choice">Usuário escolhe</Radio>
+                </RadioGroup>
+              </FormGroup>
 
-            {(payment_type.value == 'recurring' || payment_type.value == 'users_choice') ?
-            <FormGroup controlId="payment-type-id" {...recurring_period}>
-              <ControlLabel>Intervalo da recorrência</ControlLabel>
-              <RadioGroup>
-                <Radio value="30">Mensal</Radio>
-                <Radio value="180">Semestral</Radio>
-                <Radio value="365">Anual</Radio>
-              </RadioGroup>
-            </FormGroup> : ''}
+              {(payment_type.value == 'recurring' || payment_type.value == 'users_choice') ?
+              <FormGroup controlId="payment-type-id" {...recurring_period}>
+                <ControlLabel>Intervalo da recorrência</ControlLabel>
+                <RadioGroup>
+                  <Radio value="30">Mensal</Radio>
+                  <Radio value="180">Semestral</Radio>
+                  <Radio value="365">Anual</Radio>
+                </RadioGroup>
+              </FormGroup> : ''}
 
-            <FormGroup controlId="main-color-id" {...main_color}>
-              <ControlLabel>Defina a cor da página de pagamento</ControlLabel>
-              <HelpBlock>Selecione a cor no box abaixo ou insira o valor em hex, por exemplo: #DC3DCE.</HelpBlock>
-              <ColorPicker />
-            </FormGroup>
+              <FormGroup controlId="main-color-id" {...main_color}>
+                <ControlLabel>Defina a cor da página de pagamento</ControlLabel>
+                <HelpBlock>Selecione a cor no box abaixo ou insira o valor em hex, por exemplo: #DC3DCE.</HelpBlock>
+                <ColorPicker />
+              </FormGroup>
 
-            <FormGroup controlId="default-donation-value" {...default_donation_value}>
-              <ControlLabel>Defina os valores para o bloco de doação</ControlLabel>
-              <HelpBlock>Você pode ter até 5 valores por bloco de doação. Preencha apenas com números inteiros (Ex: 50)</HelpBlock>
-              <HorizontalLayout cols={5}>
-                <FormGroup controlId="donation-value1-id" {...donation_value1}>
-                  <ControlLabel>Valor 1</ControlLabel>
-                  <FormControl type="number" placeholder="Ex.: R$20" />
-                </FormGroup>
-                <FormGroup controlId="donation-value2-id" {...donation_value2}>
-                  <ControlLabel>Valor 2</ControlLabel>
-                  <FormControl type="number" placeholder="Ex.: R$50" />
-                </FormGroup>
-                <FormGroup controlId="donation-value3-id" {...donation_value3}>
-                  <ControlLabel>Valor 3</ControlLabel>
-                  <FormControl type="number" placeholder="Ex.: R$100" />
-                </FormGroup>
-                <FormGroup controlId="donation-value4-id" {...donation_value4}>
-                  <ControlLabel>Valor 4</ControlLabel>
-                  <FormControl type="number" placeholder="Ex.: R$200" />
-                </FormGroup>
-                <FormGroup controlId="donation-value5-id" {...donation_value5}>
-                  <ControlLabel>Valor 5</ControlLabel>
-                  <FormControl type="number" placeholder="Ex.: R$500" />
-                </FormGroup>
-              </HorizontalLayout>
-              <RadioGroup className="flex flex-wrap" style={{marginTop: '-1rem' }}>
-                <Radio className="sm-col sm-col-2" title="Clique para definir este valor como padrão." value="1">Definir como padrão</Radio>
-                <Radio className="sm-col sm-col-2" title="Clique para definir este valor como padrão." value="2">Definir como padrão</Radio>
-                <Radio className="sm-col sm-col-2" title="Clique para definir este valor como padrão." value="3">Definir como padrão</Radio>
-                <Radio className="sm-col sm-col-2" title="Clique para definir este valor como padrão." value="4">Definir como padrão</Radio>
-                <Radio className="sm-col sm-col-2" title="Clique para definir este valor como padrão." value="5">Definir como padrão</Radio>
-              </RadioGroup>
-              <HelpBlock>*todos os valores são em reais</HelpBlock>
-            </FormGroup>
+              <FormGroup controlId="default-donation-value" {...default_donation_value}>
+                <ControlLabel>Defina os valores para o bloco de doação</ControlLabel>
+                <HelpBlock>Você pode ter até 5 valores por bloco de doação. Preencha apenas com números inteiros (Ex: 50)</HelpBlock>
+                <HorizontalLayout cols={5}>
+                  <FormGroup controlId="donation-value1-id" {...donation_value1}>
+                    <ControlLabel>Valor 1</ControlLabel>
+                    <FormControl type="number" placeholder="Ex.: R$20" />
+                  </FormGroup>
+                  <FormGroup controlId="donation-value2-id" {...donation_value2}>
+                    <ControlLabel>Valor 2</ControlLabel>
+                    <FormControl type="number" placeholder="Ex.: R$50" />
+                  </FormGroup>
+                  <FormGroup controlId="donation-value3-id" {...donation_value3}>
+                    <ControlLabel>Valor 3</ControlLabel>
+                    <FormControl type="number" placeholder="Ex.: R$100" />
+                  </FormGroup>
+                  <FormGroup controlId="donation-value4-id" {...donation_value4}>
+                    <ControlLabel>Valor 4</ControlLabel>
+                    <FormControl type="number" placeholder="Ex.: R$200" />
+                  </FormGroup>
+                  <FormGroup controlId="donation-value5-id" {...donation_value5}>
+                    <ControlLabel>Valor 5</ControlLabel>
+                    <FormControl type="number" placeholder="Ex.: R$500" />
+                  </FormGroup>
+                </HorizontalLayout>
+                <RadioGroup className="flex flex-wrap" style={{marginTop: '-1rem' }}>
+                  <Radio className="sm-col sm-col-2" title="Clique para definir este valor como padrão." value="1">Definir como padrão</Radio>
+                  <Radio className="sm-col sm-col-2" title="Clique para definir este valor como padrão." value="2">Definir como padrão</Radio>
+                  <Radio className="sm-col sm-col-2" title="Clique para definir este valor como padrão." value="3">Definir como padrão</Radio>
+                  <Radio className="sm-col sm-col-2" title="Clique para definir este valor como padrão." value="4">Definir como padrão</Radio>
+                  <Radio className="sm-col sm-col-2" title="Clique para definir este valor como padrão." value="5">Definir como padrão</Radio>
+                </RadioGroup>
+                <HelpBlock>*todos os valores são em reais</HelpBlock>
+              </FormGroup>
 
-            <FormGroup controlId="button-text-id" {...button_text}>
-              <ControlLabel>Texto do botão de doação</ControlLabel>
-              <FormControl type="text" placeholder="Ex.: Doe agora!" />
-            </FormGroup>
+              <FormGroup controlId="button-text-id" {...button_text}>
+                <ControlLabel>Texto do botão de doação</ControlLabel>
+                <FormControl type="text" placeholder="Ex.: Doe agora!" />
+              </FormGroup>
 
-            <FormGroup controlId="payment-methods-id" {...payment_methods}>
-              <ControlLabel>Habilitar pagamento por boleto?</ControlLabel>
-              <HelpBlock>Cada boleto pago terá um custo adicional de R$3,00</HelpBlock>
-              <RadioGroup>
-                <Radio value="true">Sim</Radio>
-                <Radio value="false">Não</Radio>
-              </RadioGroup>
-            </FormGroup>
+              <FormGroup controlId="payment-methods-id" {...payment_methods}>
+                <ControlLabel>Habilitar pagamento por boleto?</ControlLabel>
+                <HelpBlock>Cada boleto pago terá um custo adicional de R$3,00</HelpBlock>
+                <RadioGroup>
+                  <Radio value="true">Sim</Radio>
+                  <Radio value="false">Não</Radio>
+                </RadioGroup>
+              </FormGroup>
 
-            <FormGroup>
-              <ControlLabel>Conta bancária</ControlLabel>
-              <HelpBlock className="mb3">Este bloco de doação está associado à conta correspondente da cidade no Pagar.me.</HelpBlock>
-            </FormGroup>
-          </FormRedux>
+              <FormGroup>
+                <ControlLabel>Conta bancária</ControlLabel>
+                <HelpBlock className="mb3">Este bloco de doação está associado à conta correspondente da cidade no Pagar.me.</HelpBlock>
+              </FormGroup>
+            </FormRedux>
+          </div>
         </div>
       </div>
     )
