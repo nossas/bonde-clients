68404758743d95d14b744a7b06e62f860a08d4ce
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2016-09-30 15:28:33 -0300

    Refactoring and Purify Login component page. #184

--- app/scripts/pages/Login/Login.jsx
+++ app/scripts/pages/Login/Login.jsx
@@ -1,20 +1,21 @@
 import React from 'react'
+
 import LoginForm from './../../components/LoginForm.jsx'
 
-export default class Login extends React.Component {
-  render() {
-    const logo = require('./logo-nossas.svg')
-    return (
-      <div className="bg-cover bg-center bg-reboo absolute top-0 right-0 bottom-0 left-0">
-        <div className="table col-3 mx-auto" style={{ height: '100%' }}>
-          <div className="table-cell align-middle">
-            <div className="col-8 mb3 mx-auto">
-              <img src={logo} alt="Logo Nossas" />
-            </div>
-            <LoginForm className="col-12 mx-auto" {...this.props} />
-          </div>
+import logo from './logo-nossas.svg'
+import './login.scss'
+
+const Login = props => (
+  <div className="bg-cover bg-center bg-reboo absolute top-0 right-0 bottom-0 left-0">
+    <div className="table col-3 mx-auto full-height">
+      <div className="table-cell align-middle">
+        <div className="col-8 mb3 mx-auto">
+          <img src={logo} alt="Logo Nossas" />
         </div>
+        <LoginForm className="col-12 mx-auto" {...props} />
       </div>
-    )
-  }
-}
+    </div>
+  </div>
+)
+
+export default Login
