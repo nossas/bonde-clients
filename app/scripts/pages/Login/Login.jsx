42b3e19d9bc26fadd22328be103d9298d907f4cf
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2016-09-30 15:21:48 -0300

    Add Nossas logo into Login page. #184

--- app/scripts/pages/Login/Login.jsx
+++ app/scripts/pages/Login/Login.jsx
@@ -3,13 +3,13 @@ import LoginForm from './../../components/LoginForm.jsx'
 
 export default class Login extends React.Component {
   render() {
-    const rebooLogo = require('./logo-login.png')
+    const logo = require('./logo-nossas.svg')
     return (
       <div className="bg-cover bg-center bg-reboo absolute top-0 right-0 bottom-0 left-0">
         <div className="table col-3 mx-auto" style={{ height: '100%' }}>
           <div className="table-cell align-middle">
-            <div className="col-12 center">
-              <img src={rebooLogo} alt="Logo Reboo" />
+            <div className="col-8 mb3 mx-auto">
+              <img src={logo} alt="Logo Nossas" />
             </div>
             <LoginForm className="col-12 mx-auto" {...this.props} />
           </div>
