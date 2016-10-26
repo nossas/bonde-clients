dd73d379ea591f80107d49e1214e75f2b720b449
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2016-10-26 17:05:43 -0200

    Add SelectableList components stack. #226

---
+++ app/components/SelectableList/SelectableList.js
@@ -0,0 +1,45 @@
+import React, { PropTypes, Component } from 'react'
+import { connect } from 'react-redux'
+import classnames from 'classnames'
+
+import {
+  MobilizationListItem,
+  MobilizationListItemAvatar,
+  MobilizationListItemName,
+  MobilizationListItemCreatedAt
+} from '../../scripts/Mobilization/components/MobilizationList/MobilizationListItem'
+import MobilizationList from '../../scripts/Mobilization/components/MobilizationList'
+import { setSelectedIndex } from './SelectableListActions'
+
+class SelectableList extends Component {
+  render() {
+    const { list, dispatch, selectedIndex } = this.props
+    return (
+      <div className="selectable-list col-12">
+        <MobilizationList>
+          {list.map((item, index) => (
+            <MobilizationListItem
+              className={classnames(
+                'border border-whisper',
+                { 'border-pagenta': selectedIndex === index }
+              )}
+              onClick={() => dispatch(
+                setSelectedIndex(selectedIndex === index ? undefined : index)
+              )}
+            >
+              <MobilizationListItemAvatar {...item} />
+              <MobilizationListItemName {...item} className="col-7" />
+              <MobilizationListItemCreatedAt {...item} />
+            </MobilizationListItem>
+          ))}
+        </MobilizationList>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => ({
+  selectedIndex: state.selectableList.selectedIndex
+})
+
+export default connect(mapStateToProps)(SelectableList)
