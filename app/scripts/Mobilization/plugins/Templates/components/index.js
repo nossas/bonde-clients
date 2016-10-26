2b7d4c3c3217f583ebb6380cb0d1bc20d61cfe88
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2016-10-26 19:25:11 -0200

    Isolate mobilization templates choosable pages content to make it reusable. #226

--- app/scripts/Mobilization/plugins/Templates/components/index.js
+++ app/scripts/Mobilization/plugins/Templates/components/index.js
@@ -1,1 +1,2 @@
 export { default as MobilizationTemplatesBasicsForm } from './MobilizationTemplatesBasicsForm'
+export { default as MobilizationTemplatesSelectableList } from './MobilizationTemplatesSelectableList'
--- app/scripts/Mobilization/plugins/Templates/pages/MobilizationTemplatesChooseCustomListPage.js
+++ app/scripts/Mobilization/plugins/Templates/pages/MobilizationTemplatesChooseCustomListPage.js
@@ -1,71 +1,29 @@
 import React, { PropTypes, Component } from 'react'
 import { Navigation } from 'react-router'
 import reactMixin from 'react-mixin'
-import { connect } from 'react-redux'
-import classnames from 'classnames'
 
 import * as Paths from '../../../../Paths'
-import { SelectableList } from '../../../../../components/SelectableList'
-import { setSelectedIndex } from '../../../../../components/SelectableList/SelectableListActions'
-import { FilterableSearchBar } from '../../../../../components/FilterableSearchBar'
+import { MobilizationTemplatesSelectableList } from '../components'
 
 @reactMixin.decorate(Navigation)
 export class MobilizationTemplatesChooseCustomListPage extends Component {
-  render () {
-    const {
-      mobilization,
-      mobilizationTemplates,
-      filterableSearchBarList,
-      selectedIndex,
-      dispatch
-    } = this.props
-
-    const buttonDisabled = !filterableSearchBarList.filter(item => item.id === selectedIndex).length
-
+  render() {
+    const { mobilization, mobilizationTemplates } = this.props
     return (
       <div className="p3 lg-col-5 mx-auto">
         <h3 className="h1 mt0 mb3 center">Meus Templates</h3>
-
-        <FilterableSearchBar list={mobilizationTemplates.list} />
-
-        <div className="bg-white rounded-bottom" style={{ padding: '1.6rem 2rem' }}>
-          <SelectableList
-            list={filterableSearchBarList}
-            activeCondition={(item, index) => item.id === selectedIndex}
-            onClick={(item, index) => {
-              dispatch(setSelectedIndex(item.id === selectedIndex ? undefined : item.id))
-            }}
-            emptyListText="Não existe nenhum template com esse nome"
-          />
-          <button
-            disabled={buttonDisabled}
-            style={{ marginTop: '1.6rem' }}
-            onClick={() => this.transitionTo(Paths.editMobilization(mobilization.id))}
-            className={classnames(
-              'btn h3 col-12 white p2 rounded',
-              buttonDisabled ? 'bg-gray95' : 'bg-pagenta'
-            )}
-          >
-            Continuar
-          </button>
-        </div>
+        <MobilizationTemplatesSelectableList
+          list={mobilizationTemplates.list}
+          onClickButton={() => this.transitionTo(Paths.editMobilization(mobilization.id))}
+        />
       </div>
     )
   }
 }
 
-MobilizationTemplatesChooseCustomListPage.contextTypes = {
-  router: PropTypes.object.isRequired
-}
-
 MobilizationTemplatesChooseCustomListPage.propTypes = {
   mobilization: PropTypes.object.isRequired,
-  mobilizationTemplates: PropTypes.object.isRequired
+  mobilizationTemplates: PropTypes.object.isRequired,
 }
 
-const mapStateToProps = state => ({
-  selectedIndex: state.selectableList.selectedIndex,
-  filterableSearchBarList: state.filterableSearchBar.list
-})
-
-export default connect(mapStateToProps)(MobilizationTemplatesChooseCustomListPage)
+export default MobilizationTemplatesChooseCustomListPage
