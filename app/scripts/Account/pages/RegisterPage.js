a7376f64a31f412e19d4b2ee43dd8c4495a8230a
Igor Santos <igr.exe@gmail.com>
2016-11-23 10:33:32 -0200

    Add dashboard form in RegisterPage

--- app/scripts/Account/pages/RegisterPage.js
+++ app/scripts/Account/pages/RegisterPage.js
@@ -2,6 +2,7 @@ import React, { Component, PropTypes } from 'react'
 import { reduxForm } from 'redux-form'
 
 import { FormRedux, FormGroup, ControlLabel, FormControl } from '../../Dashboard/Forms'
+import { isValidEmail } from '../../../util/validation-helper'
 
 
 class RegisterPage extends Component {
@@ -12,47 +13,63 @@ class RegisterPage extends Component {
   }
 
   render() {
-    const { fields: { name, last_name, email, password, password2 }, handleSubmit } = this.props
+    const { fields: { name, last_name, email, password, password2 }, ...formProps } = this.props
     return (
       <div>
         <h1>Crie sua conta no Nossas.</h1>
-        <form onSubmit={handleSubmit(this.handleSubmit.bind(this))}>
+        <FormRedux className="rounded" onSubmit={this.handleSubmit.bind(this)} {...formProps}>
           <div className="flex">
-            <div className="form-group col-6">
-              <label className="control-label">Nome</label>
-              <input className="form-control" placeholder="Seu nome" type="text" {...name} />
-            </div>
-            <div className="form-group col-6">
-              <label className="control-label">Sobrenome</label>
-              <input className="form-control" placeholder="Sobrenome" type="text" {...last_name} />
-            </div>
+            <FormGroup className="col-6" controlId="nameId" {...name}>
+              <ControlLabel>Nome</ControlLabel>
+              <FormControl type="text" placeholder="Seu nome" />
+            </FormGroup>
+            <FormGroup className="col-6" controlId="lastNameId" {...last_name}>
+              <ControlLabel>Sobrenome</ControlLabel>
+              <FormControl type="text" placeholder="Sobrenome" />
+            </FormGroup>
           </div>
-          <div className="form-group">
-            <label className="control-label">E-mail</label>
-            <input className="form-control" placeholder="exemplo@email.com.br" type="email" {...email} />
-          </div>
-          <div className="form-group">
-            <label className="control-label">Senha</label>
-            <input className="form-control" placeholder="*********" type="password" {...password} />
-          </div>
-          <div className="form-group">
-            <label className="control-label">Confirme sua senha</label>
-            <input className="form-control" placeholder="*********" type="password" {...password2} />
-          </div>
-          <button className="btn white bg-pagenta col-12 rounded-bottom py2 caps">Criar conta</button>
-        </form>
+          <FormGroup controlId="emailId" {...email}>
+            <ControlLabel>E-mail</ControlLabel>
+            <FormControl type="email" placeholder="exemplo@email.com.br" />
+          </FormGroup>
+          <FormGroup controlId="passwordId" {...password}>
+            <ControlLabel>Senha</ControlLabel>
+            <FormControl type="password" placeholder="********" />
+          </FormGroup>
+          <FormGroup controlId="password2Id" {...password2}>
+            <ControlLabel>Confirme sua senha</ControlLabel>
+            <FormControl type="password" placeholder="********" />
+          </FormGroup>
+          <button type="submit" className="btn white bg-pagenta col-12 rounded-bottom py2 caps">Criar conta</button>
+        </FormRedux>
       </div>
     )
   }
 }
 
 RegisterPage.propTypes = {
+  // Injected by redux-form
   fields: PropTypes.object.isRequired
 }
 
-const fields = ['name', 'last_name']
+const fields = ['name', 'last_name', 'email', 'password', 'password2']
 const validate = values => {
-  return {}
+  const errors = {}
+  if (!values.name) {
+    errors.name = 'Informe seu nome'
+  }
+  if (!values.email) {
+    errors.email = 'Informe seu e-mail'
+  } else if (!isValidEmail(values.email)) {
+    errors.email = 'E-mail inválido'
+  }
+  if (!values.password) {
+    errors.password = 'Informe uma senha'
+  }
+  if (values.password && values.password !== values.password2) {
+    errors.password2 = 'Senha não confere'
+  }
+  return errors
 }
 
 export default reduxForm({
