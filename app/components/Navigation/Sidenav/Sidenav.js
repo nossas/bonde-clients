927aba96521bbe2710ca6da8bd1af1d6d418d2ac
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2016-11-24 13:30:48 -0200

    [#134433609] Update sidenav header style. #259

--- app/components/Navigation/Sidenav/Sidenav.js
+++ app/components/Navigation/Sidenav/Sidenav.js
@@ -11,18 +11,35 @@ class Sidenav extends Component {
     return (
       <nav className="sidenav clearfix">
         <div className="items items-logo">
-          <Link to={Paths.mobilizations()}>
-            <div className="item">
-              <div className="item-icon">
-                <u className="logo-icon" />
-              </div>
-              <div className="item-content">
-                <div className="table-cell align-middle">
-                  <u className="logo" />
+          <div className="item">
+            <div className="item-icon">
+              <Link to={Paths.mobilizations()} style={{ height: '43px', display: 'block' }}>
+                <u
+                  className="logo-icon"
+                  style={{
+                    backgroundImage: user.community_avatar || 'url(http://placeholdit.imgix.net/~text?txtsize=14&txt=50x50&w=50&h=50&txttrack=0)'
+                  }}
+                />
+              </Link>
+            </div>
+            <div className="item-content">
+              <div className="table-cell align-middle">
+                <div>
+                  <div className="item-community-name">Nossas</div>
+                  <div className="item-community-change">
+                    <Link to={Paths.mobilizations()} className="col col-8">
+                      <i className="fa fa-cog mr1" />
+                      <span>Configurações</span>
+                    </Link>
+                    <Link to={Paths.mobilizations()} className="col col-4">
+                      <i className="fa fa-refresh mr1" />
+                      <span>Trocar</span>
+                    </Link>
+                  </div>
                 </div>
               </div>
             </div>
-          </Link>
+          </div>
         </div>
         {children}
       </nav>
