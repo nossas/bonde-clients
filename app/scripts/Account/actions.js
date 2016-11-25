e58c6de010011b7355814804c659d0e5a489da9d
Igor Santos <igr.exe@gmail.com>
2016-11-25 17:58:40 -0200

    Add factory to create container account route

---
+++ app/scripts/Account/actions.js
@@ -0,0 +1,60 @@
+import axios from 'axios'
+
+import {
+  AUTH_LOGOUT_REQUEST,
+  AUTH_LOGOUT_SUCCESS,
+  AUTH_LOGOUT_FAILURE
+} from '../constants/ActionTypes'
+
+// redux-form action
+export const register = user => (dispatch, getState, request) => {
+  return request
+    .post('/users', { user })
+    .then(({ status, data }) => {
+      if (status === 200 && data.errors) {
+        return Promise.reject({ ...data.errors })
+      } else if (status === 201) {
+        return Promise.resolve()
+      }
+    })
+    .catch(error => Promise.reject(error))
+}
+
+export const AUTH_LOGIN_SUCCESS = 'AUTH_LOGIN_SUCCESS'
+
+const loginSuccess = (user, credentials) => ({ type: AUTH_LOGIN_SUCCESS, user, credentials })
+const loginRequest = values => axios.post(`${process.env.API_URL}/auth/sign_in`, values)
+export const login = values => dispatch => loginRequest(values)
+  .then(response => {
+    const { headers, data: { data: user } } = response
+    const credentials = {
+      'Access-Token': headers['access-token'],
+      'Token-Type': headers['token-type'],
+      Expiry: headers['expiry'],
+      Uid: headers['uid'],
+      Client: headers['client']
+    }
+
+    // Create a session into the server-side rendering server
+    axios.post(`/auth/login`, { credentials, user })
+
+    dispatch(loginSuccess(user, credentials))
+  })
+  .catch(error => {
+    if (error.response) {
+      const { response: { data: { errors }, status } } = error
+      if (status === 401) return Promise.reject({ _error: errors.join('') })
+    }
+    return Promise.reject({ _error: `Response ${error}` })
+  })
+
+export const logoutRequest = () => ({ type: AUTH_LOGOUT_REQUEST })
+export const logoutSuccess = () => ({ type: AUTH_LOGOUT_SUCCESS })
+export const logoutFailure = error => ({ type: AUTH_LOGOUT_FAILURE, error })
+export const logout = () => dispatch => {
+  dispatch(logoutRequest())
+  return Auth.signOut().then(
+    user => dispatch(logoutSuccess()),
+    error => dispatch(logoutFailure(error.data.errors[0]))
+  )
+}
---
+++ app/scripts/Account/index.js
@@ -0,0 +1,4 @@
+export { default as AccountActions } from './actions'
+export { default as AccountRoutes } from './routes'
+export { default as RequireLogin } from './RequireLogin'
+export { default as FactoryContainer } from './FactoryContainer'
