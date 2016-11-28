2debd86aa9a928f2a3e24ad9a9fe157c916a7f89
Lucas Pirola <lucaspirola@gmail.com>
2016-11-28 14:20:59 -0200

    add more config options to git clone

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash -e
-
 REPO_URI="https://git.v6jv.flynnhub.com/ourcities-rebu-client-develop.git"
 if [[ "$CIRCLE_BRANCH" == "master" ]]; then
   REPO_URI="dokku@reboo.org:0-client"
@@ -9,4 +8,4 @@ git fetch --unshallow origin
 
 git remote add deploy $REPO_URI
 
-git push -f deploy $CIRCLE_SHA1:refs/heads/master
+GIT_SSL_NO_VERIFY=true git push -f deploy $CIRCLE_SHA1:refs/heads/master
