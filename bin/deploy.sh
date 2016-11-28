cc23a73bb4000c2cb953065ebd45a70eab1ada2a
Lucas Pirola <lucaspirola@gmail.com>
2016-11-28 14:11:20 -0200

    Change deploy script to deploy to new staging env

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,11 +1,12 @@
 #!/bin/bash -e
-REPO_URI="https://ubuntu:$FLYNN_KEY@git.v6jv.flynnhub.com/ourcities-rebu-client-develop.git"
+
+REPO_URI="dokku@reboo.org:0-client"
 if [[ "$CIRCLE_BRANCH" == "master" ]]; then
-  REPO_URI="dokku@reboo.org:0-client"
+  REPO_URI="https://git.v6jv.flynnhub.com/ourcities-rebu-client-develop.git"
 fi
 
 git fetch --unshallow origin
 
 git remote add deploy $REPO_URI
 
-GIT_SSL_NO_VERIFY=true git push -f deploy $CIRCLE_SHA1:refs/heads/master
+git push -f deploy $CIRCLE_SHA1:refs/heads/master
