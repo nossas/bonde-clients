0f59fda09a6d0c602836426430186d6ca6921010
Igor Santos <igr.exe@gmail.com>
2016-11-30 10:38:03 -0200

    Add field to upload in dashboard forms

---
+++ app/scripts/Dashboard/Forms/UploadImageField.js
@@ -0,0 +1,66 @@
+import React, { Component, PropTypes } from 'react'
+import ReactDOM from 'react-dom'
+import ReactS3Uploader from 'react-s3-uploader'
+
+
+class UploadImageField extends Component {
+
+  onProgress() {
+
+  }
+
+  onError(error) {
+    console.log('error', error)
+  }
+
+  onFinish(image) {
+    const { $formGroup: { onChange } } = this.context
+    const e = {
+      value: image.signedUrl.substring(0, image.signedUrl.indexOf('?'))
+    }
+    onChange(e)
+  }
+
+  onClick() {
+    ReactDOM.findDOMNode(this.refs.inputFile).click()
+  }
+
+  render() {
+    const { className, signingUrl } = this.props
+    const { $formGroup: { value } } = this.context
+
+    return (
+      <div className="uploadImageFile">
+        <button
+          type="button"
+          className={className}
+          onClick={this.onClick.bind(this)}
+        >
+          {value ? <img src={value} /> : <i className="fa fa-image" />}
+        </button>
+        <ReactS3Uploader
+          signingUrl={signingUrl}
+          accept="image/*"
+          onProgress={this.onProgress.bind(this)}
+          onError={this.onError.bind(this)}
+          onFinish={this.onFinish.bind(this)}
+          ref="inputFile"
+        />
+      </div>
+    )
+  }
+}
+
+UploadImageField.contextTypes = {
+  $formGroup: PropTypes.shape({
+    value: PropTypes.any,
+    onChange: PropTypes.func.isRequired
+  })
+}
+
+UploadImageField.propTypes = {
+  className: PropTypes.string,
+  signingUrl: PropTypes.string.isRequired
+}
+
+export default UploadImageField
