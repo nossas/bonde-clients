72c77c03961feaec6b5fa39bea991ebe81c961a6
Igor Santos <igr.exe@gmail.com>
2016-11-30 10:38:03 -0200

    Add field to upload in dashboard forms

--- app/scripts/Dashboard/Forms/UploadImageField.js
+++ app/scripts/Dashboard/Forms/UploadImageField.js
@@ -31,7 +31,11 @@ class UploadImageField extends Component {
 
     return (
       <div className="uploadImageFile">
-        <button type="button" className={className} onClick={this.onClick.bind(this)}>
+        <button
+          type="button"
+          className={className}
+          onClick={this.onClick.bind(this)}
+        >
           {value ? <img src={value} /> : <i className="fa fa-image" />}
         </button>
         <ReactS3Uploader
