36e61a9b10839adf50734f163a655347fca8379c
Igor Santos <igr.exe@gmail.com>
2016-11-30 18:52:51 -0200

    Add edit user page in sidebar

--- app/scripts/Paths.js
+++ app/scripts/Paths.js
@@ -84,3 +84,6 @@ const makePressureWidget = (mobilization_id, widget_id, path) =>
 export const formPressureWidget = (mid, wid) => makePressureWidget(mid, wid, '/form')
 export const emailPressureWidget = (mid, wid) => makePressureWidget(mid, wid, '/email')
 
+export function editAccount() {
+  return `/account/edit`
+}
