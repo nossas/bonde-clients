3fd5ea9d8299ee6a72dd8a5bff9012c25a919d86
Igor Santos <igr.exe@gmail.com>
2016-11-30 10:38:36 -0200

    Use forms dashboard in edit user

--- app/scripts/Account/pages/EditUserPage.js
+++ app/scripts/Account/pages/EditUserPage.js
@@ -1,13 +1,55 @@
 import React, { Component, PropTypes } from 'react'
+import { reduxForm } from 'redux-form'
 
+import { Tabs, Tab } from '../../../components/Navigation'
+import { SettingsPageMenuLayout, SettingsPageContentLayout } from '../../../components/Layout'
+import {
+  FormRedux,
+  SubmitButton,
+  FormGroup,
+  FormControl,
+  ControlLabel,
+  UploadImageField
+} from '../../Dashboard/Forms'
 
 class EditUserPage extends Component {
 
   render() {
-    const { auth } = this.props
+    const { fields: { picture, first_name, last_name, email }, ...formProps } = this.props
+
     return (
       <div>
-        <span>Se fudeu!</span>
+        <SettingsPageMenuLayout title="Minha conta">
+          <Tabs>
+            <Tab
+              text="Usuário"
+              isActive={true}
+            />
+          </Tabs>
+        </SettingsPageMenuLayout>
+        <SettingsPageContentLayout>
+          <FormRedux
+            nosubmit
+            onSubmit={values => console.log(values)}
+            {...formProps}>
+            <FormGroup controlId="pictureId" {...picture}>
+              <UploadImageField signingUrl={`${process.env.API_URL}/uploads`} />
+            </FormGroup>
+            <FormGroup controlId="firstNameId" {...first_name}>
+              <ControlLabel>Nome</ControlLabel>
+              <FormControl type="text" />
+            </FormGroup>
+            <FormGroup controlId="lastNameId" {...last_name}>
+              <ControlLabel>Sobrenome</ControlLabel>
+              <FormControl type="text" />
+            </FormGroup>
+            <FormGroup controlId="emailId" {...email}>
+              <ControlLabel>E-mail</ControlLabel>
+              <FormControl type="email" />
+            </FormGroup>
+            <SubmitButton position="floatTopRight">Salvar</SubmitButton>
+          </FormRedux>
+        </SettingsPageContentLayout>
       </div>
     )
   }
@@ -15,7 +57,14 @@ class EditUserPage extends Component {
 
 EditUserPage.propTypes = {
   // Injected by RequireLogin.js
-  auth: PropTypes.object.isRequired,
+  auth: PropTypes.object.isRequired
 }
 
-export default EditUserPage
+const fields = ['picture', 'first_name', 'last_name', 'email']
+
+export default reduxForm({
+  form: 'editUserForm',
+  fields
+}, (state, ownProps) => ({
+  initialValues: state.auth.user,
+}))(EditUserPage)
