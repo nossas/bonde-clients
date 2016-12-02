bf0a06565a50804e1e34a8b57258627d451fe236
Lucas Pirola <lucaspirola@gmail.com>
2016-12-01 22:16:10 -0200

    add security headers

--- src/server.js
+++ src/server.js
@@ -1,32 +1,34 @@
-import raven from 'raven';
-import newrelic from 'newrelic';
-import Express from 'express';
-import ReactDOMServer from 'react-dom/server';
-import React from 'react';
-import Location from 'react-router/lib/Location';
-import config from './config';
-import favicon from 'serve-favicon';
-import compression from 'compression';
-import proxy from 'http-proxy-middleware';
-import path from 'path';
-import createStore from './redux/create';
-import authApi from './api/api';
-import ApiClient from './ApiClient';
-import universalRouter from './universalRouter';
-import Html from './Html';
-import PrettyError from 'pretty-error';
+import raven from 'raven'
+import newrelic from 'newrelic'
+import Express from 'express'
+import ReactDOMServer from 'react-dom/server'
+import React from 'react'
+import Location from 'react-router/lib/Location'
+import config from './config'
+import favicon from 'serve-favicon'
+import compression from 'compression'
+import helmet from 'helmet'
+import proxy from 'http-proxy-middleware'
+import path from 'path'
+import createStore from './redux/create'
+import authApi from './api/api'
+import ApiClient from './ApiClient'
+import universalRouter from './universalRouter'
+import Html from './Html'
+import PrettyError from 'pretty-error'
 
-const pretty = new PrettyError();
-const app = new Express();
+const pretty = new PrettyError()
+const app = new Express()
 
 if ( (app.get('env') === 'production') || (app.get('env') === 'staging') ) {
   // The request handler must be the first item
-  app.use(raven.middleware.express.requestHandler(process.env.SENTRY_DSN));
+  app.use(raven.middleware.express.requestHandler(process.env.SENTRY_DSN))
 }
 
-app.use(compression());
-app.use(favicon(path.join(__dirname, '..', 'static', 'favicon.ico')));
-app.use(require('serve-static')(path.join(__dirname, '..', 'static')));
+app.use(compression())
+app.use(helmet())
+app.use(favicon(path.join(__dirname, '..', 'static', 'favicon.ico')))
+app.use(require('serve-static')(path.join(__dirname, '..', 'static')))
 
 //
 // Proxies
@@ -43,16 +45,16 @@ app.use('/api', proxy({
 
 app.get(/\/users|\/pt\/users|\/membros/, function(req, res) {
   res.redirect('/')
-});
+})
 
 app.get('/robots.txt', function(req, res) {
   res.send('User-Agent: * \n' +
     'Allow: /')
-});
+})
 
 if ( (app.get('env') === 'production') || (app.get('env') === 'staging') ) {
   // The error handler must be before any other error middleware
-  app.use(raven.middleware.express.errorHandler(process.env.SENTRY_DSN));
+  app.use(raven.middleware.express.errorHandler(process.env.SENTRY_DSN))
 }
 
 app.use((req, res, next) => {
@@ -88,48 +90,48 @@ app.use((req, res) => {
   if (__DEVELOPMENT__) {
     // Do not cache webpack stats: the script file would change since
     // hot module replacement is enabled in the development env
-    webpackIsomorphicTools.refresh();
+    webpackIsomorphicTools.refresh()
   }
 
-  const client = new ApiClient(req);
-  const store = createStore(client);
-  const location = new Location(req.path, req.query);
+  const client = new ApiClient(req)
+  const store = createStore(client)
+  const location = new Location(req.path, req.query)
   if (__DISABLE_SSR__) {
     res.send('<!doctype html>\n' +
-      ReactDOMServer.renderToString(<Html assets={webpackIsomorphicTools.assets()} component={<div/>} store={store}/>));
+      ReactDOMServer.renderToString(<Html assets={webpackIsomorphicTools.assets()} component={<div/>} store={store}/>))
   } else {
     universalRouter(location, undefined, store, req.headers.host)
       .then(({component, transition, isRedirect}) => {
         if (isRedirect) {
-          res.redirect(transition.redirectInfo.pathname);
-          return;
+          res.redirect(transition.redirectInfo.pathname)
+          return
         }
         res.send('<!doctype html>\n' +
-          ReactDOMServer.renderToString(<Html assets={webpackIsomorphicTools.assets()} component={component} store={store}/>));
+          ReactDOMServer.renderToString(<Html assets={webpackIsomorphicTools.assets()} component={component} store={store}/>))
       })
       .catch((error) => {
         if (error.redirect) {
-          res.redirect(error.redirect);
-          return;
+          res.redirect(error.redirect)
+          return
         }
-        console.error('ROUTER ERROR:', pretty.render(error));
-        res.status(500).send({error: error.stack});
-      });
+        console.error('ROUTER ERROR:', pretty.render(error))
+        res.status(500).send({error: error.stack})
+      })
   }
-});
+})
 
 if (config.port) {
   app.listen(config.port, (err) => {
     if (err) {
-      console.error(err);
+      console.error(err)
     } else {
       authApi().then(() => {
-        console.info('==> ✅  Server is listening');
-        console.info('==> 🌎  %s running on port %s, API on port %s', config.app.name, config.port, config.apiPort);
-        console.info('----------\n==> 🚀  Open http://%s in a browser to view the app.', process.env.APP_DOMAIN);
-      });
+        console.info('==> ✅  Server is listening')
+        console.info('==> 🌎  %s running on port %s, API on port %s', config.app.name, config.port, config.apiPort)
+        console.info('----------\n==> 🚀  Open http://%s in a browser to view the app.', process.env.APP_DOMAIN)
+      })
     }
-  });
+  })
 } else {
-  console.error('==>     ERROR: No PORT environment variable has been specified');
+  console.error('==>     ERROR: No PORT environment variable has been specified')
 }
