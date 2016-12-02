0860e7c828c4895e607f543be88e62e414437130
Lucas Pirola <lucaspirola@gmail.com>
2016-12-02 03:28:55 -0200

    Change session storage to redis;

--- src/api/api.js
+++ src/api/api.js
@@ -1,15 +1,32 @@
 import express from 'express'
 import session from 'express-session'
+import redisStore from 'connect-redis'
 import bodyParser from 'body-parser'
 import config from '../config'
 import winston from 'winston'
+import redis from 'redis'
+import url from 'url'
 
+const redisUrl = process.env.REDIS_URL ?
+  url.parse(process.env.REDIS_URL) :
+  url.parse('redis://localhost:6379/reboo')
+const redisClient = redis.createClient({url: redisUrl}) // replace with your config
+
+redisClient.on('error', function(err) {
+  winston.log('Redis error: ' + err)
+})
+const RedisStore = redisStore(session)
+const rs = new RedisStore({ client: redisClient, maxAge: 24 * 60 * 7 * 1000 })
 const app = express()
+
 app.use(session({
-  secret: 'react and redux rule!!!!',
   resave: false,
-  saveUninitialized: false
+  saveUninitialized: false,
+  store: rs,
+  secret: 'reboo-org',
+  cookie: { maxAge: 1000 * 60 * 24 * 7 }
 }))
+
 app.use(bodyParser.json())
 
 function loadAuth(req) {
