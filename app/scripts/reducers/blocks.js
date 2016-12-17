8d69e004718850aa79227140250a62ff922d5c1a
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2016-12-17 04:03:09 -0200

    Update block reducers to new async block select actions #275

--- app/scripts/reducers/blocks.js
+++ app/scripts/reducers/blocks.js
@@ -8,17 +8,16 @@ import {
   REQUEST_ASYNC_BLOCK_CREATE,
   SUCCESS_ASYNC_BLOCK_CREATE,
   FAILURE_ASYNC_BLOCK_CREATE,
+
+  REQUEST_ASYNC_BLOCK_UPDATE,
+  SUCCESS_ASYNC_BLOCK_UPDATE,
+  FAILURE_ASYNC_BLOCK_UPDATE,
+
+  REQUEST_ASYNC_BLOCK_SELECT,
+  SUCCESS_ASYNC_BLOCK_SELECT,
+  FAILURE_ASYNC_BLOCK_SELECT,
 } from '../../modules/mobilizations/blocks/action-types'
 
-const REQUEST_EDIT_BLOCK = 'REQUEST_EDIT_BLOCK'
-const SUCCESS_EDIT_BLOCK = 'SUCCESS_EDIT_BLOCK'
-const FAILURE_EDIT_BLOCK = 'FAILURE_EDIT_BLOCK'
-
-const REQUEST_FIND_BLOCKS = 'REQUEST_FIND_BLOCKS'
-const SUCCESS_FIND_BLOCKS = 'SUCCESS_FIND_BLOCKS'
-const FAILURE_FIND_BLOCKS = 'FAILURE_FIND_BLOCKS'
-
-const EDIT_BLOCK = 'EDIT_BLOCK'
 const REMOVE_BLOCK = 'REMOVE_BLOCK'
 const MOVE_BLOCK_UP = 'MOVE_BLOCK_UP'
 const MOVE_BLOCK_DOWN = 'MOVE_BLOCK_DOWN'
@@ -42,29 +41,24 @@ export default function blocks(state = initialState, action) {
     case SUCCESS_ASYNC_BLOCK_CREATE:
       return { ...state, loaded: true, data: state.data.concat([action.payload]) }
     case FAILURE_ASYNC_BLOCK_CREATE:
-      return { ...state, loaded: true }
+      return { ...state, loaded: true, error: action.payload }
 
-    case REQUEST_FIND_BLOCKS:
-      return {...state, loaded: false}
-    case SUCCESS_FIND_BLOCKS:
-      return {...state, loaded: true, data: action.result }
-    case FAILURE_FIND_BLOCKS:
-      return {...state, loaded: true}
+    case REQUEST_ASYNC_BLOCK_UPDATE:
+      return { ...state, loaded: false }
+    case SUCCESS_ASYNC_BLOCK_UPDATE:
+      const data = state.data.map(block => block.id === action.payload.id ? action.payload : block)
+      return { ...state, loaded: true, data }
+    case FAILURE_ASYNC_BLOCK_UPDATE:
+      return { ...state, loaded: true, error: action.payload }
 
-    case REQUEST_EDIT_BLOCK:
-      return {...state}
-    case SUCCESS_EDIT_BLOCK:
-      const data = state.data.map(block => block.id === action.result.id ? action.result : block)
-      return {...state, data}
-    case FAILURE_EDIT_BLOCK:
-      return {...state}
-    case EDIT_BLOCK:
-      return {...state,
-        data: state.data.map(
-          block => block.id === action.block.id ? action.block : block
-        )
-      }
+    case REQUEST_ASYNC_BLOCK_SELECT:
+      return { ...state, loaded: false }
+    case SUCCESS_ASYNC_BLOCK_SELECT:
+      return { ...state, loaded: true, data: action.payload  }
+    case FAILURE_ASYNC_BLOCK_SELECT:
+      return { ...state, loaded: true, error: action.payload }
 
+    // Needs to migrate reducers below
     case MOVE_BLOCK_UP:
       return {...state,
         data: state.data.map((block, index) => {
@@ -101,45 +95,4 @@ export default function blocks(state = initialState, action) {
 
 export function isBlocksLoaded(globalState) {
   return globalState.blocks.loaded
-}
-
-export function findBlocks(options) {
-  return {
-    types: [REQUEST_FIND_BLOCKS, SUCCESS_FIND_BLOCKS, FAILURE_FIND_BLOCKS],
-    promise: function() {
-      return new Promise(function(resolve, reject) {
-        superagent
-        .get(`${process.env.API_URL}/blocks`)
-        .send(options)
-        .end((err, res) => {
-          if (err) {
-            reject(res.body || err)
-          } else {
-            resolve(res.body)
-          }
-        })
-      })
-    }
-  }
-}
-
-export function editBlock(params) {
-  return {
-    types: [REQUEST_EDIT_BLOCK, SUCCESS_EDIT_BLOCK, FAILURE_EDIT_BLOCK],
-    promise: function() {
-      return new Promise(function(resolve, reject) {
-        superagent
-        .put(`${process.env.API_URL}/mobilizations/${params.mobilization_id}/blocks/${params.id}`)
-        .set(params.credentials)
-        .send({block: params.block})
-        .end((err, res) => {
-          if (err) {
-            reject(res.body || err)
-          } else {
-            resolve(res.body)
-          }
-        })
-      })
-    }
-  }
 }
