787f3d11d1f6ba5c88842988626855cbd8784972
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2016-12-17 04:02:35 -0200

    Add async block select action and its action types #275

---
+++ app/modules/mobilizations/blocks/action-creators/async-block-select.js
@@ -0,0 +1,31 @@
+import 'babel/polyfill'
+import { createAction } from './create-action'
+import {
+  REQUEST_ASYNC_BLOCK_SELECT,
+  SUCCESS_ASYNC_BLOCK_SELECT,
+  FAILURE_ASYNC_BLOCK_SELECT,
+} from '../action-types'
+
+const asyncBlockSelect = where => (dispatch, getState, axios) => {
+  const { auth: { credentials } } = getState()
+
+  const endpoint = '/blocks'
+  const config = { headers: credentials, params: where }
+
+  //
+  // HTTP GET method passing body?
+  // Maybe needs to refact the API endpoint.
+  //
+  dispatch({ type: REQUEST_ASYNC_BLOCK_SELECT })
+  return axios.get(endpoint, config, where)
+    .then(response => {
+      dispatch(createAction(SUCCESS_ASYNC_BLOCK_SELECT, response.data))
+      return Promise.resolve()
+    })
+    .catch(failure => {
+      dispatch(createAction(FAILURE_ASYNC_BLOCK_SELECT, failure))
+      return Promise.reject({ _error: `Response ${failure}` })
+    })
+}
+
+export default asyncBlockSelect
--- app/modules/mobilizations/blocks/action-creators/index.js
+++ app/modules/mobilizations/blocks/action-creators/index.js
@@ -1,3 +1,4 @@
 export { default as asyncBlockFetch } from './async-block-fetch'
 export { default as asyncBlockCreate } from './async-block-create'
 export { default as asyncBlockUpdate } from './async-block-update'
+export { default as asyncBlockSelect } from './async-block-select'
