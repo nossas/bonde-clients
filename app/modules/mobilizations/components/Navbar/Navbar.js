30935fba52517280a9dfe6add820395a56f7bfde
Igor Santos <igr.exe@gmail.com>
2016-12-19 16:25:34 -0200

    Init refactor mobilization module

---
+++ app/modules/mobilizations/components/Navbar/Navbar.js
@@ -0,0 +1,37 @@
+import React, { PropTypes } from 'react'
+import Menu from './Menu'
+
+
+const Navbar = props => {
+
+  const { blocks, editable } = props
+
+  const onlyVisibleBlocks = blocks.filter(
+    block => editable ? !block.hidden : !block.hidden && !block.menu_hidden
+  )
+
+  return (
+    <div className="absolute col-12 z3">
+      <div className="lg-show center">
+        <Menu blocks={onlyVisibleBlocks} />
+      </div>
+      {/* mobile version */}
+      <div className="lg-hide">
+        <Menu blocks={onlyVisibleBlocks} />
+      </div>
+    </div>
+  )
+}
+
+Navbar.propTypes = {
+  editable: PropTypes.bool.isRequired,
+  mobilization: PropTypes.object.isRequired,
+  blocks: PropTypes.array,
+}
+
+Navbar.defaultProps = {
+  editable: false,
+  blocks: [],
+}
+
+export default Navbar
---
+++ app/modules/mobilizations/components/Navbar/index.js
@@ -0,0 +1,2 @@
+export { default as Navbar } from './Navbar'
+export { default as Menu } from './Menu'
