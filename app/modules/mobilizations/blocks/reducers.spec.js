5b0025713414d45151beecdbe68537b1fdf1a2ac
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2016-12-21 16:32:48 -0200

    Migrate more tests of block reducer #275

--- app/modules/mobilizations/blocks/reducers.spec.js
+++ app/modules/mobilizations/blocks/reducers.spec.js
@@ -31,4 +31,86 @@ describe('BlockReducer', () => {
       expect(newState.editionMode).to.be.false
     })
   })
+
+  describe.skip('BlockReducers', () => {
+    before(() => {
+      _initialState = {
+        data: [
+          { id: 1, bg_color: 'bg-1' },
+          { id: 2, bg_color: 'bg-2' },
+          { id: 3, bg_color: 'bg-3' }
+        ]
+      }
+    })
+
+    describe('FETCH_BLOCKS', () => {
+      it('should return the mobilizations', () => {
+        const action = {
+          type: c.FETCH_BLOCKS,
+          blocks: [{id: 1}, {id: 2}]
+        }
+        const newState = BlockReducers(_initialState, action)
+        expect(newState).to.eql(action.blocks)
+      })
+    })
+
+    describe('EDIT_BLOCK', () => {
+      it('should return the mobilizations with edited block', () => {
+        const action = {
+          type: c.EDIT_BLOCK,
+          block: {id: 1, bg_color: 'bg-foo'}
+        }
+        const newState = BlockReducers(_initialState, action)
+        expect(newState.data).to.eql([
+          { id: 1, bg_color: 'bg-foo' },
+          { id: 2, bg_color: 'bg-2' },
+          { id: 3, bg_color: 'bg-3' }
+        ])
+      })
+    })
+
+    describe('MOVE_BLOCK_UP', () => {
+      it('should return the mobilizations with new order', () => {
+        const action = {
+          type: c.MOVE_BLOCK_UP,
+          block: {id: 2, bg_color: 'bg-2'}
+        }
+        const newState = BlockReducers(_initialState, action)
+        expect(newState.data).to.eql([
+          { id: 2, bg_color: 'bg-2' },
+          { id: 1, bg_color: 'bg-1' },
+          { id: 3, bg_color: 'bg-3' }
+        ])
+      })
+    })
+
+    describe('MOVE_BLOCK_DOWN', () => {
+      it('should return the mobilizations with new order', () => {
+        const action = {
+          type: c.MOVE_BLOCK_DOWN,
+          block: {id: 2, bg_color: 'bg-2'}
+        }
+        const newState = BlockReducers(_initialState, action)
+        expect(newState.data).to.eql([
+          { id: 1, bg_color: 'bg-1' },
+          { id: 3, bg_color: 'bg-3' },
+          { id: 2, bg_color: 'bg-2' }
+        ])
+      })
+    })
+
+    describe('REMOVE_BLOCK', () => {
+      it('should return the mobilizations without removed block', () => {
+        const action = {
+          type: c.REMOVE_BLOCK,
+          block: {id: 2}
+        }
+        const newState = BlockReducers(_initialState, action)
+        expect(newState.data).to.eql([
+          { id: 1, bg_color: 'bg-1' },
+          { id: 3, bg_color: 'bg-3' }
+        ])
+      })
+    })
+  })
 })
