7b850d1e75bfd53ef8185b7870135e43961233b9
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2016-12-21 02:33:45 -0200

    Expose utils and selectors files through mobilizations/blocks module #275

--- app/modules/mobilizations/blocks/index.js
+++ app/modules/mobilizations/blocks/index.js
@@ -1,7 +1,11 @@
 import * as actionCreators from './action-creators'
 import * as actionTypes from './action-types'
+import * as utils from './utils'
+import * as selectors from './selectors'
 
 export default {
   actions: actionCreators,
   actionTypes,
+  utils,
+  selectors,
 }
