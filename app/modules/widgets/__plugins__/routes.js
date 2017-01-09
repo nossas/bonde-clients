26b1acbec48589c037676f2518e60351371a7a61
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2017-01-09 10:39:38 -0200

    Migrate donation widget routes #280

--- app/modules/widgets/__plugins__/routes.js
+++ app/modules/widgets/__plugins__/routes.js
@@ -1,11 +1,11 @@
 import matchRoutes from './match/routes'
 import pressureRoutes from './pressure/routes'
 import formRoutes from './form/routes'
-import { createRoutes as donationCreateRoutes } from '../../../scripts/Widget/plugins/Donation'
+import donationRoutes from './donation/routes'
 
 export default () => [
   matchRoutes(),
   pressureRoutes(),
   formRoutes(),
-  donationCreateRoutes()
+  donationRoutes()
 ]
--- app/scripts/Widget/plugins/Donation/createRoutes.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Route } from 'react-router'
-
-import { SettingsDonationPage } from '../../../../modules/widgets/__plugins__/donation/pages'
-
-export default () => (
-  <Route path='/donation' key='settings-donation-page' component={SettingsDonationPage} />
-)
