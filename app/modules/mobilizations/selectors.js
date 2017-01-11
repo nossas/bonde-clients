691caf3c21f37af9ba0f14d3b8f8117bb226f8ea
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2017-01-11 08:53:02 -0200

    Refact blocks selectors methods to have more concordance and change names do make it more concise #289

--- app/modules/mobilizations/selectors.js
+++ app/modules/mobilizations/selectors.js
@@ -6,10 +6,6 @@ export const getCurrent = (state, ownProps) => {
 
 export const getList = state => state.mobilization.list.data
 
-export const getBlocks = state => state.blocks.data
-
-export const blocksIsLoaded = state => state.blocks.loaded
-
 export const getMenuActiveIndex = state => state.mobilization.list.menuActiveIndex
 
 export const isLoading = state => state.mobilization.list.loading
