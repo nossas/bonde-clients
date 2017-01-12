caeec5bbc64ac4632c7b11d2da1ff9d954ec168f
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2017-01-12 07:30:48 -0200

    Fix widgets required mobilization props that injected by container

--- app/modules/widgets/containers/settings-container.js
+++ app/modules/widgets/containers/settings-container.js
@@ -4,6 +4,9 @@ import { connect } from 'react-redux'
 // Global module dependencies
 import { SettingsPageLayout } from '../../../components/Layout'
 
+// Parent module dependencies
+import * as MobilizationSelectors from '../../mobilizations/selectors'
+
 // Current module dependencies
 import * as WidgetSelectors from '../selectors'
 
@@ -16,13 +19,12 @@ export const SettingsContainer = ({ children, ...rest }) => (
 SettingsContainer.propTypes = {
   children: PropTypes.object,
   mobilization: PropTypes.object.isRequired,
-  widget: PropTypes.object.isRequired,
-  credentials: PropTypes.object.isRequired
+  widget: PropTypes.object.isRequired
 }
 
 const mapStateToProps = (state, props) => ({
-  widget: WidgetSelectors.getWidget(state, props),
-  credentials: state.auth.credentials
+  mobilization: MobilizationSelectors.getCurrent(state, props),
+  widget: WidgetSelectors.getWidget(state, props)
 })
 
 export default connect(mapStateToProps)(SettingsContainer)
