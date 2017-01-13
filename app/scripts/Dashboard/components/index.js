6b9ee0f480ab829807370d743846b561366c8e19
Igor Santos <igr.exe@gmail.com>
2017-01-13 15:48:07 -0200

    Remove files templates no-used

--- app/scripts/Dashboard/components/index.js
+++ app/scripts/Dashboard/components/index.js
@@ -1,2 +1,1 @@
-export { default as DashboardSidebar } from './DashboardSidebar'
 export { default as Loading } from './Loading'
--- app/scripts/Mobilization/plugins/Templates/components/index.js
+++
@@ -1,2 +0,0 @@
-export { default as MobilizationTemplatesBasicsForm } from './MobilizationTemplatesBasicsForm'
-export { default as MobilizationTemplatesSelectableList } from './MobilizationTemplatesSelectableList'
