50889143a1e784098dac97c39ac8cda4d7c2fc35
Igor Santos <igr.exe@gmail.com>
2017-01-13 19:12:45 -0200

    Add select template. Fixes #301

--- app/modules/mobilizations/selectors.js
+++ app/modules/mobilizations/selectors.js
@@ -1,7 +1,11 @@
 export const getCurrent = (state, ownProps) => {
-  const { params: { mobilization_id } } = ownProps || { params: {} }
   const { list: { currentId, data } } = state.mobilization
-  return data.filter(mob => mob.id === currentId || mobilization_id)[0]
+  return data.filter(mob => mob.id === currentId)[0]
+}
+
+export const getTemplate = (state, ownProps) => {
+  const { list: { data }, templates: { list: { templateId } } } = state.mobilization
+  return data.filter(mob => mob.id === templateId)[0]
 }
 
 export const getList = state => state.mobilization.list.data
--- app/modules/mobilizations/templates/index.js
+++ app/modules/mobilizations/templates/index.js
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux'
+import * as actionCreators from './action-creators'
 
 import { reducer as filterableReducer } from '../../../components/FilterableSearchBar'
 import { reducer as selectableReducer } from '../../../components/SelectableList'
@@ -6,6 +7,7 @@ import { reducer as selectableReducer } from '../../../components/SelectableList
 import templatesReducer from './reducers'
 
 export default {
+  actionCreators,
   reducers: combineReducers(
     {
       list: templatesReducer,
