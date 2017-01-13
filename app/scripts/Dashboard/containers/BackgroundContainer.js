f7aced561a90a727628ee08eb349dc44835c7190
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2017-01-12 22:37:40 -0200

    Add scroll to communities background container solves #295

--- app/scripts/Dashboard/containers/BackgroundContainer.js
+++ app/scripts/Dashboard/containers/BackgroundContainer.js
@@ -2,16 +2,15 @@ import React, { Component } from 'react'
 
 import './BackgroundContainer.scss'
 
-
 class BackgroundContainer extends Component {
 
-  render() {
-    const { children } = this.props;
+  render () {
+    const { children } = this.props
 
     return (
-      <div className="bg-cover bg-center bg-reboo absolute top-0 right-0 bottom-0 left-0">
-        <div className="table col-3 mx-auto full-height">
-          <div className="table-cell align-middle">
+      <div className='bg-cover bg-center bg-reboo absolute top-0 right-0 bottom-0 left-0'>
+        <div className='table col-3 mx-auto full-height'>
+          <div className='table-cell align-middle'>
             {children}
           </div>
         </div>
