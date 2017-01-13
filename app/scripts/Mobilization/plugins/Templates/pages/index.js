76a44d55c94f8dca87e582f60de8f1b195a88d26
Igor Santos <igr.exe@gmail.com>
2017-01-13 15:48:07 -0200

    Remove files templates no-used

--- app/scripts/Mobilization/plugins/Templates/pages/index.js
+++
@@ -1,5 +0,0 @@
-export { default as MobilizationTemplatesCreatePage } from './MobilizationTemplatesCreatePage'
-export { default as MobilizationTemplatesListPage } from './MobilizationTemplatesListPage'
-export { default as MobilizationTemplatesChoosePage } from './MobilizationTemplatesChoosePage'
-export { default as MobilizationTemplatesChooseCustomListPage } from './MobilizationTemplatesChooseCustomListPage'
-export { default as MobilizationTemplatesChooseGlobalListPage } from './MobilizationTemplatesChooseGlobalListPage'
