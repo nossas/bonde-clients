4939e019e0817f497cb373f20ec9b46923df5c09
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2017-01-25 17:03:45 -0200

    Remove useless components and reducers; Migrate remaining files to the client folder #316

--- .old/src/views/createRoutes.js
+++ .old/src/views/createRoutes.js
@@ -1,25 +1,22 @@
 import React from 'react'
 import { Route } from 'react-router'
 
-import {
-  Application,
-  NotFound
-} from '../../app/scripts/containers'
+import Application from '~client/application'
+import * as paths from '~client/paths'
+import { NotFound } from '~components/error'
 
-import { CustomDomainPage } from '../../app/modules/mobilizations/pages'
+import { createRoutes as accountCreateRoutes, requireLoginWrapper } from '~account'
+import { createRoutes as communityCreateRoutes } from '~community'
+import { createRoutes as mobilizationCreateRoutes } from '~mobilizations'
 
-import * as Paths from '../../app/scripts/Paths'
-
-import matchRoutesExternal from '../../app/modules/widgets/__plugins__/match/routes-external'
-import { createRoutes as accountCreateRoutes, requireLoginWrapper } from '../../app/scripts/Account'
-import { createRoutes as communityCreateRoutes } from '../../app/scripts/Community'
-import { createRoutes as mobilizationCreateRoutes } from '../../app/scripts/Mobilization'
+import { CustomDomainPage } from '~mobilizations/pages'
+import matchRoutesExternal from '~mobilizations/widgets/__plugins__/match/routes-external'
 
 export default function (store, host) {
   const isAppSubdomain = (host === `app.${process.env.APP_DOMAIN}`)
 
   if (isAppSubdomain) {
-    const requiredLogin = requireLoginWrapper(store, Paths.login())
+    const requiredLogin = requireLoginWrapper(store, paths.login())
 
     return (
       <Route component={Application}>
