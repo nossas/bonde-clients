326f0f0085b324e78c038c6b4bffce4c8342817b
Lucas Pirola <lucaspirola@gmail.com>
2017-01-30 01:30:39 -0200

    describe donation events

--- app/modules/widgets/utils/analytics-events.js
+++ app/modules/widgets/utils/analytics-events.js
@@ -1,25 +1,37 @@
 import ReactGA from 'react-ga'
 
-const formIsFilled = () => {
-  ReactGA.event({
-    category: 'Formulário',
-    action: 'Preenchimento Iniciado'
-  })
+const FORM_FILLED = { category: 'Formulário', action: 'Preenchimento Iniciado' }
+const FORM_SAVED = { category: 'Formulário', action: 'Dados Salvos com Sucesso' }
+const FORM_SHARED = { category: 'Formulário', action: 'Social' }
+
+const DONATION_STARTED = { category: 'Doação', action: '' }
+const DONATION_FINISHED = { category: 'Doação', action: '' }
+const DONATION_SHARED = { category: 'Doação', action: '' }
+
+const sendEvent = (event) => {
+  ReactGA.event(event)
 }
 
-const formSavedData = () => {
-  ReactGA.event({
-    category: 'Formulário',
-    action: 'Dados Salvos com Sucesso'
-  })
-}
-
+const formIsFilled = () => { sendEvent(FORM_FILLED) }
+const formSavedData = () => { sendEvent(FORM_SAVED) }
 const formSocialShare = (source) => {
-  ReactGA.event({
-    category: 'Formulário',
-    action: 'Social',
-    label: source
-  })
+  sendEvent({...FORM_SHARED, label: source})
 }
 
-export default { formIsFilled, formSavedData, formSocialShare }
+const donationSetValue = () => { sendEvent(DONATION_STARTED) }
+const donationFinishRequest = (value) => {
+  sendEvent({...DONATION_FINISHED, label: value})
+}
+
+const donationSocialShare = (source) => {
+  sendEvent({...DONATION_SHARED, label: source})
+}
+
+export default {
+  formIsFilled,
+  formSavedData,
+  formSocialShare,
+  donationSetValue,
+  donationFinishRequest,
+  donationSocialShare
+}
