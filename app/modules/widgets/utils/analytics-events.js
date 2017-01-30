48263538dc106f43930d4945719fef06868d7884
Lucas Pirola <lucaspirola@gmail.com>
2017-01-30 01:43:01 -0200

    Describe pressure events

--- app/modules/widgets/utils/analytics-events.js
+++ app/modules/widgets/utils/analytics-events.js
@@ -4,9 +4,13 @@ const FORM_FILLED = { category: 'Formulário', action: 'Preenchimento Iniciado' 
 const FORM_SAVED = { category: 'Formulário', action: 'Dados Salvos com Sucesso' }
 const FORM_SHARED = { category: 'Formulário', action: 'Social' }
 
-const DONATION_STARTED = { category: 'Doação', action: '' }
-const DONATION_FINISHED = { category: 'Doação', action: '' }
-const DONATION_SHARED = { category: 'Doação', action: '' }
+const DONATION_STARTED = { category: 'Doação', action: 'Escolheu Valor' }
+const DONATION_FINISHED = { category: 'Doação', action: 'Servidor Recebeu Dados' }
+const DONATION_SHARED = { category: 'Doação', action: 'Social' }
+
+const PRESSURE_FILLED = { category: 'Pressão', action: 'Preenchimento Iniciado' }
+const PRESSURE_SAVED = { category: 'Pressão', action: 'Dados Salvos com Sucesso' }
+const PRESSURE_SHARED = { category: 'Pressão', action: 'Social' }
 
 const sendEvent = (event) => {
   ReactGA.event(event)
@@ -27,11 +31,20 @@ const donationSocialShare = (source) => {
   sendEvent({...DONATION_SHARED, label: source})
 }
 
+const pressureIsFilled = () => { sendEvent(PRESSURE_FILLED) }
+const pressureSavedData = () => { sendEvent(PRESSURE_SAVED) }
+const pressureSocialShare = (source) => {
+  sendEvent({...PRESSURE_SHARED, label: source})
+}
+
 export default {
   formIsFilled,
   formSavedData,
   formSocialShare,
   donationSetValue,
   donationFinishRequest,
-  donationSocialShare
+  donationSocialShare,
+  pressureIsFilled,
+  pressureSavedData,
+  pressureSocialShare
 }
