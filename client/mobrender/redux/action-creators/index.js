d9aaed90784a3e05913021f25491bb082f262756
Igor Santos <igr.exe@gmail.com>
2017-02-09 17:49:18 -0200

    Add async move up block

--- client/mobrender/redux/action-creators/index.js
+++ client/mobrender/redux/action-creators/index.js
@@ -1,4 +1,5 @@
-export { default as asyncUpdateWidget } from './async-update-widget'
+  export { default as asyncUpdateWidget } from './async-update-widget'
 export { default as asyncFetchWidgets } from './async-fetch-widgets'
 export { default as handleMouseOver } from './handle-mouse-over'
 export { default as handleMouseOut } from './handle-mouse-out'
+export { default as asyncMoveUp } from './async-move-up'
