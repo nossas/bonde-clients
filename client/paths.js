bb59485f5626eb6c54bb5cfbe9fab7ca73282de4
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2017-02-14 14:26:03 -0200

    Migrate widgets form fields page to use dynamic routing #368

--- client/paths.js
+++ client/paths.js
@@ -21,9 +21,6 @@ export const sharingMobilization = id => `/mobilizations/${id}/sharing`
 export const analyticsMobilization = id => `/mobilizations/${id}/analytics`
 export const customDomainMobilization = id => `/mobilizations/${id}/customDomain`
 
-export const fieldsMobilizationWidget = (mobilizationId, widgetId) => {
-  return `/mobilizations/${mobilizationId}/widgets/${widgetId}/fields`
-}
 export const autofireMobilizationWidget = (mobilizationId, widgetId) => {
   return `/mobilizations/${mobilizationId}/widgets/${widgetId}/autofire`
 }
