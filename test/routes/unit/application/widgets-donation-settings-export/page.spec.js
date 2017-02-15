f3ca997d29192a70288b6f9ffc97e2518bc1a6e8
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2017-02-15 14:38:06 -0200

    Create splitted donation widget expor page to use dynamic routing #368

---
+++ test/routes/unit/application/widgets-donation-settings-export/page.spec.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import { expect } from 'chai'
+
+import * as mock from '~utils/mock'
+import WidgetsDonationSettingsExportPage from '~routes/application/widgets-donation-settings-export/page'
+
+describe('routes/application/widgets-donation-settings-export/page', () => {
+  let wrapper
+  const props = {
+    params: { widget_id: '1' },
+    loading: false,
+    success: false,
+    error: undefined,
+    widget: { id: 1 },
+    mobilization: { name: 'Foo bar' },
+    // Actions
+    asyncWidgetDataExport: mock.noop,
+    dataExportMount: mock.noop
+  }
+
+  beforeAll(() => {
+    wrapper = shallow(<WidgetsDonationSettingsExportPage {...props} />)
+  })
+
+  describe('#render', () => {
+    it('should render without crash', () => {
+      expect(wrapper).to.be.ok
+    })
+  })
+})
