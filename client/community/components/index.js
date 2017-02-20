ea3a498f54ceb9caeaec8168c791c55278a5cde8
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2017-02-20 11:02:50 -0300

    Patternize community components filename to use dash-case #362

--- client/community/components/index.js
+++ client/community/components/index.js
@@ -1,3 +1,3 @@
-export { default as ListItem } from './ListItem'
-export { default as ForceDownloadViaAjax } from './ForceDownloadViaAjax'
+export { default as ListItem } from './list-item'
+export { default as ForceDownloadViaAjax } from './force-download-via-ajax'
 export { default as SettingsMenu } from './settings-menu'
