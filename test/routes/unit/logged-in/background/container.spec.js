9bd95f8d191b0b4b2ff6312d8463f7eb3a9e9735
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2017-03-06 18:15:10 -0300

    Add zendesk service widget; Apply into logged-in containers #375

---
+++ test/routes/unit/logged-in/background/container.spec.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { expect } from 'chai'
+import { shallow } from 'enzyme'
+
+import BackgroundContainer from '~routes/logged-in/background/container'
+
+describe('routes/application/container', () => {
+  let wrapper = shallow(
+    <BackgroundContainer>
+      <h1>Foo Bar</h1>
+    </BackgroundContainer>
+  )
+
+  it('render without crashed', () => {
+    expect(wrapper).to.be.ok
+  })
+
+  it('should render a zendesk widget component', () => {
+    expect(wrapper.find('ZendeskWidget')).to.have.length(1)
+  })
+})
