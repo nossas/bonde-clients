50797fafc4e1dea91e46f48cf1903f768efdbc50
Igor Santos <igr.exe@gmail.com>
2017-03-06 18:25:41 -0300

    Refactor create block action with new api response

--- client/mobrender/redux/action-creators/index.js
+++ client/mobrender/redux/action-creators/index.js
@@ -9,6 +9,7 @@ export { default as asyncMoveDown } from './async-move-down'
 export { default as asyncUpdateBlock } from './async-update-block'
 export { default as asyncFetchBlocks } from './async-fetch-blocks'
 export { default as asyncDestroyBlock } from './async-destroy-block'
+export { default as asyncAddBlock } from './async-add-block'
 
 export { default as handleEdit } from './handle-edit'
 export { default as handleCancelEdit } from './handle-cancel-edit'
