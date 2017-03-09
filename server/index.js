5371788e85e20df9050f06b07d742108f8dc7071
Lucas Pirola <lucaspirola@gmail.com>
2017-03-08 21:20:08 -0300

    fix prod bundle

--- server/index.js
+++ server/index.js
@@ -161,8 +161,8 @@ export const createServer = (config) => {
                 <script>window.INITIAL_STATE = ${JSON.stringify(initialState)};</script>
                 <script src="/wysihtml/wysihtml-toolbar.min.js"></script>
                 <script src="/wysihtml/advanced_and_extended.js"></script>
-                <script src="${__PROD__ ? assets.vendor.bundle.js : '/vendor.bundle.js'}"></script>
-                <script async src="${__PROD__ ? assets.main.bundle.js : '/main.bundle.js'}" ></script>
+                <script src="${__PROD__ ? assets.vendor.js : '/vendor.bundle.js'}"></script>
+                <script async src="${__PROD__ ? assets.main.js : '/main.bundle.js'}" ></script>
               </body>
             </html>
           `)
