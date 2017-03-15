d1884e1973f2da54c5c306be8b085d587b449787
Lucas Pirola <lucaspirola@gmail.com>
2017-03-15 00:59:34 -0300

    Remove auth proxy and client middleware

--- server/authenticate/routes/index.js
+++
@@ -1,3 +0,0 @@
-export { default as login } from './login'
-export { default as load } from './load'
-export { default as logout } from './logout'
--- server/authenticate/routes/load.js
+++
@@ -1,1 +0,0 @@
-export default req => Promise.resolve(req.session.auth || {})
