2856ea67e566fad66298023cd2549a5352c792bc
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2017-03-17 14:08:07 -0300

    Fix draft widget rendering when editable is false, close #453

--- test/client/unit/mobrender/components/widget.spec.js
+++ test/client/unit/mobrender/components/widget.spec.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { expect } from 'chai'
-import { mount, shallow } from 'enzyme'
+import { shallow } from 'enzyme'
 import Widget from '~client/mobrender/components/widget'
 import WidgetOverlay from '~client/mobrender/components/widget-overlay.connected'
 
@@ -12,29 +12,29 @@ describe('mobrender/components/widget', () => {
   }
 
   it('should render without crashed', () => {
-    const widget = mount(<Widget {...props} />)
+    const widget = shallow(<Widget {...props} />)
     expect(widget).to.be.ok
   })
 
   it('should contains classes to resize component', () => {
-    const { sm_size, md_size, lg_size } = props.widget
-    const className = `col-${sm_size} sm-col-${sm_size} md-col-${md_size} lg-col-${lg_size}`
-    const widget = mount(<Widget {...props} />)
+    const { sm_size: smSize, md_size: mdSize, lg_size: lgSize } = props.widget
+    const className = `col-${smSize} sm-col-${smSize} md-col-${mdSize} lg-col-${lgSize}`
+    const widget = shallow(<Widget {...props} />)
     expect(widget.find('div').at(0).props().className).to.contains(className)
   })
 
   it('should render component children according kind', () => {
-    const widget = mount(<Widget {...props} />)
+    const widget = shallow(<Widget {...props} editable />)
     expect(widget.find('Draft').length).to.equal(1)
   })
 
   it('should render component loading while saving widget', () => {
-    const widget = mount(<Widget {...props} saving />)
+    const widget = shallow(<Widget {...props} saving />)
     expect(widget.find('Loading').length).to.equal(1)
   })
 
   it('should passed widget and update to children', () => {
-    const widget = mount(<Widget {...props} />)
+    const widget = shallow(<Widget {...props} editable />)
     expect(widget.find('Draft').props().widget).to.deep.equal(props.widget)
     expect(widget.find('Draft').props().update).to.equal(props.update)
   })
@@ -50,5 +50,30 @@ describe('mobrender/components/widget', () => {
   it('should not render overlay when editable is false', () => {
     const widget = shallow(<Widget {...props} />)
     expect(widget.find(WidgetOverlay).length).to.equal(0)
+  })
+
+  describe('when editable is false', () => {
+    it('should not render draft widget component', () => {
+      const widget = shallow(<Widget {...props} editable={false} />)
+      expect(widget.find('Draft').length).to.equal(0)
+    })
+
+    it('should render pressure widget component', () => {
+      const widget = shallow(<Widget {...props} editable={false} />)
+      widget.setProps({ ...props, widget: { ...props.widget, kind: 'pressure' } })
+      expect(widget.find('Connect(Pressure)').length).to.equal(1)
+    })
+
+    it('should render form widget component', () => {
+      const widget = shallow(<Widget {...props} editable={false} />)
+      widget.setProps({ ...props, widget: { ...props.widget, kind: 'form' } })
+      expect(widget.find('Form').length).to.equal(1)
+    })
+
+    it('should render donation widget component', () => {
+      const widget = shallow(<Widget {...props} editable={false} />)
+      widget.setProps({ ...props, widget: { ...props.widget, kind: 'donation' } })
+      expect(widget.find('Donation').length).to.equal(1)
+    })
   })
 })
