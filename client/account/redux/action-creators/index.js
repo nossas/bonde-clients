dd21c6f6ec0b4594557a4a2eab75beab1ea32bfd
Igor Santos <igr.exe@gmail.com>
2017-03-20 12:26:40 -0300

    Add create new user in account actions. Issue #462

--- client/account/redux/action-creators/index.js
+++ client/account/redux/action-creators/index.js
@@ -2,17 +2,4 @@ export { default as load } from './load'
 export { default as login } from './login'
 export { default as logout } from './logout'
 export { default as asyncUpdateUser } from './async-update-user'
-
-// // redux-form action
-// export const register = user => (dispatch, getState, request) => {
-//   return request
-//     .post('/users', { user })
-//     .then(({ status, data }) => {
-//       if (status === 200 && data.errors) {
-//         return Promise.reject({ ...data.errors })
-//       } else if (status === 201) {
-//         return Promise.resolve()
-//       }
-//     })
-//     .catch(error => Promise.reject(error))
-// }
+export { default as asyncAddUser } from './async-add-user'
