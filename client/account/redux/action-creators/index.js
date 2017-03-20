15fff84c3a54023930b3e4531155547630a5c82d
Igor Santos <igr.exe@gmail.com>
2017-03-20 11:39:29 -0300

    Add async update user. Issue #460

--- client/account/redux/action-creators/index.js
+++ client/account/redux/action-creators/index.js
@@ -1,6 +1,7 @@
 export { default as load } from './load'
 export { default as login } from './login'
 export { default as logout } from './logout'
+export { default as asyncUpdateUser } from './async-update-user'
 
 // // redux-form action
 // export const register = user => (dispatch, getState, request) => {
@@ -12,21 +13,6 @@ export { default as logout } from './logout'
 //       } else if (status === 201) {
 //         return Promise.resolve()
 //       }
-//     })
-//     .catch(error => Promise.reject(error))
-// }
-//
-// export const edit = ({ id, ...user }) => (dispatch, getState, request) => {
-//   const headers = { ...getState().auth.credentials }
-//   return request
-//     .put(`/users/${id}`, { user }, { headers })
-//     .then(({ status, data }) => {
-//       if (status === 200) {
-//         // update reducers
-//         dispatch({ type: t.UPDATE, user: data })
-//         return Promise.resolve()
-//       }
-//       return Promise.reject({ _error: `Response code ${status}` })
 //     })
 //     .catch(error => Promise.reject(error))
 // }
