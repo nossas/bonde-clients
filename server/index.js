06c98eadcb03492c18200622341796551b7b2cb5
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2017-03-24 16:31:20 -0300

    Redirects to `app.` accessed via base app domain directly

--- server/index.js
+++ server/index.js
@@ -80,6 +80,16 @@ export const createServer = (config) => {
 
   app.use((req, res, next) => {
     const host = req.headers.host
+    const isAppDomain = host === process.env.APP_DOMAIN
+    if (isAppDomain) {
+      res.redirect(301, `${req.protocol}://app.${host}${req.originalUrl}`)
+      return
+    }
+    next()
+  })
+
+  app.use((req, res, next) => {
+    const host = req.headers.host
     const isAppSubdomain = host.indexOf(`app.${process.env.APP_DOMAIN}`) !== -1
     const www = host.match(/^www\.(.*)/)
     const domains = require('fs').readFileSync('./server/redirect.blacklist')
