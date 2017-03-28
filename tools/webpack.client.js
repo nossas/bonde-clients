6e5090c9c35e55b510e439fe62410ab5dc916f05
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2017-03-28 14:53:35 -0300

    Add standard-loader to webpack config #508

--- tools/webpack.client.js
+++ tools/webpack.client.js
@@ -166,6 +166,21 @@ module.exports = {
         })
       },
       {
+        // set up standard-loader as a preloader
+        enforce: 'pre',
+        test: /\.js$/,
+        loader: 'standard-loader',
+        exclude: /node_modules/,
+        options: {
+          // Emit errors instead of warnings (default = false)
+          error: false,
+          // enable snazzy output (default = true)
+          snazzy: true,
+          // other config options to be passed through to standard e.g.
+          parser: 'babel-eslint'
+        }
+      },
+      {
         test: /\.js$/,
         loader: 'babel-loader',
         options: {
