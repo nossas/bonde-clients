8efb025435e6d073dfea96e9c6e82a9bbd43bee9
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2017-03-29 15:22:18 -0300

    Remove community settings link located on top of sidebar component; Close #489

--- client/components/navigation/sidenav/sidenav.js
+++ client/components/navigation/sidenav/sidenav.js
@@ -24,13 +24,9 @@ const Sidenav = ({ children, community }) => (
                 <Link to={paths.mobilizations()}>{community.name || 'Bonde'}</Link>
               </div>
               <div className='item-community-change'>
-                <Link to={paths.communityInfo()} className='col col-8'>
-                  <i className='fa fa-cog mr1' />
-                  <span>Configurações</span>
-                </Link>
                 <Link to={paths.communityList()} className='col col-4'>
                   <i className='fa fa-refresh mr1' />
-                  <span>Trocar</span>
+                  <span>Trocar de comunidade</span>
                 </Link>
               </div>
             </div>
