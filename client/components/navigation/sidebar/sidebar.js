9821838aa5dcebe3be25eee57cb56554a20ce557
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2017-03-29 14:40:43 -0300

    Add community settings pages links into sidebar when does not editing a mobilization; Close #489

--- client/components/navigation/sidebar/sidebar.js
+++ client/components/navigation/sidebar/sidebar.js
@@ -18,48 +18,69 @@ const Sidebar = props => {
   return (
     <div className='top-0 right-0 bottom-0 left-0 flex flex-column absolute'>
       <Sidenav community={community}>
-        {
-          !mobilization ? null : (
-            <SidenavList className='bg-lighten-2'>
-              {!mobilization.custom_domain ? (
-                <SidenavListItem
-                  text='PUBLICAR BONDE'
-                  icon='rocket'
-                  href={paths.mobilizationLaunch(mobilization.id)}
-                  className='launch-button rounded'
-                />
-              ) : (
-                <SidenavListItem
-                  text='BONDE público'
-                  icon='check'
-                  className='launched-item'
-                />
-              )}
+        {!mobilization ? (
+          <SidenavList className='bg-lighten-2'>
+            <SidenavListItem
+              text='Informações'
+              icon='info-circle'
+              href={paths.communityInfo()}
+            />
+            <SidenavListItem
+              text='Mailchimp'
+              icon='envelope-o'
+              href={paths.communityMailchimp()}
+            />
+            <SidenavListItem
+              text='Recebedor'
+              icon='money'
+              href={paths.communityRecipient()}
+            />
+            <SidenavListItem
+              text='Relatório'
+              icon='file-excel-o'
+              href={paths.communityReport()}
+            />
+          </SidenavList>
+        ) : (
+          <SidenavList className='bg-lighten-2'>
+            {!mobilization.custom_domain ? (
               <SidenavListItem
-                text='Editar mobilização'
-                icon='pencil'
-                href={paths.editMobilization(mobilization.id)}
+                text='PUBLICAR BONDE'
+                icon='rocket'
+                href={paths.mobilizationLaunch(mobilization.id)}
+                className='launch-button rounded'
               />
+            ) : (
               <SidenavListItem
-                text='Adicionar conteúdo'
-                icon='plus'
-                href={BlockPaths.createBlock({ id: mobilization.id })}
+                text='BONDE público'
+                icon='check'
+                className='launched-item'
               />
-              <SidenavListItem
-                text='Ver em uma nova aba'
-                icon='external-link'
-                linkType='anchor'
-                href={paths.mobilization(mobilization)}
-                target='_blank'
-              />
-              <SidenavListItem
-                text='Configurações'
-                icon='cog'
-                href={paths.basicsMobilization(mobilization.id)}
-              />
-            </SidenavList>
-          )
-        }
+            )}
+            <SidenavListItem
+              text='Editar mobilização'
+              icon='pencil'
+              href={paths.editMobilization(mobilization.id)}
+            />
+            <SidenavListItem
+              text='Adicionar conteúdo'
+              icon='plus'
+              href={BlockPaths.createBlock({ id: mobilization.id })}
+            />
+            <SidenavListItem
+              text='Ver em uma nova aba'
+              icon='external-link'
+              linkType='anchor'
+              href={paths.mobilization(mobilization)}
+              target='_blank'
+            />
+            <SidenavListItem
+              text='Configurações'
+              icon='cog'
+              href={paths.basicsMobilization(mobilization.id)}
+            />
+          </SidenavList>
+        )}
         <SidenavList style={{ position: 'absolute', bottom: '0' }}>
           <SidenavListItem text='Minha Conta' icon='user' href={paths.editAccount()}>
             <div className='white h6'>{user.email}</div>
