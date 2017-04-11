34ba61c3553636b4e7321736d700857803a5a7a2
Igor Santos <igr.exe@gmail.com>
2017-04-11 15:40:03 -0300

    use steps component to create domain community page

--- routes/admin/authenticated/sidebar/community-settings/domain-create/page.js
+++ routes/admin/authenticated/sidebar/community-settings/domain-create/page.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react'
 import { FormRedux, ControlLabel, FormControl, FormGroup } from '~client/components/forms'
 import { Button } from '~client/ux/components'
 
+import { Steps, Step } from '~client/steps'
+import { DomainStep } from '~client/community/components/dns'
+
 
 class Page extends Component {
 
@@ -9,12 +12,13 @@ class Page extends Component {
     super(props)
     this.state = {
       dns: undefined,
-      renderTestConnection: false
+      renderTestConnection: false,
+      hasConnection: true
     }
   }
 
   handleTestConnection () {
-
+    this.setState({ hasConnection: true })
   }
 
   render () {
@@ -24,50 +28,49 @@ class Page extends Component {
       <div className='page'>
         <h2>Domínio da comunidade</h2>
 
-        <div className='step step-01'>
-          <h3>Insira o domínio desejado</h3>
-          <FormRedux
-            nosubmit
-            {...formProps}
-            onSubmit={values => {
-              return save(values)
-                .then(dns => {
-                  this.setState({ dns })
-                  return Promise.resolve()
-                })
-            }}
-          >
-            <FormGroup {...domain_name}>
-              <ControlLabel>Domínio da sua comunidade</ControlLabel>
-              <FormControl type='text' placeholder='Ex. minhacomunidade.org' />
-            </FormGroup>
-            <Button disabled={saving} type='submit'>Adicionar</Button>
-          </FormRedux>
-        </div>
-
-        {this.state.dns && (
-          <div className='step step-02'>
-            <h3>Altere os servidores do seu provedor DNS</h3>
-            <div>
-              <p>1. Faça login no seu provedor de DNS (onde seu domínio está registrado, por exemplo GoDaddy, Locaweb, RegistroBR)</p>
-              <p>2. Encontre a página de <b>gerenciador de DNS</b>, e altere os <b>nomes de servidor</b> para os servidores do Bonde:</p>
-              <br />
-              {this.state.dns.delegation_set_servers.map(server => <p>{server}</p>)}
-              <Button onClick={() => this.setState({ renderTestConnection: true })}>Continuar</Button>
-            </div>
-          </div>
-        )}
-
-        {this.state.renderTestConnection && (
-          <div className='step step-03'>
-            <h3>Teste a conexão</h3>
+        <Steps
+          renderRule={(position, current) => position <= current}
+          progressValidations={[
+            () => this.state.dns !== undefined,
+            () => this.state.renderTestConnection,
+            () => this.state.hasConnection
+          ]}
+        >
+          <Step title='Insira o domínio desejado' stepComponent={DomainStep}>
+            <FormRedux
+              nosubmit
+              {...formProps}
+              onSubmit={values => {
+                return save(values)
+                  .then(dns => {
+                    this.setState({ dns })
+                    return Promise.resolve()
+                  })
+              }}
+            >
+              <FormGroup {...domain_name}>
+                <ControlLabel>Domínio da sua comunidade</ControlLabel>
+                <FormControl type='text' placeholder='Ex. minhacomunidade.org' />
+              </FormGroup>
+              <Button disabled={saving} type='submit'>Adicionar</Button>
+            </FormRedux>
+          </Step>
+          <Step title='Altere os servidores do seu provedor DNS' stepComponent={DomainStep}>
+            <p>1. Faça login no seu provedor de DNS (onde seu domínio está registrado, por exemplo GoDaddy, Locaweb, RegistroBR)</p>
+            <p>2. Encontre a página de <b>gerenciador de DNS</b>, e altere os <b>nomes de servidor</b> para os servidores do Bonde:</p>
+            <br />
+            {this.state.dns && this.state.dns.delegation_set_servers.map(server => <p>{server}</p>)}
+            <Button onClick={() => this.setState({ renderTestConnection: true })}>Continuar</Button>
+          </Step>
+          <Step title='Teste a conexão' stepComponent={DomainStep}>
             <div>
               <p>Clique no botão abaixo para verificar se tudo está certo.</p>
               <p>Atenção: a mudança de DNS pode demorar até 48 horas para ser propagada pela internet.</p>
             </div>
-            <Button onClick={() => this.handleTestConnection().bind(this)}></Button>
-          </div>
+            <Button onClick={() => this.handleTestConnection()}>Testar</Button>
+          </Step>
         )}
+        </Steps>
       </div>
     )
   }
