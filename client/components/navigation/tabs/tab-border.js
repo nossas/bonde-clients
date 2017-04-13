f0c68bd05f09b6e0d043694ddfb282f4ff820041
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2017-04-13 00:22:51 -0300

    Add TabBorder style component #520

---
+++ client/components/navigation/tabs/tab-border.js
@@ -0,0 +1,37 @@
+import React, { PropTypes } from 'react'
+import classnames from 'classnames'
+
+if (require('exenv').canUseDOM) {
+  require('./tab-border.scss')
+}
+
+const TabBorder = ({ children, Component, path, className, isActive, style }) => {
+  const optionalProps = {}
+
+  if (path) optionalProps.to = path
+
+  return (
+    <Component
+      {...optionalProps}
+      className={classnames(
+        'tab components--tab-border',
+        { 'is-active': isActive },
+        className
+      )}
+      style={style}
+    >
+      {children}
+    </Component>
+  )
+}
+
+TabBorder.propTypes = {
+  children: PropTypes.node.isRequired,
+  Component: PropTypes.node.isRequired,
+  className: PropTypes.oneOfType([PropTypes.string, PropTypes.array]),
+  path: PropTypes.string,
+  isActive: PropTypes.bool,
+  style: PropTypes.string
+}
+
+export default TabBorder
