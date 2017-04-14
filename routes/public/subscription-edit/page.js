e6743b1da071ae304f9e1414a8adc280f6138f66
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2017-04-14 05:15:30 -0300

    Purify the subscriptions edit page component #520

--- routes/public/subscription-edit/page.js
+++ routes/public/subscription-edit/page.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import classnames from 'classnames'
 import uuid from 'uuid'
 import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup'
@@ -18,17 +18,17 @@ const CreditCardFormImplementation = CreditCardForm({
   }
 })
 
-class SubscriptionEditPage extends Component {
-  displayForm ({ form, modificationType }) {
-    const {
-      animationStack,
-      setModificationType,
-      appendAnimationStack,
-      removeAnimationStack
-    } = this.props
-
+const SubscriptionEditPage = props => {
+  const {
+    modificationType,
+    animationStack,
+    setModificationType,
+    appendAnimationStack,
+    removeAnimationStack
+  } = props
+  const displayForm = (form, type) => {
     const append = () => {
-      setModificationType(modificationType)
+      setModificationType(type)
       appendAnimationStack(form)
     }
 
@@ -38,74 +38,64 @@ class SubscriptionEditPage extends Component {
     } else append()
   }
 
-  render () {
-    const { modificationType, animationStack } = this.props
+  const RecurringForm = props => (
+    <b className='h1 p3 center block'>RecurringForm</b>
+  )
 
-    const RecurringForm = props => (
-      <b className='h1 p3 center block'>RecurringForm</b>
-    )
-
-    return (
-      <div className='routes--subscription-edit-page'>
-        <Background image={
-          require('exenv').canUseDOM
-            ? require('~client/images/bg-login.png')
-            : ''
-        }>
-          <section className='section--choose-type'>
-            <h1 style={{
-              color: '#333',
-              marginTop: 0,
-              fontWeight: 'bold',
-              fontSize: '2em'
-            }}>
-              Dados da Assinatura
-            </h1>
-            <p className='paragraph--helper-text'>
-              Selecione abaixo qual informação da sua assinatura você quer alterar.
-            </p>
-            <div className='container--tab-buttons'>
-              <button
-                className={classnames(
-                  'button--creditcard button',
-                  { active: modificationType === 'creditcard' }
-                )}
-                onClick={() => this.displayForm({
-                  form: CreditCardFormImplementation,
-                  modificationType: 'creditcard'
-                })}
-              >
-                Cartão de crédito
-              </button>
-              <button
-                className={classnames(
-                  'button--recurring button',
-                  { active: modificationType === 'recurring' }
-                )}
-                onClick={() => this.displayForm({
-                  form: RecurringForm,
-                  modificationType: 'recurring'
-                })}
-              >
-                Data da doação
-              </button>
-            </div>
-            <CSSTransitionGroup
-              transitionName={`transition--form-${modificationType}`}
-              transitionEnterTimeout={1000}
-              transitionLeaveTimeout={1000}
+  return (
+    <div className='routes--subscription-edit-page'>
+      <Background image={
+        require('exenv').canUseDOM
+          ? require('~client/images/bg-login.png')
+          : ''
+      }>
+        <section className='section--choose-type'>
+          <h1 style={{
+            color: '#333',
+            marginTop: 0,
+            fontWeight: 'bold',
+            fontSize: '2em'
+          }}>
+            Dados da Assinatura
+          </h1>
+          <p className='paragraph--helper-text'>
+            Selecione abaixo qual informação da sua assinatura você quer alterar.
+          </p>
+          <div className='container--tab-buttons'>
+            <button
+              className={classnames(
+                'button--creditcard button',
+                { active: modificationType === 'creditcard' }
+              )}
+              onClick={() => displayForm(CreditCardFormImplementation, 'creditcard')}
             >
-              {animationStack.map(ItemComponent => (
-                <div key={uuid()} style={{ overflowY: 'hidden' }}>
-                  <ItemComponent {...this.props} FormComponent={FlatForm} />
-                </div>
-              ))}
-            </CSSTransitionGroup>
-          </section>
-        </Background>
-      </div>
-    )
-  }
+              Cartão de crédito
+            </button>
+            <button
+              className={classnames(
+                'button--recurring button',
+                { active: modificationType === 'recurring' }
+              )}
+              onClick={() => displayForm(RecurringForm, 'recurring')}
+            >
+              Data da doação
+            </button>
+          </div>
+          <CSSTransitionGroup
+            transitionName={`transition--form-${modificationType}`}
+            transitionEnterTimeout={1000}
+            transitionLeaveTimeout={1000}
+          >
+            {animationStack.map(ItemComponent => (
+              <div key={uuid()} style={{ overflowY: 'hidden' }}>
+                <ItemComponent {...props} FormComponent={FlatForm} />
+              </div>
+            ))}
+          </CSSTransitionGroup>
+        </section>
+      </Background>
+    </div>
+  )
 }
 
 export default SubscriptionEditPage
