07c60e40f380ebe1398b787657ac01dc06f10b70
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2017-04-20 11:50:59 -0300

    Fix href prop passed to TellAFriend fixes #569
    Even with `og:url` with custom domain forced, the
    TellAFriend component do not receiving the correct prop.

--- client/components/share/tell-a-friend.connected.js
+++ client/components/share/tell-a-friend.connected.js
@@ -1,9 +1,15 @@
 import { connect } from 'react-redux'
 import TellAFriend from './tell-a-friend'
+import MobSelectors from '~client/mobrender/redux/selectors'
 
-const mapStateToProps = ({ sourceRequest: { protocol, host } }) => ({
-  href: `${protocol}://${host}`
-})
+const mapStateToProps = state => {
+  const { sourceRequest: { protocol, host } } = state
+  const { custom_domain: customDomain } = MobSelectors(state).getMobilization()
+
+  return {
+    href: customDomain || `${protocol}://${host}`
+  }
+}
 
 export { default as TellAFriend } from './tell-a-friend'
 export default connect(mapStateToProps)(TellAFriend)
