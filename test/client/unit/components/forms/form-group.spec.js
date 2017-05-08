3672b544ec8c087aaa3cdbcc3288ffbe2065e876
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2017-05-08 16:00:54 -0300

    refactor(components): Forgotten tests prop-types codemods #554

--- test/client/unit/components/forms/form-group.spec.js
+++ test/client/unit/components/forms/form-group.spec.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react'
+import PropTypes from 'prop-types'
+import React, { Component } from 'react'
 import { expect } from 'chai'
 import { mount } from 'enzyme'
 
