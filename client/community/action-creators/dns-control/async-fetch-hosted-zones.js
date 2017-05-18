9f1e10ad02d5fb5a939f24b60b5674f86bad6b95
Igor Santos <igr.exe@gmail.com>
2017-05-18 16:26:08 -0300

    fix(fetch): add credentials for fetch mobilizations and communities #590

--- client/community/action-creators/dns-control/async-fetch-hosted-zones.js
+++ client/community/action-creators/dns-control/async-fetch-hosted-zones.js
@@ -1,12 +1,14 @@
 import * as t from '../../action-types'
 import { createAction } from '../create-action'
 import * as CommunitySelectors from '../../selectors'
+import AuthSelectors from '~client/account/redux/selectors'
 
 export default () => (dispatch, getState, { api }) => {
+  const headers = AuthSelectors(getState()).getCredentials()
   const community = CommunitySelectors.getCurrent(getState())
 
   dispatch(createAction(t.FETCH_DNS_HOSTED_ZONES_REQUEST))
-  return api.get(`/communities/${community.id}/dns_hosted_zones`)
+  return api.get(`/communities/${community.id}/dns_hosted_zones`, { headers })
     .then(res => {
       dispatch(createAction(t.FETCH_DNS_HOSTED_ZONES_SUCCESS, res.data))
     })
