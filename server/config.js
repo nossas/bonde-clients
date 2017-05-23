78c1af3dd12e84d3ac882789f26d1547b9c14d69
Lucas Pirola <lucaspirola@gmail.com>
2017-05-23 10:33:22 -0300

    Add sentry dsn as config

--- server/config.js
+++ server/config.js
@@ -1,4 +1,5 @@
 const config = {
+  sentryDsn: process.env.SENTRY_DSN || 'https://1111:2222@sentry.io/86182',
   nodeEnv: process.env.NODE_ENV,
   webConcurrency: process.env.WEB_CONCURRENCY || 1,
   port: process.env.PORT || 1337,
