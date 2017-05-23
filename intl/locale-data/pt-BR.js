0dc5481bfe77c597ad4cd67d67e879116402f2a6
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2017-05-23 13:01:35 -0300

    chore(intl): create account register page keys #614

--- intl/locale-data/pt-BR.js
+++ intl/locale-data/pt-BR.js
@@ -1,4 +1,5 @@
 export default {
+  // page account login
   'page--account-login.label.email': 'E-mail',
   'page--account-login.label.password': 'Senha',
   'page--account-login.placeholder.email': 'exemplo@email.com',
@@ -6,4 +7,17 @@ export default {
   'page--account-login.signin': 'Entrar',
   'page--account-login.ask-register': 'Ainda não é cadastrado?',
   'page--account-login.cta-signup': 'Clique para criar uma conta.'
+
+  // page account register
+  'page--account-register.title': 'Crie sua conta no Bonde.',
+  'page--account-register.form.name.label': 'Nome',
+  'page--account-register.form.name.placeholder': 'Seu nome',
+  'page--account-register.form.lastname.label': 'Sobrenome',
+  'page--account-register.form.lastname.placeholder': 'Sobrenome',
+  'page--account-register.form.email.label': 'E-mail',
+  'page--account-register.form.email.placeholder': 'exemplo@email.com.br',
+  'page--account-register.form.password.label': 'Senha',
+  'page--account-register.form.password-confirm.label': 'Confirme sua senha',
+  'page--account-register.form.submit-button.default': 'Criar conta',
+  'page--account-register.form.submit-button.saving': 'Salvando...',
 }
