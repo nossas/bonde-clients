6cea421c5943b954e4b49a55d19633306b2035f2
Igor Santos <igr.exe@gmail.com>
2017-05-25 17:53:18 -0300

    chore(graphql): change playground to test query #622

--- routes/admin/not-authenticated/playground-js/page.js
+++ routes/admin/not-authenticated/playground-js/page.js
@@ -1,69 +1,44 @@
 import React, { Component } from 'react'
-import { StepsContainer, StepContent, StepForm, StepButton } from '~client/components/steps'
-import { ControlLabel, FormControl, FormGroup } from '~client/components/forms'
-import { Button } from '~client/ux/components'
+import { gql, graphql } from 'react-apollo'
+
+const ActivistsList = gql`
+  query ActivistsList {
+    allActivists {
+      edges {
+        node {
+          name
+        }
+      }
+    }
+  }
+`
+
+const Styles = { 
+  width: '500px',
+  heigth: '150px',
+  backgroundColor: '#fff',
+  padding: '10px'
+}
 
 class Page extends Component {
 
-  constructor (props) {
-    super(props)
-    this.state = { isConnected: false }
-  }
-
-  handleConnectionTest () {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        this.setState({ isConnected: true })
-        resolve()
-      }, 1000)
-    })
-  }
-
   render () {
-    const { fields: { domain }, ...formProps } = this.props
-
+    const { data: { loading, error, allActivists } } = this.props
+    
     return (
-      <div className='page'>
-        <StepsContainer title='Domínio da comunidade'>
-          <StepContent title='Insira o domínio desejado'>
-            <StepForm
-              {...formProps}
-              onSubmit={values => new Promise((resolve, reject) => {
-                console.log(values)
-                return resolve()
-              })}
-            >
-              <FormGroup {...domain}>
-                <ControlLabel>Domínio da sua comunidade</ControlLabel>
-                <FormControl type='text' placeholder='Ex. minhacomunidade.org' />
-              </FormGroup>
-              <Button type='submit'>Adicionar</Button>
-            </StepForm>
-          </StepContent>
-          <StepContent title='Altere os servidores no seu provedor DNS'>
-            <div>
-              <p>1. Faça login no seu provedor de DNS (onde seu domínio está registrado, por exemplo GoDaddy, Locaweb, RegistroBR)</p>
-              <p>2. Encontre a página de <b>gerenciador de DNS</b>, e altere os <b>nomes de servidor</b> para os servidores do Bonde:</p>
-              <br />
-              <p>ns1.1098.19872.0871.98</p>
-              <p>ns2.1098.19872.0871.98</p>
-              <p>ns3.1098.19872.0871.98</p>
-            </div>
-            <StepButton>Continuar</StepButton>
-          </StepContent>
-          <StepContent title='Teste a conexão'>
-            <div>
-              <p>Clique no botão abaixo para verificar se tudo está certo.</p>
-              <p>Atenção: a mudança de DNS pode demorar até 48 horas para ser propagada pela internet.</p>
-            </div>
-            <StepButton onClick={() => this.handleConnectionTest()}>
-              {!this.state.isConnected ? 'Testar' : 'Pronto'}
-            </StepButton>
-          </StepContent>
-        </StepsContainer>
+      <div style={Styles}>
+        {loading && <p>Carregando ativistas</p>}
+        {error && <p>{error.message}</p>}
+        {allActivists && (
+          <ul>
+            {allActivists.edges.map(a => <li>{a.node.name}</li>)}
+          </ul>
+        )}
       </div>
     )
   }
 }
 
-export default Page
+const PageWithData = graphql(ActivistsList)(Page)
+
+export default PageWithData
