392073f0b53388e2a106530f39a454a33ead499c
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2017-05-30 11:59:49 -0300

    style: fix closing component coding style #604

--- routes/admin/authenticated/sidebar/account-edit/page.js
+++ routes/admin/authenticated/sidebar/account-edit/page.js
@@ -43,7 +43,8 @@ const EditUserPage = ({
             />
           }
           path={Paths.editAccount()}
-          isActive />
+          isActive
+        />
       </Tabs>
     </SettingsPageMenuLayout>
     <SettingsPageContentLayout>
