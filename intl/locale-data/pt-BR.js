127b2ea73ebe49e39b644b66a2857e24ce95d01c
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2017-05-31 22:43:29 -0300

    chore(mobilizations): add domain page helper text #629

--- intl/locale-data/pt-BR.js
+++ intl/locale-data/pt-BR.js
@@ -590,6 +590,11 @@ export default {
   // routepath:
   //   - /mobilizations/:mobilization_id/customDomain
   //   - /mobilizations/:mobilization_id/launch
+  'mobilizations.components--form-domain.helper-text-first-line': 'Estamos quase lá, agora você precisa escolher qual endereço será utilizado.',
+  'mobilizations.components--form-domain.helper-text-second-line': 'Se você já cadastrou um domínio na comunidade, pode selecionar abaixo qual das opções melhor se encaixa!',
+  'mobilizations.components--form-domain.helper-text-third-line': 'Para fazer o cadastro de um novo domínio, {link}.',
+  'mobilizations.components--form-domain.helper-text-third-line.link': 'clique aqui',
+
   'mobilizations.components--form-domain.create-domain-text.first-line': 'Ops, você ainda não tem um domínio configurado na sua comunidade. Se quiser cadastar, {link}.',
   'mobilizations.components--form-domain.create-domain-text.first-line.link': 'clique aqui',
   'mobilizations.components--form-domain.create-domain-text.second-line': 'Senão você pode, abaixo, usar um domínio externo para configurar o endereço da sua mobilização.',
