f9dd06f2b82736c0cc3711f5c4613661836acb55
Igor Santos <igr.exe@gmail.com>
2017-06-05 09:59:23 -0300

    feat(ux): add background fixed to dialog #632

--- client/ux/components/dialog/index.js
+++ client/ux/components/dialog/index.js
@@ -4,13 +4,15 @@ import Button from '../button'
 if (require('exenv').canUseDOM) require('./styles.scss')
 
 const Dialog = ({ children, onConfirm, onCancel }) => (
-  <div className='dialog'>
-    <div className='content--dialog'>
-      {children}
-    </div>
-    <div className='footer--dialog'>
-      <Button onClick={onConfirm}>Confirmar</Button>
-      <Button onClick={onCancel}>Cancelar</Button>
+  <div className='dialog--out'>  
+    <div className='dialog'>
+      <div className='content--dialog'>
+        {children}
+      </div>
+      <div className='footer--dialog'>
+        <Button onClick={onConfirm}>Confirmar</Button>
+        <Button onClick={onCancel}>Cancelar</Button>
+      </div>
     </div>
   </div>
 )
