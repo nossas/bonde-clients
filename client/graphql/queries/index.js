61a9017037a38452e400b7939d1df6dbd9435448
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2017-08-30 00:07:54 -0300

    chore(donation-widget): fetch goal stats via graphql #814

--- client/graphql/queries/index.js
+++ client/graphql/queries/index.js
@@ -1,3 +1,4 @@
 export { default as CountTwilioCallsByWidget } from './count-twilio-calls-by-widget'
+export { default as fetchDonationGoalStats } from './fetch-donation-goal-stats'
 export { default as fetchTwilioConfiguration } from './fetch-twilio-configuration'
 export { default as watchTwilioCallTransitions } from './watch-twilio-call-transitions'
