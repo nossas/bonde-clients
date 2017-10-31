ff47744f75a0d1a87fa3f99e23798383b76cce22
Igor Santos <igr.exe@gmail.com>
2017-10-31 18:20:07 -0200

    feat(activists): add button to resync mailchimp

--- routes/admin/authenticated/sidebar/community-settings/mailchimp/page.connected.js
+++ routes/admin/authenticated/sidebar/community-settings/mailchimp/page.connected.js
@@ -1,7 +1,8 @@
 import { connect } from 'react-redux'
 import { reduxForm } from 'redux-form'
 import { injectIntl } from 'react-intl'
-import * as CommunityActions from '~client/community/action-creators'
+import { notify } from '~client/utils/notifications'
+import { asyncEdit, resyncMailchimp } from '~client/community/action-creators'
 import * as CommunitySelectors from '~client/community/selectors'
 
 import Page from './page'
@@ -10,13 +11,41 @@ const mapStateToProps = state => ({
   initialValues: { ...CommunitySelectors.getCurrent(state) }
 })
 
-const mapDispatchToProps = {
-  submit: CommunityActions.asyncEdit
-}
+const mapDispatchToProps = (dispatch, ownProps) => ({
+  submit: (values) => dispatch(asyncEdit(values)),
+  resyncMailchimp: () => {
+    // dispatch notifications
+    dispatch(resyncMailchimp())
+      .then(({ sync_requested_at }) => {
+        notify({
+          status: 'success',
+          message: {
+            id: 'routes.admin.authenticated.sidebar.community-settings.mailchimp.resync.message',
+            defaultMessage: 'Sincronia com Mailchimp solicitada com sucesso.'
+          }
+        }, dispatch, ownProps)
+      })
+      .catch((err) => {
+        notify({
+          status: 'error',
+          title: 'Oops!',
+          message: {
+            id: 'routes.admin.authenticated.sidebar.community-settings.mailchimp.resync.message',
+            defaultMessage: 'Houve um problema ao tentar sincronizar com mailchimp: {error}',
+            context: {
+              error: err.message || err
+            }
+          }
+        }, dispatch, ownProps)
+      })
+  }
+})
 
-export default connect(mapStateToProps, mapDispatchToProps)(
-  injectIntl(reduxForm({
-    form: 'mailchimpForm',
-    fields: ['id', 'mailchimp_api_key', 'mailchimp_list_id']
-  })(Page))
+export default injectIntl(
+  connect(mapStateToProps, mapDispatchToProps)(
+    reduxForm({
+      form: 'mailchimpForm',
+      fields: ['id', 'mailchimp_api_key', 'mailchimp_list_id']
+    })(Page)
+  )
 )
