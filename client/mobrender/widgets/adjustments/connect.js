bf433e5730cdcff599d54a91e6039b79a7634bfa
Igor Santos <igr.exe@gmail.com>
2017-11-13 10:15:43 -0200

    chore(adjustments): fix adjustments to use in all place like page

--- client/mobrender/widgets/adjustments/connect.js
+++ client/mobrender/widgets/adjustments/connect.js
@@ -20,10 +20,28 @@ const adjustFields = (fields) => {
   return adjusts
 }
 
+const validate = (values) => {
+  const errors = {}
+  if (!values.button_text) {
+    errors.button_text = 'Insira o texto do botão'
+  } else if (values.button_text.length > 50) {
+    errors.button_text = 'O limite de caracteres foi atingido.'
+  }
+  return errors
+}
+
 export const adjustmentsFormExtend = ({ fields, ...config }) => ModelForm({
   form: 'adjustmentsForm',
   fields: adjustFields(fields),
   ...config
 })
 
-export const adjustmentsForm = adjustmentsFormExtend({})
+export const adjustmentsForm = adjustmentsFormExtend({
+  mapInitialValues: (widget) => ({
+    // Fix field to config title on widget
+    call_to_action: widget.settings
+      ? widget.settings.call_to_action ||
+      widget.settings.title_text : ''
+  }),
+  validate
+})
