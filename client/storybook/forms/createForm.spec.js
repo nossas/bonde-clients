dd4d4fc42d2a0c8a48519a7038e0a186bcf159c1
Igor Santos <igr.exe@gmail.com>
2017-11-16 11:40:26 -0200

    chore(forms): add tests for input config on third apps

--- client/storybook/forms/createForm.spec.js
+++ client/storybook/forms/createForm.spec.js
@@ -74,4 +74,51 @@ describe('createForm', () => {
       expect(Form.displayName).to.equal('withForm(CustomForm)')
     })
   })
+
+  describe('third apps', () => {
+    // Mock connect of react-redux for test config
+    let reactReduxConfig
+    const reactRedux = (mapStateToProps, mapActionsToProps) => () => {
+      reactReduxConfig = { mapStateToProps, mapActionsToProps }
+    }
+    let reduxFormConfig // eslint-disable-line
+    const reduxForm = (config) => () => {
+      reduxFormConfig = config
+    }
+    const createForm = connectCreateForm(reactRedux, reduxForm)
+
+    describe('react-redux', () => {
+      it('should receive a mapStateToProps with initialValues mapped', () => {
+        const initialValues = { buttonText: 'Submit!' }
+        createForm({ initialValues })
+        const { mapStateToProps } = reactReduxConfig
+        expect(mapStateToProps(state, ownProps)).to.deep.equal({ initialValues })
+      })
+      it('should receive a mapActionsToProps with submit action mapped', () => {
+        const dispatch = 'dispatch'
+        const submit = (values, dispatch) => [values, dispatch]
+        createForm({ submit })
+        const { mapActionsToProps } = reactReduxConfig
+        const actions = mapActionsToProps(dispatch)
+        expect(actions.submit({ buttonText })).to.deep.equal([{ buttonText }, dispatch])
+      })
+    })
+
+    describe('redux-form', () => {
+      // Config variables
+      const error = { name: 'required' }
+      const name = 'testForm'
+      const fields = ['name', 'email']
+      const validate = (values) => {
+        if (!values.name) {
+          return error
+        }
+      }
+      const config = { name, fields, validate }
+      it('should receive a start config form', () => {
+        createForm(config)
+        expect(reduxFormConfig).to.deep.equal({ form: name, fields, validate })
+      })
+    })
+  })
 })
