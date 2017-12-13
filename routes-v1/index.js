b4d1092a58a32c921fc907cbed4dcd7474f2df1e
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2017-12-13 10:09:29 -0200

    chore: fetch mobilization list

--- routes-v1/index.js
+++ routes-v1/index.js
@@ -7,6 +7,7 @@ import {
   withRouter,
   Switch
 } from 'react-router-dom'
+import { connect } from 'react-redux'
 
 import '~client/styles/main.scss'
 
@@ -31,6 +32,51 @@ const withUser = Component => props => (
   </CurrentUserContainer>
 )
 
+
+import MobSelectors from '~client/mobrender/redux/selectors'
+import * as MobActions from '~client/mobrender/redux/action-creators'
+import * as CommunitySelectors from '~client/community/selectors'
+import DNSControlSelectors from '~client/community/dns-control-selectors'
+import * as DNSControlActions from '~client/community/action-creators/dns-control'
+import SidebarModule from '~client/components/navigation/sidebar'
+import { Loading } from '~client/components/await'
+
+const mapStateToSidebarProps = (state, props) => ({
+  community: CommunitySelectors.getCurrent(state),
+  loading: MobSelectors(state, props).mobilizationsIsLoading(),
+  loaded: MobSelectors(state, props).mobilizationsIsLoaded(),
+  sidebarProps: SidebarModule.getSidebarProps(state, props),
+  dnsControlSelectors: DNSControlSelectors(state)
+})
+const mapDispatchToSidebarProps = { ...MobActions, ...DNSControlActions }
+const withSidebar = Component => connect(mapStateToSidebarProps, mapDispatchToSidebarProps)(
+  class SidebarHOC extends React.Component {
+    componentDidMount () {
+      const promises = []
+      const community = this.props.community
+      const shouldMakeFetch = community && !this.props.loaded && !this.props.loading
+
+      shouldMakeFetch && promises.push(this.props.asyncFetchMobilizations(community.id))
+      !this.props.dnsControlSelectors.dnsHostedZones().isLoaded() && promises.push(
+        this.props.asyncFetchHostedZones()
+      )
+      return Promise.all(promises)
+    }
+
+    render () {
+      return (
+        <div>
+          {this.props.loading ? <Loading /> : (
+            <SidebarModule.Sidebar {...this.props.sidebarProps}>
+              <Component {...this.props} />
+            </SidebarModule.Sidebar>
+          )}
+        </div>
+      )
+    }
+  }
+)
+
 const About = () => (
   <div>
     <h2>About</h2>
@@ -44,7 +90,7 @@ const AuthExample = () => (
         <Route exact path="/login" component={withBackground(AccountLogin)} />
         <PrivateRoute exact path="/" component={withUser(MobilizationsList)} />
         <PrivateRoute exact path="/community" component={withUser(CommunityList)} />
-        <PrivateRoute exact path="/mobilizations" component={withUser(MobilizationsList)} />
+        <PrivateRoute exact path="/mobilizations" component={withSidebar(MobilizationsList)} />
         <Route component={TechnicalIssues} />
       </Switch>
     </div>
--- routes-v1/private-route.js
+++ routes-v1/private-route.js
@@ -3,31 +3,29 @@ import { Route, Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
 import AccountSelectors from '~client/account/redux/selectors'
 
-const PrivateRoute = (ownProps) => {
-  const {
-    component: Component,
-    exact = false,
-    path,
-    authenticated,
-    ...rest
-  } = ownProps
-  console.log('ownProps', ownProps)
-  return (
-    <Route
-      {...rest}
-      render={props => (
-        authenticated ? <Component {...props} /> : (
-          <Redirect
-            to={{
-              pathname: '/login',
-              state: { from: props.location }
-            }}
-          />
-        )
-      )}
-    />
-  )
-}
+const PrivateRoute = ({
+  component: Component,
+  exact = false,
+  path,
+  authenticated,
+  ...rest
+}) => (
+  <Route
+    {...rest}
+    exact={exact}
+    path={path}
+    render={props => (
+      authenticated ? <Component {...props} /> : (
+        <Redirect
+          to={{
+            pathname: '/login',
+            state: { from: props.location }
+          }}
+        />
+      )
+    )}
+  />
+)
 
 const mapStateToProps = state => ({
   authenticated: AccountSelectors(state).getCredentials()
