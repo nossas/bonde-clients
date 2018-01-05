dd5fbeabee550c4276e57e534e75349761c7509c
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2018-01-05 16:13:25 -0200

    chore(routes-v1): save current widget id on widgets route node

--- routes-v1/subroutes/widgets.js
+++ routes-v1/subroutes/widgets.js
@@ -1,11 +1,45 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import { Route } from 'react-router-dom'
+
+// Components
+import { Loading } from '~client/components/await'
+
+// Redux
+import { selectWidget } from '~client/mobrender/redux/action-creators'
+import MobSelectors from '~client/mobrender/redux/selectors'
 
 // Pages
 import WidgetsDonationRoutes from '~root/routes-v1/subroutes/widgets-donation'
 
-export default ({ match: { path } }) => (
-  <React.Fragment>
-    <Route path={`${path}/:widget_id/donation`} component={WidgetsDonationRoutes} />
-  </React.Fragment>
-)
+const stateToProps = state => ({
+  widget: MobSelectors(state).getWidget()
+})
+
+const actionsToProps = {
+  select: selectWidget
+}
+
+export default connect(stateToProps, actionsToProps)(class extends React.Component {
+  componentDidMount () {
+    const {
+      widget,
+      select,
+      match: { params: { widget_id: id } }
+    } = this.props
+
+    if (!widget && id) {
+      return Promise.all([select(id)])
+    }
+  }
+
+  render () {
+    const { match: { path }, widget } = this.props
+
+    return !widget ? <Loading /> : (
+      <React.Fragment>
+        <Route path={`${path}/donation`} component={WidgetsDonationRoutes} />
+      </React.Fragment>
+    )
+  }
+})
