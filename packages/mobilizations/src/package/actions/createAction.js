61a74c861113fb75a40b69b878e53fd07f9287c4
Igor Santos <igr.exe@gmail.com>
2018-01-12 18:05:22 -0200

    chore(redux): add tests to named actions and reducer utils

--- packages/mobilizations/src/package/actions/createAction.js
+++ packages/mobilizations/src/package/actions/createAction.js
@@ -1,6 +1,6 @@
 export const createNamedWrapperAction = (actionFunction, reducerName) => {
   if (typeof actionFunction !== 'function') {
-    throw Error('actionFunction should be a function')
+    throw new Error('actionFunction should be a function')
   }
   const action = actionFunction()
   return (payload) => ({ ...action, name: reducerName, payload })
