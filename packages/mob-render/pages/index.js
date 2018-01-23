38db9f3dc57b78e0a198f3b4c2fbb4e767c3026e
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2018-01-23 14:27:20 -0200

    fix(mob-render): ssr or client strategy to render favicon

--- packages/mob-render/pages/index.js
+++ packages/mob-render/pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Head from 'next/head'
 import withRedux from 'next-redux-wrapper'
+import { canUseDOM } from 'exenv'
 
 // Intl
 import { IntlProvider } from 'react-intl'
@@ -19,6 +20,9 @@ import {
   Reducer as MobilizationRedux
 } from '../webviewer/webviewer'
 import styles from './../webviewer/main.dba55199cd8fb7024923.css'
+
+const defaultFavicon32 = canUseDOM ? require('../images/icon/favicon-32.png') : ''
+const defaultFavicon16 = canUseDOM ? require('../images/icon/favicon-16.png') : ''
 
 class Page extends React.Component {
   static async getInitialProps ({ store }) {
@@ -74,8 +78,8 @@ class Page extends React.Component {
           <meta property='og:title' content={facebookShareTitle} />
           <meta property='og:description' content={facebookShareDescription} />
           <meta property='og:image' content={facebookShareImage} />
-          <link rel='icon' type='image/png' sizes='32x32' href={favicon || require('../images/icon/favicon-32.png')} />
-          <link rel='icon' type='image/png' sizes='16x16' href={favicon || require('../images/icon/favicon-16.png')} />
+          <link rel='icon' type='image/png' sizes='32x32' href={favicon || defaultFavicon32} />
+          <link rel='icon' type='image/png' sizes='16x16' href={favicon || defaultFavicon16} />
           <link
             type='text/css'
             href='https://fonts.googleapis.com/css?family=Abel|Anton|Archivo+Narrow:400,400i,700,700i|Arvo:400,400i,700,700i|Asap:400,400i,700,700i|Baloo+Bhai|Bitter:400,400i,700|Bree+Serif|Cabin:400,400i,700,700i|Catamaran:400,700|Crimson+Text:400,400i,700,700i|Cuprum:400,400i,700,700i|David+Libre:400,700|Dosis:400,700|Droid+Sans:400,700|Exo+2:400,400i,700,700i|Exo:400,400i,700,700i|Fira+Sans:400,400i,700,700i|Fjalla+One|Francois+One|Gidugu|Hind:400,700|Inconsolata:400,700|Indie+Flower|Josefin+Sans:400,400i,700,700i|Karla:400,400i,700,700i|Lalezar|Lato:400,400i,700,700i|Libre+Baskerville:400,400i,700|Lobster|Lora:400,400i,700,700i|Merriweather+Sans:400,400i,700,700i|Montserrat:400,700|Muli:400,400i|Noto+Serif:400,400i,700,700i|Nunito:400,700|Open+Sans+Condensed:300,300i,700|Open+Sans:400,400i,700,700i|Oswald:400,700|Oxygen:400,700|PT+Sans:400,400i,700,700i|PT+Serif:400,400i,700,700i|Pacifico|Playfair+Display:400,400i,700,700i|Poiret+One|Poppins:400,700|Quicksand:400,700|Raleway:400,400i,700,700i|Roboto+Condensed:400,400i,700,700i|Roboto+Mono:400,400i,700,700i|Roboto+Slab:400,700|Roboto:400,400i,700,700i|Ruslan+Display|Signika:400,700|Slabo+27px|Source+Sans+Pro:200,300,400,700|Titillium+Web:400,400i,700,700i|Ubuntu+Condensed|Ubuntu:400,400i,700,700i|Varela+Round|Yanone+Kaffeesatz:400,700'
