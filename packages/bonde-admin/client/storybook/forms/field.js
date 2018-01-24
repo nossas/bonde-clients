27edf606b5e6b3d36da48eef1071888899b124e7
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2018-01-24 18:52:34 -0200

    fix(forms): field component event or value key depth

--- packages/bonde-admin/client/storybook/forms/field.js
+++ packages/bonde-admin/client/storybook/forms/field.js
@@ -4,7 +4,7 @@ import { fromJS } from 'immutable'
 
 const getValue = eventOrValue =>
   typeof eventOrValue === 'object'
-    ? eventOrValue.target.value : eventOrValue
+    ? eventOrValue.value : eventOrValue
 
 class Field extends React.Component {
   /**
