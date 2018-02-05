bcfc1e9220bd616c87315e082a38b2503296864d
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2018-02-05 08:12:42 -0200

    chore(icon): add remaining icons

---
+++ packages/bonde-styleguide/src/Icon/AngleDown.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default ({ color, size }) => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width={size || '11'}
+    height={size || '5'}
+    viewBox='0 0 11 5'
+  >
+    <path
+      fill='none'
+      fillRule='evenodd'
+      stroke={color || '#000'}
+      strokeLinecap='square'
+      d='M5.333 4.01L8.955.247 9.192 0l.475.493-.238.247-3.621 3.763.004.004L5.337 5l-.004-.004L5.33 5l-.475-.493.004-.004L1.237.74 1 .493 1.475 0l.237.247L5.333 4.01z'
+    />
+  </svg>
+)
