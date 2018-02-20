c34355b0481f4d3f8e68743d82b3c910661ec1d0
Igor Santos <igr.exe@gmail.com>
2018-02-20 16:30:43 -0300

    chore(bonde-styleguide): mark important styles on title

--- packages/bonde-styleguide/src/Title.js
+++ packages/bonde-styleguide/src/Title.js
@@ -3,21 +3,21 @@ import styled from 'styled-components'
 import Text from './Text'
 
 const H1 = Text.withComponent('h1').extend`
-  font-size: 44px;
-  font-weight: 900;
-  line-height: 1.14;
+  font-size: 44px !important;
+  font-weight: 900 !important;
+  line-height: 1.14 !important;
 `
 
 const H2 = Text.withComponent('h2').extend`
-  font-size: 32px;
-  font-weight: 900;
-  line-height: 1.12;
+  font-size: 32px !important;
+  font-weight: 900 !important;
+  line-height: 1.12 !important;
 `
 
 const H3 = Text.withComponent('h3').extend`
-  font-size: 21px;
-  font-weight: 800;
-  line-height: 0.95;
+  font-size: 21px !important;
+  font-weight: 800 !important;
+  line-height: 0.95 !important;
 }`
 
 export default { H1, H2, H3 }
