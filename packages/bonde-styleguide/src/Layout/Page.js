d9ca1e7813476c118aa8b8e11ef680e6e9f6df30
Igor Santos <igr.exe@gmail.com>
2018-02-20 15:55:21 -0300

    chore(bonde-styleguide): add Page layout content

--- packages/bonde-styleguide/src/Layout/Page.js
+++ packages/bonde-styleguide/src/Layout/Page.js
@@ -1,17 +1,23 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const Container = styled.div`{
+const PageContainer = styled.div`{
   position: relative;
   padding-top: 34px;
   backgroud: ${props => props.bg || '#fff'};
 }`
 
+const PageContent = styled.div`{
+  position: relative;
+  display: flex;
+  padding-top: 51px;
+}`
+
 export default ({ children, menuComponent: MenuComponent }) => (
-  <Container>
+  <PageContainer>
     {MenuComponent && <MenuComponent />}
-    <div className='ScrollingContent'>
+    <PageContent>
     {children}
-    </div>
-  </Container>
+    </PageContent>
+  </PageContainer>
 )
