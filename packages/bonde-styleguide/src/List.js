de9069dd79dc802188327630f032cd2406e4b8cb
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2018-02-21 14:08:37 -0300

    feat(bonde-styleguide): add List component [#154769299]

---
+++ packages/bonde-styleguide/src/List.js
@@ -0,0 +1,101 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
+//
+// Size and Spacing variables
+//
+const vars = {
+  avatarSize: '40px',
+  padding: '18px',
+  textGap: '23px'
+}
+
+const List = styled.ul`
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  overflow-x: hidden;
+  text-overflow: ellipsis;
+`
+
+const Avatar = styled.div`
+  background-color: #CCCCCC;
+  background-size: cover;
+  background-position: center center;
+  border-radius: 50%;
+  position: absolute;
+  background-image: url(${({ image }) => image || ''});
+  width: ${vars.avatarSize};
+  height: ${vars.avatarSize};
+  left: ${vars.padding};
+  top: ${vars.padding};
+
+  ${({ image, label }) => !image && `
+    &:before {
+      content: '${label.slice(0, 1).toUpperCase()}';
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100%;
+      font-weight: 900;
+      color: #FFFFFF;
+    }
+  `}
+`
+
+const Label = styled.div.attrs({
+  title: props => props.label
+})`
+  margin-left: ${vars.textGap};
+  height: calc(${vars.avatarSize} / 2);
+  font-family: Nunito Sans;
+  line-height: 1.35;
+  font-size: 16px;
+  font-weight: 900;
+  color: #000000;
+
+  & > div {
+    white-space: nowrap;
+    overflow-x: hidden;
+    text-overflow: ellipsis;
+  }
+`
+
+const Description = Label.extend.attrs({
+  title: props => props.description
+})`
+  font-weight: 400;
+  font-size: 13px;
+  display: flex;
+  align-items: flex-end;
+  color: #4a4a4a;
+  line-height: 1;
+`
+
+const Item = ({ className, href, ...props }) => (
+  <li className={className}>
+    <Avatar {...props} />
+    <Label {...props}>
+      <div>{props.label}</div>
+    </Label>
+    <Description {...props}>
+      <div>{props.description}</div>
+    </Description>
+  </li>
+)
+
+List.Item = styled(Item)`
+  position: relative;
+  padding: 18px;
+  padding-left: calc(40px + 18px);
+`
+
+const { string } = PropTypes
+
+List.Item.propTypes = {
+  label: string.isRequired,
+  description: string.isRequired,
+}
+
+export default List
---
+++ packages/bonde-styleguide/stories/List.stories.js
@@ -0,0 +1,149 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
+import Wrapper from './Wrapper'
+import { Preformatted, Hightlight } from './Icon.stories'
+import { Row, Cell } from '../src/Grid'
+import { Title } from '../src'
+import List from '../src/List'
+
+storiesOf('List', module)
+  .addDecorator(story => (
+    <Wrapper position='relative' padding='15px' width='inherit'>
+      {story()}
+    </Wrapper>
+  ))
+  .add('Default', () => (
+    <div>
+      <Title.H1>List</Title.H1>
+      <p>
+        Components that render items in list style. The default style is demonstrated below:
+      </p>
+      <p>
+        <strong>The label and description props are required.</strong>
+      </p>
+
+      <List>
+        <List.Item
+          label='Apoie a Minha Sampa'
+          description='Página de doação da Minha Sampa'
+        />
+        <List.Item
+          label='CPI do Ônibus pra valer'
+          description='Prorrogar a CPI dos ônibus por mais quatro meses'
+        />
+      </List>
+
+      <Preformatted>
+        <Hightlight code={
+`import List from 'bonde-styleguide/List'
+
+<List>
+  <List.Item
+    label='Apoie a Minha Sampa'
+    description='Página de doação da Minha Sampa'
+  />
+  <List.Item
+    label='CPI do Ônibus pra valer'
+    description='Prorrogar a CPI dos ônibus por mais quatro meses'
+  />
+</List>`
+        } />
+      </Preformatted>
+    </div>
+  ))
+  .add('avatar', () => (
+    <div>
+      <Title.H1>List: avatar</Title.H1>
+      <p>
+        Render list item with the avatar image url passed as prop to component.
+      </p>
+
+      <Row>
+        <Cell size={[4, 4, 6, 6, 12, 12]}>
+          <List>
+            <List.Item
+              image='https://s3.amazonaws.com/hub-central/uploads/1466539647_share.001.png'
+              label='Apoie a Minha Sampa'
+              description='Página de doação da Minha Sampa'
+            />
+            <List.Item
+              image='https://s3.amazonaws.com/hub-central/uploads/1510696294_share.png'
+              label='CPI do Ônibus pra valer'
+              description='Prorrogar a CPI dos ônibus por mais quatro meses'
+            />
+          </List>
+        </Cell>
+        <Cell size={[4, 4, 6, 6, 12, 12]}>
+          <List>
+            <List.Item
+              image='https://s3.amazonaws.com/hub-central/uploads/1483477621_sindrome_de_down16.jpg'
+              label='Por escolas inclusivas de verdade no Rio'
+              description={
+                'Pelo direito das 13mil crianças com deficiência  na rede municipal ' +
+                'aos agentes de apoio à educação especial'
+              }
+            />
+            <List.Item
+              image='https://s3.amazonaws.com/hub-central/uploads/1461673081_MapJam_CompFacebook.png'
+              label='MapJam: Mapeando Iniciativas Colaborativas no Rio'
+              description={
+                'Que tal mapear as iniciativas colaborativas e públicas da cidade do Rio ' +
+                'de Janeiro e disponibilizar essas informações para todo mundo? O MapJam é ' +
+                'um evento fomentado pelo Shareable (www.shareable.net) onde várias cidades ' +
+                'so mundo se juntam em um mês e mapeiam os projetos colaborativos da sua ' +
+                'comunidade. No final tudo isso é colocado em um único mapa disponibilizado online.'
+              }
+            />
+          </List>
+        </Cell>
+      </Row>
+
+      <Preformatted>
+        <Hightlight code={
+`import { Row, Cell } from 'bonde-styleguide/Grid'
+import List from 'bonde-styleguide/List'
+
+<Row>
+  <Cell size={[4, 4, 6, 6, 12, 12]}>
+    <List>
+      <List.Item
+        image='https://s3.amazonaws.com/hub-central/uploads/1466539647_share.001.png'
+        label='Apoie a Minha Sampa'
+        description='Página de doação da Minha Sampa'
+      />
+      <List.Item
+        image='https://s3.amazonaws.com/hub-central/uploads/1510696294_share.png'
+        label='CPI do Ônibus pra valer'
+        description='Prorrogar a CPI dos ônibus por mais quatro meses'
+      />
+    </List>
+  </Cell>
+  <Cell size={[4, 4, 6, 6, 12, 12]}>
+    <List>
+      <List.Item
+        image='https://s3.amazonaws.com/hub-central/uploads/1483477621_sindrome_de_down16.jpg'
+        label='Por escolas inclusivas de verdade no Rio'
+        description={
+          'Pelo direito das 13mil crianças com deficiência  na rede municipal ' +
+          'aos agentes de apoio à educação especial'
+        }
+      />
+      <List.Item
+        image='https://s3.amazonaws.com/hub-central/uploads/1461673081_MapJam_CompFacebook.png'
+        label='MapJam: Mapeando Iniciativas Colaborativas no Rio'
+        description={
+          'Que tal mapear as iniciativas colaborativas e públicas da cidade do Rio ' +
+          'de Janeiro e disponibilizar essas informações para todo mundo? O MapJam é ' +
+          'um evento fomentado pelo Shareable (www.shareable.net) onde várias cidades ' +
+          'so mundo se juntam em um mês e mapeiam os projetos colaborativos da sua ' +
+          'comunidade. No final tudo isso é colocado em um único mapa disponibilizado online.'
+        }
+      />
+    </List>
+  </Cell>
+</Row>`
+        } />
+      </Preformatted>
+    </div>
+  ))
