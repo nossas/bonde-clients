163fa4e5d1377e1efb3e55bcd892468221dc927d
Igor Santos <igr.exe@gmail.com>
2018-02-22 16:15:45 -0300

    chore(bonde-styleguide): use addons-knobs in tabs

--- packages/bonde-styleguide/stories/Tabs.stories.js
+++ packages/bonde-styleguide/stories/Tabs.stories.js
@@ -1,23 +1,21 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
+import { withKnobs, text, boolean } from '@storybook/addon-knobs/react'
 import Wrapper from './Wrapper'
 import Tabs, { Tab } from '../src/Tabs'
 
+const withBG = (story) =>(<Wrapper bg='#000'>{story()}</Wrapper>)
+
 storiesOf('Tabs', module)
-  .addDecorator(story =>(
-    <Wrapper bg='#000'>
-      {story()}
-    </Wrapper>
-  ))
+  .addDecorator(withKnobs)
+  .addDecorator(withBG)
   .addWithJSX('default', () => (
+    <Tab active={boolean('Active', false)}>{text('Children', 'Dashboard')}</Tab>
+  ))
+  .addWithJSX('with any tabs', () => (
     <Tabs>
       <Tab>Dashboard</Tab>
       <Tab>Mobilizações</Tab>
-    </Tabs>
-  ))
-  .addWithJSX('with Tab active', () => (
-    <Tabs>
-      <Tab active>Dashboard</Tab>
-      <Tab>Mobilizações</Tab>
+      <Tab>Comunidades</Tab>
     </Tabs>
   ))
