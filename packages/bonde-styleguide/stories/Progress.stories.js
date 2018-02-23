96cef0eec95ccd9575b9bf426119f32a5e1f5814
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2018-02-23 12:49:56 -0300

    chore(bonde-styleguide): Progress story use knobs color
    Pivotal Tracker Story: [#154769312]

--- packages/bonde-styleguide/stories/Progress.stories.js
+++ packages/bonde-styleguide/stories/Progress.stories.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import { storiesOf } from '@storybook/react'
-import { withKnobs, text, number, select } from '@storybook/addon-knobs/react'
+import { withKnobs, number, select, color } from '@storybook/addon-knobs/react'
 
 import Progress, { sizes } from '../src/Progress'
 
@@ -17,9 +17,9 @@ const createKnobsProgress = () => (
   <Progress.Bar
     size={select('size', sizeOptions, 'normal')}
     value={number('value', 10)}
-    trackColor={text('trackColor', '#eeeeee')}
-    thumbColor={text('thumbColor', '#50e3c2')}
-    textColor={text('textColor', '#000000')}
+    trackColor={color('trackColor', '#eeeeee')}
+    thumbColor={color('thumbColor', '#50e3c2')}
+    textColor={color('textColor', '#000000')}
   />
 )
 
