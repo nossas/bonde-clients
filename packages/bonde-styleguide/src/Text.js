687d7d29138daba71422c02a0d94c960600c3b83
Igor Santos <igr.exe@gmail.com>
2018-02-28 22:29:48 -0300

    chore(bonde-styleguide): move text components to same story

--- packages/bonde-styleguide/src/Text.js
+++ packages/bonde-styleguide/src/Text.js
@@ -1,16 +1,44 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import styled from 'styled-components'
+import { px, borderSpacing, borderSpacingPropTypes } from './utils'
 
+/**
+ * The only true paragraph.
+ */
 const Text = styled.p`{
   font-family: 'Nunito Sans', sans-serif;
-  font-size: ${props => props.fontSize ? `${props.fontSize}px` : '18px'}; 
-  font-weight: normal;
-  line-height: 1.39;
-  color: ${props => props.color || '#000'};
-  text-align: ${props => props.align || 'left'};
-  margin: ${props => props.margin}
+  font-size: ${props => px(props.fontSize)}; 
+  font-weight: ${props => props.fontWeight};
+  line-height: ${props => props.lineHeight};
+  color: ${props => props.color};
+  text-align: ${props => props.align};
+  text-transform: ${props => props.uppercase ? 'uppercase': null};
+  ${props => borderSpacing('margin', props.margin)}
 }`
 
 Text.displayName = 'Text'
+
+Text.propTypes = {
+  margin: borderSpacingPropTypes,
+  uppercase: PropTypes.bool,
+  align: PropTypes.oneOf(['left', 'center', 'right']),
+  color: PropTypes.string,
+  lineHeight: PropTypes.number,
+  fontWeight: PropTypes.oneOfType([
+    PropTypes.number,
+    PropTypes.string
+  ]),
+  fontSize: PropTypes.number
+}
+
+Text.defaultProps = {
+  color: '#000',
+  lineHeight: 1.39,
+  fontWeight: 'normal',
+  fontSize: 18,
+  align: 'left',
+  margin: {}
+}
 
 export default Text
--- packages/bonde-styleguide/src/Title.js
+++ packages/bonde-styleguide/src/Title.js
@@ -2,28 +2,37 @@ import React from 'react'
 import styled from 'styled-components'
 import Text from './Text'
 
-const H1 = Text.withComponent('h1').extend`
-  font-size: 44px !important;
-  font-weight: 900 !important;
-  line-height: 1.14 !important;
-`
+const createTitle = (element, { displayName, ...defaultTextOpts}) =>
+  ({ children, ...props }) => {
+    const Title = Text.withComponent(element)
+    Title.displayName = displayName
+  
+    return (
+      <Title {...props} {...defaultTextOpts}>
+        {children}
+      </Title>
+    )
+  }
 
-H1.displayName = 'Title.H1'
+const H1 = createTitle('h1', {
+  displayName: 'Title.H1',
+  fontSize: 44,
+  fontWeight: 900,
+  lineHeight: 1.14
+})
 
-const H2 = Text.withComponent('h2').extend`
-  font-size: 32px !important;
-  font-weight: 900 !important;
-  line-height: 1.12 !important;
-`
+const H2 = createTitle('h2', {
+  displayName: 'Title.H2',
+  fontSize: 32,
+  fontWeight: 900,
+  lineHeight: 1.12
+})
 
-H2.displayName = 'Title.H2'
-
-const H3 = Text.withComponent('h3').extend`
-  font-size: 21px !important;
-  font-weight: 800 !important;
-  line-height: 0.95 !important;
-}`
-
-H3.displayName = 'Title.H3'
+const H3 = createTitle('h3', {
+  displayName: 'Title.H3',
+  fontSize: 21,
+  fontWeight: 800,
+  lineHeight: 0.95
+})
 
 export default { H1, H2, H3 }
