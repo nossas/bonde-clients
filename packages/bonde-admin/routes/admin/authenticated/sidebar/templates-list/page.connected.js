797a76bf24e65db640d53b06983d701d1a80249e
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2018-03-28 02:48:49 -0300

    fix(bonde-admin): fetch templates of current community
    Pivotal Tracker: [#156143488]

--- packages/bonde-admin/routes/admin/authenticated/sidebar/templates-list/page.connected.js
+++ packages/bonde-admin/routes/admin/authenticated/sidebar/templates-list/page.connected.js
@@ -6,6 +6,7 @@ import { injectIntl } from 'react-intl'
 
 import MobSelectors from '~client/mobrender/redux/selectors'
 import { toggleMobilizationMenu } from '~client/mobrender/redux/action-creators'
+import * as CommunitySelectors from '~client/community/selectors'
 import * as TemplateActions from '~client/mobilizations/templates/action-creators'
 import * as TemplateSelectors from '~client/mobilizations/templates/selectors'
 
@@ -14,6 +15,7 @@ import Page from './page'
 const mapStateToProps = state => ({
   loaded: TemplateSelectors.isLoaded(state),
   loading: TemplateSelectors.isLoading(state),
+  community: CommunitySelectors.getCurrent(state),
   menuActiveIndex: MobSelectors(state).getMobilizationMenuActive(),
   mobilizationTemplates: TemplateSelectors.getCustomTemplates(state)
 })
