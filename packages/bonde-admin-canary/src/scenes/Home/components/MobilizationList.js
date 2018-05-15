ccedb996e8daf540425267eb797a07110ba71576
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2018-05-15 14:47:09 -0300

    feat(admin-canary): stylize home page mobilization list component

--- packages/bonde-admin-canary/src/scenes/Home/components/MobilizationList.js
+++ packages/bonde-admin-canary/src/scenes/Home/components/MobilizationList.js
@@ -1,58 +1,101 @@
 import React from 'react'
-import { DataListCard, Image, Text } from 'bonde-styleguide'
+import { DataListCard, Image, Text, Icon } from 'bonde-styleguide'
 
-const communities = [
+const mobilizations = [
   {
-    image: 'https://goo.gl/nBC6NB',
-    text: {
-      title: 'Minha Sampa',
-      description: 'Nós aproximamos os cidadãos das decisões que definem o rumo da cidade de São Paulo.'
-    }
+    image: 'https://goo.gl/JFHz3h',
+    title: 'SParaMulheres',
+    community: 'Minha Sampa',
+    status: 'active',
+    actionsCount: 16320,
   },
   {
-    image: 'https://goo.gl/nAtki5',
-    text: {
-      title: 'Meu Rio',
-      description: 'Nós aproximamos os cidadãos das decisões que definem o rumo da cidade do Rio de Janeiro.'
-    }
+    image: 'https://goo.gl/FnqZWj',
+    title: 'Ilumina SP',
+    community: 'Minha Sampa',
+    status: 'draft',
+    actionsCount: 0,
   },
   {
-    image: 'https://goo.gl/8kqLZf',
-    text: {
-      title: 'CPI dos Ônibus pra valer',
-      description: 'Prorrogar a CPI dos ônibus por mais quatro meses.'
-    }
+    image: 'https://goo.gl/RJZTGF',
+    title: 'Heróis Invisíveis',
+    community: 'Minha Sampa',
+    status: 'active',
+    actionsCount: 974,
+  },
+  {
+    image: 'https://goo.gl/2ygft6',
+    title: 'Queima de Arquivo',
+    community: 'Minha Sampa',
+    status: 'active',
+    actionsCount: 2453,
+  },
+  {
+    image: 'https://goo.gl/3Chgix',
+    title: 'Paulista Aberta',
+    community: 'Minha Sampa',
+    status: 'draft',
+    actionsCount: 0,
   }
 ]
 
 const MobilizationList = ({ t }) => (
   <DataListCard
     sectionTitle={t('my-mobilizations')}
-    border={false}
     fields={{
       image: {
         width: 40,
-        render: (field) => (
-          <Image
-            src={field}
-            width={40}
-            height={40}
-            rounded={40}
-          />
+        render: image => (
+          <Image src={image} width={40} height={40} rounded={40} />
         )
       },
-      text: {
-        render: (field) => {
-          return (
-            <React.Fragment>
-              <Text fontSize={16} fontWeight={900} lineHeight={1.25}>{field.title}</Text>
-              <Text fontSize={13} lineHeight={1.54} color='#4a4a4a'>{field.description}</Text>
-            </React.Fragment>
-          )
-        }
+      title: {
+        render: title => (
+          <Text fontSize={16} fontWeight={900} lineHeight={1.25}>
+            {title}
+          </Text>
+        )
+      },
+      community: {
+        render: community => (
+          <Text fontSize={13} lineHeight={1.54} color='#4a4a4a'>
+            {community}
+          </Text>
+        )
+      },
+      status: {
+        render: status => (
+          <Text
+            fontSize={13}
+            lineHeight={1.54}
+            {...{
+              active: { color: '#50e3c2', fontWeight: 'bold' },
+              draft: { color: '#aaaaaa', fontWeight: 'normal' },
+            }[status]}
+          >
+            {status === 'active' && <Icon name='tick' color='#50e3c2' />}
+            {{
+              active: t('active'),
+              draft: t('draft')
+            }[status]}
+          </Text>
+        )
+      },
+      actionsCount: {
+        render: count => (
+          <Text fontSize={13} lineHeight={1.54} color='#4a4a4a'>
+            {count || '—'}
+          </Text>
+        )
+      },
+      redir: {
+        align: 'right',
+        render: () => (
+          <Icon name='angle-right' />
+        )
       }
     }}
-    items={communities}
+    items={mobilizations}
   />
 )
 
