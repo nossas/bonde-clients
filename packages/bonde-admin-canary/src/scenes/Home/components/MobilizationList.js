03bf5aa2183b06796e32f62347c56e4ebf42377e
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2018-05-15 15:29:12 -0300

    refactor(admin-canary): mobilization status props and label

--- packages/bonde-admin-canary/src/scenes/Home/components/MobilizationList.js
+++ packages/bonde-admin-canary/src/scenes/Home/components/MobilizationList.js
@@ -39,64 +39,71 @@ const mobilizations = [
   }
 ]
 
-const MobilizationList = ({ t }) => (
-  <DataListCard
-    sectionTitle={t('my-mobilizations')}
-    fields={{
-      image: {
-        width: 40,
-        render: image => (
-          <Image src={image} width={40} height={40} rounded={40} />
-        )
-      },
-      title: {
-        render: title => (
-          <Text fontSize={16} fontWeight={900} lineHeight={1.25}>
-            {title}
-          </Text>
-        )
-      },
-      community: {
-        render: community => (
-          <Text fontSize={13} lineHeight={1.54} color='#4a4a4a'>
-            {community}
-          </Text>
-        )
-      },
-      status: {
-        render: status => (
-          <Text
-            fontSize={13}
-            lineHeight={1.54}
-            {...{
-              active: { color: '#50e3c2', fontWeight: 'bold' },
-              draft: { color: '#aaaaaa', fontWeight: 'normal' },
-            }[status]}
-          >
-            {status === 'active' && <Icon name='tick' color='#50e3c2' />}
-            {{
-              active: t('active'),
-              draft: t('draft')
-            }[status]}
-          </Text>
-        )
-      },
-      actionsCount: {
-        render: count => (
-          <Text fontSize={13} lineHeight={1.54} color='#4a4a4a'>
-            {count || '—'}
-          </Text>
-        )
-      },
-      redir: {
-        align: 'right',
-        render: () => (
-          <Icon name='angle-right' />
-        )
-      }
-    }}
-    items={mobilizations}
-  />
-)
+const MobilizationList = ({ t }) => {
+  const statuses = {
+    active: {
+      label: t('active'),
+      props: { color: '#50e3c2', fontWeight: 'bold' },
+    },
+    draft: {
+      label: t('draft'),
+      props: { color: '#aaaaaa', fontWeight: 'normal' },
+    },
+  }
+
+  return (
+    <DataListCard
+      sectionTitle={t('my-mobilizations')}
+      fields={{
+        image: {
+          width: 40,
+          render: image => (
+            <Image src={image} width={40} height={40} rounded={40} />
+          )
+        },
+        title: {
+          render: title => (
+            <Text fontSize={16} fontWeight={900} lineHeight={1.25}>
+              {title}
+            </Text>
+          )
+        },
+        community: {
+          render: community => (
+            <Text fontSize={13} lineHeight={1.54} color='#4a4a4a'>
+              {community}
+            </Text>
+          )
+        },
+        status: {
+          render: status => (
+            <Text
+              fontSize={13}
+              lineHeight={1.54}
+              {...statuses[status].props}
+            >
+              {status === 'active' && <Icon name='tick' color='#50e3c2' margin='0 5px 0 0' />}
+              {statuses[status].label}
+            </Text>
+          )
+        },
+        actionsCount: {
+          render: count => (
+            <Text fontSize={13} lineHeight={1.54} color='#4a4a4a'>
+              {count || '—'}
+            </Text>
+          )
+        },
+        redir: {
+          align: 'right',
+          render: () => (
+            <Icon name='angle-right' />
+          )
+        }
+      }}
+      items={mobilizations}
+    />
+  )
+}
 
 export default MobilizationList
