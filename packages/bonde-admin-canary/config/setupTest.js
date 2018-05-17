04037f7a06c168f629ecd6c8306ff89b096c6ff0
Igor Santos <igr.exe@gmail.com>
2018-05-17 14:07:32 -0300

    fix(bonde-admin-canary): add mock to css

--- packages/bonde-admin-canary/config/setupTest.js
+++ packages/bonde-admin-canary/config/setupTest.js
@@ -51,3 +51,4 @@ global.XMLHttpRequest = XMLHttpRequest;
 import requireHacker from 'require-hacker'
 
 requireHacker.hook('png', () => 'module.exports = ""')
+requireHacker.hook('css', () => 'module.exports = ""')
