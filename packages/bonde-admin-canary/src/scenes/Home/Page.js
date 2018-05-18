07789d3c7388c7de93fb3f663b387170423eaf42
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2018-05-17 21:50:53 -0300

    chore(admin-canary): use PageAdmin component in home scene

--- packages/bonde-admin-canary/src/scenes/Home/Page.js
+++ packages/bonde-admin-canary/src/scenes/Home/Page.js
@@ -1,66 +1,22 @@
 import React from 'react'
 import { translate } from '../../services/i18n'
-import {
-  Header, Page, Footer,
-  Navbar, MainNav, UserNav,
-  Button,
-  Grid, Cell
-} from 'bonde-styleguide'
+import { Grid, Cell } from 'bonde-styleguide'
 
+import { PageAdmin } from '../../components'
 import { CommunityList, MobilizationList, TrendingMobs } from './components'
 
-const Home = ({ t, i18n }) => {
-  // const changeLanguage = (lng) => {
-  //   i18n.changeLanguage(lng)
-  // }
-
-  return (
-    <div>
-      <Header
-        pageTitle={t('home')}
-        navbar={() => (
-          <Navbar>
-            <MainNav />
-            <UserNav />
-          </Navbar>
-        )}
-        actionButtons={[
-          <Button dark onClick={() => alert('Button: onClick')}>
-            {t('create-mobilization')}
-          </Button>,
-          <Button onClick={() => alert('Button: onClick')}>
-            {t('create-community')}
-          </Button>
-        ]}
-      />
-
-      <Page>
+const Home = ({ t, i18n }) => (
+  <PageAdmin title={t('title')}>
+    <Grid>
+      <Cell size={[12, 12, 12]}>
         <Grid>
-          <Cell size={[12, 12, 12]}>
-            <Grid>
-              <Cell size={[4, 4]}>
-                <CommunityList t={t} />
-              </Cell>
-              <Cell size={[8, 8]}>
-                <MobilizationList t={t} />
-              </Cell>
-            </Grid>
-          </Cell>
-          <Cell size={[12, 12, 12]}>
-            <TrendingMobs t={t} />
-          </Cell>
+          <Cell size={[4, 4]}><CommunityList t={t} /></Cell>
+          <Cell size={[8, 8]}><MobilizationList t={t} /></Cell>
         </Grid>
-      </Page>
-
-      <Footer
-        btnHelpLabel={t('help')}
-        btnHelpClick={() => alert('help clicked!')}
-      >
-        <a href='#about' title='Sobre'>{t('about')}</a>
-        <a href='#contact' title='Contato'>{t('contact')}</a>
-      </Footer>
-    </div>
-  )
-}
+      </Cell>
+      <Cell size={[12, 12, 12]}><TrendingMobs t={t} /></Cell>
+    </Grid>
+  </PageAdmin>
+)
 
 export default translate('home')(Home)
