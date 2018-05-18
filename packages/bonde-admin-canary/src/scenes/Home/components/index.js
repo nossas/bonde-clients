9c35207e6a7020e93ceb123bb652167e079b963b
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2018-05-18 07:22:31 -0300

    feat(admin-canary): add home onboarding tooltip component

--- packages/bonde-admin-canary/src/scenes/Home/components/index.js
+++ packages/bonde-admin-canary/src/scenes/Home/components/index.js
@@ -1,3 +1,4 @@
 export { default as CommunityList } from './CommunityList'
 export { default as MobilizationList } from './MobilizationList'
 export { default as TrendingMobs } from './TrendingMobs'
+export { default as OnboardingTooltip } from './OnboardingTooltip'
