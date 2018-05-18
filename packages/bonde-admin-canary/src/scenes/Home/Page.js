53deb0ec1f8c993cb1d1d9ee42e1b156d6ec0a3f
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2018-05-18 07:23:03 -0300

    feat(admin-canary): add onboarding steps to user home page

--- packages/bonde-admin-canary/src/scenes/Home/Page.js
+++ packages/bonde-admin-canary/src/scenes/Home/Page.js
@@ -3,27 +3,58 @@ import { translate } from '../../services/i18n'
 import { Grid, Cell, Backdrop, Loading, Title, Text } from 'bonde-styleguide'
 
 import { PageAdmin } from '../../components'
-import { CommunityList, MobilizationList, TrendingMobs } from './components'
+import { CommunityList, MobilizationList, TrendingMobs, OnboardingTooltip } from './components'
 
 class Home extends React.Component {
   state = {
     loading: true,
     splash: false,
+    step: 0,
   }
 
+  handleStep = step => this.setState({ step })
+  initSplash = cb => setTimeout(() => { this.setState({ loading: false, splash: true }); cb() }, 5000)
+  initOnboarding = () => setTimeout(() => this.setState({ splash: false, step: 1 }), 3000)
+
   componentDidMount () {
-    setTimeout(() => {
-      this.setState({ splash: true, loading: false })
-      setTimeout(() => this.setState({ splash: false }), 3000)
-    }, 5000)
+    this.initSplash(
+      this.initOnboarding
+    )
   }
 
   render () {
     const { t } = this.props
-    const { loading, splash } = this.state
+    const { loading, splash, step } = this.state
 
     return (
-      <PageAdmin title={t('title')}>
+      <PageAdmin
+        Title={({ Default }) => (
+          <OnboardingTooltip
+            title='Gerencie seu BONDE por aqui'
+            subtitle='Aqui em cima você confere em que página está e navega entre as principais sessões das suas comunidades e mobilizações.'
+            steps={5}
+            handleStep={this.handleStep}
+            currentStep={1}
+            position={{ left: '330%', top: '-40px' }}
+            show={step === 1}
+          >
+            <Default>{t('title')}</Default>
+          </OnboardingTooltip>
+        )}
+        ActionButtonsWrapper={({ children }) => (
+          <OnboardingTooltip
+            title='Fique de olho nesse cantinho'
+            subtitle='Seu perfil, suas notificações e as principais ações sempre ficam por aqui.'
+            steps={5}
+            handleStep={this.handleStep}
+            currentStep={2}
+            placement='bottom-right'
+            show={step === 2}
+          >
+            {children}
+          </OnboardingTooltip>
+        )}
+      >
         {loading && (
           <Backdrop color='#FFFFFF'>
             <Text align='center' margin={{ top: '20vh' }}>
@@ -50,13 +81,54 @@ class Home extends React.Component {
         )}
 
         <Grid>
-          <Cell size={[12, 12, 12]}>
+          <Cell size={[12, 12, 12, 12, 12, 12]}>
             <Grid>
-              <Cell size={[4, 4]}><CommunityList t={t} /></Cell>
-              <Cell size={[8, 8]}><MobilizationList t={t} /></Cell>
+              <Cell size={[4, 12, 12, 12, 12, 12]}>
+                <OnboardingTooltip
+                  title='Acesse suas comunidades'
+                  subtitle='A comunidade é um grupo que se une por uma causa. A partir dela você pode criar mobilizações e convidar outras pessoas pra chegar junto.'
+                  steps={5}
+                  handleStep={this.handleStep}
+                  currentStep={3}
+                  position={{ left: '70%', top: '70px' }}
+                  show={step === 3}
+                >
+                  <CommunityList t={t} />
+                </OnboardingTooltip>
+              </Cell>
+              <Cell size={[8, 12, 12, 12, 12, 12]}>
+                <OnboardingTooltip
+                  title='Crie mobilizações pra causar'
+                  subtitle='É através das mobilizações que você vai gerar um impacto. Aqui você pode ver as mobs das suas comunidades e acessá-las com um clique.'
+                  steps={5}
+                  handleStep={this.handleStep}
+                  currentStep={4}
+                  position={{ left: '60%', top: '80px' }}
+                  show={step === 4}
+                >
+                  <MobilizationList t={t} />
+                </OnboardingTooltip>
+              </Cell>
             </Grid>
           </Cell>
-          <Cell size={[12, 12, 12]}><TrendingMobs t={t} /></Cell>
+          <Cell size={[12, 12, 12, 12, 12, 12]}>
+            <TrendingMobs
+              t={t}
+              Tooltip={({ children }) => (
+                <OnboardingTooltip
+                  title='Crie mobilizações pra causar'
+                  subtitle='É através das mobilizações que você vai gerar um impacto. Aqui você pode ver as mobs das suas comunidades e acessá-las com um clique.'
+                  steps={5}
+                  handleStep={this.handleStep}
+                  currentStep={5}
+                  position={{ left: '50%', top: '-30px' }}
+                  show={step === 5}
+                >
+                  {children}
+                </OnboardingTooltip>
+              )}
+            />
+          </Cell>
         </Grid>
       </PageAdmin>
     )
