ccd6f9b30814b6d0d5543e923d7b47faff3dd8e8
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2018-05-18 02:50:54 -0300

    feat(admin-canary): add home loading and splash steps

--- packages/bonde-admin-canary/src/scenes/Home/Page.js
+++ packages/bonde-admin-canary/src/scenes/Home/Page.js
@@ -1,22 +1,66 @@
 import React from 'react'
 import { translate } from '../../services/i18n'
-import { Grid, Cell } from 'bonde-styleguide'
+import { Grid, Cell, Backdrop, Loading, Flexbox2 as Flexbox, Title, Text } from 'bonde-styleguide'
 
 import { PageAdmin } from '../../components'
 import { CommunityList, MobilizationList, TrendingMobs } from './components'
 
-const Home = ({ t, i18n }) => (
-  <PageAdmin title={t('title')}>
-    <Grid>
-      <Cell size={[12, 12, 12]}>
+class Home extends React.Component {
+  state = {
+    loading: true,
+    splash: false,
+  }
+
+  componentDidMount () {
+    setTimeout(() => {
+      this.setState({ splash: true, loading: false })
+      setTimeout(() => this.setState({ splash: false }), 3000)
+    }, 5000)
+  }
+
+  render () {
+    const { t } = this.props
+    const { loading, splash } = this.state
+
+    return (
+      <PageAdmin title={t('title')}>
+        {loading && (
+          <Backdrop color='#FFFFFF'>
+            <Text align='center' margin={{ top: '20vh' }}>
+              <Loading size={109} />
+            </Text>
+            <Title.H3 align='center' lineHeight={1.29}>
+              Preparando<br />
+              o seu BONDE
+            </Title.H3>
+          </Backdrop>
+        )}
+
+        {splash && (
+          <Backdrop color='rgba(255, 255, 255, .7)'>
+            <Text align='center' margin={{ top: '15vh' }}>
+              <Loading size={109} />
+            </Text>
+            <Title.H3 align='center' lineHeight={1.29}>
+              Esse BONDE é<br />
+              todo seu, Maria.<br />
+              Chega mais!
+            </Title.H3>
+          </Backdrop>
+        )}
+
         <Grid>
-          <Cell size={[4, 4]}><CommunityList t={t} /></Cell>
-          <Cell size={[8, 8]}><MobilizationList t={t} /></Cell>
+          <Cell size={[12, 12, 12]}>
+            <Grid>
+              <Cell size={[4, 4]}><CommunityList t={t} /></Cell>
+              <Cell size={[8, 8]}><MobilizationList t={t} /></Cell>
+            </Grid>
+          </Cell>
+          <Cell size={[12, 12, 12]}><TrendingMobs t={t} /></Cell>
         </Grid>
-      </Cell>
-      <Cell size={[12, 12, 12]}><TrendingMobs t={t} /></Cell>
-    </Grid>
-  </PageAdmin>
-)
+      </PageAdmin>
+    )
+  }
+}
 
 export default translate('home')(Home)
