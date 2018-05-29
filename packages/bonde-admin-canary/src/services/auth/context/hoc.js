434dda9eedda786f82d91675b2b171e9e2fdb17b
Igor Santos <igr.exe@gmail.com>
2018-05-28 23:54:45 -0300

    feat(admin-canary): add logout in auth hoc

--- packages/bonde-admin-canary/src/services/auth/context/hoc.js
+++ packages/bonde-admin-canary/src/services/auth/context/hoc.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import { connect } from '../../redux'
+import AuthAPI from '../api'
 import { AuthContext } from './Context'
 
-const auth = () => Component => (props) => (
-  <AuthContext.Consumer>
-    {user => (<Component user={user} {...props} />)}
-  </AuthContext.Consumer>
-)
+const auth = () => Component => (props) => {
+  
+  const ComponentRedux = connect(undefined, { logout: AuthAPI.logout })(Component)
 
+  return (
+    <AuthContext.Consumer>
+      {user => (<ComponentRedux user={user} {...props} />)}
+    </AuthContext.Consumer>
+  )
+}
 export default auth
