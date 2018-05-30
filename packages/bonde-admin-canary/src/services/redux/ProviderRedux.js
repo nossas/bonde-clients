75aaac1e0ae2153800f27f4cb8a9d7059e9838f5
Igor Santos <igr.exe@gmail.com>
2018-05-30 10:22:08 -0300

    chore(admin-canary): fix import root modules in graphql and redux services

--- packages/bonde-admin-canary/src/services/redux/ProviderRedux.js
+++ packages/bonde-admin-canary/src/services/redux/ProviderRedux.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Provider } from 'react-redux'
-import { db } from '../session'
+import { db } from 'services/session'
 import configureStore from './configureStore'
 
 const initialState = {
