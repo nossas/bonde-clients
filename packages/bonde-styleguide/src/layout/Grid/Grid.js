06c0ac0f21664fd9e2d95bd3c0ad75ea4cc554a8
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2018-06-08 14:38:18 -0300

    fix(styleguide): grid component with full width

--- packages/bonde-styleguide/src/layout/Grid/Grid.js
+++ packages/bonde-styleguide/src/layout/Grid/Grid.js
@@ -4,6 +4,7 @@ import styled from 'styled-components'
  * Gris system that wraps multiple `Cell` components.
  */
 const Grid = styled.div`
+  width: 100%;
   display: grid;
   grid-template-columns: repeat(12, minmax(0, 1fr));
   grid-gap: 20px;
