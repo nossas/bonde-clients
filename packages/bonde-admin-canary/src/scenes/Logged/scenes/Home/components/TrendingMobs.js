80378b12008c32a955b4410e44e4b49979822381
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2018-06-08 10:09:36 -0300

    feat(admin-canary): connect trending mobs with graphql mutation

--- packages/bonde-admin-canary/src/scenes/Logged/scenes/Home/components/TrendingMobs.js
+++ packages/bonde-admin-canary/src/scenes/Logged/scenes/Home/components/TrendingMobs.js
@@ -1,44 +1,83 @@
 import React from 'react'
-import { Gadget, Grid, Cell, Panel } from 'bonde-styleguide'
+import gql from 'graphql-tag'
+import { Mutation } from 'react-apollo'
+import { Gadget, Grid, Cell, Panel, Flexbox, Dropdown, DropdownItem } from 'bonde-styleguide'
+import { mutation } from 'graphql'
+import { Mobilizations } from 'graphql/mutations'
 
-const TrendingMobs = ({ t }) => (
-  <Gadget title='Causando por aí'>
-    <Grid style={{ marginTop: '32px' }}>
-      <Cell size={[3, 3]}>
-        <Panel
-          image='https://goo.gl/hggWmp'
-          title='Cinzas dos Muros'
-          description='Nossos muros têm voz, têm vida.'
-          author='Por Minha Sampa'
-        />
-      </Cell>
-      <Cell size={[3, 3]}>
-        <Panel
-          image='https://goo.gl/rga5rh'
-          title='Temer Jamais'
-          description='Bora encher a caixa de email do Temer!'
-          author='Por Meu Rio'
-          notitle
-        />
-      </Cell>
-      <Cell size={[3, 3]}>
-        <Panel
-          image='https://goo.gl/8pydwk'
-          title='Mulheres Inspiradoras'
-          description='Algumas histórias precisam ser contadas.'
-          author='Por Coletivo Feminista'
-        />
-      </Cell>
-      <Cell size={[3, 3]}>
-        <Panel
-          image='https://goo.gl/jncGPx'
-          title='Vegan Já!'
-          description='Todos os seres merecem o seu respeito.'
-          author='Por Go Vegan'
-        />
-      </Cell>
-    </Grid>
-  </Gadget>
-)
+const FILTERS = {
+  now: {
+    label: 'Para agir agora',
+    period: 2
+  },
+  inspire: {
+    label: 'Para se inspirar',
+    period: 90
+  },
+}
 
-export default TrendingMobs
+class TrendingMobs extends React.Component {
+  state = { filter: 'now' }
+
+  handleFilter = filter => {
+    this.setState({ filter })
+    this.props.mutate({ variables: { days: FILTERS[filter].period } })
+  }
+
+  componentWillMount (old, next) {
+    this.props.mutate({ variables: { days: FILTERS.now.period } })
+  }
+
+  render () {
+    const { t, mutate, loading, mobilizations } = this.props
+    const { filter } = this.state
+
+    return (
+      <Gadget title={[(
+        <Flexbox horizontal style={{ marginTop: 32 }}>
+          Causando por aí
+          <div>
+            Mobilizações
+            <Dropdown
+              light
+              label={FILTERS[filter].label}
+              style={{ display: 'inline-block', marginLeft: 15 }}
+            >
+              <DropdownItem onClick={() => this.handleFilter('now')}>
+                {FILTERS.now.label}
+              </DropdownItem>
+              <DropdownItem onClick={() => this.handleFilter('inspire')}>
+                {FILTERS.inspire.label}
+              </DropdownItem>
+            </Dropdown>
+          </div>
+        </Flexbox>
+      )]}>
+        <Grid>
+          {loading
+            ? Array(4).fill('').map(() => (
+              <Cell size={[3, 3]}>
+                <Panel loading image='' title='' description='' author='' />
+              </Cell>
+            ))
+            : mobilizations.slice(0, 4).map(mobilization => (
+              <Cell size={[3, 3]}>
+                <Panel
+                  image={mobilization.facebook_share_image}
+                  title={mobilization.name}
+                  description={mobilization.goal}
+                  author={`Por ${mobilization.community_name}`}
+                />
+              </Cell>
+            )
+          )}
+        </Grid>
+      </Gadget>
+    )
+  }
+}
+
+export default mutation({
+  mutation: Mobilizations.mutation,
+  props: Mobilizations.props
+})(TrendingMobs)
