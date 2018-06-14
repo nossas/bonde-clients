c95bafe946cf0b24b651ff02151c3e6fb6b14a55
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2018-06-14 15:50:52 -0300

    chore(admin-canary): remove test page

--- packages/bonde-admin-canary/src/scenes/Logged/Root.js
+++ packages/bonde-admin-canary/src/scenes/Logged/Root.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { AuthProvider, Route } from 'services/auth'
 import { FullScreenLoadable, LoadingFullScreen } from 'components/Loadable'
 import { Page as TagsPage } from './scenes/Tags'
-import ScenesTest from './scenes/Test'
 
 const HomePage = FullScreenLoadable({
   loader: () => import('./scenes/Home')
@@ -19,11 +18,6 @@ const Root = ({ match }) => (
     <Route
       path={`${match.url}/tags`}
       component={TagsPage}
-    />
-
-    <Route
-      path={`${match.url}/test`}
-      component={ScenesTest}
     />
   </AuthProvider>
 )
