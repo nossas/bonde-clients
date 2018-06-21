dbf7d96dedcd060baf07c76a03558e81f3aa7dfc
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2018-06-21 09:47:36 -0300

    feat(admin-canary): add SelectDropdown component

---
+++ packages/bonde-admin-canary/src/components/SelectDropdown/index.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Dropdown, DropdownItem } from 'bonde-styleguide'
+
+class SelectDropdown extends React.Component {
+  constructor (...args) {
+    super(...args)
+
+    this.state = {
+      currentOption: this.props.initialOption || this.props.options[0]
+    }
+  }
+
+  onChange = currentOption => {
+    if (currentOption.value !== this.state.currentOption.value) {
+      this.setState({ ...this.state, currentOption })
+      this.props.onChange(currentOption)
+    }
+  }
+
+  render () {
+    const { options, onChange } = this.props
+    const { currentOption } = this.state
+
+    return (
+      <Dropdown label={currentOption.label}>
+        {options.map(option => (
+          <React.Fragment key={Math.random()}>
+            <DropdownItem onClick={() => this.onChange(option)}>
+              {option.label}
+            </DropdownItem>
+          </React.Fragment>
+        ))}
+      </Dropdown>
+    )
+  }
+}
+
+const { oneOfType, arrayOf, shape, string, number, node, func } = PropTypes
+
+const optionShape = shape({
+  label: oneOfType([node, func, string]),
+  value: oneOfType([string, number])
+})
+
+SelectDropdown.propTypes = {
+  options: arrayOf(optionShape).isRequired,
+
+  onChange: func,
+  initialOption: optionShape,
+}
+
+SelectDropdown.defaultProps = {
+  onChange: f => f
+}
+
+export default SelectDropdown
--- packages/bonde-admin-canary/src/components/index.js
+++ packages/bonde-admin-canary/src/components/index.js
@@ -1,4 +1,5 @@
 export { default as Gadget } from './Gadget'
 export { default as Link } from './Link'
 export { default as NotFound } from './NotFound'
+export { default as SelectDropdown } from './SelectDropdown'
 export { default as Tutorial } from './Tutorial'
