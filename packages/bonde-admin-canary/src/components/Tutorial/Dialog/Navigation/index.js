b23632f5ed26adc31af67a34ab3bcd1372f325d1
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2018-06-21 16:25:57 -0300

    chore(admin-canary): repass tutorial navigation props

--- packages/bonde-admin-canary/src/components/Tutorial/Dialog/Navigation/index.js
+++ packages/bonde-admin-canary/src/components/Tutorial/Dialog/Navigation/index.js
@@ -2,10 +2,10 @@ import React from 'react'
 import { I18n } from 'react-i18next'
 import Navigation from './Navigation'
 
-export default () => (
+export default props => (
   <I18n ns='home'>
     {t => (
-      <Navigation t={t} />
+      <Navigation t={t} {...props} />
     )}
   </I18n>
 )
