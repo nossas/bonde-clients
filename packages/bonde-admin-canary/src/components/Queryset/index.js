98832230de6fd6d5009c87e1f2a4ac1e4b9ed9be
Igor Santos <igr.exe@gmail.com>
2018-06-22 01:49:08 -0300

    feat(admin-canary): add pagination behavior to Queryset

--- packages/bonde-admin-canary/src/components/Queryset/index.js
+++ packages/bonde-admin-canary/src/components/Queryset/index.js
@@ -12,7 +12,7 @@ class Queryset extends React.Component {
   state = {
     offset: 0,
     limit: this.props.limit,
-    page: 1,
+    pageIndex: 0,
     filter: this.props.filter
   }
 
@@ -21,6 +21,23 @@ class Queryset extends React.Component {
     this.setState({ offset, page: 1, filter })
 
     refetch({ ...filter })
+  }
+
+  handleChangePage (page, fetchMore) {
+    const pageIndex = page - 1
+    const offset = pageIndex * this.state.limit
+    this.setState({ pageIndex, offset })
+
+    fetchMore({
+      variables: { offset },
+      updateQuery: (prev, { fetchMoreResult }) => {
+        if (!fetchMoreResult) return prev
+        
+        return Object.assign({}, prev, {
+          data: fetchMoreResult.data
+        })
+      }
+    })
   }
 
   render () {
@@ -33,12 +50,14 @@ class Queryset extends React.Component {
           ...this.state.filter
         }}
       >
-        {({ loading, data, refetch }) => {
+        {({ loading, data, refetch, fetchMore }) => {
           return this.props.children({
             data,
             loading,
             filter: this.state.filter,
-            onChangeFilter: f => this.handleChangeFilter(f, refetch)
+            onChangeFilter: f => this.handleChangeFilter(f, refetch),
+            page: this.state.pageIndex + 1,
+            onChangePage: p => this.handleChangePage(p, fetchMore)
           })
         }}
       </Query>
