2e6ae3e8f448617393397905c4d81a373a9c28d1
Igor Santos <igr.exe@gmail.com>
2018-07-09 16:51:08 -0300

    chore(admin-canary): mount page to recovery password by email

--- packages/bonde-admin-canary/src/scenes/Auth/scenes/ForgetPassword/index.js
+++ packages/bonde-admin-canary/src/scenes/Auth/scenes/ForgetPassword/index.js
@@ -1,5 +1,43 @@
 import React from 'react'
+import { I18n } from 'react-i18next'
+import {
+  Button,
+  Flexbox,
+  FormField,
+  Input,
+  Title,
+  Text
+} from 'bonde-styleguide'
+import { Form, Field } from 'components/Form'
+import { ButtonLink } from 'components/Link'
 
 export default () => (
-  <h2>Forget Password</h2>
+  <I18n ns='auth'>
+  {(t) => (
+    <Flexbox>
+      <Title.H2>{t('forgetPassword.title')}</Title.H2>
+      <Text>{t('forgetPassword.description')}</Text>
+      <Form onSubmit={v => console.log('[onSubmit]', v)}>
+        <Field
+          label={t('forgetPassword.email.label')}
+          name='email'
+          placeholder={t('forgetPassword.email.placeholder')}
+          component={FormField}
+          inputComponent={Input}
+        />
+        <Flexbox horizontal>
+          <ButtonLink
+            to='/auth/login'
+            title={t('forgetPassword.goback')}
+          >
+            {t('forgetPassword.goback')}
+          </ButtonLink>
+          <Button type='submit'>
+            {t('forgetPassword.submit')}
+          </Button>
+        </Flexbox>
+      </Form>
+    </Flexbox>
+  )}
+  </I18n>
 )
