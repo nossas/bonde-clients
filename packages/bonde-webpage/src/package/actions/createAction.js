3c094e43c13e5917e218e64ef8a0123351c14a63
Lucas Pirola <lucaspirola@gmail.com>
2018-11-05 19:30:05 -0200

    chore(depreacted): Remove unused package

--- packages/bonde-webpage/src/package/actions/createAction.js
+++
@@ -1,7 +0,0 @@
-export const createNamedWrapperAction = (actionFunction, reducerName) => {
-  if (typeof actionFunction !== 'function') {
-    throw new Error('actionFunction should be a function')
-  }
-  const action = actionFunction()
-  return (payload) => ({ ...action, name: reducerName, payload })
-}
--- packages/bonde-webpage/src/package/actions/index.js
+++
@@ -1,6 +0,0 @@
-import { Types } from '../rootReducer'
-
-export const selectPage = (meta, blocks, widgets) => ({
-  type: Types.SELECT_PAGE,
-  payload: { meta, blocks, widgets }
-})
