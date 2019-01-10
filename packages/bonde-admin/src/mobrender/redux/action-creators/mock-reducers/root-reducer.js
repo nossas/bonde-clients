b14f48ce7dec5bf6dd5726dc32113698f7aa354e
Lucas Pirola <lucaspirola@gmail.com>
2019-01-10 16:36:36 -0200

    chore(cleanup): Remove unused files

--- packages/bonde-admin/src/mobrender/redux/action-creators/mock-reducers/root-reducer.js
+++ packages/bonde-admin/src/mobrender/redux/action-creators/mock-reducers/root-reducer.js
@@ -1,7 +1,7 @@
 import authReducer from '@/account/redux/reducers'
-import { reducer as mobilizations } from '@/mobrender/redux'
+import reducer from '@/mobrender/redux'
 
 export default {
   auth: authReducer,
-  mobilizations
+  mobilizations: reducer
 }
--- packages/bonde-admin/styleguide.config.js
+++ packages/bonde-admin/styleguide.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   webpackConfig: require('./node_modules/react-scripts/config/webpack.config.js'),
   components: [
-    'src/components/**/*.{js,jsx,ts,tsx}',
     'src/1corelib/**/*.{js,jsx,ts,tsx}'
   ],
 };
