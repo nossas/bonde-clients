1131258d98c0a291ec66aa89533bf6f5ce5aa1d7
Igor Santos <igr.exe@gmail.com>
2019-01-17 12:31:44 -0200

    chore(corelib): fix warnings pressure plugin

--- packages/bonde-admin/src/1corelib/plugins/pressure/utils.js
+++ packages/bonde-admin/src/1corelib/plugins/pressure/utils.js
@@ -53,7 +53,7 @@ const getType = (targets = []) => {
 export const pressureUtils = { getType, PRESSURE_TYPE_EMAIL, PRESSURE_TYPE_PHONE }
 
 // # validateUtils
-
+// eslint-disable-next-line
 const regexEmail = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 const isValidEmail = email => regexEmail.test(email)
 
