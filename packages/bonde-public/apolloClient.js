35afe5407dd58e2c75b00e15ca55a5d62deecb92
Igor Santos <igr.exe@gmail.com>
2019-01-30 15:14:26 -0200

    chore(public): change default graphql url

--- packages/bonde-public/apolloClient.js
+++ packages/bonde-public/apolloClient.js
@@ -1,8 +1,9 @@
+import 'isomorphic-fetch'
 import { ApolloClient, createNetworkInterface } from 'react-apollo'
 
 const GRAPHQL_URL = process.env.REACT_APP_DOMAIN_API_GRAPHQL !== undefined
   ? process.env.REACT_APP_DOMAIN_API_GRAPHQL
-  : 'http://api-v2.bonde.devel'
+  : 'http://api-v2.bonde.devel/graphql'
 
 const networkInterface = createNetworkInterface({
   uri: GRAPHQL_URL,
