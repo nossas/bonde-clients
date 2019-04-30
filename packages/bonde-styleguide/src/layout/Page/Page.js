b4af1d9ddf8be9663d7d9acd39c6bcf2bfbf2b44
Igor Santos <igr.exe@gmail.com>
2019-04-30 11:07:42 -0300

    feat(styleguide): add props to resize page

--- packages/bonde-styleguide/src/layout/Page/Page.js
+++ packages/bonde-styleguide/src/layout/Page/Page.js
@@ -1,20 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
+import { px } from '../../utils'
 
 const PageContainer = styled.div`{
   padding-top: ${props => props.top ? `calc(${props.top}px + 32px)` : '32px'};
   padding-bottom: 32px;
   width: 80%;
   margin: 0 auto;
+  ${props => props.height && `height: ${px(props.height)};`}
 `
 
 const PageContent = styled.div`{
   display: flex;
 }`
 
-const Page = ({ children, menuComponent: MenuComponent, top }) => (
-  <PageContainer top={top}>
+const Page = ({ children, menuComponent: MenuComponent, top, height }) => (
+  <PageContainer top={top} height={height}>
     {MenuComponent && <MenuComponent />}
     <PageContent>
     {children}
