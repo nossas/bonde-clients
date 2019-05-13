8020b75aa1c34953f39d4a44d6036f4db2b4ebf7
Gabriel Rocha de Oliveira <rolivegab@gmail.com>
2019-05-13 15:45:04 -0300

    feat(webpage): refactor whatsapp-share-button test

--- packages/bonde-webpage/src/lib/ux/tell-a-friend-base/whatsapp-share-button.spec.js
+++ packages/bonde-webpage/src/lib/ux/tell-a-friend-base/whatsapp-share-button.spec.js
@@ -1,35 +1,32 @@
-import React from 'react'
-import { expect } from 'chai'
+import * as React from 'react'
+import test from 'ava'
 import { shallow } from 'enzyme'
 
 import WhatsAppShareButton from './whatsapp-share-button'
 
-describe('client/components/share/whatsapp-share-button', () => {
-  let wrapper
-  const props = {
-    href: 'http://www.minhasampa.org.br',
-    whatsappText: 'Foo Bar WhatsApp Text'
-  }
+let wrapper
+const props = {
+  href: 'http://www.minhasampa.org.br',
+  whatsappText: 'Foo Bar WhatsApp Text'
+}
 
-  describe('#render', () => {
-    beforeAll(() => {
-      wrapper = shallow(<WhatsAppShareButton {...props} />)
-    })
+test.beforeEach(() => {
+  wrapper = shallow(<WhatsAppShareButton {...props} />)
+})
 
-    it('should render an <a /> tag element', () => {
-      expect(wrapper.find('a')).to.have.length(1)
-    })
-    it('should render an <a /> tag with its href properly', () => {
-      const text = encodeURIComponent(props.whatsappText)
-      expect(wrapper.find('a').props().href).to.be.equal(`whatsapp://send?text=${text}`)
-    })
-    it('should render className hide when desktop version', () => {
-      expect(wrapper.find('a').props().className).to.contains('lg-hide')
-    })
-    it('should render without className hide when preview is true', () => {
-      wrapper.setProps({ preview: true })
-      expect(wrapper.find('a').props().className).to.not.contains('lg-hide')
-      wrapper.setProps(props)
-    })
-  })
+test('should render an <a /> tag element', t => {
+  t.is(wrapper.find('a').length, 1)
+})
+
+test('should render an <a /> tag with its href properly', t => {
+  const text = encodeURIComponent(props.whatsappText)
+  t.is(wrapper.find('a').props().href, `whatsapp://send?text=${text}`)
+})
+test('should render className hide when desktop version', t => {
+  t.true(wrapper.find('a').props().className.includes('lg-hide'))
+})
+test('should render without className hide when preview is true', t => {
+  wrapper.setProps({ preview: true })
+  t.false(wrapper.find('a').props().className.includes('lg-hide'))
+  wrapper.setProps(props)
 })
