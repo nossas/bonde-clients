c046ea68103c99a24cf12bfd95bba0855fce45f3
Gabriel Rocha de Oliveira <rolivegab@gmail.com>
2019-05-13 13:57:05 -0300

    feat(webpage): refactor blocks test

--- packages/bonde-webpage/src/lib/redux/reducers/blocks.spec.js
+++ packages/bonde-webpage/src/lib/redux/reducers/blocks.spec.js
@@ -1,220 +1,238 @@
-// import { expect } from 'chai'
-// import * as t from '../action-types'
-// import reducer, { initialState } from './blocks'
+import test from 'ava'
+import * as at from '../action-types'
+import reducer, { initialState } from './blocks'
 
-// describe('client/mobrender/redux/reducers/blocks', () => {
-//   describe('doing fetch', () => {
-//     it('request', () => {
-//       const action = { type: t.FETCH_BLOCKS_REQUEST }
-//       const nextState = reducer(initialState, action)
-//       expect(nextState).to.deep.equal({
-//         ...initialState,
-//         fetching: true
-//       })
-//     })
+test('#FETCH_BLOCKS_REQUEST', t => {
+    const action = { type: at.FETCH_BLOCKS_REQUEST }
+    const nextState = reducer(initialState, action)
+    t.deepEqual(nextState, {
+        ...initialState,
+        fetching: true
+    })
+})
 
-//     it('success', () => {
-//       const requestState = {...initialState,
-//         fetching: true
-//       }
-//       const data = [
-//         { id: 1 },
-//         { id: 2 }
-//       ]
-//       const action = { type: t.FETCH_BLOCKS_SUCCESS, payload: data }
-//       const nextState = reducer(requestState, action)
-//       expect(nextState).to.deep.equal({...requestState,
-//         isLoaded: true,
-//         fetching: false,
-//         data
-//       })
-//     })
+test('#FETCH_BLOCKS_SUCCESS', t => {
+    const requestState = {
+        ...initialState,
+        fetching: true
+    }
+    const data = [
+        { id: 1 },
+        { id: 2 }
+    ]
+    const action = { type: at.FETCH_BLOCKS_SUCCESS, payload: data }
+    const nextState = reducer(requestState, action)
+    t.deepEqual(nextState, {
+        ...requestState,
+        isLoaded: true,
+        fetching: false,
+        data
+    })
+})
 
-//     it('failure', () => {
-//       const requestState = {...initialState,
-//         fetching: true
-//       }
-//       const error = '500 Internal Server Error'
-//       const action = { type: t.FETCH_BLOCKS_FAILURE, payload: error }
-//       const nextState = reducer(requestState, action)
-//       expect(nextState).to.deep.equal({...requestState,
-//         isLoaded: true,
-//         fetching: false,
-//         error
-//       })
-//     })
-//   })
+test('#FETCH_BLOCKS_FAILURE', t => {
+    const requestState = {
+        ...initialState,
+        fetching: true
+    }
+    const error = '500 Internal Server Error'
+    const action = { type: at.FETCH_BLOCKS_FAILURE, payload: error }
+    const nextState = reducer(requestState, action)
+    t.deepEqual(nextState, {
+        ...requestState,
+        isLoaded: true,
+        fetching: false,
+        error
+    })
+});
 
-//   describe('doing update', () => {
-//     const fetchState = {...initialState,
-//       isLoaded: true,
-//       data: [
-//         { id: 1, name: 'Lorem' },
-//         { id: 2, name: 'Ipsum' }
-//       ]
-//     }
+(() => {
+    const fetchState = {
+        ...initialState,
+        isLoaded: true,
+        data: [
+            { id: 1, name: 'Lorem' },
+            { id: 2, name: 'Ipsum' }
+        ]
+    }
 
-//     const fetchRequestState = {...fetchState,
-//       saving: true
-//     }
+    const fetchRequestState = {
+        ...fetchState,
+        saving: true
+    }
 
-//     it('request', () => {
-//       const action = { type: t.UPDATE_BLOCK_REQUEST }
-//       const nextState = reducer(fetchState, action)
-//       expect(nextState).to.deep.equal(fetchRequestState)
-//     })
+    test('#UPDATE_BLOCK_REQUEST', t => {
+        const action = { type: at.UPDATE_BLOCK_REQUEST }
+        const nextState = reducer(fetchState, action)
+        t.deepEqual(nextState, fetchRequestState)
+    })
 
-//     it('success', () => {
-//       const block = { id: 1, name: 'Dolor' }
-//       const action = { type: t.UPDATE_BLOCK_SUCCESS, payload: block }
-//       const nextState = reducer(fetchRequestState, action)
-//       expect(nextState).to.deep.equal({...fetchRequestState,
-//         saving: false,
-//         data: fetchRequestState.data.map(b => {
-//           if (b.id === block.id) return block
-//           else return b
-//         })
-//       })
-//     })
+    test('#UPDATE_BLOCK_SUCCESS', t => {
+        const block = { id: 1, name: 'Dolor' }
+        const action = { type: at.UPDATE_BLOCK_SUCCESS, payload: block }
+        const nextState = reducer(fetchRequestState, action)
+        t.deepEqual(nextState, {
+            ...fetchRequestState,
+            saving: false,
+            data: fetchRequestState.data.map(b => {
+                if (b.id === block.id) return block
+                else return b
+            })
+        })
+    })
 
-//     it('failure', () => {
-//       const error = '500 Internal Server Error'
-//       const action = { type: t.UPDATE_BLOCK_FAILURE, payload: error }
-//       const nextState = reducer(fetchRequestState, action)
-//       expect(nextState).to.deep.equal({...fetchRequestState,
-//         saving: false,
-//         error
-//       })
-//     })
-//   })
+    test('#UPDATE_BLOCK_FAILURE', t => {
+        const error = '500 Internal Server Error'
+        const action = { type: at.UPDATE_BLOCK_FAILURE, payload: error }
+        const nextState = reducer(fetchRequestState, action)
+        t.deepEqual(nextState, {
+            ...fetchRequestState,
+            saving: false,
+            error
+        })
+    })
+})();
 
-//   describe('doing update block', () => {
-//     const data = [
-//       { id: 1, name: 'Lorem', bg_image: 'tmp://old.png' },
-//       { id: 2, name: 'Ipsum', bg_class: 'bg-5' }
-//     ]
-//     const fetchState = {...initialState,
-//       isLoaded: true,
-//       data
-//     }
+(() => {
+    const data = [
+        { id: 1, name: 'Lorem', bg_image: 'tmp://old.png' },
+        { id: 2, name: 'Ipsum', bg_class: 'bg-5' }
+    ]
+    const fetchState = {
+        ...initialState,
+        isLoaded: true,
+        data
+    }
 
-//     it('change background', () => {
-//       const payload = {...data[0], bg_image: 'tmp://new.png'}
-//       const action = { type: t.CHANGE_BLOCK_BACKGROUND, payload }
-//       const nextState = reducer(fetchState, action)
-//       expect(nextState).to.deep.equal({...nextState,
-//         data: [payload, data[1]]
-//       })
-//     })
+    test('change background', t => {
+        const payload = { ...data[0], bg_image: 'tmp://new.png' }
+        const action = { type: at.CHANGE_BLOCK_BACKGROUND, payload }
+        const nextState = reducer(fetchState, action)
+        t.deepEqual(nextState, {
+            ...nextState,
+            data: [payload, data[1]]
+        })
+    })
 
-//     it('batch', () => {
-//       const payload = {
-//         blocks: {
-//           blocks: [
-//             {...data[0], bg_image: 'tmp://new.png'}
-//           ]
-//         }
-//       }
+    test('batch', t => {
+        const payload = {
+            blocks: {
+                blocks: [
+                    { ...data[0], bg_image: 'tmp://new.png' }
+                ]
+            }
+        }
 
-//       const action = { type: t.UPDATE_BLOCK_BATCH, payload }
-//       const nextState = reducer(fetchState, action)
-//       expect(nextState).to.deep.equal({...nextState,
-//         data: [payload.blocks.blocks[0], data[1]]
-//       })
-//     })
-//   })
+        const action = { type: at.UPDATE_BLOCK_BATCH, payload }
+        const nextState = reducer(fetchState, action)
+        t.deepEqual(nextState, {
+            ...nextState,
+            data: [payload.blocks.blocks[0], data[1]]
+        })
+    })
+})();
 
-//   describe('doing add', () => {
-//     const data = [
-//       { id: 1, name: 'Lorem', bg_image: 'tmp://old.png' },
-//       { id: 2, name: 'Ipsum', bg_class: 'bg-5' }
-//     ]
-//     const fetchState = {...initialState,
-//       isLoaded: true,
-//       data
-//     }
+(() => {
 
-//     it('request', () => {
-//       const action = { type: t.ADD_BLOCK_REQUEST }
-//       const nextState = reducer(fetchState, action)
-//       expect(nextState).to.deep.equal({...fetchState,
-//         saving: true
-//       })
-//     })
+    const data = [
+        { id: 1, name: 'Lorem', bg_image: 'tmp://old.png' },
+        { id: 2, name: 'Ipsum', bg_class: 'bg-5' }
+    ]
+    const fetchState = {
+        ...initialState,
+        isLoaded: true,
+        data
+    }
 
-//     it('success', () => {
-//       const payload = { id: 3, name: 'Dolor', bg_class: 'bg-2' }
-//       const state = { ...fetchState, saving: true }
-//       const action = { type: t.ADD_BLOCK_SUCCESS, payload }
-//       const nextState = reducer(state, action)
-//       expect(nextState).to.deep.equal({...state,
-//         saving: false,
-//         data: [...data, payload]
-//       })
-//     })
+    test('#ADD_BLOCK_REQUEST', t => {
+        const action = { type: at.ADD_BLOCK_REQUEST }
+        const nextState = reducer(fetchState, action)
+        t.deepEqual(nextState, {
+            ...fetchState,
+            saving: true
+        })
+    })
 
-//     it('failure', () => {
-//       const payload = '500 Internal server error'
-//       const state = { ...fetchState, saving: true }
-//       const action = { type: t.ADD_BLOCK_FAILURE, payload }
-//       const nextState = reducer(state, action)
-//       expect(nextState).to.deep.equal({...state,
-//         saving: false,
-//         error: payload
-//       })
-//     })
-//   })
+    test('#ADD_BLOCK_SUCCESS', t => {
+        const payload = { id: 3, name: 'Dolor', bg_class: 'bg-2' }
+        const state = { ...fetchState, saving: true }
+        const action = { type: at.ADD_BLOCK_SUCCESS, payload }
+        const nextState = reducer(state, action)
+        t.deepEqual(nextState, {
+            ...state,
+            saving: false,
+            data: [...data, payload]
+        })
+    })
 
-//   describe('doing destroy', () => {
-//     const data = [
-//       { id: 1, name: 'Lorem', bg_image: 'tmp://old.png' },
-//       { id: 2, name: 'Ipsum', bg_class: 'bg-5' }
-//     ]
-//     const fetchState = {...initialState,
-//       isLoaded: true,
-//       data
-//     }
+    test('failure', t => {
+        const payload = '500 Internal server error'
+        const state = { ...fetchState, saving: true }
+        const action = { type: at.ADD_BLOCK_FAILURE, payload }
+        const nextState = reducer(state, action)
+        t.deepEqual(nextState, {
+            ...state,
+            saving: false,
+            error: payload
+        })
+    })
+})();
 
-//     it('request', () => {
-//       const action = { type: t.DESTROY_BLOCK_REQUEST }
-//       const nextState = reducer(fetchState, action)
-//       expect(nextState).to.deep.equal({...fetchState,
-//         saving: true
-//       })
-//     })
+(() => {
 
-//     it('success', () => {
-//       const payload = data[0]
-//       const state = { ...fetchState, saving: true }
-//       const action = { type: t.DESTROY_BLOCK_SUCCESS, payload }
-//       const nextState = reducer(state, action)
-//       expect(nextState).to.deep.equal({...state,
-//         saving: false,
-//         data: data.filter(b => b.id !== payload.id)
-//       })
-//     })
+    const data = [
+        { id: 1, name: 'Lorem', bg_image: 'tmp://old.png' },
+        { id: 2, name: 'Ipsum', bg_class: 'bg-5' }
+    ]
+    const fetchState = {
+        ...initialState,
+        isLoaded: true,
+        data
+    }
 
-//     it('failure', () => {
-//       const payload = '500 Internal server error'
-//       const state = { ...fetchState, saving: true }
-//       const action = { type: t.DESTROY_BLOCK_FAILURE, payload }
-//       const nextState = reducer(state, action)
-//       expect(nextState).to.deep.equal({...state,
-//         saving: false,
-//         error: payload
-//       })
-//     })
-//   })
+    test('request', t => {
+        const action = { type: at.DESTROY_BLOCK_REQUEST }
+        const nextState = reducer(fetchState, action)
+        t.deepEqual(nextState, {
+            ...fetchState,
+            saving: true
+        })
+    })
 
-//   it('should reset blocks reducer when SELECT_MOBILIZATION', () => {
-//     const fetchState = {...initialState,
-//       isLoaded: true,
-//       data: [
-//         { id: 1, name: 'Lorem', bg_class: 'bg-5' }
-//       ]
-//     }
-//     const action = { type: t.SELECT_MOBILIZATION }
-//     const nextState = reducer(fetchState, action)
-//     expect(nextState).to.deep.equal(initialState)
-//   })
-// })
+    test('success', t => {
+        const payload = data[0]
+        const state = { ...fetchState, saving: true }
+        const action = { type: at.DESTROY_BLOCK_SUCCESS, payload }
+        const nextState = reducer(state, action)
+        t.deepEqual(nextState, {
+            ...state,
+            saving: false,
+            data: data.filter(b => b.id !== payload.id)
+        })
+    })
+
+    test('#DESTROY_BLOCK_FAILURE', t => {
+        const payload = '500 Internal server error'
+        const state = { ...fetchState, saving: true }
+        const action = { type: at.DESTROY_BLOCK_FAILURE, payload }
+        const nextState = reducer(state, action)
+        t.deepEqual(nextState, {
+            ...state,
+            saving: false,
+            error: payload
+        })
+    })
+
+    test('should reset blocks reducer when SELECT_MOBILIZATION', t => {
+        const fetchState = {
+            ...initialState,
+            isLoaded: true,
+            data: [
+                { id: 1, name: 'Lorem', bg_class: 'bg-5' }
+            ]
+        }
+        const action = { type: at.SELECT_MOBILIZATION }
+        const nextState = reducer(fetchState, action)
+        t.deepEqual(nextState, initialState)
+    })
+})();
