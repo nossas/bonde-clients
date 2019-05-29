b329b353521cbb08a27ec3f5043d867c527cfdef
Amanda Albuquerque <amandlei@hotmail.com>
2019-05-29 11:44:22 -0300

    chore(admin-canary): refactor structure to cra3

--- packages/bonde-admin-canary/src/index.js
+++ packages/bonde-admin-canary/src/index.js
@@ -1,8 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './Root'
-import registerServiceWorker from './registerServiceWorker'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './Root';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
