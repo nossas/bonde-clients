ae81fd9bda2b8afe63a20d1ec4ea4f281010ba56
Amanda Albuquerque <amandlei@hotmail.com>
2019-06-06 16:12:48 -0300

    feat(admin): add plugin in widget

---
+++ packages/bonde-admin/src/mobilizations/widgets/__plugins__/donation/components/donation-finish-post-donation.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { bool, object } from 'prop-types'
+
+import { FinishPostDonation } from 'bonde-webpage/lib/plugins/donation/components'
+
+console.log(FinishPostDonation)
+
+const DonationFinishPostDonation = ({ preview, mobilization, widget }) => {
+  return (
+    <div>teste</div>
+  )
+}
+
+DonationFinishPostDonation.propTypes = {
+  preview: bool,
+  mobilization: object.isRequired,
+  widget: object.isRequired
+}
+
+export default DonationFinishPostDonation
