cdafc74496ef3fc14ac3bfb971f915837eae71f7
Gabriel Rocha de Oliveira <rolivegab@gmail.com>
2019-06-08 00:06:34 -0300

    feature(webpage): refactoring FinishDonationForm tests

--- packages/bonde-webpage/src/setupTests.js
+++ packages/bonde-webpage/src/setupTests.js
@@ -3,3 +3,8 @@ import { configure } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 
 configure({ adapter: new Adapter() })
+
+import {JSDOM} from 'jsdom'
+const doc = new JSDOM('<!doctype html><html><body></body></html>')
+global.window = doc.window
+global.document = window.document
