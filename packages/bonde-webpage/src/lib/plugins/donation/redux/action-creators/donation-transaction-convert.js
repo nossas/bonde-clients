b7bf0fc82332554ffd49212f9bab98794d6e16e6
Amanda Albuquerque <amandlei@hotmail.com>
2019-06-13 15:14:22 -0300

    feat(webpage): remove assigned values action convert

--- packages/bonde-webpage/src/lib/plugins/donation/redux/action-creators/donation-transaction-convert.js
+++ packages/bonde-webpage/src/lib/plugins/donation/redux/action-creators/donation-transaction-convert.js
@@ -1,7 +1,5 @@
 const donationTransactionConvert = params => (dispatch, getState, { api, intl }) => {
   const endpoint = `/convert-donation`
-  const { donation_id, amount } = params
-  
   return api.get(endpoint, { params })
 }
   
