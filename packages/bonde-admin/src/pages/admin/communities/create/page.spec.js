e74ee8944706f76144fdc9c85284744af3d7f63e
Gabriel Rocha de Oliveira <rolivegab@gmail.com>
2019-06-17 16:11:25 -0300

    fix(admin): communities create console.error

--- packages/bonde-admin/src/pages/admin/communities/create/page.spec.js
+++ packages/bonde-admin/src/pages/admin/communities/create/page.spec.js
@@ -3,6 +3,10 @@ import shallowWithIntl from 'intl/helpers/shallow-with-intl'
 
 import * as mock from 'utils/mock'
 import Page from './page'
+import { IntlProvider } from 'react-intl';
+
+const intlProvider = new IntlProvider({ locale: 'en' }, {});
+const { intl } = intlProvider.getChildContext();
 
 describe('routes/admin/authenticated/external/community-new/page', () => {
   const props = {
@@ -12,10 +16,15 @@ describe('routes/admin/authenticated/external/community-new/page', () => {
     },
     submitting: false,
     // Actions
-    create: mock.noop
+    create: mock.noop,
+    asyncCreate: () => {},
+    handleSubmit: () => {},
+    submitFailed: false,
+    dirty: false,
+    valid: false
   }
 
   it('should render without crashed', () => {
-    shallowWithIntl(<Page {...props} />)
+    shallowWithIntl(<Page {...props} intl={intl} />)
   })
 })
