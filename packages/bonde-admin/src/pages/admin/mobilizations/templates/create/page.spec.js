9e9e4cb29a4bf8bbc879d049146ca09bc7d7aacf
Gabriel Rocha de Oliveira <rolivegab@gmail.com>
2019-06-17 16:08:10 -0300

    fix(admin): page console.error

--- packages/bonde-admin/src/pages/admin/mobilizations/templates/create/page.spec.js
+++ packages/bonde-admin/src/pages/admin/mobilizations/templates/create/page.spec.js
@@ -3,19 +3,27 @@ import shallowWithIntl from 'intl/helpers/shallow-with-intl'
 import { expect } from 'chai'
 
 import Page from './page'
+import { IntlProvider } from 'react-intl';
+
+const intlProvider = new IntlProvider({ locale: 'en' }, {});
+const { intl } = intlProvider.getChildContext();
 
 describe('routes/admin/authenticated/sidebar/templates-create/page', () => {
   let wrapper
   const props = {
-    mobilization: { id: 1 },
+    mobilization: { id: 1, name: '', goal: '' },
     fields: {
       name: {},
       goal: {}
-    }
+    },
+    handleSubmit: () => {},
+    submitFailed: false,
+    dirty: false,
+    valid: false
   }
 
   beforeAll(() => {
-    wrapper = shallowWithIntl(<Page {...props} />)
+    wrapper = shallowWithIntl(<Page {...props} intl={intl} />)
   })
 
   describe('#render', () => {
