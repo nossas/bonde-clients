528489f43e06c1528c9e7fe6bb649e4c52a7a083
Gabriel Rocha de Oliveira <rolivegab@gmail.com>
2019-07-05 08:12:07 -0300

    chore(admin-canary): lints src folder

--- packages/bonde-admin-canary/src/Root.js
+++ packages/bonde-admin-canary/src/Root.js
@@ -3,26 +3,26 @@ import { BrowserRouter as Router, Redirect, Switch } from 'react-router-dom'
 import { ProviderRedux } from './services/redux'
 import { ProviderGraphQL } from './services/graphql'
 import { ProviderLastLocation } from './services/router'
-import { SessionProvider } from './services/auth'
+import { SessionProvider, PrivateRoute, PublicRoute, Route } from './services/auth'
 // Routes
 import { Root as LoggedRoot } from './scenes/Logged'
 import { Root as AuthRoot } from './scenes/Auth'
-import { PrivateRoute, PublicRoute, Route } from './services/auth'
+
 import { NotFound } from './components'
 
 const Root = () => (
   <SessionProvider>
     <ProviderGraphQL>
-      <ProviderRedux> 
+      <ProviderRedux>
         <Router>
           <ProviderLastLocation>
-            <Switch> 
+            <Switch>
               <PublicRoute
                 path='/auth'
                 redirectTo='/admin'
                 component={AuthRoot}
               />
-              
+
               <PrivateRoute
                 path='/admin'
                 redirectTo='/auth/login'
