51c15e52aa3546a71e1b2704f9ce34775944fef9
Gabriel Rocha de Oliveira <rolivegab@gmail.com>
2019-07-22 13:41:31 -0300

    feat(public): create _app.js default file

--- packages/bonde-public/pages/_app.js
+++ packages/bonde-public/pages/_app.js
@@ -1,39 +1,30 @@
 // pages/_app.js
 import React from "react";
-import {createStore} from "redux";
-import {Provider} from "react-redux";
-import App, {Container} from "next/app";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import App, { Container } from "next/app";
 import withRedux from "next-redux-wrapper";
-import configureStore from '../configureStore'
-
-const reducer = (state = {foo: ''}, action) => {
-    switch (action.type) {
-        case 'FOO':
-            return {...state, foo: action.payload};
-        default:
-            return state
-    }
-};
+import configureStore from '../src/configureStore'
 
 class MyApp extends App {
-    static async getInitialProps ({ Component, ctx }) {
-        return {
-          pageProps: {
-            ...(Component.getInitialProps ? await Component.getInitialProps(ctx) : {}),
-          }
-        };
+  static async getInitialProps({ Component, ctx }) {
+    return {
+      pageProps: {
+        ...(Component.getInitialProps ? await Component.getInitialProps(ctx) : {}),
       }
+    };
+  }
 
-    render() {
-        const {Component, pageProps, store} = this.props;
-        return (
-            <Container>
-                <Provider store={store}>
-                    <Component {...pageProps} />
-                </Provider>
-            </Container>
-        );
-    }
+  render() {
+    const { Component, pageProps, store } = this.props;
+    return (
+      <Container>
+        <Provider store={store}>
+          <Component {...pageProps} />
+        </Provider>
+      </Container>
+    );
+  }
 
 }
 
