0b141119b0308184d33d3a99de24e8a721176037
Amanda Albuquerque <amandlei@hotmail.com>
2019-07-26 10:49:41 -0300

    feat(admin-canary): add bonde-diagram in bonde-admin-canary

--- packages/bonde-admin-canary/src/scenes/Logged/Root.js
+++ packages/bonde-admin-canary/src/scenes/Logged/Root.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { AuthProvider, Route } from 'services/auth'
 import { FullScreenLoadable, LoadingFullScreen } from 'components/Loadable'
 import TagsPage from './scenes/Tags'
-import PropTypes from 'prop-types'
+import ChatbotPage from './scenes/Chatbot'
 
 const HomePage = FullScreenLoadable({
   loader: () => import('./scenes/Home')
@@ -19,6 +20,11 @@ const Root = ({ match }) => (
     <Route
       path={`${match.url}/tags`}
       component={TagsPage}
+    />
+
+    <Route
+      path={`${match.url}/chatbot`}
+      component={ChatbotPage}
     />
   </AuthProvider>
 )
