3adfb9156cbbd74433f9d68a3125ff86dff5a63c
Lucas Pirola <lucaspirola@gmail.com>
2019-07-29 22:39:05 -0300

    feature(upgrade): styleguidist and lock files;

--- packages/bonde-styleguide/styleguide.config.js
+++ packages/bonde-styleguide/styleguide.config.js
@@ -1,67 +1,58 @@
-const path = require('path')
+// styleguide.config.js
+// const path = require('path')
+// module.exports = {
+//   styleguideComponents: {
+//     Wrapper: path.join(__dirname, 'src/components/styleguide/Wrapper')
+//   }
+// }
 
-module.exports = {
+// styleguide.config.js
+module.exports ={
   sections: [
     {
       name: 'Content',
       description: 'The list of content components.',
-      components: 'src/content/[A-Z]*/[A-Z]*.js'
+      components: 'src/components/content/[A-Z]*/[A-Z]*.js'
     },
     {
       name: 'Cards',
       description: 'The list of card components.',
-      components: 'src/cards/[A-Z]*/[A-Z]*.js'
+      components: 'src/components/cards/[A-Z]*/[A-Z]*.js'
     },
     {
       name: 'List',
       description: 'The list of list components.',
-      components: 'src/list/[A-Z]*/[A-Z]*.js'
+      components: 'src/components/list/[A-Z]*/[A-Z]*.js'
     },
     {
       name: 'Progress',
       description: 'The list of progress components.',
-      components: 'src/progress/[A-Z]*/[A-Z]*.js'
+      components: 'src/components/progress/[A-Z]*/[A-Z]*.js'
     },
     {
       name: 'Navigation',
       description: 'The list of navigation components.',
-      components: 'src/navigation/[A-Z]*/[A-Z]*.js'
+      components: 'src/components/navigation/[A-Z]*/[A-Z]*.js'
     },
     {
       name: 'Form',
       description: 'The list of form components.',
-      components: 'src/form/[A-Z]*/[A-Z]*.js'
+      components: 'src/components/form/[A-Z]*/[A-Z]*.js'
     },
     {
       name: 'Await',
       description: 'The list of components to await steps.',
-      components: 'src/await/[A-Z]*/[A-Z]*.js'
+      components: 'src/components/await/[A-Z]*/[A-Z]*.js'
     },
     {
       name: 'Layout',
       description: 'The list of components that modify layout.',
-      components: 'src/layout/[A-Z]*/[A-Z]*.js'
+      components: 'src/components/layout/[A-Z]*/[A-Z]*.js'
     },
     {
       name: 'Assets',
       description: 'The list of assets components.',
-      components: 'src/assets/[A-Z]*/[A-Z]*.js'
+      components: 'src/components/assets/[A-Z]*/[A-Z]*.js'
     }
   ],
-  contextDependencies: [path.resolve(__dirname, 'src')],
-  showUsage: true,
-  showCode: false,
-  skipComponentsWithoutExample: true,
-  
-  webpackConfig: require('./config/webpack.config.dev.js'),
-
-  getComponentPathLine(componentPath) {
-    const name = path.basename(componentPath, '.js')
-    const dir = path.dirname(componentPath)
-    return `import { ${name} } from 'bonde-styleguide'`
-  },
-
-  styleguideComponents: {
-    Wrapper: path.join(__dirname, 'src/ThemeWrapper'),
-  } 
-}
+};
