9fd2bf884026925f0788ad30edaa2a2ab2adbb20
Igor Santos <igr.exe@gmail.com>
2019-08-12 17:16:52 -0300

    feat(styleguide): wrapper TabItem to customize render

--- packages/bonde-styleguide/src/components/navigation/TabItem/TabItem.js
+++ packages/bonde-styleguide/src/components/navigation/TabItem/TabItem.js
@@ -1,41 +1,50 @@
+import React from 'react'
 import PropTypes from 'prop-types'
 import styled, { css } from 'styled-components'
 
-export const TabItem = styled.a`
-  display: inline-block;
-  font-family: 'Nunito Sans', sans-serif;
-  font-size: 13px;
-  font-weight: 800;
-  line-height: 1.15;
-  color: ${props => props.inverted ? '#000' : '#fff'};
-  text-transform: uppercase;
-  cursor: pointer;
-  margin: 0 15px 0 0;
-  padding-bottom: 14px;
+export const TabItem = ({ component: Component, ...rest }) => {
+  const StyledComponent = styled(Component)`
+    display: inline-block;
+    font-family: 'Nunito Sans', sans-serif;
+    font-size: 13px;
+    font-weight: 800;
+    line-height: 1.15;
+    color: ${props => props.inverted ? '#000' : '#fff'};
+    text-transform: uppercase;
+    cursor: pointer;
+    margin: 0 15px 0 0;
+    padding-bottom: 14px;
+    text-decoration: none;
 
-  ${props => props.active && css`
-    border-bottom: 3px solid #ee0099;
-    padding-bottom: 11px;
-  `}
+    ${props => props.active && css`
+      border-bottom: 3px solid #ee0099;
+      padding-bottom: 11px;
+    `}
 
-  &:hover, &:active {
-    border-bottom: 3px solid #ee0099;
-    padding-bottom: 11px;
-  }
-`
+    &:hover, &:active {
+      border-bottom: 3px solid #ee0099;
+      padding-bottom: 11px;
+    }
+  `
 
-const { bool } = PropTypes
+  return <StyledComponent {...rest} />
+}
+
+const { any, bool } = PropTypes
 
 TabItem.propTypes = {
   /** Invert the tab color style. */
   inverted: bool,
   /** Mark the current item as active style. */
-  active: bool
+  active: bool,
+  /** Component used to render */
+  component: any
 }
 
 TabItem.defaultProps = {
   inverted: false,
-  active: false
+  active: false,
+  component: 'a'
 }
 
 TabItem.displayName = 'TabItem'
