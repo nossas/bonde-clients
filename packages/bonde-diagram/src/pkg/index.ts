ac971c22bacd6b5551b33d1091018b0a6a35cd3a
Igor Santos <igr.exe@gmail.com>
2019-10-04 17:11:24 -0300

    chore(diagram): fix lint

--- packages/bonde-diagram/src/pkg/index.ts
+++ packages/bonde-diagram/src/pkg/index.ts
@@ -8,3 +8,31 @@ export { default as Layer } from "./components/Layer"
 export { default as ActionMessageModel } from "./messages/action/ActionMessageModel"
 export { default as ReplyMessageModel } from "./messages/reply/ReplyMessageModel"
 export { default as TextMessageModel } from "./messages/text/TextMessageModel"
+
+import DiagramApplication from "./Application"
+import TextMessageModel from "./messages/text/TextMessageModel"
+
+export const createFirstMessage = (text: string): any => {
+  const DummyUI = {
+    addReply: Layer,
+    content: Layer,
+    inPort: Layer,
+    layer: Layer,
+    outPort: Layer,
+  }
+  const DummyThemeUI = {
+    action: DummyUI,
+    message: DummyUI,
+    reply: DummyUI,
+  }
+
+  const node = new TextMessageModel({ text })
+  node.setPosition(200, 50)
+
+  const app = new DiagramApplication(DummyThemeUI)
+
+  app.getActiveDiagram().addNode(node)
+  app.getDiagramEngine().repaintCanvas()
+
+  return app.getActiveDiagram().serialize()
+}
