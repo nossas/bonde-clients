8c422e8ef3ef863c9c3b9018a9b489f3c9604185
vvdias <vivipirolad@gmail.com>
2019-11-14 16:15:10 -0300

    Values of input form are changing and computing

--- packages/bonde-admin-canary/src/scenes/Auth/scenes/ResetPassword/ResetPasswordForm/ResetPasswordForm.js
+++ packages/bonde-admin-canary/src/scenes/Auth/scenes/ResetPassword/ResetPasswordForm/ResetPasswordForm.js
@@ -1,20 +1,39 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+
+import { required, min } from 'services/validations'
+import { AuthAPI } from 'services/auth'
+import resetPassword from './resetPassword.graphql'
+
 import { Flexbox2 as Flexbox, Title } from 'bonde-styleguide'
 import { Field, MutationForm, SubmitButton } from 'components/Forms'
 import { ButtonLink } from 'components/Link'
-import { required, min } from 'services/validations'
+import { notify } from 'components/Notification'
 import { PasswordField } from '../../components'
-import resetPassword from './resetPassword.graphql'
-import PropTypes from 'prop-types'
 
-const ResetPasswordForm = ({ t, token, handleSuccess }) => (
+const formName = 'ResetPasswordForm'
+
+const ResetPasswordForm = ({ t, token }) => (
   <Flexbox vertical>
     <Title.H2 margin={{ bottom: 18 }}>{t('resetPassword.form.title')}</Title.H2>
     <Title.H4 margin={{ bottom: 30 }}>{t('resetPassword.form.subtitle')}</Title.H4>
     <MutationForm
       mutation={resetPassword}
       variables={{ token }}
-      onSuccess={handleSuccess}
+      onSuccess={({ data }) => {
+        const { changePasswordField } = data.resetPasswordChangePassword
+        const user = { name: changePasswordField.userFirstName }
+
+        AuthAPI.login({
+          jwtToken: changePasswordField.token
+        })
+          .then(() => {
+            notify(t('resetPassword.success', { user }))
+            // should redirect with window to rehydrate session
+            window.location.href = '/'
+          })
+      }}
+      formId={formName}
     >
       <Field
         name='password'
@@ -28,7 +47,7 @@ const ResetPasswordForm = ({ t, token, handleSuccess }) => (
       />
       <Flexbox padding={{ top: 25 }} horizontal spacing='between'>
         <ButtonLink to='/auth/login'>{t('resetPassword.form.cancel')}</ButtonLink>
-        <SubmitButton>{t('resetPassword.form.submit')}</SubmitButton>
+        <SubmitButton title={t('resetPassword.form.submit')} formId={formName}>{t('resetPassword.form.submit')}</SubmitButton>
       </Flexbox>
     </MutationForm>
   </Flexbox>
--- packages/bonde-admin-canary/src/scenes/Auth/scenes/ResetPassword/index.js
+++ packages/bonde-admin-canary/src/scenes/Auth/scenes/ResetPassword/index.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import { I18n } from 'react-i18next'
 import { Query } from 'react-apollo'
-import { AuthAPI } from 'services/auth'
-import { notify } from 'components/Notification'
 
 import tokenVerify from './tokenVerify.graphql'
 import CheckingToken from './CheckingToken'
@@ -26,19 +24,6 @@ const ResetPassword = ({ match }) => (
               <ResetPasswordForm
                 t={t}
                 token={token}
-                handleSuccess={({ data }) => {
-                  const { changePasswordField } = data.resetPasswordChangePassword
-                  const user = { name: changePasswordField.userFirstName }
-
-                  AuthAPI.login({
-                    jwtToken: changePasswordField.token
-                  })
-                    .then(() => {
-                      notify(t('resetPassword.success', { user }))
-                      // should redirect with window to rehydrate session
-                      window.location.href = '/'
-                    })
-                }}
               />
             )
           }}
