edd93e2e122aa441f2c3d9759c0ef67ebc584f93
Igor Santos <igr.exe@gmail.com>
2019-11-27 17:22:01 -0300

    fix(canary): add parse to fields works on settings form in admin

--- packages/bonde-admin-canary/src/scenes/Dashboard/components/UserCommunities.js
+++ packages/bonde-admin-canary/src/scenes/Dashboard/components/UserCommunities.js
@@ -3,6 +3,35 @@ import PropTypes from 'prop-types'
 import { Query } from 'react-apollo'
 import { allUserCommunitiesQuery } from 'scenes/Dashboard/graphql'
 
+const parseBankAccount = (communities) => communities.map(community => {
+  if (community.recipient) {
+    const getValue = (key) => community.recipient.bank_account[key]
+
+    return {
+      ...community,
+      recipient: {
+        ...community.recipient,
+        bank_account: {
+          id: getValue('id'),
+          object: getValue('object'),
+          type: getValue('type'),
+          bank_code: getValue('bank_code'),
+          agency: getValue('agencia'),
+          agency_dig: getValue('agencia_dv'),
+          account: getValue('conta'),
+          account_dig: getValue('conta_dv'),
+          document_type: getValue('document_type'),
+          document_number: getValue('document_number'),
+          legal_name: getValue('legal_name'),
+          charge_transfer_fees: getValue('charge_transfer_fees'),
+          date_created: getValue('date_created')
+        }
+      }
+    }
+  }
+  return community
+})
+
 const UserCommunities = ({ loading: Loading, component: Component, ...rest }) => {
   const [offset, setOffset] = useState(0)
   return (
@@ -12,7 +41,7 @@ const UserCommunities = ({ loading: Loading, component: Component, ...rest }) =>
         if (error) return <span>{error}</span>
 
         // LIMIT OF QUERY HARD SET LIKE 20
-        const communities = data && data.communities ? data.communities : []
+        const communities = data && data.communities ? parseBankAccount(data.communities) : []
         const count = data && data.communities_aggregate ? data.communities_aggregate.aggregate.count : 0
 
         return (
