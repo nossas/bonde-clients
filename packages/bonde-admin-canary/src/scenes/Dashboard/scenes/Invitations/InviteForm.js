0dc938d2be34ce60dc89e621c80847b0ddc0edad
Igor Santos <igr.exe@gmail.com>
2019-12-09 13:15:44 -0300

    feat(canary): add callback_url to invite user

--- packages/bonde-admin-canary/src/scenes/Dashboard/scenes/Invitations/InviteForm.js
+++ packages/bonde-admin-canary/src/scenes/Dashboard/scenes/Invitations/InviteForm.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import { PropTypes } from 'prop-types'
 import gql from 'graphql-tag'
+import urljoin from 'url-join'
 import { Flexbox2 as Flexbox, Input } from 'bonde-styleguide'
 import { MutationForm, Field, FormField, SubmitButton } from 'components/Forms'
+import { Auth } from 'services/auth'
 import { isEmail, required } from 'services/validations'
 import { InvitationsQuery } from './InvitationsDataset'
 
 const InviteMutation = gql`
-  mutation Invite ($community_id: Int!, $email: String!) {
+  mutation Invite ($input: invitations_insert_input!) {
     insert_invitations (
-      objects: { role: 2, community_id: $community_id, email: $email }
+      objects: [$input]
     ) {
       returning {
         id
@@ -28,32 +30,51 @@ const InviteMutation = gql`
 
 const FORM_ID = 'InviteForm'
 
-const InviteForm = ({ community }) => (
-  <MutationForm
-    cleanForm
-    formId={FORM_ID}
-    mutation={InviteMutation}
-    values={{ community_id: community.id }}
-    refetchQueries={[{ query: InvitationsQuery, variables: { community_id: community.id } }]}
-  >
-    <Flexbox horizontal end>
-      <div style={{ flexGrow: 1 }}>
-        <Field
-          name='email'
-          label='Enviar convite para:'
-          placeholder='Insira aqui o e-mail de cadastro de quem quiser convidar'
-          component={FormField}
-          inputComponent={Input}
-          validade={[
-            required('required field'),
-            isEmail('invalid email')
-          ]}
-        />
-      </div>
-      <SubmitButton formId={FORM_ID}>Convidar</SubmitButton>
-    </Flexbox>
-  </MutationForm>
-)
+const InviteForm = ({ community }) => {
+  const input = {
+    role: 2,
+    community_id: community.id,
+    callback_url: urljoin(process.env.REACT_APP_DOMAIN_ADMIN_CANARY, '/auth/register')
+  }
+
+  return (
+    <Auth>
+      {({ user }) => {
+      // TO admin users user_id not set default with X-HASURA-USER-ID
+        if (user.admin) {
+          input.user_id = user.id
+        }
+
+        return (
+          <MutationForm
+            cleanForm
+            formId={FORM_ID}
+            mutation={InviteMutation}
+            values={{ input }}
+            refetchQueries={[{ query: InvitationsQuery, variables: { community_id: community.id } }]}
+          >
+            <Flexbox horizontal end>
+              <div style={{ flexGrow: 1 }}>
+                <Field
+                  name='input.email'
+                  label='Enviar convite para:'
+                  placeholder='Insira aqui o e-mail de cadastro de quem quiser convidar'
+                  component={FormField}
+                  inputComponent={Input}
+                  validade={[
+                    required('required field'),
+                    isEmail('invalid email')
+                  ]}
+                />
+              </div>
+              <SubmitButton formId={FORM_ID}>Convidar</SubmitButton>
+            </Flexbox>
+          </MutationForm>
+        )
+      }}
+    </Auth>
+  )
+}
 
 InviteForm.propTypes = {
   community: PropTypes.object.isRequired
--- packages/bonde-admin-canary/src/services/auth/currentUser.graphql.js
+++ packages/bonde-admin-canary/src/services/auth/currentUser.graphql.js
@@ -8,7 +8,8 @@ export default gql`
       lastName,
       email,
       avatar,
-      tags
+      tags,
+      admin
     }
   }
 `
