f7cabeb0fa4dd088bbf019888fbd3c9b2e46a609
Viviane <vivianedias@protonmail.com>
2020-08-05 11:59:56 -0300

    chore: added components libraries to root

--- packages/redes-client/src/App.tsx
+++ packages/redes-client/src/App.tsx
@@ -1,15 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import styled from "styled-components"
+import { Text } from "bonde-components"
+
+const AppHeader = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <AppHeader>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <Text>
           Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        </Text>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,7 +31,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </AppHeader>
     </div>
   );
 }
