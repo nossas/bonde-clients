7c710eacb05e538a35f2b309c2c2f2609583210b
Viviane Dias <vivianedias@protonmail.com>
2020-08-27 11:47:35 -0300

    feat(page-relations): hoist filters so it doesnt get updated constantly

--- packages/redes-client/src/pages/Relations/index.tsx
+++ packages/redes-client/src/pages/Relations/index.tsx
@@ -1,14 +1,9 @@
 import React from "react";
 import { Header } from "bonde-components";
-import styled from "styled-components";
 // import { useSession } from "bonde-core-tools";
 import { Table, Filters } from "../../components";
 import { useFilter } from "../../services/FilterProvider";
 import columns from "./columns";
-
-export const Wrap = styled.div`
-  margin: 20px 0;
-`;
 
 type Props = {
   loading?: boolean;
@@ -20,20 +15,7 @@ type Props = {
       children: (data: {
         relationships: Array<any>;
         groups: Array<{ is_volunteer: boolean; name: string }>;
-        relationships_count: {
-          aggregate: {
-            count: number;
-          };
-        };
-        agents: Array<{
-          community_users: Array<{
-            user: {
-              first_name: string;
-              last_name: string;
-              id: number;
-            };
-          }>;
-        }>;
+        relationships_count: number;
       }) => React.ReactElement;
     }) => React.ReactElement | null;
     FilterOptions: {
@@ -47,78 +29,66 @@ export default ({
 }: Props): React.ReactElement => {
   const [state, dispatch] = useFilter();
 
-  const save = async (values: any) =>
-    dispatch({ type: "relationships", value: values });
+  const save = async (values: any) => {
+    console.log({ values });
+    return dispatch({ type: "relationships", value: values });
+  };
 
   const reset = () =>
     dispatch({
       type: "relationships",
       value: {
-        search: undefined,
+        query: undefined,
         state: undefined,
         agent: undefined,
-        status: undefined,
+        relationshipStatus: undefined,
       },
     });
 
   return (
-    <FetchMatches>
-      {({
-        groups,
-        relationships,
-        relationships_count: {
-          aggregate: { count },
-        },
-        agents,
-      }) => {
-        const pagination = {
-          totalPages: Math.round(count / state.rows),
-          goToPage: (e: number) => dispatch({ type: "page", value: e }),
-          setPageSize: (e: number) => dispatch({ type: "rows", value: e }),
-          pageIndex: state.page,
-          pageSize: state.rows,
-        };
+    <>
+      <Filters
+        save={save}
+        options={FilterOptions}
+        initialValues={state.relationships}
+        reset={reset}
+        searchPlaceholder="Buscar nome, email..."
+        search
+        state
+        agent
+        relationshipStatus
+      />
+      <FetchMatches>
+        {({ groups, relationships, relationships_count }) => {
+          const pagination = {
+            totalPages: Math.round(relationships_count / state.rows),
+            goToPage: (e: number) => dispatch({ type: "page", value: e }),
+            setPageSize: (e: number) => dispatch({ type: "rows", value: e }),
+            pageIndex: state.page,
+            pageSize: state.rows,
+          };
 
-        return count < 1 ? (
-          <Wrap>
+          return relationships_count < 1 ? (
             <Header.H4>
               Não existem conexões realizadas nessa comunidade.
             </Header.H4>
-          </Wrap>
-        ) : groups.length < 2 ? (
-          <Wrap>
+          ) : groups.length < 2 ? (
             <Header.H4>
               Não existem grupos suficientes nessa comunidade.
             </Header.H4>
-          </Wrap>
-        ) : (
-          <Wrap>
-            <Filters
-              save={save}
-              options={{
-                ...FilterOptions,
-                agents: agents[0].community_users.map(({ user }) => ({
-                  label: `${user.first_name} ${user.last_name}`,
-                  value: user.id,
-                })),
-              }}
-              initialValues={state.relationships}
-              reset={reset}
-              search
-              state
-              agent
-              relationshipStatus
-            />
-            <Header.H4>Total ({count})</Header.H4>
-            <Table
-              data={relationships}
-              columns={columns(groups)}
-              sticky="end"
-              pagination={pagination}
-            />
-          </Wrap>
-        );
-      }}
-    </FetchMatches>
+          ) : (
+            <>
+              <Header.H4>Total ({relationships_count})</Header.H4>
+              <Table
+                data={relationships}
+                columns={columns(groups)}
+                sticky="end"
+                pagination={pagination}
+              />
+            </>
+          );
+        }}
+      </FetchMatches>
+    </>
   );
 };
