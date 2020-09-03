da786d2ab066b2f26e292b828da3a04e52aa3b9a
Viviane Dias <vivipirolad@gmail.com>
2020-09-03 16:43:27 -0300

    feat: adds display name and prop typing to AutoSaveFilters

--- packages/redes-client/src/components/AutoSaveFilters.tsx
+++ packages/redes-client/src/components/AutoSaveFilters.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { FormSpy } from "bonde-components";
 
-export default (props: any) => (
-  <FormSpy
-    {...props}
-    subscription={{ active: true, values: true }}
-    onChange={(newValues) => props.save(newValues.values)}
-  />
-);
+export default function AutoSaveFilters(props: {
+  save: (e: any) => Promise<any>;
+}): React.ReactElement {
+  return (
+    <FormSpy
+      {...props}
+      subscription={{ active: true, values: true }}
+      onChange={(newValues) => props.save(newValues.values)}
+    />
+  );
+}
+
+AutoSaveFilters.displayName = "AutoSaveFilters";
