5e01b2b39f7d87da4fe59d4648caac2928ed09ba
Viviane Dias <vivipirolad@gmail.com>
2020-09-03 18:04:40 -0300

    feat(AutoSaveFilters): only change filter state if values are different

--- packages/redes-client/src/components/AutoSaveFilters.tsx
+++ packages/redes-client/src/components/AutoSaveFilters.tsx
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { FormSpy } from "bonde-components";
+import { diff } from "deep-object-diff";
 
 export default function AutoSaveFilters(props: {
   save: (e: any) => Promise<any>;
 }): React.ReactElement {
+  const [state, setState] = useState({});
+
+  const onChange = (newValues: any) => {
+    const difference = diff(state, newValues);
+    if (Object.keys(difference).length) {
+      // values have changed
+      setState(newValues);
+      props.save(difference);
+    }
+  };
   return (
     <FormSpy
       {...props}
       subscription={{ active: true, values: true }}
-      onChange={(newValues) => props.save(newValues.values)}
+      onChange={(newValues) => onChange(newValues.values)}
     />
   );
 }
