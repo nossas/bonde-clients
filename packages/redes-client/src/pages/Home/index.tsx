b1ee1b771f92348962bf4ab042c04dbb6691de08
Viviane Dias <vivipirolad@gmail.com>
2020-09-04 17:58:44 -0300

    feat(shortcuts): add shortcuts to home page

--- packages/redes-client/src/pages/Home/index.tsx
+++ packages/redes-client/src/pages/Home/index.tsx
@@ -1,9 +1,12 @@
 import React from "react";
-import { Question, Header } from "bonde-components";
+import { Header, Shortcut, Icon, Question } from "bonde-components";
 import { useSession } from "bonde-core-tools";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { useCommunityExtra } from "../../services/CommunityExtraProvider";
+import { useFilter } from "../../services/FilterProvider";
 
-const Grid = styled.div`
+const DashboardGrid = styled.div`
   border-color: 1px solid #000;
   display: grid;
   grid-template-columns: repeat(3, minmax(auto, 450px));
@@ -12,6 +15,13 @@ const Grid = styled.div`
   & iframe > html {
     display: none;
   }
+`;
+
+const Grid = styled.div`
+  display: grid;
+  grid-auto-flow: column;
+  grid-column-gap: 20px;
+  justify-content: start;
 `;
 
 const BlocksGrid = styled.div`
@@ -24,10 +34,60 @@ const BlocksGrid = styled.div`
 
 export default function Home(): React.ReactElement {
   const { community } = useSession();
+  const [, dispatch] = useFilter();
+  const { groups } = useCommunityExtra();
+  const volunteerGroup = groups?.find((group) => !!group.isVolunteer);
+  const individualGroup = groups?.find((group) => !group.isVolunteer);
   return community?.id === 40 ? (
     <>
-      <Header.H5>DADOS GERAIS</Header.H5>
+      <Header.H5>ATALHOS</Header.H5>
       <Grid>
+        <Link
+          style={{ textDecoration: "none" }}
+          to="/pessoas"
+          onClick={() =>
+            dispatch({
+              type: "group",
+              value: {
+                label: individualGroup?.name || "",
+                value: individualGroup?.id || 0,
+              },
+            })
+          }
+        >
+          <Shortcut
+            text="Fazer match de MSR"
+            icon={<Icon name="User" size="small" />}
+          />
+        </Link>
+        <Link
+          to="/pessoas"
+          style={{ textDecoration: "none" }}
+          onClick={() =>
+            dispatch({
+              type: "group",
+              value: {
+                label: volunteerGroup?.name || "",
+                value: volunteerGroup?.id || 0,
+              },
+            })
+          }
+        >
+          <Shortcut
+            icon={<Icon name="Favorite" size="small" />}
+            text="Fazer match de Voluntária"
+          />
+        </Link>
+        <Link to="/matchs" style={{ textDecoration: "none" }}>
+          <Shortcut
+            icon={<Icon name="Show" size="small" />}
+            text="Ver meus matchs"
+          />
+        </Link>
+      </Grid>
+      <Header.H5>DADOS DA SEMANA</Header.H5>
+      <Header.H5>DADOS GERAIS</Header.H5>
+      <DashboardGrid>
         <Question
           siteUrl={process.env.REACT_APP_METABASE_SITE_URL || ""}
           secretKey={process.env.REACT_APP_METABASE_SECRET_KEY || ""}
@@ -116,7 +176,7 @@ export default function Home(): React.ReactElement {
           config={{ resource: { question: 1189 }, params: {} }}
           questionOptions={"#bordered=false&titled=true"}
         />
-      </Grid>
+      </DashboardGrid>
     </>
   ) : (
     <Header.H4>
