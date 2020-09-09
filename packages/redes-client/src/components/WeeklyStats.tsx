622d59244ee4fc00749c2c106a780c7dde8f2f93
Viviane Dias <vivipirolad@gmail.com>
2020-09-09 10:56:18 -0300

    feat(WeeklyStats): mount weekly stats component

---
+++ packages/redes-client/src/components/WeeklyStats.tsx
@@ -0,0 +1,184 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Header, Text } from "bonde-components";
+import { StatsCard } from "./";
+import { WeeklyStatsData } from "../types";
+
+type Props = {
+  FetchWeeklyStats: ({
+    children,
+  }: {
+    children: (data: WeeklyStatsData) => React.ReactElement;
+  }) => React.ReactElement | null;
+  FilterOptions: {
+    [x: string]: { label: string; value: string | number }[];
+  };
+  volunteerGroup?: {
+    name: string;
+    id: number;
+    isVolunteer: boolean;
+  };
+  individualGroup?: {
+    name: string;
+    id: number;
+    isVolunteer: boolean;
+  };
+  communityId: number;
+  dispatch: (arg0: any) => void;
+};
+
+export default function WeeklyStats({
+  FetchWeeklyStats,
+  FilterOptions,
+  volunteerGroup,
+  individualGroup,
+  communityId,
+  dispatch,
+}: Props): React.ReactElement {
+  const getOption = (status: string, field: string) =>
+    FilterOptions[field].find((i) => i.label === status);
+
+  return (
+    <FetchWeeklyStats>
+      {({
+        newRecipients,
+        encaminhamentosRealizados,
+        encaminhamentosServicoPublico,
+        atendimentosIniciados,
+        newlyApprovedVolunteers,
+        newlyAvailableVolunteers,
+      }) => (
+        <>
+          <Link to="/pessoas">
+            <StatsCard
+              onClick={() => {
+                dispatch({
+                  type: "group",
+                  value: {
+                    label: individualGroup?.name || "",
+                    value: individualGroup?.id || 0,
+                  },
+                });
+                return dispatch({
+                  type: "individuals",
+                  value: {
+                    availability:
+                      communityId === 40
+                        ? getOption("MSR-Inscrita", "availability")
+                        : getOption("Inscrita", "userStatus"),
+                  },
+                });
+              }}
+            >
+              <Header.H2>{newRecipients.aggregate.count}</Header.H2>
+              <Text>{`Novas(os) ${individualGroup?.name} inscritas(os)`}</Text>
+            </StatsCard>
+          </Link>
+          <Link to="/matchs">
+            <StatsCard
+              onClick={() =>
+                dispatch({
+                  type: "relationships",
+                  value: {
+                    relationshipStatus: getOption(
+                      "Encaminhamento: Realizado",
+                      "relationshipStatus"
+                    ),
+                  },
+                })
+              }
+            >
+              <Header.H2>{encaminhamentosRealizados.aggregate.count}</Header.H2>
+              <Text>Encaminhamentos Realizados</Text>
+            </StatsCard>
+          </Link>
+          {communityId === 40 && (
+            <Link to="/matchs">
+              <StatsCard
+                onClick={() =>
+                  dispatch({
+                    type: "relationships",
+                    value: {
+                      relationshipStatus: getOption(
+                        "Encaminhamento: Realizado para Serviço Público",
+                        "relationshipStatus"
+                      ),
+                    },
+                  })
+                }
+              >
+                <Header.H2>
+                  {encaminhamentosServicoPublico.aggregate.count}
+                </Header.H2>
+                <Text>Encaminhamentos para Serviço Público</Text>
+              </StatsCard>
+            </Link>
+          )}
+          <Link to="/matchs">
+            <StatsCard
+              onClick={() =>
+                dispatch({
+                  type: "relationships",
+                  value: {
+                    relationshipStatus: getOption(
+                      "Atendimento: Iniciado",
+                      "relationshipStatus"
+                    ),
+                  },
+                })
+              }
+            >
+              <Header.H2>{atendimentosIniciados.aggregate.count}</Header.H2>
+              <Text>Atendimento Iniciado</Text>
+            </StatsCard>
+          </Link>
+          <Link to="/pessoas">
+            <StatsCard
+              onClick={() => {
+                dispatch({
+                  type: "group",
+                  value: {
+                    label: volunteerGroup?.name || "",
+                    value: volunteerGroup?.id || 0,
+                  },
+                });
+                return dispatch({
+                  type: "individuals",
+                  value: {
+                    userStatus: getOption("Aprovada", "userStatus"),
+                  },
+                });
+              }}
+            >
+              <Header.H2>{newlyApprovedVolunteers.aggregate.count}</Header.H2>
+              <Text>Novas(os) voluntárias(os) aprovadas(os)</Text>
+            </StatsCard>
+          </Link>
+          <Link to="/pessoas">
+            <StatsCard
+              onClick={() => {
+                dispatch({
+                  type: "group",
+                  value: {
+                    label: volunteerGroup?.name || "",
+                    value: volunteerGroup?.id || 0,
+                  },
+                });
+                return dispatch({
+                  type: "individuals",
+                  value: {
+                    userStatus: getOption("Aprovada", "userStatus"),
+                    availability: getOption("Disponível", "availability"),
+                  },
+                });
+              }}
+            >
+              <Header.H2>{newlyAvailableVolunteers.aggregate.count}</Header.H2>
+              <Text>Novas(os) voluntárias(os) disponíveis</Text>
+            </StatsCard>
+          </Link>
+        </>
+      )}
+    </FetchWeeklyStats>
+  );
+}
