4f217401f8b2554a0407926edff9248959f30972
Viviane Dias <vivianedias@protonmail.com>
2020-10-23 17:25:16 -0300

    fix(redes): fix inconsistent subheader problem by using styleguide comp

--- packages/redes-client/src/components/Header.tsx
+++ packages/redes-client/src/components/Header.tsx
@@ -1,66 +1,48 @@
 import React from "react";
-import { Navigation, Tab, Header as Heading } from "bonde-components";
+import { Navigation, Tab, Header as Heading, SubHeader } from "bonde-components";
 import { Link, useRouteMatch } from "react-router-dom";
-import styled from "styled-components";
 
-const StyledNavigation = styled(Navigation)`
-  & > a {
-    color: inherit;
-    text-decoration: none;
-  }
-`;
-
-const Wrap = styled.div`
-  display: flex;
-  flex-direction: column;
-  background-color: rgb(0, 0, 0);
-  padding: 0px 60px;
-`;
-
-const Header = (): React.ReactElement => {
-  return (
-    <Wrap>
-      <Heading.H3 style={{ color: "#fff" }}>Redes</Heading.H3>
-      <StyledNavigation>
-        <Link to="/">
-          <Tab
-            active={
-              !!useRouteMatch({
-                path: "/",
-                exact: true,
-              })
-            }
-          >
-            Início
-          </Tab>
-        </Link>
-        <Link to="/pessoas">
-          <Tab
-            active={
-              !!useRouteMatch({
-                path: "/pessoas",
-                exact: false,
-              })
-            }
-          >
-            Pessoas
-          </Tab>
-        </Link>
-        <Link to="/matches">
-          <Tab
-            active={
-              !!useRouteMatch({
-                path: "/matches",
-                exact: false,
-              })
-            }
-          >
-            Relações
-          </Tab>
-        </Link>
-      </StyledNavigation>
-    </Wrap>
-  );
-};
+const Header = (): React.ReactElement =>
+  <SubHeader>
+    <Heading.H3>Redes</Heading.H3>
+    <Navigation>
+      <Link to="/">
+        <Tab
+          active={
+            !!useRouteMatch({
+              path: "/",
+              exact: true,
+            })
+          }
+        >
+          Início
+        </Tab>
+      </Link>
+      <Link to="/pessoas">
+        <Tab
+          active={
+            !!useRouteMatch({
+              path: "/pessoas",
+              exact: false,
+            })
+          }
+        >
+          Pessoas
+        </Tab>
+      </Link>
+      <Link to="/matches">
+        <Tab
+          active={
+            !!useRouteMatch({
+              path: "/matches",
+              exact: false,
+            })
+          }
+        >
+          Relações
+        </Tab>
+      </Link>
+    </Navigation>
+  </SubHeader>
 
 export default Header;
