9e745108a1c4f1f6348c1670c1a97694de2264bc
Viviane Dias <vivianedias@protonmail.com>
2020-10-23 19:05:47 -0300

    feat(redes): update UpdateStatus select layout

--- packages/redes-client/src/pages/Individuals/scenes/BtnSearchMatch.tsx
+++ packages/redes-client/src/pages/Individuals/scenes/BtnSearchMatch.tsx
@@ -5,16 +5,15 @@ import { Button } from "bonde-components"
 const BtnSearchMatch = ({ original }: { original: { email: string; userStatus: string; availability: string } }): React.ReactElement =>
   <div
     style={{
-      display: "grid",
-      gridTemplateColumns: "200px",
+      display: "flex",
       justifyContent: "center",
+      width: "100%"
     }}
   >
     <Link
       style={{ textDecoration: "none" }}
       to={{
         pathname: "/match",
-        search: `?email=${original.email}`,
         state: { ...original },
       }}
     >
