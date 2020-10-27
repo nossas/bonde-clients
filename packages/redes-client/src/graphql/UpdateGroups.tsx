55d718fc958d7cbd2c6fad5962898cff08a69778
Viviane Dias <vivianedias@protonmail.com>
2020-10-27 13:46:11 -0300

    feat(redes): update redes settings after form submission

--- packages/redes-client/src/graphql/UpdateGroups.tsx
+++ packages/redes-client/src/graphql/UpdateGroups.tsx
@@ -3,9 +3,12 @@ import { gql } from "bonde-core-tools";
 export default gql`
   mutation updateRedeGroup(
     $redeGroupId: Int_comparison_exp!
-    $updatedGroup: rede_groups_set_input!
+    $updatedSettings: jsonb!
   ) {
-    update_rede_groups(where: { id: $redeGroupId }, _set: $updatedGroup) {
+    update_rede_groups(
+      where: { id: $redeGroupId }
+      _append: { settings: $updatedSettings }
+    ) {
       returning {
         id
         settings
