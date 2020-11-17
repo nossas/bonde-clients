8399f6265a835ca820db27d75be45e14161034ea
Igor Santos <igr.exe@gmail.com>
2020-11-17 11:46:08 -0300

    chore(canary): add validate to email template on setings community

--- packages/canary-client/src/scenes/Community/Settings/index.tsx
+++ packages/canary-client/src/scenes/Community/Settings/index.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
-import { InputField, Button, Tooltip, Success } from 'bonde-components';
+import { InputField, Button, Tooltip, Success, Validators } from 'bonde-components';
 import { useTranslation } from "react-i18next";
 import { Container, Row, Col } from 'react-grid-system';
 import UploadField from "../../../components/UploadFile";
 import CommunityForm from '../BaseForm';
 import Panel from '../Panel';
+
+const { isEmail } = Validators;
+
+export const isValidFromEmail = (value: any) => {
+  const regex = /^[a-zà-úA-ZÀ-Ú0-9 ]+<(.*)>$/
+  if (regex.test(value)) {
+    const email = value.match(regex)[1]
+    return isEmail('E-mail inválido')(email);
+  } else {
+    return 'Padrão inválido. Ex: Nome do remente <email@host.com>';
+  }
+}
 
 const SettingsPage = () => {
   const { t } = useTranslation('community');
@@ -51,6 +63,7 @@ const SettingsPage = () => {
                   />
                 )}
                 placeholder={t('info.form.fields.email_template_from.placeholder')}
+                validate={isValidFromEmail}
               />
               <Row justify='end'>
                 <Col xs={3}>
