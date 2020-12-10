649058d306580462173893aa7573ce8b63daff91
Viviane Dias <vivianedias@protonmail.com>
2020-12-10 17:33:45 -0300

    feat(canary): mount multiple target form

---
+++ packages/canary-client/src/scenes/WidgetActions/ConfigurePressureTargets/MultipleTargetsForm.tsx
@@ -0,0 +1,99 @@
+import React from "react";
+import { css } from "styled-components/macro";
+import {
+  Card,
+  Header,
+  ConnectedForm,
+  InputField,
+  TextareaField,
+  Button,
+  Label
+} from "bonde-components";
+import { useMutation } from "bonde-core-tools";
+import UPDATE_WIDGET_SETTINGS from "../UpdateWidgetSettings";
+
+type Form = {
+  subject: string;
+  body: string;
+};
+
+type Props = {
+  widgetId: number;
+};
+
+const SimpleTargetsForm = ({ widgetId }: Props): React.ReactElement => {
+  const [saveSimpleTargets] = useMutation(UPDATE_WIDGET_SETTINGS);
+  const onSubmit = async (widgetId: number, { subject, body }: Form) => {
+    try {
+      await saveSimpleTargets({
+        variables: {
+          id: { _eq: widgetId },
+          settings: {
+            pressure_subject: subject,
+            pressure_body: body,
+          },
+        },
+      });
+    } catch (e) {
+      console.log(e);
+      return JSON.stringify(e);
+    }
+  };
+  return (
+    <>
+      <Card padding={{ x: 40, y: 30 }}>
+        <Header.H4 style={{ marginBottom: "15px" }}>Definir alvos</Header.H4>
+        <ConnectedForm onSubmit={(e) => onSubmit(widgetId, e)}>
+          {({ submitting }: any) => (
+            <>
+              <div
+                css={css`
+                  position: absolute;
+                  bottom: 680px;
+                  left: 1090px;
+                  width: 150px;
+                `}
+              >
+                <Button type="submit" disabled={submitting}>
+                  Salvar
+                </Button>
+              </div>
+              <div
+                css={css`
+                  display: grid;
+                  grid-row-gap: 20px;
+                `}
+              >
+                <InputField
+                  name="name"
+                  placeholder="Ex: Selecione seu estado"
+                  label="NOME DO CAMPO DE SELEÇÃO"
+                />
+                <Label>ADICIONAR GRUPO DE ALVOS</Label>
+                <div
+                  css={css`
+                    border: 1px solid #eee;
+                  `}
+                >
+                  <Button secondary> + ADD GRUPO DE ALVOS</Button>
+                </div>
+                <InputField
+                  name="subject"
+                  placeholder="Escreva um assunto"
+                  label="ASSUNTO DO EMAIL PARA OS ALVOS"
+                />
+                <TextareaField
+                  name="body"
+                  placeholder="Escreva aqui o email..."
+                  label="CORPO DO EMAIL PARA OS ALVOS"
+                />
+              </div>
+            </>
+          )}
+        </ConnectedForm>
+      </Card>
+    </>
+  );
+};
+
+export default SimpleTargetsForm;
--- packages/canary-client/src/scenes/WidgetActions/ConfigurePressureTargets/index.tsx
+++ packages/canary-client/src/scenes/WidgetActions/ConfigurePressureTargets/index.tsx
@@ -1,41 +1,101 @@
 import React, { useState } from "react";
-import { Header, Text } from "bonde-components";
-import { css } from 'styled-components/macro'
+import { Header } from "bonde-components";
+import { css } from "styled-components/macro";
 
-import SimpleTargetsForm from './SimpleTargetsForm'
+import SimpleTargetsForm from "./SimpleTargetsForm";
+import MultipleTargetsForm from "./MultipleTargetsForm";
 
-const ConfigurePressureTargets = (): React.ReactElement => {
-  const [pressureType, setPressureType] = useState("group");
+// const RadioInput = styled.span`
+//   display: inline-block;
+//   position: relative;
+//   padding: 0 6px;
+//   margin: 10px 0 0;
+
+//   & > input[type="radio"] {
+//     display: none;
+//   }
+
+//   & > label {
+//     color: #666;
+//     font-weight: normal;
+//   }
+
+//   & > label:before {
+//     content: " ";
+//     display: inline-block;
+//     position: relative;
+//     top: 5px;
+//     margin: 0 5px 0 0;
+//     width: 20px;
+//     height: 20px;
+//     border-radius: 11px;
+//     border: 2px solid #ee0099;
+//     background-color: transparent;
+//   }
+
+//   & > input[type="radio"]:checked + label:after {
+//     border-radius: 11px;
+//     width: 12px;
+//     height: 12px;
+//     position: absolute;
+//     top: 9px;
+//     left: 10px;
+//     content: " ";
+//     display: block;
+//     background: #ee0099;
+//   }
+// `;
+
+type Props = {
+  widgetId: number;
+};
+
+const ConfigurePressureTargets = ({ widgetId }: Props): React.ReactElement => {
+  const [pressureType, setPressureType] = useState("groups");
   return (
     <>
-      <Header.H5 style={{ fontWeight: 600 }}>TIPO DE PRESSÃO</Header.H5>
-      <div css={css`
-        display: flex;
-      `}>
-        <input
-          type="radio"
-          value="group"
-          checked={pressureType === "group"}
-          name="pressureType"
-          onChange={(e) => setPressureType(e.target.value)}
-        />{" "}
-        <Text>Um grupo de alvos</Text>
-        <input
-          type="radio"
-          value="groups"
-          checked={pressureType === "groups"}
-          name="pressureType"
-          onChange={(e) => setPressureType(e.target.value)}
-        />{" "}
-        <Text>Mais de um grupo de alvos (Ex: Por estado)</Text>
+      <Header.H5 style={{ fontWeight: 600, marginTop: "15px" }}>
+        TIPO DE PRESSÃO
+      </Header.H5>
+      <div
+        css={css`
+          display: flex;
+          margin-bottom: 30px;
+        `}
+      >
+        {/* <RadioInput> */}
+          <input
+            type="radio"
+            name="pressureType"
+            value="group"
+            checked={pressureType === "group"}
+            onChange={(e) => setPressureType(e.target.value)}
+          />
+          <label>Um grupo de alvos</label>
+        {/* </RadioInput> */}
+        {/* <RadioInput> */}
+          <input
+            type="radio"
+            name="pressureType"
+            value="groups"
+            checked={pressureType === "groups"}
+            onChange={(e) => setPressureType(e.target.value)}
+          />
+          <label>Mais de um grupo de alvos (Ex: Por estado)</label>
+        {/* </RadioInput> */}
       </div>
-      <div css={css`
-        display: grid;
-        grid-template-columns: 55% 45%;
-        grid-column-gap: 20px;
-        height: 100%;
-      `}>
-        {pressureType === 'group' && <SimpleTargetsForm />}
+      <div
+        css={css`
+          display: grid;
+          grid-template-columns: 55% 45%;
+          grid-column-gap: 20px;
+          height: 100%;
+        `}
+      >
+        {pressureType === "group" && <SimpleTargetsForm widgetId={widgetId} />}
+        {pressureType === "groups" && (
+          <MultipleTargetsForm widgetId={widgetId} />
+        )}
       </div>
     </>
   );
