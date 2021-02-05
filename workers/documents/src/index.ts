bcb1928bc88a50ef6d0fa916f14fc0a068dda40e
Lucas Pirola <lucaspirola@gmail.com>
2021-02-05 11:55:49 -0300

    feat: Move to bee-queue

--- workers/documents/src/index.ts
+++ workers/documents/src/index.ts
@@ -1,6 +1,6 @@
 import { Command } from 'commander';
 import * as db from './db';
-import queue from './queue';
+import { queue } from './queue';
 
 const program = new Command();
 program.version('0.0.1');
@@ -9,11 +9,11 @@ program.version('0.0.1');
 // Add nested commands using `.command()`.
 const brew = program.command('brew');
 brew
-.command('tea')
-.action(async () => {
-  await db.check();
-  const Model = db.model();
-  queue.send("createmessages");
+  .command('tea')
+  .action(async () => {
+    await db.check();
+    const Model = db.model();
+    // queue.send("createmessages");
 
     const formEntries = await db.getActions(8, 'form', Model.FormEntry, Model)
     console.log(`There are ${(formEntries.count)} form entries`);
@@ -23,33 +23,37 @@ brew
         m.blocks.forEach((b: any) => {
           b.widgets.forEach((w: any) => {
             w.form_entries.forEach((fe: any) => {
-              console.log(fe.id)
-                  // {
-                  //   id: text,
-                  //   community_id: number,
-                  //   community_name: text,
-                  //   mobilization_id: number,
-                  //   mobilization_name: text,
-                  //   activist_id: number,
-                  //   activist_email: text,
-                  //   activist_ip: text,
-                  //   activist_first_name: text,
-                  //   activist_last_name: text,
-                  //   activist_fullname: text,
-                  //   activist_city: text,
-                  //   activist_state: text,
-                  //   activist_phone: text,
-                  //   widget_id: number,
-                  //   action_id: number,
-                  //   action_type: text,
-                  //   action_data: text,
-                  //   action_created: date,
-                  //   donation_value: number,
-                  //   donation_recurring: number,
-                  //   donation_status: text,
-                  //   pressure_targets_count: number,
-                  //   form_custom_field_value: text
-                  // }
+              const job = queue.createJob(({
+                id: "",
+                community_id: c.id,
+                community_name: c.name,
+                mobilization_id: m.id,
+                mobilization_name: m.name,
+                activist_id: "",
+                activist_email: "",
+                activist_ip: "",
+                activist_first_name: "",
+                activist_last_name: "",
+                activist_fullname: "",
+                activist_city: "",
+                activist_state: "",
+                activist_phone: "",
+                widget_id: w.id,
+                action_id: fe.id,
+                action_type: "form",
+                action_data: "",
+                action_created: fe.created_at,
+                donation_value: "",
+                donation_recurring: "",
+                donation_status: "",
+                pressure_targets_count: "",
+                form_custom_field_value: ""
+              }));
+              job.save();
+
+              job.on('succeeded', (result: any) => {
+                console.log(`Received result for job ${job.id}:` ,result);
+              });
             });
           });
         });
--- workers/documents/src/queue.ts
+++ workers/documents/src/queue.ts
@@ -1,77 +1,36 @@
-const fs = require("fs");
-const RSMQWorker = require("rsmq-worker");
+const Queue = require('bee-queue');
 
-export const fnCheck = function (msg: any) {
-    // check function to not exceed the message if the content is `createmessages`
-    if (msg.message === "createmessages") {
-        return true
-    }
-    return false
-}
+export const queue = new Queue('example');
 
-const worker = new RSMQWorker("myqueue", {
-    interval: [.1, 1],				// wait 100ms between every receive and step up to 1,3 on empty receives
-    invisibletime: 2,						// hide received message for 5 sec
-    maxReceiveCount: 2,					// only receive a message 2 times until delete
-    autostart: true,						// start worker on init
-    customExceedCheck: fnCheck	// set the custom exceed check
+// Process jobs from as many servers or processes as you like
+queue.process(function (job: any, done: any) {
+  console.log(`Processing job`, job.data);
+  return done(null, job.data);
 });
 
-// Listen to errors
-worker.on('error', function (err: any, msg: any) {
-    console.log("ERROR", err, msg.id);
-});
-worker.on('timeout', function (msg: any) {
-    console.log("TIMEOUT", msg.id, msg.rc);
-});
-
-// handle exceeded messages
-// grab the internal rsmq instance
-export const rsmq = worker._getRsmq();
-worker.on('exceeded', function (msg: any) {
-    console.log("EXCEEDED", msg.id);
-    // NOTE: make sure this queue exists
-    rsmq.sendMessage("YOUR_EXCEEDED_QUEUE", msg, function (err: any, resp: any) {
-        if (err) {
-            console.error("write-to-exceeded-queue", err)
-        }
-    });
-});
-
-// listen to messages
-worker.on("message", function (message: any, next: any, id: number) {
-
-    console.log("message", message);
-
-    if (message === "createmessages") {
-        next(false)
-        worker.send(JSON.stringify({ type: "writefile", filename: "./test.txt", txt: "Foo Bar" }));
-        worker.send(JSON.stringify({ type: "deletefile", filename: "./test.txt" }));
-        return
-    }
-
-    const _data = JSON.parse(message)
-    switch (_data.type) {
-        case "writefile":
-            fs.writeFile(_data.filename, _data.txt, function (err: any) {
-                if (err) {
-                    next(err);
-                } else {
-                    next()
-                }
-            });
-            break;
-        case "deletefile":
-            fs.unlink(_data.filename, function (err: any) {
-                if (err) {
-                    next(err);
-                } else {
-                    next()
-                }
-            });
-            break;
-    }
-
-});
-
-export default worker;
+// {
+//     id: text,
+//     community_id: number,
+//     community_name: text,
+//     mobilization_id: number,
+//     mobilization_name: text,
+//     activist_id: number,
+//     activist_email: text,
+//     activist_ip: text,
+//     activist_first_name: text,
+//     activist_last_name: text,
+//     activist_fullname: text,
+//     activist_city: text,
+//     activist_state: text,
+//     activist_phone: text,
+//     widget_id: number,
+//     action_id: number,
+//     action_type: text,
+//     action_data: text,
+//     action_created: date,
+//     donation_value: number,
+//     donation_recurring: number,
+//     donation_status: text,
+//     pressure_targets_count: number,
+//     form_custom_field_value: text
+// }
