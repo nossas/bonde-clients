32a2c206f585c3be4b58f048fca3e8e2df1c563f
Lucas Pirola <lucaspirola@gmail.com>
2021-05-13 15:17:09 -0300

    feat: Add worker to process form entries queue

--- workers/activists/ecosystem.config.js
+++ workers/activists/ecosystem.config.js
@@ -5,10 +5,12 @@ module.exports = {
       script: "./worker.js",
       watch: true,
       env: {
+        DATABASE_URL:
+          "postgres://monkey_user:monkey_pass@localhost:49153/bonde",
         NODE_ENV: "development",
         PGUSER: "monkey_user",
         PGPASSWORD: "monkey_pass",
-        PGPORT: "49165",
+        PGPORT: "49153",
         PGDATABASE: "bonde",
       },
       env_production: {
@@ -20,10 +22,12 @@ module.exports = {
       script: "./server.js",
       watch: true,
       env: {
+        DATABASE_URL:
+          "postgres://monkey_user:monkey_pass@localhost:49153/bonde",
         NODE_ENV: "development",
         PGUSER: "monkey_user",
         PGPASSWORD: "monkey_pass",
-        PGPORT: "49165",
+        PGPORT: "49153",
         PGDATABASE: "bonde",
       },
       env_production: {
