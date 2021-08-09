5241329fa542ae24e88403a050d3e89efb4bad52
Gloria Teodoro <gloriateodoro1@gmail.com>
2021-08-09 18:32:08 -0300

    feat: add and export icon

--- clients/packages/canary-client/src/scenes/Community/Domains/Icons/index.ts
+++ clients/packages/canary-client/src/scenes/Community/Domains/Icons/index.ts
@@ -3,3 +3,5 @@ export { default as CertificateDomainIcon } from './CertificateDomainIcon';
 export { default as ConnectDomainIcon } from './ConnectDomainIcon';
 export { default as InsertDomainIcon } from './InsertDomainIcon';
 export { default as PropagateDomainIcon } from './PropagateDomainIcon';
+export { default as OptimizedPressure } from './OptimizedPressure';
+export { default as Targets } from './Targets';
