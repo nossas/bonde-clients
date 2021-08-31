e7f46698d4d0ba8a1fda1b029c1526e565850c6c
Gloria Teodoro <gloriateodoro1@gmail.com>
2021-08-30 22:49:05 -0300

    feat: change button size and button spacing

--- clients/packages/accounts-client/src/scenes/LoginPage/Form.tsx
+++ clients/packages/accounts-client/src/scenes/LoginPage/Form.tsx
@@ -20,41 +20,41 @@ const LoginForm: React.FC<LoginFormProps> = ({ onSubmit }) => {
   const { t } = useTranslation('auth');
 
   return (
-      <ConnectedForm onSubmit={onSubmit}>
-        {({ submitting }: any) => (
-          <>
-            <InputField
-              name='email'
-              label={t('fields.email.label')}
-              placeholder={t('fields.email.placeholder')}
-              validate={composeValidators(
-                required(t('fields.email.errors.isEmpty')),
-                isEmail(t('fields.email.errors.isEmail'))
-              )}
-            />
-            <InputField
-              name='password'
-              label={t('fields.password.label')}
-              placeholder={t('fields.password.placeholder')}
-              type='password'
-              validate={composeValidators(
-                required(t('fields.password.errors.isEmptyLogin')),
-                min(6, t('fields.password.errors.min'))
-              )}
-            />
+    <ConnectedForm onSubmit={onSubmit}>
+      {({ submitting }: any) => (
+        <>
+          <InputField
+            name='email'
+            label={t('fields.email.label')}
+            placeholder={t('fields.email.placeholder')}
+            validate={composeValidators(
+              required(t('fields.email.errors.isEmpty')),
+              isEmail(t('fields.email.errors.isEmail'))
+            )}
+          />
+          <InputField
+            name='password'
+            label={t('fields.password.label')}
+            placeholder={t('fields.password.placeholder')}
+            type='password'
+            validate={composeValidators(
+              required(t('fields.password.errors.isEmptyLogin')),
+              min(6, t('fields.password.errors.min'))
+            )}
+          />
 
-            <Stack direction="row" alignItems="center" spacing={4}>
-              <LinkStyled component={Link} to='/forget-password'>
-                {t('links.forgetPassword')}
-              </LinkStyled>
+          <Stack direction="row" alignItems="center" justifyContent="space-between">
+            <LinkStyled component={Link} to='/forget-password'>
+              {t('links.forgetPassword')}
+            </LinkStyled>
 
-              <Button type='submit' disabled={submitting}>
-                {t('button.submit')}
-              </Button>
-            </Stack>
-          </>
-        )}
-      </ConnectedForm>
+            <Button type='submit' disabled={submitting} size="xs">
+              {t('button.submit')}
+            </Button>
+          </Stack>
+        </>
+      )}
+    </ConnectedForm>
   );
 };
 
