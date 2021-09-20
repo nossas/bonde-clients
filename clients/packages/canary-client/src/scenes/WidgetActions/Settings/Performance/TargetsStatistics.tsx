597d83dffa8ebc2328553db4d67f9eb2f018e528
Igor Santos <igr.exe@gmail.com>
2021-09-20 18:10:03 -0300

    feat(canary): count numbers to targets statistics table

--- clients/packages/canary-client/src/scenes/WidgetActions/Settings/Performance/TargetsStatistics.tsx
+++ clients/packages/canary-client/src/scenes/WidgetActions/Settings/Performance/TargetsStatistics.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { gql, useQuery } from "bonde-core-tools";
 import {
+  // Box,
   Heading,
   Stack,
   Table,
@@ -28,19 +29,57 @@ const ACTIVITY_FEED_GQL = gql`
   }
 `;
 
+type ActivityFeed = {
+  email: string
+  total: number
+  events: {
+    total: number
+    event_type: string
+  }[]
+}
+
 type Props = {
   widgetId: number
   offset?: number
 }
 
-const EVENTS_TYPES = [
-  "deferred",
-  "bounce",
-  "open",
-  "delivered",
-  "processed",
-  "dropped"
-]
+// const EVENTS_TYPES = [
+//   "deferred",
+//   "bounce",
+//   "open",
+//   "delivered",
+//   "processed",
+//   "dropped",
+//   "blocked",
+//   "click",
+//   "spamreport"
+// ]
+
+const OpenedLabel: React.FC<{ activityFeed: ActivityFeed }> = ({ activityFeed }) => {
+  const isOpened = activityFeed.events.filter(
+    (evt) => evt.event_type === "open" || evt.event_type === "click"
+  ).length > 0;
+
+  return (
+    <Button variant="tag" colorScheme={isOpened ? "green" : "yellow"}>
+      {isOpened ? "Abriu" : "Não abriu"}
+    </Button>
+  )
+}
+
+const DeliveredLabel: React.FC<{ activityFeed: ActivityFeed }> = ({ activityFeed }) => {
+  const total = activityFeed.events.filter((evt) => [
+    "open",
+    "delivered",
+    "click"
+  ].filter(e => e === evt.event_type).length).map((evt) => evt.total).reduce((a, b) => a + b, 0);
+
+  return (
+    <span>
+      {total ? `${Math.round((total / activityFeed.total) * 100)}% entregue` : "0% entregue"}
+    </span>
+  );
+}
 
 const TargetsStatistics: React.FC<Props> = ({ widgetId, offset }) => {
   const { data, loading, errors, fetchMore } = useQuery(ACTIVITY_FEED_GQL, {
@@ -74,23 +113,31 @@ const TargetsStatistics: React.FC<Props> = ({ widgetId, offset }) => {
         <Thead>
           <Tr>
             <Th>Email</Th>
-            <Th isNumeric>Enviados</Th>
-            {EVENTS_TYPES.map((label) => (<Th key={label} isNumeric>{label}</Th>))}
+            <Th>Enviados</Th>
+            <Th>Entregues</Th>
+            <Th>Abertura</Th>
+            {/* {EVENTS_TYPES.map((label) => (<Th key={label} isNumeric>{label}</Th>))} */}
           </Tr>
         </Thead>
         <Tbody>
-          {data.activity_feed.data.map((activityFeed: any, index: number) => (
+          {data.activity_feed.data.map((activityFeed: ActivityFeed, index: number) => (
             <Tr key={`activity-feed-${index}`}>
               <Td>{activityFeed.email}</Td>
-              <Td isNumeric>{activityFeed.total}</Td>
-              {EVENTS_TYPES.map((eventType: string, index: number) => {
+              <Td>{`${activityFeed.total} envios`}</Td>
+              <Td>
+                <DeliveredLabel activityFeed={activityFeed} />
+              </Td>
+              <Td>
+                <OpenedLabel activityFeed={activityFeed} />
+              </Td>
+              {/* {EVENTS_TYPES.map((eventType: string, index: number) => {
                 const event = activityFeed.events.filter((evt: any) => evt.event_type === eventType)[0]
 
                 if (event) {
                   return <Td key={index} isNumeric>{event.total}</Td>
                 }
                 return <Td key={index} isNumeric>0</Td>
-              })}
+              })} */}
             </Tr>
           ))}
         </Tbody>
