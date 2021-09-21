6e126b22a5910e9b6c1dc52e58740ef517eb898a
Igor Santos <igr.exe@gmail.com>
2021-09-21 14:59:20 -0300

    feat(canary): add Cards to render events numbers

--- clients/packages/canary-client/src/scenes/WidgetActions/Settings/Performance/index.tsx
+++ clients/packages/canary-client/src/scenes/WidgetActions/Settings/Performance/index.tsx
@@ -1,15 +1,17 @@
 import React from "react";
-import { Flex, Heading, Stack, Text } from "bonde-components";
+import { Stack } from "bonde-components";
 import TargetsStatistics from "./TargetsStatistics";
 import { Widget } from "../../FetchWidgets";
+import Card from "./Card";
+import EventsCards from "./EventsCards";
 
 const CARDS_LABELS = [
   "Pressões",
   "Alvos",
-  "Envios totais",
-  "Entregues",
-  "SPAM",
-  "Falha"
+  // "Envios totais",
+  // "Entregues",
+  // "SPAM",
+  // "Falha"
 ]
 
 type Props = {
@@ -22,22 +24,13 @@ const PerformanceScene: React.FC<Props> = ({ widget }) => {
     <Stack spacing={6} mt={4}>
       <Stack direction="row" spacing={4}>
         {CARDS_LABELS.map((label: string, index: number) => (
-          <Stack key={`card-item-${index}`} flex={1} spacing={2} maxW="204px" minH="103px">
-            <Heading as="h5" size="sm" color="gray.300" textTransform="uppercase">
-              {label}
-            </Heading>
-            <Flex
-              bg="white"
-              boxShadow="sm"
-              flex={1}
-              px={6}
-              py={2}
-              align="end"
-            >
-              <Text fontSize="2xl" fontWeight="extrabold">{Math.round(Math.random() * 100000)}</Text>
-            </Flex>
-          </Stack>
+          <Card
+            key={`card-item-${index}`}
+            label={label}
+            value={label === "Alvos" ? widget.settings.targets?.length : Math.round(Math.random() * 100000)}
+          />
         ))}
+        <EventsCards widget={widget} />
       </Stack>
       <TargetsStatistics widgetId={widget.id} />
     </Stack>
