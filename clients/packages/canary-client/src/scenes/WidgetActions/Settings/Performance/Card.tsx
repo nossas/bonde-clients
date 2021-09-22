411b3c54cd1a5fd7cf41415bca93ecac353769db
Igor Santos <igr.exe@gmail.com>
2021-09-22 13:47:36 -0300

    feat(canary): add Tooltip to explain events numbers

--- clients/packages/canary-client/src/scenes/WidgetActions/Settings/Performance/Card.tsx
+++ clients/packages/canary-client/src/scenes/WidgetActions/Settings/Performance/Card.tsx
@@ -1,24 +1,39 @@
 import React from "react";
-import { Stack, Heading, Flex, Text } from "bonde-components";
+import {
+  Stack,
+  Heading,
+  Flex,
+  Text,
+  Tooltip,
+  InfoIcon
+} from "bonde-components";
 
 type Props = {
   label: string
+  helpText?: string
   value: number | string
 }
 
-const Card: React.FC<Props> = ({ label, value }) => (
-  <Stack flex={1} spacing={2} maxW="204px" minH="103px">
-    <Heading
-      as="h5"
-      size="xs"
-      fontWeight="normal"
-      color="gray.400"
-      textTransform="uppercase">
-      {label}
-    </Heading>
+const Card: React.FC<Props> = ({ label, helpText, value }) => (
+  <Flex direction="column" flex={1} maxW="204px" minH="123px">
+    <Stack direction="row" spacing={2} minH={6}>
+      <Heading
+        as="h5"
+        size="xs"
+        fontWeight="normal"
+        color="gray.400"
+        textTransform="uppercase">
+        {label}
+      </Heading>
+      {helpText && (
+        <Tooltip label={helpText} maxW="220px">
+          <InfoIcon boxSize={6} />
+        </Tooltip>
+      )}
+    </Stack>
     <Flex
       bg="white"
-      boxShadow="sm"
+      boxShadow="sm"                                                                                                                                
       flex={1}
       px={6}
       py={2}
@@ -32,7 +47,7 @@ const Card: React.FC<Props> = ({ label, value }) => (
         {value}
       </Text>
     </Flex>
-  </Stack>
+  </Flex>
 );
 
 export default Card;
