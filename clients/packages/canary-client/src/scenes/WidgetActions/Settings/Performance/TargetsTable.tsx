d468e24879af0720b42b52003c540e5d13010bae
Gloria Teodoro <gloriateodoro1@gmail.com>
2021-10-05 10:38:43 -0300

    feat: add target table for when widget has no sendgrid events

---
+++ clients/packages/canary-client/src/scenes/WidgetActions/Settings/Performance/TargetsTable.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import {
+  Heading,
+  Stack,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+} from "bonde-components";
+import { ActivityFeedEmail } from "./hooks/usePerformance";
+
+interface Props {
+  aggregateEmails: ActivityFeedEmail[]
+  activeTargets: string[]
+}
+
+interface ActivityFeedEmailWithDisabled extends ActivityFeedEmail {
+  disabled: boolean
+}
+
+const TargetsTable: React.FC<Props> = ({ aggregateEmails, activeTargets }) => {
+  const activeEmails = activeTargets.map((target) => (target.match(/^[\w ]+<([\w.@]+)>$/) || [])[1]);
+  const targetsCount = aggregateEmails.length
+
+  return (
+    <Stack spacing={4}>
+      <Heading
+        as="h5"
+        size="xs"
+        fontWeight="normal"
+        color="gray.400"
+        textTransform="uppercase"
+      >
+        Todos os alvos ({targetsCount})
+      </Heading>
+      <Table variant="simple" bg="white">
+        <Thead>
+          <Tr>
+            <Th>Email</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {aggregateEmails
+            .map((activityFeed: ActivityFeedEmail) => ({
+              ...activityFeed,
+              disabled: activeEmails.findIndex((email) => email === activityFeed.email) === -1
+            }))
+            .sort((a) => a.disabled ? 1 : -1)
+            .map((activityFeed: ActivityFeedEmailWithDisabled, index: number) => {
+              return (
+                <Tr key={`activity-feed-${index}`} color={activityFeed.disabled ? "gray.300" : "inherit"}>
+                  <Td>{activityFeed.email}</Td>
+                </Tr>
+              );
+            })
+          }
+        </Tbody>
+      </Table>
+    </Stack>
+  );
+}
+
+export default TargetsTable;
