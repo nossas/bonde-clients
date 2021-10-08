25c14f851c52246af901c7951614d03527637d52
Igor Santos <igr.exe@gmail.com>
2021-10-08 11:32:48 -0300

    fix(canary): accents name to add targets in Configure Targets Scene

--- clients/packages/canary-client/src/scenes/WidgetActions/Settings/Performance/TargetsStatistics.tsx
+++ clients/packages/canary-client/src/scenes/WidgetActions/Settings/Performance/TargetsStatistics.tsx
@@ -88,7 +88,7 @@ interface ActivityFeedEmailWithDisabled extends ActivityFeedEmail {
 }
 
 const TargetsStatistics: React.FC<Props> = ({ aggregateEmails, activeTargets }) => {
-  const activeEmails = activeTargets.map((target) => (target.match(/^[\w ]+<([\w.@]+)>$/) || [])[1]);
+  const activeEmails = activeTargets.map((target) => (target.match(/^[A-zÀ-ú0-9 ]+<([\w.@]+)>$/) || [])[1]);
   const targetsCount = aggregateEmails.length
 
   // Transforma todos e-mails ativos em eventos
