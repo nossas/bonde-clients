ad598bd592499956f5fa081d66b50af0419fdbb0
Igor Santos <igr.exe@gmail.com>
2021-10-15 17:39:10 -0300

    chore(components): refactor RadioField with tests

--- monorepo/libs/components/src/form/RadioField.tsx
+++ monorepo/libs/components/src/form/RadioField.tsx
@@ -1,33 +1,34 @@
 import React from "react";
-import { FormControl, FormLabel, Flex, Stack, RadioGroup } from "@chakra-ui/react";
+import { Stack, RadioGroup } from "@chakra-ui/react";
 import { useField } from "react-final-form";
-import Hint from './Hint';
+import FormField from "./FormField";
 
-type RadioFieldProps = {
+interface RadioFieldProps extends Record<string, any> {
   children: any
-  label: string
+  label?: string
+  helpText?: string
   name: string
   direction?: "row" | "column"
 }
 
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-const RadioField = ({ children, label, name, direction }: RadioFieldProps) => {
-  const { input, meta } = useField(name);
+const RadioField: React.FC<RadioFieldProps> = ({
+  children,
+  label,
+  helpText,
+  name,
+  direction,
+  ...config
+}) => {
+  const { input, meta } = useField(name, config);
 
   return (
-    <FormControl isInvalid={(meta.error || meta.submitError) && meta.touched} mb={4}>
-      <Flex direction="row" justify="space-between">
-        <FormLabel>{label}</FormLabel>
-        {(meta.error || meta.submitError) && meta.touched && (
-          <Hint color="error">{meta.error || meta.submitError}</Hint>
-        )}
-      </Flex>
+    <FormField label={label} helpText={helpText} meta={meta}>
       <RadioGroup onChange={input.onChange} value={input.value}>
         <Stack direction={direction} spacing={4}>
           {children}
         </Stack>
       </RadioGroup>
-    </FormControl>
+    </FormField>
   );
 }
 
