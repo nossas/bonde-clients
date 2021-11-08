b02e3e6a6426677851f60a6fe673ad6f874f7d44
Igor Santos <igr.exe@gmail.com>
2021-11-08 20:25:20 -0300

    chore(admin): remove unused files

--- monorepo/clients/admin/src/accounts/src/index.tsx
+++
@@ -1,8 +0,0 @@
-import ReactDOM from "react-dom";
-import App from "./App";
-import "./i18n";
-
-ReactDOM.render(
-  <App />,
-  document.querySelector("#root")
-);
--- monorepo/clients/admin/src/api/getFruits.ts
+++
@@ -1,7 +0,0 @@
-import type { IFruit } from 'types'
-
-export default async function getFruits(): Promise<IFruit[]> {
-	return (
-		await fetch('https://614c99f03c438c00179faa84.mockapi.io/fruits')
-	).json() as Promise<IFruit[]>
-}
