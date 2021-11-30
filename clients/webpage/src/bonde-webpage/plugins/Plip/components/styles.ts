d0b7126d00e449c05a6ef5f44610184b71979757
Gloria Teodoro <gloriateodoro1@gmail.com>
2021-11-30 08:33:20 -0300

    feat: add pdf button styles

---
+++ clients/webpage/src/bonde-webpage/plugins/Plip/components/styles.ts
@@ -0,0 +1,25 @@
+import styled from '@emotion/styled';
+
+export const Wrap = styled.div`
+  background-color: #ffffff;
+  padding: 2rem;
+  border-radius: 3px;
+`;
+
+export const Header = styled.h3`
+  color: #FFFFFF;
+  font-family: Nunito Sans;
+
+  font-weight: extrabold;
+  text-align: center;
+  font-size: 13px;
+`;
+
+export const IconWrapper = styled.div`
+display: inline-block;
+padding-inline: 0.5rem;
+`;
+
+export const WrapButton = styled.div`
+  display: grid;
+`;
