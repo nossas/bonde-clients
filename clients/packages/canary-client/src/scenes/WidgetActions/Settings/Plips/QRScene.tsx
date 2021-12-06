848820bf18129842ea2a78d54b37121496466ee8
Igor Santos <igr.exe@gmail.com>
2021-12-06 13:57:07 -0300

    chore(canary): update scene to check plip form

--- clients/packages/canary-client/src/scenes/WidgetActions/Settings/Plips/QRScene.tsx
+++ clients/packages/canary-client/src/scenes/WidgetActions/Settings/Plips/QRScene.tsx
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import QrReader from "react-qr-reader";
+import { Flex, Text, ArrowLeftIcon, IconButton, Stack } from "bonde-components";
+import styled from "@emotion/styled";
+import QRCodeIcon from "./QRCodeIcon";
+
+const Styles = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  .qr-code-reader > section > div {
+    box-shadow: none;
+  }
+`
 
 const QRCode = () => {
-  const [result, setResult] = React.useState();
+  const [_result, setResult] = useState();
 
   const handleScan = (data: any) => {
     if (data) setResult(data);
@@ -13,17 +25,28 @@ const QRCode = () => {
   }
   
   return (
-    <div>
-      <h2>QR Code page</h2>
-      <hr />
+    <Styles>
+      <Flex align="center" justify="space-between" mb={4}>
+        <IconButton
+          icon={<ArrowLeftIcon />}
+          variant="ghost"
+          colorScheme="gray"
+          onClick={() => console.log("click")}
+        />
+        <Text textTransform="uppercase">Atualizar dados</Text>
+      </Flex>
       <QrReader
         delay={300}
         onError={handleError}
         onScan={handleScan}
         style={{ width: '100%' }}
+        className="qr-code-reader"
       />
-      <p>{result}</p>
-    </div>
+      <Stack bg="white" px={8} py={4} align="center" spacing={4}>
+        <QRCodeIcon />
+        <Text textAlign="center">Aproxime sua câmera do QR CODE na ficha para atualizar os dados ou <strong>digite manualmente</strong>.</Text>
+      </Stack>
+    </Styles>
   )
 }
 
