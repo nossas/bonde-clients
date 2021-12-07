356335941ca4811571b7d6219c9da20d0653ee2e
Gloria Teodoro <gloriateodoro1@gmail.com>
2021-12-07 12:33:45 -0300

    chore: fix not allowed to navigate top frame to data url in chrome browser

--- clients/webpage/src/bonde-webpage/plugins/Plip/components/PdfButton.tsx
+++ clients/webpage/src/bonde-webpage/plugins/Plip/components/PdfButton.tsx
@@ -8,7 +8,9 @@ interface PdfButtonProps {
 const PdfButton = (props: PdfButtonProps) => {
   const handleClick = (event:any) => {
     event?.preventDefault()
-    window.open(encodeURI(`data:application/pdf;filename=generated.pdf;base64,${props.dataPdf}`)); 
+    const win = window.open();
+    const data = encodeURI(`data:application/pdf;filename=generated.pdf;base64,${props.dataPdf}`)
+    win?.document.write('<iframe src="' + data + '" frameborder="0" style="border:0; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%;" allowfullscreen></iframe>')
   };
 
   return (
