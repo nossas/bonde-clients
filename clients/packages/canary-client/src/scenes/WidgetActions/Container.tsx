1567bc207d891b96911d48a01a8bdc6f3f7adb3d
Igor Santos <igr.exe@gmail.com>
2021-12-07 10:20:45 -0300

    feat(canary): remove second navigation to mobile version

--- clients/packages/canary-client/src/scenes/WidgetActions/Container.tsx
+++ clients/packages/canary-client/src/scenes/WidgetActions/Container.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Heading, Stack, Flex, DarkMode, Container as Content } from 'bonde-components';
+import { isMobile } from 'react-device-detect';
 import TabRoute from './TabRoute';
 
 type Props = {
@@ -18,14 +19,14 @@ const Container: React.FC<Props> = ({ children, title, navigation }): React.Reac
     <TabRoute>
       {({ push, is }) => (
         <Flex direction="column" flex={1}>
-          <Stack spacing={4} bg="black" px={[6, null, null, 12]}>
+          {!isMobile ? <Stack spacing={4} bg="black" px={[6, null, null, 12]}>
             <Heading as="h2" size="2xl" color="white" fontWeight="extrabold">{title}</Heading>
             <DarkMode>
               <Flex direction="row">
                 {navigation({ push, is })}
               </Flex>
             </DarkMode>
-          </Stack>
+          </Stack> : null}
           <Content display="flex" flex={1} flexDirection="column" height={["100%", null, null, "auto"]}>
             {children}
           </Content>
