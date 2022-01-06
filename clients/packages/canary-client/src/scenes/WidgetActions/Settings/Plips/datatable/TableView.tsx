3f0810103b0298eaeba7286a442248a1fbc9a52a
Igor Santos <igr.exe@gmail.com>
2022-01-06 17:41:02 -0300

    fix(canary): persist multi filters plip form table

--- clients/packages/canary-client/src/scenes/WidgetActions/Settings/Plips/datatable/TableView.tsx
+++ clients/packages/canary-client/src/scenes/WidgetActions/Settings/Plips/datatable/TableView.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import {
-  // Button,
+  FormControl,
+  FormLabel,
+  Button,
+  Flex,
   Table,
   Thead,
   Tr,
   Th,
   Td,
   Tbody,
+  TableCaption,
   Skeleton,
   Stack,
   Heading,
@@ -18,61 +22,95 @@ import { useQueryFilters } from './useQueryFilters';
 const PlipsFormTable: React.FC<{ widgetId: number }> = ({ widgetId }) => {
   const {
     data,
+    total,
     loading,
-    limit,
-    // onNextPage,
-    // onPreviousPage,
-    onChangeStatus
-  }: any = useQueryFilters(widgetId);
+    pageIndex,
+    pages,
+    onNextPage,
+    onPreviousPage,
+    onChangeStatus,
+    onChangeLimit
+  } = useQueryFilters(widgetId);
 
+  console.log("pageIndex, pages", { pageIndex, pages });
   return (
     <Stack spacing={4}>
-      <Heading
-        as="h5"
-        size="xs"
-        fontWeight="normal"
-        color="gray.400"
-        textTransform="uppercase"
-      >
-        Todas as fichas ({limit})
-        {/* <Button onClick={onPreviousPage} colorScheme="gray">Previous Page</Button>
-        <Button onClick={onNextPage}>Next Page</Button> */}
-        <Select onChange={(e: any) => onChangeStatus(e.target.value)}>
-          <option value="todos">Todos</option>
-          <option value="pendentes">Pendentes</option>
-          <option value="concluidos">Concluídos</option>
-          <option value="inscritos">Inscritos</option>
-        </Select>
-        {/* <Button onClick={() => onChangeStatus('pendentes')}>Change status</Button> */}
-      </Heading>
+      <Flex direction='row' justify="space-between" align='end'>
+        <Heading
+          as="h5"
+          size="xs"
+          fontWeight="normal"
+          color="gray.400"
+          textTransform="uppercase"
+        >
+          Todas as fichas ({total})
+        </Heading>
+        <Stack direction='row' spacing={2}>
+          {/* <FormControl as={Stack} spacing={2} direction='row' align='end'>
+            <FormLabel>Estado</FormLabel>
+            <Select w='auto' minW='150px' onChange={(e: any) => onChangeStatus(e.target.value)}>
+              <option value="todos">Todos</option>
+              <option value="ES">ES</option>
+              <option value="AC">AC</option>
+              <option value="PE">PE</option>
+            </Select>
+          </FormControl> */}
+          <FormControl as={Stack} spacing={2} direction='row' align='end'>
+            <FormLabel>Status</FormLabel>
+            <Select w='auto' minW='150px' onChange={(e: any) => onChangeStatus(e.target.value)}>
+              <option value="todos">Todos</option>
+              <option value="pendentes">Pendentes</option>
+              <option value="concluidos">Concluídos</option>
+              <option value="inscritos">Inscritos</option>
+            </Select>
+          </FormControl>
+        </Stack>
+      </Flex>
       <Table variant="simple" bg="white">
-        <Thead>
-          <Tr>
-            <Th>Nome</Th>
-            <Th>E-mail</Th>
-            <Th>Estado</Th>
-            <Th>Assinaturas</Th>
-            <Th>Data da inscrição</Th>
-            <Th>Status</Th>
-          </Tr>
-        </Thead>
-        {loading ? (
-          <Skeleton height="150px" />
-        ) : (
-          <Tbody>
-            {data.plips.map((pf: any, index: number) => (
-              <Tr key={`plips-form-${index}`}>
-                <Td>{pf.name}</Td>
-                <Td>{pf.email}</Td>
-                <Td>{pf.state}</Td>
-                <Td>{pf.expected_signatures || 0}</Td>
-                <Td>{new Date(pf.created_at).toLocaleDateString()}</Td>
-                <Td>
-                  {checkStatus(pf)}
-                </Td>
+        <TableCaption>
+          <Stack direction="row" align='center' justify="center">
+            <Button variant='ghost' onClick={onPreviousPage} disabled={pageIndex === 0}>Anterior</Button>
+            <FormControl as={Stack} spacing={2} direction='row' align='end'>
+            <FormLabel>Status</FormLabel>
+              <Select w='auto' minW='150px' onChange={(e: any) => onChangeLimit(Number(e.target.value))}>
+                <option value="2">2</option>
+                <option value="10">10</option>
+                <option value="20">20</option>
+                <option value="30">30</option>
+              </Select>
+            </FormControl>
+            <Button variant='ghost' onClick={onNextPage} disabled={pageIndex === pages}>Próxima</Button>
+          </Stack>
+        </TableCaption>
+        {loading
+          ? <Skeleton width="100%" h="530px" />
+          : (
+          <>
+            <Thead>
+              <Tr>
+                <Th>Nome</Th>
+                <Th>E-mail</Th>
+                <Th>Estado</Th>
+                <Th>Assinaturas</Th>
+                <Th>Data da inscrição</Th>
+                <Th>Status</Th>
               </Tr>
-            ))}
-          </Tbody>
+            </Thead>
+            <Tbody>
+              {data?.plips.map((pf: any, index: number) => (
+                <Tr key={`plips-form-${index}`}>
+                  <Td>{pf.name}</Td>
+                  <Td>{pf.email}</Td>
+                  <Td>{pf.state}</Td>
+                  <Td>{pf.expected_signatures || 0}</Td>
+                  <Td>{new Date(pf.created_at).toLocaleDateString()}</Td>
+                  <Td>
+                    {checkStatus(pf)}
+                  </Td>
+                </Tr>
+              ))}
+            </Tbody>
+          </>
         )}
       </Table>
     </Stack>
--- clients/packages/canary-client/src/scenes/WidgetActions/Settings/Plips/datatable/useQueryFilters.ts
+++ clients/packages/canary-client/src/scenes/WidgetActions/Settings/Plips/datatable/useQueryFilters.ts
@@ -16,6 +16,12 @@ const QUERY = gql`
       confirmed_signatures
       created_at
     }
+
+    plips_aggregate(where: $where) {
+      aggregate {
+        count
+      }
+    }
   }
 `;
 
@@ -28,118 +34,147 @@ export interface PlipForm {
   created_at: string;
 }
 
-export const useQueryFilters = (widgetId: number) => {
-  const [limit, setLimit] = useState(10);
-  const [pageIndex, setPageIndex] = useState(0)
-  const [status, setStatus] = useState<'pendentes' | 'inscritos' | 'concluidos' | null>(null);
+export type FilterStatus = 'pendentes' | 'inscritos' | 'concluidos' | 'todos';
+export type FilterState = 'ES' | 'AC' | 'PE' | 'todos';
+
+export interface DataQueryFilters {
+  // Fetch query
+  data: { plips: PlipForm[] };
+  loading: boolean;
+  error?: any;
+  total: number;
+  // Limit filters
+  limit: number;
+  onChangeLimit: (limit: number) => void;
+  // Status filters
+  status: FilterStatus;
+  onChangeStatus: (status: FilterStatus) => void;
+  // State filters
+  // state: FilterState;
+  // onChangeState: (state: FilterState) => void;
+  // Page filters
+  pages: number;
+  pageIndex: number;
+  onNextPage: () => void;
+  onPreviousPage: () => void;
+}
+
+interface Variables {
+  where: any;
+  offset: number;
+  limit: number;
+}
+
+type Refetch = (args: any) => void
+
+interface Opts {
+  defaultLimit?: number;
+}
+
+interface VariablesOpts {
+  widgetId: number;
+  limit: number;
+  pageIndex: number;
+  status?: FilterStatus;
+}
+
+const createVariables = ({ widgetId, limit, pageIndex, status }: VariablesOpts) => {
+  // Pagination default
+  const variables: any = {
+    offset: limit * pageIndex,
+    limit
+  }
+  // Filters default
+  const where: any = { widget_id: { _eq: widgetId } };
+
+  // Status filter
+  if (status === 'pendentes') {
+    where['confirmed_signatures'] = { _is_null: true };
+    where['_or'] = [10, 20, 30, 40, 50, 100].map((value, index) => {
+      const date = new Date();
+      date.setDate(date.getDate() - (30 * (index + 1)));
+      return {
+        _and: [
+          { created_at: { _lte: date.toDateString() } },
+          { expected_signatures: { _eq: value } }
+        ]
+      }
+    })
+    // Change offset when change status
+    variables['offset'] = 0;
+  } else if (status === 'inscritos') {
+    where['confirmed_signatures'] = { _is_null: true };
+    where['_or'] = [10, 20, 30, 40, 50, 100].map((value, index) => {
+      const date = new Date();
+      date.setDate(date.getDate() - (30 * (index + 1)));
+      return {
+        _and: [
+          { created_at: { _gte: date.toDateString() } },
+          { expected_signatures: { _eq: value } }
+        ]
+      }
+    })
+    // Change offset when change status
+    variables['offset'] = 0;
+  } else if (status === 'todos') {
+    // Change offset when change status
+    variables['offset'] = 0;
+  } else if (status === 'concluidos') {
+    where['confirmed_signatures'] = { _is_null: false };
+    // Change offset when change status
+    variables['offset'] = 0;
+  }
+
+  return { ...variables, where };
+}
+
+export const useQueryFilters = (widgetId: number, opts?: Opts): DataQueryFilters => {
+  // Limit
+  const [limit, setLimit] = useState(opts?.defaultLimit || 10);
+  // Pagination
+  const [pageIndex, setPageIndex] = useState(0);
+  // Status
+  const [status, setStatus] = useState<FilterStatus>('todos');
+  // Fetch query
   const { data, loading, error, refetch } = useQuery(QUERY, {
-    variables: {
-      where: { widget_id: { _eq: widgetId } },
-      offset: limit * pageIndex,
-      limit
-    }
+    variables: createVariables({ widgetId, limit, pageIndex, status })
   });
+  
+  const total = data?.plips_aggregate.aggregate.count || 0;
+  const pages = Math.round(total / limit) - 1;
 
   return {
     data,
-    limit,
     loading,
     error,
-    status,
+    total,
+    pageIndex,
+    pages: pages === -1 ? 0 : pages,
     onNextPage: () => {
       const newPageIndex = pageIndex + 1;
-      refetch({
-        where: { widget_id: { _eq: widgetId } },
-        offset: limit * newPageIndex,
-        limit
-      });
-      setPageIndex(newPageIndex)
+      if (newPageIndex <= total) {
+        refetch(createVariables({ widgetId, limit, pageIndex: newPageIndex, status }));
+        setPageIndex(newPageIndex)
+      }
     },
     onPreviousPage: () => {
       const newPageIndex = pageIndex - 1;
       if (newPageIndex >= 0) {
-        refetch({
-          where: { widget_id: { _eq: widgetId } },
-          offset: limit * newPageIndex,
-          limit
-        });
-        setPageIndex(newPageIndex)
+        refetch(createVariables({ widgetId, limit, pageIndex: newPageIndex, status }));
+        setPageIndex(newPageIndex);
       }
     },
+    limit,
     onChangeLimit: (i: number) => {
-      refetch({
-        where: { widget_id: { _eq: widgetId } },
-        offset: 0,
-        limit
-      });
+      refetch(createVariables({ widgetId, limit: i, pageIndex: 0, status }));
       setLimit(i);
       setPageIndex(0);
     },
-    onChangeStatus: (status?: 'pendentes' | 'inscritos' | 'concluidos' | 'todos') => {
-      if (status === 'pendentes') {
-        refetch({
-          where: {
-            widget_id: { _eq: widgetId },
-            confirmed_signatures: { _is_null: true },
-            _or: [10, 20, 30, 40, 50, 100].map((value, index) => {
-              const date = new Date();
-              date.setDate(date.getDate() - (30 * (index + 1)));
-              return {
-                _and: [
-                  { created_at: { _lte: date.toDateString() } },
-                  { expected_signatures: { _eq: value } }
-                ]
-              }
-            })
-          },
-          offset: 0,
-          limit
-        });
-        setPageIndex(0);
-        setStatus(status);
-      } else if (status === 'inscritos') {
-        refetch({
-          where: {
-            widget_id: { _eq: widgetId },
-            confirmed_signatures: { _is_null: true },
-            _or: [10, 20, 30, 40, 50, 100].map((value, index) => {
-              const date = new Date();
-              date.setDate(date.getDate() - (30 * (index + 1)));
-              return {
-                _and: [
-                  { created_at: { _gte: date.toDateString() } },
-                  { expected_signatures: { _eq: value } }
-                ]
-              }
-            })
-          },
-          offset: 0,
-          limit
-        });
-        setPageIndex(0);
-        setStatus(status);
-      } else if (status === 'todos') {
-        refetch({
-          where: {
-            widget_id: { _eq: widgetId }
-          },
-          offset: 0,
-          limit
-        });
-        setPageIndex(0);
-        setStatus(null);
-      } else if (status === 'concluidos') {
-        refetch({
-          where: {
-            widget_id: { _eq: widgetId },
-            confirmed_signatures: { _is_null: false }
-          },
-          offset: 0,
-          limit
-        });
-        setPageIndex(0);
-        setStatus(status);
-      }
+    status,
+    onChangeStatus: (i: FilterStatus) => {
+      refetch(createVariables({ widgetId, limit, pageIndex: 0, status: i }));
+      setPageIndex(0);
+      setStatus(i);
     }
   }
 }
