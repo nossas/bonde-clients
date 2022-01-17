17f6cfab13a9f454948bd891685d84c7cb051dd8
Igor Santos <igr.exe@gmail.com>
2022-01-17 13:14:20 -0300

    chore(admin): render sidebar with core-tools graphql

--- clients/packages/admin-client/src/graphql/mutations/index.js
+++ clients/packages/admin-client/src/graphql/mutations/index.js
@@ -1,3 +1,2 @@
-export { default as addTwilioCall } from './add-twilio-call'
 export { default as addTwilioConfiguration } from './add-twilio-configuration'
 export { default as updateTwilioConfiguration } from './update-twilio-configuration'
