9993bd7fa7ced4226f6fbfb8cf2b8b23437a16a1
Igor Santos <igr.exe@gmail.com>
2022-01-18 17:37:34 -0300

    fix(admin): fetch communities by session

--- clients/packages/admin-client/src/community/selectors.js
+++ clients/packages/admin-client/src/community/selectors.js
@@ -1,7 +1,13 @@
+import Cookie from 'js-cookie';
+
 export const getList = state => state.community.list.data
-export const getCurrentId = state => state.community.list.currentId
+
+export const getCurrentId = () => JSON.parse(Cookie.get('community')).id
+
 export const isLoaded = state => state.community.list.isLoaded
+
 export const isLoading = state => state.community.list.loading
+
 export const isForcedSubmit = state => state.community.list.forcedSubmit
 
-export const getCurrent = state => getList(state).filter(c => c.id === getCurrentId(state))[0]
+export const getCurrent = () => JSON.parse(Cookie.get('community'))
