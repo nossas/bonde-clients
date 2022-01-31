f949a1de8a7ac6f20abc38f5ae4e04a07bb8db40
Igor Santos <igr.exe@gmail.com>
2022-01-31 15:57:31 -0300

    fix(admin): render choose custom template page

--- clients/packages/admin-client/src/pages/admin/mobilizations/templates/choose-custom/page.connected.js
+++ clients/packages/admin-client/src/pages/admin/mobilizations/templates/choose-custom/page.connected.js
@@ -1,63 +1,66 @@
 import React from 'react';
-// import { gql } from 'bonde-core-tools';
-// //
-// // @route /mobilizations/:mobilization_id/templates/choose/custom
-// //
-// import { connect } from 'react-redux'
-// import { injectIntl } from 'react-intl'
+import { gql, useQuery } from 'bonde-core-tools';
+//
+// @route /mobilizations/:mobilization_id/templates/choose/custom
+//
+import { connect } from 'react-redux'
+import { injectIntl } from 'react-intl'
 
-// import * as SelectableActions from 'components/selectable-list/actions'
-// import MobSelectors from 'mobrender/redux/selectors'
-// import * as MobActions from 'mobrender/redux/action-creators'
-// import * as TemplateSelectors from 'mobilizations/templates/selectors'
-// import * as CommunitySelectors from 'community/selectors'
+import * as SelectableActions from 'components/selectable-list/actions'
+import MobSelectors from 'mobrender/redux/selectors'
+import * as MobActions from 'mobrender/redux/action-creators'
+import * as TemplateSelectors from 'mobilizations/templates/selectors'
+import * as CommunitySelectors from 'community/selectors'
 
-// import { setFilterableSearchBarList } from 'components/filterable-search-bar/actions'
+import { setFilterableSearchBarList } from 'components/filterable-search-bar/actions'
 
-// import Page from './page'
+import Page from './page'
 
-// const mapStateToProps = (state, props) => ({
-//   communityId: CommunitySelectors.getCurrentId(state),
-//   mobilization: MobSelectors(state, props).getMobilization(),
-//   selectedIndex: TemplateSelectors.getSelectableIndex(state),
-//   filterableTemplates: TemplateSelectors.getFilterableTemplates(state)
-// })
+const mapStateToProps = (state, props) => ({
+  communityId: CommunitySelectors.getCurrentId(state),
+  mobilization: MobSelectors(state, props).getMobilization(),
+  selectedIndex: TemplateSelectors.getSelectableIndex(state),
+  filterableTemplates: TemplateSelectors.getFilterableTemplates(state)
+})
 
-// const mapActionsToProps = {
-//   setFilterableSearchBarList,
-//   setSelectedIndex: SelectableActions.setSelectedIndex,
-//   createMobilizationFromTemplate: MobActions.asyncUpdateMobilization
-// }
+const mapActionsToProps = {
+  setFilterableSearchBarList,
+  setSelectedIndex: SelectableActions.setSelectedIndex,
+  createMobilizationFromTemplate: MobActions.asyncUpdateMobilization
+}
 
-// const GraphPage = graphql(gql`
-//   query customTemplates($communityId: Int!) {
-//     customTemplates (ctxCommunityId: $communityId) {
-//       nodes {
-//         id
-//         name
-//         goal
-//         image: facebookShareImage
-//         createdAt
-//       }
-//     }
-//   }
-// `, {
-//   options: ({ communityId }) => ({
-//     variables: { communityId },
-//     fetchPolicy: 'network-only'
-//   }),
-//   props: ({ ownProps, data: { loading, customTemplates } }) => ({
-//     loading,
-//     templates: customTemplates ? customTemplates.nodes : []
-//   })
-// })(injectIntl(Page))
+const FETCH_TEMPLATES = gql`
+  query customTemplates($communityId: Int!) {
+    customTemplates (ctxCommunityId: $communityId) {
+      nodes {
+        id
+        name
+        goal
+        image: facebookShareImage
+        createdAt
+      }
+    }
+  }
+`;
 
-// export default connect(mapStateToProps, mapActionsToProps)(GraphPage)
+const PageGraphQL = (props) => {
+  const { data, loading, error } = useQuery(FETCH_TEMPLATES, {
+    variables: { communityId: props.communityId },
+    fetchPolicy: 'network-only'
+  });
 
-const CustomTemplates = () => {
+  if (error) {
+    console.log("PageGraphQL error", error);
+    return 'Failed!';
+  }
+
   return (
-    <h3>Override CustomTemplates</h3>
+    <Page
+      {...props}
+      loading={loading}
+      templates={(data || {}).customTemplates ? data.customTemplates.nodes : []}
+    />
   );
 }
 
-export default CustomTemplates;
+export default connect(mapStateToProps, mapActionsToProps)(injectIntl(PageGraphQL))
