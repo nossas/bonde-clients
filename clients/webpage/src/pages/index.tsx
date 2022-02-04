19b69bacd0b59a4b0f3d940daac712e400fd2290
Lucas Pirola <lucaspirola@gmail.com>
2022-02-04 17:27:41 -0300

    chore: move font load to css external;

--- clients/webpage/src/pages/index.tsx
+++ clients/webpage/src/pages/index.tsx
@@ -1,4 +1,7 @@
 import Head from 'next/head';
+import getConfig from 'next/config';
+import i18n from "i18next";
+import { I18nextProvider, initReactI18next } from "react-i18next";
 import Error404 from './404';
 import asyncFilterBlocksGraphql from '../graphql-app/filterBlocks';
 import asyncFilterMobilizationsGraphql from '../graphql-app/filterMobilizations';
@@ -6,10 +9,8 @@ import asyncFilterWidgetsGraphql from '../graphql-app/filterWidgets';
 import MeuRioStyles from '../components/MeuRioStyles';
 import Styles from '../bonde-webpage/Styles';
 import MobilizationConnected from '../components/MobilizationConnected';
-import i18n from "i18next";
-import { I18nextProvider, initReactI18next } from "react-i18next";
 import resources from "../initialI18nStore";
-import getConfig from 'next/config'
+import "index.css";
 
 const { publicRuntimeConfig } = getConfig()
 
@@ -20,22 +21,22 @@ interface PageProperties {
   widgets: any[];
 }
 
-function Page({ mobilization, blocks, widgets }: PageProperties) {  
+function Page({ mobilization, blocks, widgets }: PageProperties) {
   if (!mobilization) return <Error404 />;
 
   i18n
-  .use(initReactI18next) // passes i18n down to react-i18next
-  .init({
-    // the translations
-    // (tip move them in a JSON file and import them,
-    // or even better, manage them via a UI: https://react.i18next.com/guides/multiple-translation-files#manage-your-translations-with-a-management-gui)
-    resources,
-    // lng: "pt-br", // if you're using a language detector, do not define the lng option
-    fallbackLng: mobilization.language,
-    interpolation: {
-      escapeValue: false // react already safes from xss => https://www.i18next.com/translation-function/interpolation#unescape
-    }
-  });
+    .use(initReactI18next) // passes i18n down to react-i18next
+    .init({
+      // the translations
+      // (tip move them in a JSON file and import them,
+      // or even better, manage them via a UI: https://react.i18next.com/guides/multiple-translation-files#manage-your-translations-with-a-management-gui)
+      resources,
+      // lng: "pt-br", // if you're using a language detector, do not define the lng option
+      fallbackLng: mobilization.language,
+      interpolation: {
+        escapeValue: false // react already safes from xss => https://www.i18next.com/translation-function/interpolation#unescape
+      }
+    });
 
   const {
     name,
@@ -80,16 +81,6 @@ function Page({ mobilization, blocks, widgets }: PageProperties) {
           type="image/png"
           sizes="16x16"
           href={favicon || '/static/icon/favicon-16.png'}
-        />
-        <link
-          type="text/css"
-          href="https://fonts.googleapis.com/css?family=Abel|Anton|Archivo+Narrow:400,400i,700,700i|Arvo:400,400i,700,700i|Asap:400,400i,700,700i|Baloo+Bhai|Bitter:400,400i,700|Bree+Serif|Cabin:400,400i,700,700i|Catamaran:400,700|Crimson+Text:400,400i,700,700i|Cuprum:400,400i,700,700i|David+Libre:400,700|Dosis:400,700|Droid+Sans:400,700|Exo+2:400,400i,700,700i|Exo:400,400i,700,700i|Fira+Sans:400,400i,700,700i|Fjalla+One|Francois+One|Gidugu|Hind:400,700|Inconsolata:400,700|Indie+Flower|Josefin+Sans:400,400i,700,700i|Karla:400,400i,700,700i|Lalezar|Lato:400,400i,700,700i|Libre+Baskerville:400,400i,700|Lobster|Lora:400,400i,700,700i|Merriweather+Sans:400,400i,700,700i|Montserrat:400,700|Muli:400,400i|Noto+Serif:400,400i,700,700i|Nunito:400,700|Open+Sans+Condensed:300,300i,700|Open+Sans:400,400i,700,700i|Oswald:400,700|Oxygen:400,700|PT+Sans:400,400i,700,700i|PT+Serif:400,400i,700,700i|Pacifico|Playfair+Display:400,400i,700,700i|Poiret+One|Poppins:400,700|Quicksand:400,700|Raleway:400,400i,700,700i|Roboto+Condensed:400,400i,700,700i|Roboto+Mono:400,400i,700,700i|Roboto+Slab:400,700|Roboto:400,400i,700,700i|Ruslan+Display|Signika:400,700|Slabo+27px|Source+Sans+Pro:200,300,400,700|Titillium+Web:400,400i,700,700i|Ubuntu+Condensed|Ubuntu:400,400i,700,700i|Varela+Round|Yanone+Kaffeesatz:400,700&display=optional"
-          rel="stylesheet"
-        />
-        <script
-          type="text/javascript"
-          src="https://assets.pagar.me/checkout/checkout.js"
-          async
         />
         <script
           async
