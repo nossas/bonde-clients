9414889900b02cd5030340ae3f64918a62d5f019
Igor Santos <igr.exe@gmail.com>
2022-02-07 16:40:19 -0300

    fix(webpage): download pdf with file-saver on plip form

--- clients/webpage/src/bonde-webpage/plugins/Plip/components/PdfButton.tsx
+++ clients/webpage/src/bonde-webpage/plugins/Plip/components/PdfButton.tsx
@@ -1,5 +1,27 @@
 import React from 'react';
+import { saveAs } from 'file-saver';
 import EyeIcon from '../icons/EyeIcon';
+
+/* Helper function */
+const b64toBlob = (b64Data: string, contentType='', sliceSize=512) => {
+  const byteCharacters = atob(b64Data);
+  const byteArrays: Uint8Array[] = [];
+
+  for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+    const slice = byteCharacters.slice(offset, offset + sliceSize);
+
+    const byteNumbers = new Array(slice.length);
+    for (let i = 0; i < slice.length; i++) {
+      byteNumbers[i] = slice.charCodeAt(i);
+    }
+
+    const byteArray = new Uint8Array(byteNumbers);
+    byteArrays.push(byteArray);
+  }
+
+  const blob = new Blob(byteArrays, {type: contentType});
+  return blob;
+}
 
 interface PdfButtonProps {
   dataPdf: string
@@ -7,15 +29,17 @@ interface PdfButtonProps {
 }
 
 const PdfButton = (props: PdfButtonProps) => {
+  const blob = b64toBlob(props.dataPdf.replace('data:application/pdf;filename=generated.pdf;base64,',''));
 
   return (
-    <a
-      href={`data:application/octet-stream;attachment;base64,${props.dataPdf}`}
-      download={`Ficha de Assinatura de ${props.fileName}`}
+    <button
+      onClick={() => {
+        saveAs(blob, props.fileName);
+      }}
     >
       <EyeIcon />
       Ver ficha de assinatura
-    </a>
+    </button>
   );
 };
 
