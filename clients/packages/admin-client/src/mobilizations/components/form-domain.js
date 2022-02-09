499bab0180c997f231a35bb7b5055f35fadb3f73
Gloria Teodoro <gloriateodoro1@gmail.com>
2022-02-09 13:45:42 -0300

    chore: add https to faq

--- clients/packages/admin-client/src/mobilizations/components/form-domain.js
+++ clients/packages/admin-client/src/mobilizations/components/form-domain.js
@@ -156,7 +156,7 @@ class FormDomain extends Component {
             }
             values={{
               link: (
-                <a href='http://www.faq.bonde.org/#block-7283' target='_blank' rel='noopener noreferrer'>
+                <a href='https://www.faq.bonde.org/#block-7283' target='_blank' rel='noopener noreferrer'>
                   <FormattedMessage
                     id='mobilizations.components--form-domain.cname-table.footer.helper-text.link'
                     defaultMessage='Trilho {icon}'
