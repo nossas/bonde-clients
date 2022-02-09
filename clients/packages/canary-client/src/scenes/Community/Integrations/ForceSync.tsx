b129364d4dbd14b890676c2c758b15a3784fa4d6
Adriele A Secundino <dri.leela@gmail.com>
2022-02-09 11:29:29 -0300

    chore(canary): add query to return status

--- clients/packages/canary-client/src/scenes/Community/Integrations/ForceSync.tsx
+++ clients/packages/canary-client/src/scenes/Community/Integrations/ForceSync.tsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { useMutation, gql, Context as SessionContext } from 'bonde-core-tools';
+import { useQuery, useMutation, gql, Context as SessionContext } from 'bonde-core-tools';
 import { MailchimpStart } from './types';
 import {
   Heading,
@@ -11,6 +11,21 @@ import {
   toast,  
 } from 'bonde-components';
 
+const fetchGraphqlQuery = gql`
+
+query($id:Int!, $is_community:Boolean!) {
+  resync_mailchimp_status(
+    is_community: $is_community
+    id: $id
+  ) {
+    completed
+    failed
+    last_sync
+    waiting
+    status
+  }
+}
+`
 const fetchGraphqlMutation = gql`
 mutation($id:Int!, $is_community:Boolean!) {
   resync_mailchimp_start(
@@ -28,22 +43,49 @@ const ForceSync: React.FC = () => {
     { variables: { is_community: true, id: community?.id }, skip: true }
   );
 
+  const { data, loading, error }  = useQuery(
+    fetchGraphqlQuery,
+    {
+      variables: { is_community: true, id: community?.id },
+      pollInterval: 5000,
+    },
+  )
+
   const done = async () => {
     const a: MailchimpStart = await setPropagating();
     if (typeof a !== 'undefined' && 
         typeof a.data.resync_mailchimp_start.status !== 'undefined' &&
         a.data.resync_mailchimp_start.status === 'started to add contacts to the queue') {
-      toast(<Success message={`Ae! Sincronização em andamento.`} />, { type: toast.TYPE.SUCCESS });
+      toast(<Success message={`Ae! Sincronização em andamento.`} />, { type: toast.TYPE.SUCCESS }); 
     } else {
       toast(`Falha na atualização da base de contatos do mailchimp! ${a.data.resync_mailchimp_start.status}`, { type: toast.TYPE.ERROR });
     }
   }; 
 
-  
+  if (loading) return <Text>Carregando Mailchimp Status</Text>;
+  else if (error) return <Text>Failed ${error}</Text>;
+
+  if (data.resync_mailchimp_status.waiting > 0){
+    return <Stack>
+      <Heading as="h4" size="sm">Forçar sincronização</Heading>
+      <Text>Sua base no Mailchimp não está atualizada? Tudo bem! Clique em sincronizar pra dar um empurrãozinho:</Text>
+      <Heading as="h4" size="sm">Status</Heading>
+      <Text size="sm">{data.resync_mailchimp_status.status} ({data.resync_mailchimp_status.completed})</Text>
+      <Flex justifyContent="flex-end">
+        <Button onClick={done} disabled='true' type='button' marginTop={4}>Sincronizar</Button>
+      </Flex>
+    </Stack>
+  } 
+
   return (
     <Stack>
       <Heading as="h4" size="sm">Forçar sincronização</Heading>
       <Text>Sua base no Mailchimp não está atualizada? Tudo bem! Clique em sincronizar pra dar um empurrãozinho:</Text>
+      <Heading as="h4" size="sm">Status</Heading>
+      <Text size="sm">{data.resync_mailchimp_status.status}</Text>
+      <Text size="sm">{((typeof data.resync_mailchimp_status.last_sync === 'undefined' || data.resync_mailchimp_status.last_sync === "")? '' 
+                      : `Data da última atualização: ${data.resync_mailchimp_status.last_sync}`)}
+      </Text>
       <Flex justifyContent="flex-end">
         <Button onClick={done} type='button' marginTop={4}>Sincronizar</Button>
       </Flex>
