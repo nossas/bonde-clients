7b80b27bf464bea22d80f3ea6a34f526c6dc1e1c
Igor Santos <igr.exe@gmail.com>
2022-02-10 15:59:48 -0300

    fix(canary): menu actions on footer performance plip

--- clients/packages/canary-client/src/scenes/WidgetActions/Settings/Plips/PerformanceScene.tsx
+++ clients/packages/canary-client/src/scenes/WidgetActions/Settings/Plips/PerformanceScene.tsx
@@ -6,6 +6,7 @@ import { isMobile } from "react-device-detect";
 import { Header } from "../../../../components/CardWithHeader";
 import type { Widget } from "../../FetchWidgets";
 import Chart from "./Chart";
+import MenuActions from "./components/MenuActions";
 import PlipsFormTable from './datatable/TableView';
 import { usePerformanceQuery } from "./performance/fetchData";
 import eleitorado from "./performance/eleitorado";
@@ -26,7 +27,7 @@ const ValueWithLabel: React.FC<Props> = ({ value, label, variant = "md" }) => va
       color="black"
       fontWeight="extrabold"
       fontSize="4xl"
-      lineHeight={1}
+      lineHeight={[1.3, null, null, null, 1]}
     >
       {value}
     </Text>
@@ -86,87 +87,91 @@ const PerformanceScene: React.FC<Properties> = ({ widget }) => {
   if (error) return <Text color="red">{JSON.stringify(error)}</Text>;
 
   return (
-    <Stack spacing={4}>
-      <Grid templateColumns={['repeat(2, 1fr)', null, 'repeat(7, 1fr)']} gap={4}>
-        <GridItem colSpan={1}>
-          <Stack>
-            <Header label="Total assinaturas" helpText="Total de assinaturas entregues e registradas pela equipe." />
-            <Flex bg="white" p={4} minH="131px" align="end">
-              <ValueWithLabel
-                variant="lg"
-                value={data?.confirmed_signatures || "0"}
-                label={`${calcPercentage(data?.confirmed_signatures, eleitorado.total * eleitorado.goal.total)}% da meta`}
-              />
-            </Flex>
-          </Stack>
-        </GridItem>
-        <GridItem colSpan={1}>
-          <Stack>
-            <Header label="Pendentes" helpText="Total de assinaturas geradas que ainda não foram entregues e registradas pela equipe." />
-            <Flex bg="white" p={4} minH="131px" align="end">
-              <ValueWithLabel
-                variant="lg"
-                value={data?.pending_signatures || "0"}
-                label={`${calcPercentage(data?.pending_signatures, eleitorado.total * eleitorado.goal.total)}% da meta`}
-              />
-            </Flex>
-          </Stack>
-        </GridItem>
-        <GridItem colSpan={2} rowSpan={2}>
-          <Stack>
-            <Header label="Total por estado" />
-            <Box bg="white">
-              <Table>
-                <Tbody>
-                {data?.states_signatures
-                  .filter((ss) => !!ss.state)
-                  .sort((a, b) => {
-                    const bpercent = calcPercentage(b.confirmed_signatures, eleitorado.states[b.state] * eleitorado.goal.state);
-                    const apercent = calcPercentage(a.confirmed_signatures, eleitorado.states[a.state] * eleitorado.goal.state);
-                    return bpercent - apercent
-                  })
-                  .splice(0, isMobile ? 5 : 7)
-                  // eslint-disable-next-line react/display-name
-                  .map((ss) => <Row data={ss} />)}
-                </Tbody>
-              </Table>
-            </Box>
-          </Stack>
-        </GridItem>
-        {!isMobile && (
-          <GridItem colSpan={3} rowSpan={2}>
-            <Chart
-              subscribers={data?.subscribers_range}
-              start={data?.subscribers_range_start}
-              end={data?.subscribers_range_end}
-            />
-          </GridItem>
-        )}
-        <GridItem colSpan={2}>
-          <Stack flex={1} height="100%">
-            <Header label="Ativistas" />
-            <Stack bg="white" px={4} py={6} spacing={4} flex={1} height="100%" justifyContent="space-around">
-              <ValueWithLabel
-                variant="lg"
-                value={data?.total_subscribers || "0"}
-                label="Total de inscritos"
-              />
-              <Flex direction="row" justify="space-between">
-                <ValueWithLabel value={`${calcPercentage(data?.confirmed_subscribers, data?.total_subscribers)}%`} label="Concluídos" />
-                <ValueWithLabel value={`${calcPercentage((data?.total_subscribers || 0) - (data?.pending_subscribers || 0) - (data?.confirmed_subscribers || 0), data?.total_subscribers)}%`} label="Inscritos" />
-                <ValueWithLabel value={`${calcPercentage(data?.pending_subscribers, data?.total_subscribers)}%`} label="Pendentes" />
+    <>
+      <Stack spacing={4} overflowY={isMobile ? "auto" : null} mb={isMobile ? "75px" : null}>
+        <Grid templateColumns={['repeat(2, 1fr)', null, 'repeat(7, 1fr)']} gap={4}>
+          <GridItem colSpan={1}>
+            <Stack>
+              <Header label="Total assinaturas" helpText="Total de assinaturas entregues e registradas pela equipe." />
+              <Flex bg="white" p={4} minH="131px" align="end">
+                <ValueWithLabel
+                  variant="lg"
+                  value={data?.confirmed_signatures || "0"}
+                  label={`${calcPercentage(data?.confirmed_signatures, eleitorado.total * eleitorado.goal.total)}% da meta`}
+                />
               </Flex>
             </Stack>
-          </Stack>
-        </GridItem>
-      </Grid>
-      {!isMobile && (
-        <PlipsFormTable widgetId={widget.id} />
-      )}
+          </GridItem>
+          <GridItem colSpan={1}>
+            <Stack>
+              <Header label="Pendentes" helpText="Total de assinaturas geradas que ainda não foram entregues e registradas pela equipe." />
+              <Flex bg="white" p={4} minH="131px" align="end">
+                <ValueWithLabel
+                  variant="lg"
+                  value={data?.pending_signatures || "0"}
+                  label={`${calcPercentage(data?.pending_signatures, eleitorado.total * eleitorado.goal.total)}% da meta`}
+                />
+              </Flex>
+            </Stack>
+          </GridItem>
+          <GridItem colSpan={2} rowSpan={2}>
+            <Stack>
+              <Header label="Total por estado" />
+              <Box bg="white">
+                <Table>
+                  <Tbody>
+                  {data?.states_signatures
+                    .filter((ss) => !!ss.state)
+                    .sort((a, b) => {
+                      const bpercent = calcPercentage(b.confirmed_signatures, eleitorado.states[b.state] * eleitorado.goal.state);
+                      const apercent = calcPercentage(a.confirmed_signatures, eleitorado.states[a.state] * eleitorado.goal.state);
+                      return bpercent - apercent
+                    })
+                    .splice(0, isMobile ? 5 : 7)
+                    // eslint-disable-next-line react/display-name
+                    .map((ss) => <Row data={ss} />)}
+                  </Tbody>
+                </Table>
+              </Box>
+            </Stack>
+          </GridItem>
+          {!isMobile && (
+            <GridItem colSpan={3} rowSpan={2}>
+              <Chart
+                subscribers={data?.subscribers_range}
+                start={data?.subscribers_range_start}
+                end={data?.subscribers_range_end}
+              />
+            </GridItem>
+          )}
+          <GridItem colSpan={2}>
+            <Stack flex={1} height="100%">
+              <Header label="Ativistas" />
+              <Stack bg="white" px={4} py={6} spacing={4} flex={1} height="100%" justifyContent="space-around">
+                <ValueWithLabel
+                  variant="lg"
+                  value={data?.total_subscribers || "0"}
+                  label="Total de inscritos"
+                />
+                <Flex direction="row" justify="space-between">
+                  <ValueWithLabel value={`${calcPercentage(data?.confirmed_subscribers, data?.total_subscribers)}%`} label="Concluídos" />
+                  <ValueWithLabel value={`${calcPercentage((data?.total_subscribers || 0) - (data?.pending_subscribers || 0) - (data?.confirmed_subscribers || 0), data?.total_subscribers)}%`} label="Inscritos" />
+                  <ValueWithLabel value={`${calcPercentage(data?.pending_subscribers, data?.total_subscribers)}%`} label="Pendentes" />
+                </Flex>
+              </Stack>
+            </Stack>
+          </GridItem>
+        </Grid>
+        {!isMobile && (
+          <PlipsFormTable widgetId={widget.id} />
+        )}
+      </Stack>
       {isMobile && (
-        <Button minH="42px" as={Link} to={pathname + "/workflow"}>Atualizar ficha</Button>
+        <MenuActions>
+          <Button minH="42px" as={Link} to={pathname + "/workflow"}>Atualizar ficha</Button>
+        </MenuActions>
       )}
-    </Stack>
+    </>
   )
 }
 
