a92b9a91caf9090d45bf1956ecc94ddc4b7815ef
Gloria Teodoro <gloriateodoro1@gmail.com>
2022-02-11 09:31:33 -0300

    chore: update screen text post-submit

--- clients/webpage/src/bonde-webpage/plugins/Plip/components/PlipDetails.tsx
+++ clients/webpage/src/bonde-webpage/plugins/Plip/components/PlipDetails.tsx
@@ -10,15 +10,14 @@ interface PropsPlipDetails {
 const PlipDetails = (props: PropsPlipDetails) => {
   return (
     <div className={styles.PlipDetails}>
-      <h3>Eba! Geramos uma ficha para você assinar:</h3>
+      <h3><strong>Eba! Geramos uma ficha para você assinar:</strong></h3>
       <PdfButton dataPdf={props.pdf.data.data?.pdf_data} fileName={props.pdf.data.data?.filename} />
       <ol>
-        <li><strong>Assine:</strong> Imprima a sua ficha, assine e chame mais 7 pessoas para assinar também.</li>
-        <li><strong>Envie a ficha:</strong> Junto com a ficha tem uma etiqueta de envio dos correios. É só levar até a agência mais próxima de você e enviar, é por nossa conta.</li>
+        <li><strong>Imprima sua ficha:</strong> Não pode imprimir agora? Tudo bem! Já enviamos uma cópia da ficha no seu e-mail também.</li>
+        <li><strong>Colete assinaturas:</strong>Assine e chame mais pessoas para assinar também.</li>
+        <li><strong>Envie a ficha:</strong>Na ficha tem o endereço para o envio dos correios. É só levar até a agência mais próxima de você e enviar!</li>
       </ol>
-      <p><strong>Contamos com você! 🌳✊</strong></p>
-      <br />
-      <p>Não pode imprimir agora? Tudo bem! Já enviamos uma cópia no seu e-mail também.</p>
+      <p><strong>A Amazônia conta com você! 🌳✊</strong></p>
     </div>
   )
 }
