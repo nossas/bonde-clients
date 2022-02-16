d5f3953094d2b8596f27b6bae3d9ca605b587ae9
Lucas Pirola <lucaspirola@gmail.com>
2022-02-16 09:42:32 -0300

    chore: remove form from image config modal

--- clients/libs/slate-editor-image-plugin/src/ImageDataModal.js
+++ clients/libs/slate-editor-image-plugin/src/ImageDataModal.js
@@ -1,22 +1,27 @@
 /* eslint-disable react/prop-types */
-import React, { Component } from 'react'
-import { Modal, ModalButton, ModalContent, ModalForm } from '@slate-editor/components'
-import { updateInlineImage, deleteInlineImage } from './ImageUtils'
+import React, { Component } from "react";
+import {
+  Modal,
+  ModalButton,
+  ModalContent,
+  ModalForm,
+} from "@slate-editor/components";
+import { updateInlineImage, deleteInlineImage } from "./ImageUtils";
 
 class ImageDataModal extends Component {
   constructor(props) {
-    super(props)
+    super(props);
 
-    const { node } = this.props
+    const { node } = this.props;
 
     this.state = {
       imageAttributes: {
-        src: node.data.get('src'),
-        title: node.data.get('title'),
-        href: node.data.get('href'),
-        openExternal: node.data.get('openExternal'),
+        src: node.data.get("src"),
+        title: node.data.get("title"),
+        href: node.data.get("href"),
+        openExternal: node.data.get("openExternal"),
       },
-    }
+    };
   }
 
   setImageAttribute(event, value) {
@@ -24,44 +29,42 @@ class ImageDataModal extends Component {
       imageAttributes: {
         ...this.state.imageAttributes,
         [event.target.name]: value,
-      }
-    })
+      },
+    });
   }
 
   render() {
-    const { node, value, onChange, changeModalState } = this.props
+    const { node, value, onChange, changeModalState } = this.props;
 
     return (
       <Modal>
-        <Modal.Header
-          closeButtonAction={() => changeModalState(false)}
-        />
+        <Modal.Header closeButtonAction={() => changeModalState(false)} />
 
         <ModalContent>
           <ModalContent.Left>
-            <img src={node.data.get('src')} alt={node.data.get('title')} role="presentation" />
+            <img
+              src={node.data.get("src")}
+              alt={node.data.get("title")}
+              role="presentation"
+            />
           </ModalContent.Left>
 
           <ModalContent.Right>
-            <ModalForm onSubmit={e => {
-              e.preventDefault()
-              onChange(updateInlineImage({ change: value.change(), data: this.state.imageAttributes }))
-              changeModalState(false)
-            }}>
+            <div className="modal--form">
               <ModalForm.Group>
                 <label htmlFor="image-plugin--edit-title">Título</label>
                 <ModalForm.LabelHelper>
-                  Esta mensagem aparecerá quando o cursor do mouse
-                  estiver posicionado sobre a imagem.
+                  Esta mensagem aparecerá quando o cursor do mouse estiver
+                  posicionado sobre a imagem.
                 </ModalForm.LabelHelper>
                 <input
                   id="image-plugin--edit-title"
                   type="text"
                   name="title"
-                  onClick={e => e.stopPropagation()}
-                  onChange={e => this.setImageAttribute(e, e.target.value)}
-                  value={this.state.imageAttributes.title || ''}
-                  placeholder='Insira uma descrição para a imagem'
+                  onClick={(e) => e.stopPropagation()}
+                  onChange={(e) => this.setImageAttribute(e, e.target.value)}
+                  value={this.state.imageAttributes.title || ""}
+                  placeholder="Insira uma descrição para a imagem"
                 />
               </ModalForm.Group>
 
@@ -71,10 +74,10 @@ class ImageDataModal extends Component {
                   id="image-plugin--edit-href"
                   type="text"
                   name="href"
-                  onClick={e => e.stopPropagation()}
-                  onChange={e => this.setImageAttribute(e, e.target.value)}
-                  value={this.state.imageAttributes.href || ''}
-                  placeholder='Ex: http://dominio.com'
+                  onClick={(e) => e.stopPropagation()}
+                  onChange={(e) => this.setImageAttribute(e, e.target.value)}
+                  value={this.state.imageAttributes.href || ""}
+                  placeholder="Ex: http://dominio.com"
                 />
               </ModalForm.Group>
 
@@ -84,8 +87,10 @@ class ImageDataModal extends Component {
                     id="image-plugin--edit-open-external"
                     type="checkbox"
                     name="openExternal"
-                    onClick={e => e.stopPropagation()}
-                    onChange={e => this.setImageAttribute(e, e.target.checked)}
+                    onClick={(e) => e.stopPropagation()}
+                    onChange={(e) =>
+                      this.setImageAttribute(e, e.target.checked)
+                    }
                     checked={!!this.state.imageAttributes.openExternal}
                   />
                   Abrir em nova aba
@@ -94,8 +99,17 @@ class ImageDataModal extends Component {
 
               <ModalButton.Container>
                 <ModalButton.Primary
-                  type="submit"
+                  type="button"
                   text="Salvar"
+                  onClick={() => {
+                    onChange(
+                      updateInlineImage({
+                        change: value.change(),
+                        data: this.state.imageAttributes,
+                      })
+                    );
+                    changeModalState(false);
+                  }}
                 />
                 <ModalButton.Opaque
                   text="Cancelar"
@@ -103,19 +117,19 @@ class ImageDataModal extends Component {
                 />
                 <ModalButton.Danger
                   text="Deletar"
-                  onClick={e => {
-                    e.preventDefault()
-                    e.stopPropagation()
-                    onChange(deleteInlineImage({ change: value.change() }))
+                  onClick={(e) => {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    onChange(deleteInlineImage({ change: value.change() }));
                   }}
                 />
               </ModalButton.Container>
-            </ModalForm>
+            </div>
           </ModalContent.Right>
         </ModalContent>
       </Modal>
-    )
+    );
   }
 }
 
-export default ImageDataModal
+export default ImageDataModal;
