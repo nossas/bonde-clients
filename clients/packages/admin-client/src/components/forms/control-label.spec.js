da4a2941f6c8d0d211db349c629e4c4b44abf15c
Lucas Pirola <lucaspirola@gmail.com>
2022-02-17 00:41:47 -0300

    chore(admin): fix components tests

--- clients/packages/admin-client/src/components/forms/control-label.spec.js
+++ clients/packages/admin-client/src/components/forms/control-label.spec.js
@@ -1,41 +1,44 @@
+/**
+ * @jest-environment jsdom
+ */
 /* eslint-disable no-unused-expressions */
-import React from 'react'
-import { expect } from 'chai'
-import { mount } from 'enzyme'
+import React from 'react';
+import { expect } from 'chai';
+import { mount } from 'enzyme';
 
-import { ControlLabel } from 'components/forms'
+import { ControlLabel } from 'components/forms';
 
 describe('client/components/forms/control-label', () => {
-  let wrapper
+  let wrapper;
   beforeEach(() => {
-    wrapper = mount(<ControlLabel>label</ControlLabel>, { context: {} })
-  })
+    wrapper = mount(<ControlLabel>label</ControlLabel>, { context: {} });
+  });
 
   it('should render ok by default', () => {
-    expect(wrapper).to.be.ok
-  })
+    expect(wrapper).to.be.ok;
+  });
   it('should set htmlFor when $formGroup.controlId passed by context', () => {
-    const $formGroup = { controlId: 'form-group-id' }
-    wrapper.setContext({ $formGroup })
-    expect(wrapper.find('label').props().htmlFor).to.equal('form-group-id')
-  })
+    const $formGroup = { controlId: 'form-group-id' };
+    wrapper.setContext({ $formGroup });
+    expect(wrapper.find('label').props().htmlFor).to.equal('form-group-id');
+  });
   it('should render text children', () => {
-    expect(wrapper.find('label').text()).to.equal('label')
-  })
+    expect(wrapper.find('label').text()).to.equal('label');
+  });
   it('should not render <Raise /> when it has error and not touched passed via $formGroup', () => {
-    const $formGroup = { error: 'Foo Bar Error!', touched: false }
-    wrapper.setContext({ $formGroup })
-    expect(wrapper.find('Raise')).to.have.length(0)
-  })
+    const $formGroup = { error: 'Foo Bar Error!', touched: false };
+    wrapper.setContext({ $formGroup });
+    expect(wrapper.find('Raise')).to.have.length(0);
+  });
   it('should render <Raise /> when it has error and touched passed via $formGroup', () => {
-    const $formGroup = { error: 'Foo Bar Error!', touched: true }
-    wrapper.setContext({ $formGroup })
-    expect(wrapper.find('Raise')).to.have.length(1)
-  })
+    const $formGroup = { error: 'Foo Bar Error!', touched: true };
+    wrapper.setContext({ $formGroup });
+    expect(wrapper.find('Raise')).to.have.length(1);
+  });
   it('should render <InputCounter /> if maxLength passed', () => {
-    const $formGroup = { value: 'xunda', touched: true }
-    wrapper.setContext({ $formGroup })
-    wrapper.setProps({ maxLength: 100 })
-    expect(wrapper.find('InputCounter')).to.have.length(1)
-  })
-})
+    const $formGroup = { value: 'xunda', touched: true };
+    wrapper.setContext({ $formGroup });
+    wrapper.setProps({ maxLength: 100 });
+    expect(wrapper.find('InputCounter')).to.have.length(1);
+  });
+});
--- clients/packages/admin-client/src/components/navigation/navbar/navbar-form.spec.js
+++ clients/packages/admin-client/src/components/navigation/navbar/navbar-form.spec.js
@@ -1,18 +1,21 @@
+/**
+ * @jest-environment jsdom
+ */
 /* eslint-disable no-unused-expressions */
-import React from 'react'
-import { expect } from 'chai'
-import { mount } from 'enzyme'
-import sinon from 'sinon'
+import React from 'react';
+import { expect } from 'chai';
+import { mount } from 'enzyme';
+import sinon from 'sinon';
 
-import { NavbarForm } from 'components/navigation/navbar'
+import { NavbarForm } from 'components/navigation/navbar';
 
-const handleCloseForm = () => {}
-const mobilization = {}
-const block = {name: 'My block'}
-const blockUpdate = () => {}
-const auth = {}
+const handleCloseForm = () => {};
+const mobilization = {};
+const block = { name: 'My block' };
+const blockUpdate = () => {};
+const auth = {};
 
-function generateComponent (options = {}) {
+function generateComponent(options = {}) {
   return (
     <NavbarForm
       handleCloseForm={options.handleCloseForm || handleCloseForm}
@@ -22,52 +25,54 @@ function generateComponent (options = {}) {
       auth={options.auth || auth}
       defaultValue={block.name}
     />
-  )
+  );
 }
 
 describe('client/components/navigation/navbar/navbar-form', () => {
   it('should initialize state with a name', () => {
-    const wrapper = mount(generateComponent())
-    expect(wrapper.state().name).to.be.eq(block.name)
-  })
+    const wrapper = mount(generateComponent());
+    expect(wrapper.state().name).to.be.eq(block.name);
+  });
 
   describe('#componentDidMount', () => {
     it.skip('should add an event listenter for the window', () => {
-      sinon.spy(window, 'addEventListener')
-      mount(generateComponent())
-      expect(window.addEventListener).to.have.been.calledOnce
-    })
-  })
+      sinon.spy(window, 'addEventListener');
+      mount(generateComponent());
+      expect(window.addEventListener).to.have.been.calledOnce;
+    });
+  });
 
   describe('#componentWillUnmount', () => {
     it.skip('should remove an event listenter from the window', () => {
-      sinon.spy(window, 'removeEventListener')
-      expect(window.removeEventListener).to.have.been.calledOnce
-    })
-  })
+      sinon.spy(window, 'removeEventListener');
+      expect(window.removeEventListener).to.have.been.calledOnce;
+    });
+  });
 
   describe('#handleKeyUp', () => {
     it.skip('should call submit when ESC is pressed', () => {
-      const wrapper = mount(generateComponent())
-      sinon.spy(wrapper.instance(), 'submit')
-      wrapper.simulate('keyUp', { preventDefault: () => {}, keyCode: 27 })
-      expect(wrapper.instance().submit).to.have.been.calledOnce
-    })
-  })
+      const wrapper = mount(generateComponent());
+      sinon.spy(wrapper.instance(), 'submit');
+      wrapper.simulate('keyUp', { preventDefault: () => {}, keyCode: 27 });
+      expect(wrapper.instance().submit).to.have.been.calledOnce;
+    });
+  });
 
   describe('#submit', () => {
     it.skip('should dispatch the edit block action', () => {
-      const mockedDispatch = sinon.spy()
-      const wrapper = mount(generateComponent({ blockUpdate: mockedDispatch }))
-      wrapper.instance().submit({ preventDefault: () => {} })
-      expect(mockedDispatch).to.have.been.calledOnce
-    })
+      const mockedDispatch = sinon.spy();
+      const wrapper = mount(generateComponent({ blockUpdate: mockedDispatch }));
+      wrapper.instance().submit({ preventDefault: () => {} });
+      expect(mockedDispatch).to.have.been.calledOnce;
+    });
 
     it.skip('should call close form callback', () => {
-      const mockedHandleCloseForm = sinon.spy()
-      const wrapper = mount(generateComponent({ handleCloseForm: mockedHandleCloseForm }))
-      wrapper.instance().submit({ preventDefault: () => {} })
-      expect(mockedHandleCloseForm).to.have.been.calledOnce
-    })
-  })
-})
+      const mockedHandleCloseForm = sinon.spy();
+      const wrapper = mount(
+        generateComponent({ handleCloseForm: mockedHandleCloseForm })
+      );
+      wrapper.instance().submit({ preventDefault: () => {} });
+      expect(mockedHandleCloseForm).to.have.been.calledOnce;
+    });
+  });
+});
