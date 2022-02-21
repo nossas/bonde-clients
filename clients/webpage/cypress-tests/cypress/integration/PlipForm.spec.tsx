4aa723aba0e67859ad406f489f05b3b29a9d66d0
Igor Santos <igr.exe@gmail.com>
2022-02-21 18:49:01 -0300

    fix(cypress): change plip download pdf link to button

--- clients/webpage/cypress-tests/cypress/integration/PlipForm.spec.tsx
+++ clients/webpage/cypress-tests/cypress/integration/PlipForm.spec.tsx
@@ -22,7 +22,7 @@ describe('Submit Plip Form', () => {
         .click();
 
       //download signature form
-      cy.get('.PlipDetails_PlipDetails__KQbZT > a')
+      cy.get('.PlipDetails_PlipDetails__KQbZT > button')
         .should('be.visible').click();
     });
 
