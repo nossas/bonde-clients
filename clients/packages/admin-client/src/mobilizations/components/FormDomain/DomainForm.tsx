39d6d61ca14a82009229636731e26430428280a4
Igor Santos <igr.exe@gmail.com>
2022-04-08 15:52:39 -0300

    feat(admin): add tests to domain and subdomain fields

--- clients/packages/admin-client/src/mobilizations/components/FormDomain/DomainForm.tsx
+++ clients/packages/admin-client/src/mobilizations/components/FormDomain/DomainForm.tsx
@@ -1,3 +1,30 @@
 import React from 'react';
+import { HStack, VStack, Text, Select, Link } from 'bonde-components/chakra';
+import { Field } from 'bonde-components/form';
 
-export default () => <div />;
+interface Properties {
+  createNewDomainPath?: string;
+  hostedZones?: { domain_name: string }[];
+}
+
+const DomainForm: React.FC<Properties> = ({ createNewDomainPath, hostedZones = [] }) => (
+  <VStack>
+    <Text>Selecione o domínio cadastrado na sua comunidade:</Text>
+    <HStack>
+      <Text>https://www.</Text>
+      <Field name="domain">
+        {({ input }) => (
+          <Select {...input}>
+            {hostedZones.map(({ domain_name: domain }) =>
+              <option key={domain}>{domain}</option>
+            )}
+          </Select>
+        )}
+      </Field>
+    </HStack>
+    <Text>Não encontro o domínio na lista?</Text>
+    <Text><Link href={createNewDomainPath || '#'} target='_self' >Clique aqui</Link> para cadastrar um novo domínio na comunidade.</Text>
+  </VStack>
+);
+
+export default DomainForm;
