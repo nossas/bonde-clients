76553155ebff391c677e1d7ab96af592c3e00bc7
Lucas Pirola <lucaspirola@gmail.com>
2022-05-25 12:58:16 -0300

    fix(webpage): Fix build with ping api route

--- clients/packages/webpage-client/src/pages/api/ping.spec.ts
+++ clients/packages/webpage-client/src/pages/api/ping.spec.ts
@@ -3,11 +3,19 @@ import '@testing-library/jest-dom/extend-expect';
 import ping from './ping';
 
 describe('ping', () => {
-    const wrapper = ping();
+    const mockJson = jest.fn();
+    const mockRes = {
+      status: jest.fn().mockImplementation(() => ({
+        json: mockJson
+      }))
+    }
+    const wrapper = ping({},mockRes);
 
     it('should call  with text', () => {
         console.log(wrapper);
-        expect(wrapper).toBeTruthy();
-        expect(wrapper).toEqual({});
+        // expect(wrapper).toBeTruthy();
+        expect(mockRes.status.mock.calls[0][0]).toEqual(200);
+        expect(mockJson.mock.calls[0][0]).toEqual({ });
+
     });
 });
