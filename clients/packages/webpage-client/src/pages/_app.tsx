913c423e73628bf1c7e0ef14165d269c6fe33aec
Lucas Pirola <lucaspirola@gmail.com>
2022-06-06 17:00:06 -0300

    chore(webpage): Move chakra-ui to subscriptions page;

--- clients/packages/webpage-client/src/pages/_app.tsx
+++ clients/packages/webpage-client/src/pages/_app.tsx
@@ -1,25 +1,5 @@
-// import { createGlobalStyle, ThemeProvider } from 'styled-components'
-import {
-  CSSReset,
-  ChakraProvider as UIProvider,
-  theme,
-} from 'bonde-components/chakra';
 import type { AppProps, NextWebVitalsMetric } from 'next/app'
 import './_app.css';
-
-// const GlobalStyle = createGlobalStyle`
-//   body {
-//     margin: 0;
-//     padding: 0;
-//     box-sizing: border-box;
-//   }
-// `
-
-// const theme = {
-//   colors: {
-//     primary: '#0070f3',
-//   },
-// }
 
 export function reportWebVitals(metric: NextWebVitalsMetric) {
   console.log(metric)
@@ -27,21 +7,5 @@ export function reportWebVitals(metric: NextWebVitalsMetric) {
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      {/* <GlobalStyle />
-      <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
-      </ThemeProvider> */}
-      <UIProvider theme={theme}>
-        {/* Google Fonts Loader in _app.css */}
-        <CSSReset />
-        {/* <ToastContainer
-          className='BondeToastify'
-          hideProgressBar={true}
-        /> */}
-        <Component {...pageProps} />
-      </UIProvider>
-    </>
-  )
+  return (<Component {...pageProps} />)
 }
