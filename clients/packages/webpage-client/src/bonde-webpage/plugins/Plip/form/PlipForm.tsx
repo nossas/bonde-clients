ccad4dd968c57b7347390e6417781b6fb3fe73f3
Gloria Teodoro <gloriateodoro1@gmail.com>
2022-06-13 21:22:54 -0300

    feat: update plip form

--- clients/packages/webpage-client/src/bonde-webpage/plugins/Plip/form/PlipForm.tsx
+++ clients/packages/webpage-client/src/bonde-webpage/plugins/Plip/form/PlipForm.tsx
@@ -137,10 +137,9 @@ const PlipForm: React.FC<Props> = ({ asyncFillWidget, widget }) => {
                     <option>selecione a quantidade de assinaturas</option>
                     <option value="10">10</option>
                     <option value="20">20</option>
-                    <option value="30">30</option>
-                    <option value="40">40</option>
                     <option value="50">50</option>
-                    <option value="100">100</option>
+                    <option value="500">500</option>
+                    <option value="1000">1000</option>
                   </select>
                   {meta.error && meta.touched && <span>Quantidade de assinaturas {meta.error}</span>}
                 </div>
