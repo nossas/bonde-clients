609e282bda9e154ee5eb7fd4ba742b4c142ffa35
Gloria Teodoro <gloriateodoro1@gmail.com>
2022-07-21 15:19:18 -0300

    feat: add fetch signatures query

---
+++ clients/packages/webpage-client/src/pages/api/plip-counter/index.ts
@@ -0,0 +1,34 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { client } from '../../../apis/graphql';
+import { gql } from '@apollo/client';
+
+const query = gql`
+query {
+  plips_aggregate(where: {widget_id: {_eq: 70801}, status: {_in: ["PENDENTE", "INSCRITO"]}}) {
+    aggregate {
+      sum {
+        expected_signatures
+      }
+    }
+  }
+  plip_signatures_aggregate(where: {widget_id: {_eq: 70801}}) {
+     aggregate {
+      sum {
+         confirmed_signatures
+      }
+     }
+   }
+}
+`
+
+export default async function fetchSignatures(req: NextApiRequest, res: NextApiResponse): Promise<void> {
+  const { method } = req;
+  if (method === "GET") {
+    const signaturesTotal = await client.query({
+      query,
+    })
+    return res.status(200).json({
+      data: signaturesTotal
+    })
+  }
+}
