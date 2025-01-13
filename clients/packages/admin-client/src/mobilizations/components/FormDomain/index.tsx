74de3621ecdd0d47ba0b28014da5b40f06ed4d2b
Igor Santos <igr.exe@gmail.com>
2025-01-13 14:44:26 -0300

    fix(admin): remove certificate status in mobilization domain interface

--- clients/packages/admin-client/src/mobilizations/components/FormDomain/index.tsx
+++ clients/packages/admin-client/src/mobilizations/components/FormDomain/index.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import { Text, Stack } from 'bonde-components/chakra';
 import FormPanel from './FormPanel';
-import CertificateStatus from './CertificateStatus';
 
 const FormDomain = ({ mobilization, hostedZones }) => (
   <Stack direction="row" spacing={140} maxW="1900px">
     <Stack direction="column" spacing={8}>
       <Text>Defina abaixo o domínio (também conhecido como endereço, ou URL) para as pessoas acessarem sua página:</Text>
       <FormPanel mobilization={mobilization} hostedZones={hostedZones} />
-      <CertificateStatus customDomain={mobilization.custom_domain} hostedZones={hostedZones} />
     </Stack>
   </Stack>
 );
