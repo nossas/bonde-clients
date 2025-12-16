5490247335d5fe9c7597215f3f8c7509e960bb81
evelinmarques <contatoevelinmarques@hotmail.com>
2025-12-16 00:41:56 -0300

    feat(templates): applies template to graphql

--- packages/admin-client/src/mobrender/redux/action-creators/async-update-mobilization-with-template.js
+++ packages/admin-client/src/mobrender/redux/action-creators/async-update-mobilization-with-template.js
@@ -1,40 +1,76 @@
-/* eslint-disable prefer-promise-reject-errors */
-import { createAction } from './create-action'
-import * as t from '../action-types'
-import AuthSelectors from '../../../account/redux/selectors';
+import { GraphQLClient, gql } from 'graphql-request';
+import { createAction } from './create-action';
+import * as t from '../action-types';
+import asyncFetchBlocks from './async-fetch-blocks';
+import asyncFetchWidgets from './async-fetch-widgets';
 
-import asyncFetchBlocks from './async-fetch-blocks'
-import asyncFetchWidgets from './async-fetch-widgets'
+const graphQLClient = new GraphQLClient(
+  process.env.REACT_APP_DOMAIN_API_GRAPHQL || '', 
+  { credentials: 'include' }
+);
 
-export default ({ fieldName, ...mobilization }) =>
-  (dispatch, getState, { api }) => {
-      const headers = AuthSelectors(getState()).getCredentials();
-    const endpoint = `/mobilizations/${mobilization.id}`
-    const config = { headers }
-
-    dispatch({ type: t.UPDATE_MOBILIZATION_REQUEST })
-    return api
-      .put(endpoint, { mobilization }, config)
-      .then(({ status, data }) => {
-        dispatch({ type: t.UPDATE_MOBILIZATION_SUCCESS, payload: data })
-        if (mobilization.template_mobilization_id) {
-          dispatch(asyncFetchBlocks(data.id))
-          dispatch(asyncFetchWidgets(data.id))
-        }
-        return Promise.resolve(data)
-      })
-      .catch(({ response, ...errors }) => {
-        if (response.status === 422 && response.data.errors) {
-          const errors = response.data.errors
-          if (response.data.errors.custom_domain) {
-            errors[fieldName] = errors.custom_domain
-            delete errors.custom_domain
-          }
-          dispatch(createAction(t.UPDATE_MOBILIZATION_FAILURE, errors))
-          return Promise.reject({ ...errors })
-        } else {
-          dispatch(createAction(t.UPDATE_MOBILIZATION_FAILURE, errors))
-          return Promise.reject({ error: `Response code ${errors}` })
-        }
-      })
+export const APPLY_TEMPLATE_MUTATION = gql`
+  mutation copyTemplateMobilization(
+    $p_mobilization_id: bigint!
+    $p_template_mobilization_id: bigint!
+  ) {
+    copy_template_mobilization(
+      args: {
+        p_mobilization_id: $p_mobilization_id
+        p_template_mobilization_id: $p_template_mobilization_id
+      }
+    ) {
+      id
+      name
+      user_id
+      color_scheme
+      facebook_share_title
+      facebook_share_description
+      header_font
+      body_font
+      facebook_share_image
+      slug
+      custom_domain
+      twitter_share_text
+      community_id
+      goal
+      favicon
+      created_at
+      updated_at
+    }
   }
+`;
+
+export default ({ id, template_mobilization_id }) =>
+  (dispatch) => {
+    dispatch(createAction(t.UPDATE_MOBILIZATION_REQUEST));
+
+    return graphQLClient
+      .request(APPLY_TEMPLATE_MUTATION, {
+        p_mobilization_id: id,
+        p_template_mobilization_id: template_mobilization_id,
+      })
+      .then((data) => {
+        // retorna um array, pega o primeiro item
+        const mobilization = data.copy_template_mobilization[0];
+        
+        dispatch(createAction(t.UPDATE_MOBILIZATION_SUCCESS, mobilization));
+        
+        // Busca blocks e widgets criados
+        dispatch(asyncFetchBlocks(mobilization.id));
+        dispatch(asyncFetchWidgets(mobilization.id));
+        
+        return mobilization;
+      })
+      .catch((error) => {
+        console.error('Error applying template:', error);
+        
+        const errorMessage = error?.response?.errors?.[0]?.message || 
+                           error?.message || 
+                           'Failed to apply template';
+        
+        dispatch(createAction(t.UPDATE_MOBILIZATION_FAILURE, errorMessage));
+        
+        return Promise.reject({ _error: errorMessage });
+      });
+  };
