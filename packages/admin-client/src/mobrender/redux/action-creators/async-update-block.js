c151975f02ee36b79d3d799adaf50f41603e20c8
Igor Santos <igr.exe@gmail.com>
2026-01-06 17:26:58 -0300

    fix(admin): change update block to graphql

--- packages/admin-client/src/mobrender/redux/action-creators/async-update-block.js
+++ packages/admin-client/src/mobrender/redux/action-creators/async-update-block.js
@@ -1,20 +1,40 @@
+import { GraphQLClient, gql } from 'graphql-request';
 import { createAction } from './create-action'
 import * as t from '../action-types'
 
 import AuthSelectors from '../../../account/redux/selectors'
 import Selectors from '../selectors'
 
-export default (block) => (dispatch, getState, { api }) => {
-  const headers = AuthSelectors(getState()).getCredentials()
-  const mobilization = Selectors(getState()).getMobilization()
+const graphQLClient = new GraphQLClient(process.env.REACT_APP_DOMAIN_API_GRAPHQL || '', { credentials: 'include' });
+
+export const UPDATE_BLOCK_MUTATION = gql`
+mutation ($block: blocks_set_input!, $id: Int!) {
+  update_blocks_by_pk(pk_columns: {id: $id}, _set: $block) {
+    id
+    mobilization_id
+    created_at
+    updated_at
+    bg_class
+    position
+    hidden
+    bg_image
+    name
+    menu_hidden
+    deleted_at
+  }
+}
+`
+
+export default ({ id, offsetTop, scrollTopReached, ...block }) => (dispatch, getState, { api }) => {
   dispatch(createAction(t.UPDATE_BLOCK_REQUEST))
-  return api
-    .put(`/mobilizations/${mobilization.id}/blocks/${block.id}`, { block }, { headers })
-    .then(res => {
-      dispatch(createAction(t.UPDATE_BLOCK_SUCCESS, res.data))
-    })
-    .catch(ex => {
-      dispatch(createAction(t.UPDATE_BLOCK_FAILURE, ex))
-      return Promise.reject(ex)
-    })
+
+  return graphQLClient.request(
+    UPDATE_BLOCK_MUTATION, { id, block }
+  ).then(response => {
+    dispatch(createAction(t.UPDATE_BLOCK_SUCCESS, response.update_blocks_by_pk))
+    return Promise.resolve(response.update_blocks_by_pk)
+  }).catch(ex => {
+    dispatch(createAction(t.UPDATE_BLOCK_FAILURE, ex))
+    return Promise.reject(ex)
+  });
 }
